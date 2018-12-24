53ec39eec9f08abec4646cee7ec43a0a63f37e10
liyihz2008 <liyi_wt@126.com>
2018-12-24 20:42:49 +0800

    为CMS栏目信息保存适应Cihper操作

--- o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/document/ActionPublishContent.java
+++ o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/document/ActionPublishContent.java
@@ -28,7 +28,6 @@ import com.x.cms.core.entity.CategoryInfo;
 import com.x.cms.core.entity.Document;
 import com.x.cms.core.entity.FileInfo;
 import com.x.cms.core.entity.element.Form;
-import com.x.cms.core.entity.tools.LogUtil;
 
 /**
  * 直接发布文档内容
