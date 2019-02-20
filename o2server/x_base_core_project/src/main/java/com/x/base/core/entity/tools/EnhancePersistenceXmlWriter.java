6c882b9ae31ae5da1f7454f71a6a6bbc37826f41
roo00 <zhouruicn@outlook.com>
2019-02-20 18:16:44 +0800

    修改persistence.xml生成位置

--- o2server/x_base_core_project/src/main/java/com/x/base/core/entity/tools/EnhancePersistenceXmlWriter.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/entity/tools/EnhancePersistenceXmlWriter.java
@@ -52,6 +52,7 @@ public class EnhancePersistenceXmlWriter {
 			OutputFormat format = OutputFormat.createPrettyPrint();
 			format.setEncoding("UTF-8");
 			File dir = new File(arg.getPath(), "target/classes/META-INF");
+			// File dir = new File(arg.getPath(), "src/main/resources/META-INF");
 			FileUtils.forceMkdir(dir);
 			File file = new File(dir, "persistence.xml");
 			XMLWriter writer = new XMLWriter(new FileWriter(file), format);
