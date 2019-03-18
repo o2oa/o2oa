6bb1388d0144a564bde8208245540032aa670d0a
roo00 <zhouruicn@outlook.com>
2019-03-18 17:16:37 +0800

    windows服务注册脚本

--- o2server/service_windows.bat
+++ o2server/service_windows.bat
@@ -1,16 +1,16 @@
 @echo off
 if not "%1" == "install" ( if not "%1" == "uninstall"  ( if not "%1" == "start" ( if not "%1" == "stop" (
-echo     usage: service_windows.bat install ^| uninstall ^| start ^| stop
-echo     depend Microsoft.NET Framework 4
+echo     使用: service_windows.bat install ^| uninstall ^| start ^| stop
+echo     依赖于 Microsoft.NET Framework 4
 goto out
 ))))
 echo ^<configuration^> > %~dp0local\service.xml
-echo ^<id^>o2server^<^/id^> >> %~dp0local\service.xml
-echo ^<name^>o2server service^<^/name^> >> %~dp0local\service.xml
-echo ^<description^>winsw warpper o2server service.^<^/description^> >> %~dp0local\service.xml
+echo ^<id^>O2SERVER^<^/id^> >> %~dp0local\service.xml
+echo ^<name^>O2SERVER SERVICE^<^/name^> >> %~dp0local\service.xml
+echo ^<description^>O2OA平台服务.^<^/description^> >> %~dp0local\service.xml
 echo ^<executable^>%~dp0start_windows.bat^</executable^> >> %~dp0local\service.xml
 echo ^<log mode="none"^/^> >> %~dp0local\service.xml
 echo ^</configuration^> >> %~dp0local\service.xml
 copy %~dp0commons\winsw.exe %~dp0local\service.exe
 %~dp0local\service.exe %1
-:out 
+:out
