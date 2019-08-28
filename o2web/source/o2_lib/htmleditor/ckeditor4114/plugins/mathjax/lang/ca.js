518dda15b0ada5bc7b7db3244ddb51f4f2e5b2f3
hq_1980@hotmail.com <hq_1980@hotmail.com>
2019-08-28 20:21:00 +0800

    2019-08-28

---
+++ o2web/source/o2_lib/htmleditor/ckeditor4114/plugins/mathjax/lang/ca.js
@@ -0,0 +1,13 @@
+/*
+Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+*/
+CKEDITOR.plugins.setLang( 'mathjax', 'ca', {
+	title: 'Matemàtiques a TeX',
+	button: 'Matemàtiques',
+	dialogInput: 'Escriu el TeX aquí',
+	docUrl: 'http://en.wikibooks.org/wiki/LaTeX/Mathematics',
+	docLabel: 'Documentació TeX',
+	loading: 'carregant...',
+	pathName: 'matemàtiques'
+} );
