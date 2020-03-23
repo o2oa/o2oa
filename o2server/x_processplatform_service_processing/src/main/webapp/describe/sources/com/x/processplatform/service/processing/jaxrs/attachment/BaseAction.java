7a379fd128a8ecaa7a756355ff3cf3293e0e4ad2
Zhou Rui <zhourui@o2oa.net>
2020-03-23 13:22:24 +0800

    暂存

--- o2server/x_processplatform_service_processing/src/main/webapp/describe/sources/com/x/processplatform/service/processing/jaxrs/attachment/BaseAction.java
+++
@@ -1,7 +0,0 @@
-package com.x.processplatform.service.processing.jaxrs.attachment;
-
-import com.x.base.core.project.jaxrs.StandardJaxrsAction;
-
-abstract class BaseAction extends StandardJaxrsAction {
-
-}
--- o2server/x_processplatform_service_processing/src/main/webapp/describe/sources/com/x/processplatform/service/processing/jaxrs/task/BaseAction.java
+++
@@ -1,7 +0,0 @@
-package com.x.processplatform.service.processing.jaxrs.task;
-
-import com.x.base.core.project.jaxrs.StandardJaxrsAction;
-
-abstract class BaseAction extends StandardJaxrsAction {
-
-}
