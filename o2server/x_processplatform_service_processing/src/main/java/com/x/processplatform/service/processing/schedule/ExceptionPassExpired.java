bc2279aba68b5515dcead62ccba9812491e8c28a
Zhou Rui <zhourui@o2oa.net>
2020-03-24 19:15:35 +0800

    修正报错的提示信息.

--- o2server/x_processplatform_service_processing/src/main/java/com/x/processplatform/service/processing/schedule/ExceptionPassExpired.java
+++ o2server/x_processplatform_service_processing/src/main/java/com/x/processplatform/service/processing/schedule/ExceptionPassExpired.java
@@ -6,8 +6,8 @@ class ExceptionPassExpired extends PromptException {
 
 	private static final long serialVersionUID = -7038279889683420366L;
 
-	ExceptionPassExpired(Exception e, String id, String title, String sequence) {
-		super(e, "超时工作默认路由流转失败, id:{}, title:{}, sequence:{}.");
+	ExceptionPassExpired(Exception e, String id, String title) {
+		super(e, "超时工作默认路由流转失败, id:{}, title:{}.", id, title);
 	}
 
 }
