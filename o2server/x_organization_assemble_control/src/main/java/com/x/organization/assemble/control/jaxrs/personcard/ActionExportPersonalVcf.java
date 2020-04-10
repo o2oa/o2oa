e320a6fe5f538bd342da79c2311913db6cdce149
luojing <13968186856@139.com>
2020-04-10 15:59:31 +0800

    通讯录导出vcf

--- o2server/x_organization_assemble_control/src/main/java/com/x/organization/assemble/control/jaxrs/personcard/ActionExportPersonalVcf.java
+++ o2server/x_organization_assemble_control/src/main/java/com/x/organization/assemble/control/jaxrs/personcard/ActionExportPersonalVcf.java
@@ -1,112 +1,96 @@
 package com.x.organization.assemble.control.jaxrs.personcard;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
+import java.io.ByteArrayOutputStream;
 
-import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.List;
 
 
 
 
-import com.google.gson.JsonElement;
 import com.x.base.core.container.EntityManagerContainer;
 import com.x.base.core.container.factory.EntityManagerContainerFactory;
-import com.x.base.core.project.annotation.FieldDescribe;
 import com.x.base.core.project.exception.ExceptionPersonNotExist;
-import com.x.base.core.project.gson.GsonPropertyObject;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
-import com.x.base.core.project.jaxrs.WoId;
-
-import com.x.organization.assemble.control.Business;
-import com.x.organization.assemble.control.staticconfig.FollowConfig;
+import com.x.base.core.project.jaxrs.WoFile;
+import com.x.base.core.project.logger.Logger;
+import com.x.base.core.project.logger.LoggerFactory;
 import com.x.organization.core.entity.PersonCard;
 
 
 
 class ActionExportPersonalVcf extends BaseAction {
 
-
+	private static Logger logger = LoggerFactory.getLogger(ActionExportPersonalVcf.class);
 	//导出通讯录vcf
-	ActionResult<String> exportPersonalVcf(EffectivePerson effectivePerson, JsonElement jsonElement) throws Exception {
+	ActionResult<Wo> exportPersonalVcf(EffectivePerson effectivePerson, String idList) throws Exception {
 		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-			ActionResult<String> result = new ActionResult<>();
-			Business business = new Business(emc);
-			Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
-			List<String> personIdList = wi.getPersonList();
+			ActionResult<Wo> result = new ActionResult<>();
+			List<String> personIdList = Arrays.asList(idList.split(","));
+			Wo wo = null;
 			if(!personIdList.isEmpty()){
-				//String path = "D:\\tmp\\export_contacts.vcf";
-				String path = FollowConfig.VCFPATH+"export_personal_contacts"+System.currentTimeMillis()+".vcf";
-				File file = new File(path);
-				if (file.exists()) {
-					file.createNewFile();
-				}
-				BufferedWriter reader = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
+				ByteArrayOutputStream os = new ByteArrayOutputStream();
+				
 				for (String flag : personIdList) {
 					PersonCard personCard = emc.find(flag, PersonCard.class);
 					if (null == personCard) {
 						throw new ExceptionPersonNotExist(flag);
 					}
-					reader.write("BEGIN:VCARD");
-					reader.write("\r\n");
-					reader.write("VERSION:3.0");
-					reader.write("\r\n");
-					reader.write("N:"+personCard.getName()+";;;;");
-					reader.write("\r\n");
-					reader.write("FN:"+personCard.getName());
-					reader.write("\r\n");	    
+					os.write("BEGIN:VCARD".getBytes());
+					os.write("\r\n".getBytes());
+					os.write("VERSION:3.0".getBytes());
+					os.write("\r\n".getBytes());
+					os.write(("N:"+personCard.getName()+";;;;").getBytes());
+					os.write("\r\n".getBytes());
+					os.write(("FN:"+personCard.getName()).getBytes());
+					os.write("\r\n".getBytes());	    
 					if(personCard.getOfficePhone()!=null && !personCard.getOfficePhone().equals("")){
-						reader.write("TEL:"+personCard.getOfficePhone());
-						reader.write("\r\n");
+						os.write(("TEL:"+personCard.getOfficePhone()).getBytes());
+						os.write(("\r\n").getBytes());
 					}
 					if(personCard.getMobile()!=null && !personCard.getMobile().equals("")){
-						reader.write("TEL;CELL:"+personCard.getMobile());
-						reader.write("\r\n");
+						os.write(("TEL;CELL:"+personCard.getMobile()).getBytes());
+						os.write("\r\n".getBytes());
 					}
 					if(personCard.getAddress()!=null && !personCard.getAddress().equals("")){
-						reader.write("ADR;HOME;POSTAL:;;"+personCard.getAddress());
-						reader.write("\r\n");
+						os.write(("ADR;HOME;POSTAL:;;"+personCard.getAddress()).getBytes());
+						os.write("\r\n".getBytes());
 					}
 					if(personCard.getGroupType()!=null && !personCard.getGroupType().equals("")){
-						reader.write("ORG:"+personCard.getGroupType());
-						reader.write("\r\n");
+						os.write(("ORG:"+personCard.getGroupType()).getBytes());
+						os.write("\r\n".getBytes());
 					}
 					if(personCard.getDescription()!=null && !personCard.getDescription().equals("")){
-						reader.write("NOTE:"+personCard.getDescription());
-						reader.write("\r\n");
+						os.write(("NOTE:"+personCard.getDescription()).getBytes());
+						os.write("\r\n".getBytes());
 					}									
-					reader.write("END:VCARD");
-					reader.write("\r\n");
+					os.write("END:VCARD".getBytes());
+					os.write("\r\n".getBytes());
 					
 				}
-				reader.flush();
-				reader.close();
-				result.setData(path);
+				String fileName = "export_personal_contacts"+System.currentTimeMillis()+".vcf";
+				try {
+					wo = new Wo(os.toByteArray(), 
+							this.contentType(false, fileName), 
+							this.contentDisposition(false, fileName));
+				} finally {
+					os.close();
+				}
+				result.setData(wo);
 			}
 
 			return result;
 		}
 	}
 	
-	public static class Wi extends GsonPropertyObject {
+	public static class Wo extends WoFile {
 
-		@FieldDescribe("个人通讯录人员标识")
-		private List<String> personList = new ArrayList<>();
-
-		public List<String> getPersonList() {
-			return personList;
+		public Wo(byte[] bytes, String contentType, String contentDisposition) {
+			super(bytes, contentType, contentDisposition);
 		}
 
-		public void setPersonList(List<String> personList) {
-			this.personList = personList;
-		}
-
-	}
-	public static class Wo extends WoId {
-		
 	}
 
 }
