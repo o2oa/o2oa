54ea47c19594f712f39177d33473339f8b37aaa7
fancy <fancylou@outlook.com>
2020-04-16 09:27:33 +0800

    部门统计查询修改输出

--- o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/jaxrs/dingdingstatistic/ActionUnitStatistic.java
+++ o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/jaxrs/dingdingstatistic/ActionUnitStatistic.java
@@ -19,15 +19,19 @@ import java.util.List;
 public class ActionUnitStatistic extends BaseAction {
 
 
-    ActionResult<List<Wo>> execute(String unit, String year, String month) throws Exception {
-        ActionResult<List<Wo>> result = new ActionResult<>();
+    ActionResult<Wo> execute(String unit, String year, String month) throws Exception {
+        ActionResult<Wo> result = new ActionResult<>();
         if (StringUtils.isEmpty(unit) || StringUtils.isEmpty(year) || StringUtils.isEmpty(month)) {
             throw new EmptyArgsException();
         }
         try ( EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
             Business business = new Business(emc);
             List<StatisticDingdingUnitForMonth> list = business.dingdingAttendanceFactory().findUnitStatistic(unit, year, month);
-            result.setData(Wo.copier.copy(list));
+            if (list!=null && !list.isEmpty()) {
+                result.setData(Wo.copier.copy(list.get(0)));
+            }else {
+                result.setData(new Wo());
+            }
         }
         return result;
     }
