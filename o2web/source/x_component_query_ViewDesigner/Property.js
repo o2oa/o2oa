259760bad9b54036f624ff5ce4650e7019b9fae5
unknown <caixiangyi@zoneland.net>
2020-04-29 13:44:14 +0800

    修复视图问题

--- o2web/source/x_component_query_ViewDesigner/Property.js
+++ o2web/source/x_component_query_ViewDesigner/Property.js
@@ -603,6 +603,7 @@ MWF.xApplication.query.ViewDesigner.Property = MWF.FVProperty = new Class({
                 }
             }.bind(this));
             select.addEvent("change", function(e){
+                debugger;
                 var v = select.options[select.selectedIndex].value;
                 if (v!="none"){
                     var flag = false;
@@ -629,6 +630,7 @@ MWF.xApplication.query.ViewDesigner.Property = MWF.FVProperty = new Class({
             var group = this.view.data.data.group;
             if (group.column==this.data.column) radios[0].set("checked", true);
             radios.addEvent("click", function(e){
+                debugger;
                 if (this.checked){
                     if (this.value=="true") {
                         _self.view.data.data.group.column = _self.data.column;
@@ -640,6 +642,9 @@ MWF.xApplication.query.ViewDesigner.Property = MWF.FVProperty = new Class({
                                     if (r.value=="false") r.set("checked", true);
                                 });
                             }
+                        });
+                        ( _self.view.data.data.selectList ).each(function(s) {
+                            if( s.column !== _self.data.column )s.groupEntry = false;
                         });
                         this.set("checked", true);
                     }else{
