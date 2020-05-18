aaf8936d3b6e01ea75c77a948592153b26920790
fancy <fancylou@outlook.com>
2020-05-18 16:04:12 +0800

    表情聊天

--- o2web/source/o2_core/o2/xDesktop/WebSocket.js
+++ o2web/source/o2_core/o2/xDesktop/WebSocket.js
@@ -307,9 +307,13 @@ MWF.xDesktop.WebSocket = new Class({
         var imBody = data.body;
         var jsonBody = imBody.body;
         var conversationId = imBody.conversationId;
-        //todo 目前只有一种text类型
+
         var body = JSON.parse(jsonBody);
-        var content = "<font style='color: #333; font-weight: bold'>"+data.title+"</font>"+body.body;
+        var msgBody = body.body; //默认text 文本消息
+        if (body.type && body.type == "emoji") { //表情 消息
+            msgBody = "[表情]";
+        }
+        var content = "<font style='color: #333; font-weight: bold'>"+data.title+"</font>"+msgBody;
         var msg = {
             "subject": MWF.LP.desktop.messsage.customMessageTitle,
             "content": content
