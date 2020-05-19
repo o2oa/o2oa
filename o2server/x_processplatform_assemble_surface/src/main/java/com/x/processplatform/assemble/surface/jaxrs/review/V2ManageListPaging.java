c88c0b38ebcf50bf6e13ba9a1728c7acd345a765
o2sword <171715986@qq.com>
2020-05-19 15:21:08 +0800

    增加管理员分页查询参阅

--- o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/review/V2ManageListPaging.java
+++ o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/review/V2ManageListPaging.java
@@ -1,13 +1,10 @@
 package com.x.processplatform.assemble.surface.jaxrs.review;
 
-import java.util.List;
-
-import javax.persistence.criteria.Predicate;
-
 import com.google.gson.JsonElement;
 import com.x.base.core.container.EntityManagerContainer;
 import com.x.base.core.container.factory.EntityManagerContainerFactory;
 import com.x.base.core.entity.JpaObject;
+import com.x.base.core.project.annotation.FieldDescribe;
 import com.x.base.core.project.bean.WrapCopier;
 import com.x.base.core.project.bean.WrapCopierFactory;
 import com.x.base.core.project.http.ActionResult;
@@ -15,16 +12,21 @@ import com.x.base.core.project.http.EffectivePerson;
 import com.x.processplatform.assemble.surface.Business;
 import com.x.processplatform.core.entity.content.Review;
 
-class V2ListNext extends V2Base {
+import javax.persistence.criteria.Predicate;
+import java.util.List;
 
-	ActionResult<List<Wo>> execute(EffectivePerson effectivePerson, String id, Integer count, JsonElement jsonElement)
+class V2ManageListPaging extends V2Base {
+
+	ActionResult<List<Wo>> execute(EffectivePerson effectivePerson, Integer page, Integer size, JsonElement jsonElement)
 			throws Exception {
 		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-			Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
 			Business business = new Business(emc);
-			Predicate p = this.toFilterPredicate(effectivePerson, business, wi, null);
-			ActionResult<List<Wo>> result = this.standardListNext(Wo.copier, id, count, JpaObject.sequence_FIELDNAME,
-					DESC, p);
+			ActionResult<List<Wo>> result = new ActionResult<>();
+			Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
+			Predicate p = this.toFilterPredicate(effectivePerson, business, wi, wi.getPersonList());
+			List<Wo> wos = emc.fetchDescPaging(Review.class, Wo.copier, p, page, size, Review.sequence_FIELDNAME);
+			result.setData(wos);
+			result.setCount(emc.count(Review.class, p));
 			this.relate(business, result.getData(), wi);
 			return result;
 		}
@@ -32,10 +34,20 @@ class V2ListNext extends V2Base {
 
 	public static class Wi extends RelateFilterWi {
 
+		@FieldDescribe("参阅用户")
+		private List<String> personList;
+
+		public List<String> getPersonList() {
+			return personList;
+		}
+
+		public void setPersonList(List<String> personList) {
+			this.personList = personList;
+		}
 	}
 
 	public static class Wo extends AbstractWo {
-		private static final long serialVersionUID = -4773789253221941109L;
+		private static final long serialVersionUID = 6612518284150311901L;
 		static WrapCopier<Review, Wo> copier = WrapCopierFactory.wo(Review.class, Wo.class,
 				JpaObject.singularAttributeField(Review.class, true, false), JpaObject.FieldsInvisible);
 	}
