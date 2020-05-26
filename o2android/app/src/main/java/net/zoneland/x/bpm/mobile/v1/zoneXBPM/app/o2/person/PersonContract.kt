a5b5330dd9a07f37fe55ba7e58a215e919546c7f
fancy <fancylou@outlook.com>
2020-05-26 16:09:46 +0800

    个人信息界面发起聊天

--- o2android/app/src/main/java/net/zoneland/x/bpm/mobile/v1/zoneXBPM/app/o2/person/PersonContract.kt
+++ o2android/app/src/main/java/net/zoneland/x/bpm/mobile/v1/zoneXBPM/app/o2/person/PersonContract.kt
@@ -2,6 +2,7 @@ package net.zoneland.x.bpm.mobile.v1.zoneXBPM.app.o2.person
 
 import net.zoneland.x.bpm.mobile.v1.zoneXBPM.app.base.BasePresenter
 import net.zoneland.x.bpm.mobile.v1.zoneXBPM.app.base.BaseView
+import net.zoneland.x.bpm.mobile.v1.zoneXBPM.model.bo.api.im.IMConversationInfo
 import net.zoneland.x.bpm.mobile.v1.zoneXBPM.model.bo.api.main.person.PersonJson
 
 
@@ -10,6 +11,8 @@ object PersonContract {
         fun isUsuallyPerson(flag:Boolean)
         fun loadPersonInfo(personInfo: PersonJson)
         fun loadPersonInfoFail()
+        fun createConvSuccess(conv: IMConversationInfo)
+        fun createConvFail(message: String)
     }
 
     interface Presenter : BasePresenter<View> {
@@ -17,5 +20,6 @@ object PersonContract {
         fun collectionUsuallyPerson(owner:String, person:String, ownerDisplay:String,personDisplay:String, gender:String, mobile:String)
         fun deleteUsuallyPerson(owner: String, person: String)
         fun isUsuallyPerson(owner: String, person: String)
+        fun startSingleTalk(user: String)
     }
 }
