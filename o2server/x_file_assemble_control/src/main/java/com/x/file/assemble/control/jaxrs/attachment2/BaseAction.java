e231d770958c48cde24ebf384b3ab09c55983bba
o2sword <171715986@qq.com>
2020-06-04 10:53:47 +0800

    修复云文件不同用户上传同一文件到根目录失败的问题

--- o2server/x_file_assemble_control/src/main/java/com/x/file/assemble/control/jaxrs/attachment2/BaseAction.java
+++ o2server/x_file_assemble_control/src/main/java/com/x/file/assemble/control/jaxrs/attachment2/BaseAction.java
@@ -4,7 +4,16 @@ import com.x.base.core.project.gson.XGsonBuilder;
 import com.x.base.core.project.jaxrs.StandardJaxrsAction;
 import com.x.base.core.project.message.MessageConnector;
 import com.x.base.core.project.organization.OrganizationDefinition;
+import com.x.file.assemble.control.Business;
 import com.x.file.core.entity.personal.Attachment;
+import com.x.file.core.entity.personal.Attachment2;
+import com.x.file.core.entity.personal.Attachment2_;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 abstract class BaseAction extends StandardJaxrsAction {
 
@@ -41,5 +50,16 @@ abstract class BaseAction extends StandardJaxrsAction {
 		String title = "(" + OrganizationDefinition.name(editor) + ")对文件:" + attachment.getName() + ",进行了修改.";
 		MessageConnector.send(MessageConnector.TYPE_ATTACHMENT_EDITORMODIFY, title, person,
 				XGsonBuilder.convert(attachment, Attachment.class));
+	}
+
+	protected Boolean exist(Business business, String fileName, String folderId, String person) throws Exception {
+		EntityManager em = business.entityManagerContainer().get(Attachment2.class);
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+		Root<Attachment2> root = cq.from(Attachment2.class);
+		Predicate p = cb.equal(root.get(Attachment2_.name), fileName);
+		p = cb.and(p, cb.equal(root.get(Attachment2_.person), person));
+		p = cb.and(p, cb.equal(root.get(Attachment2_.folder), folderId));
+		return em.createQuery(cq.select(cb.count(root)).where(p)).getSingleResult() > 0;
 	}
 }
