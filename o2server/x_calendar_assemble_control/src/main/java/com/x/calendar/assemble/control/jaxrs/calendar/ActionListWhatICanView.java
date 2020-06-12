8d31f7da129eec22f7850587e7555101139a8b7a
o2lee <liyi_wt@126.com>
2020-06-12 10:06:29 +0800

    修复在某些特殊情况下，日程管理编辑时报错的问题以及对可见日程列表查询的相关优化

--- o2server/x_calendar_assemble_control/src/main/java/com/x/calendar/assemble/control/jaxrs/calendar/ActionListWhatICanView.java
+++ o2server/x_calendar_assemble_control/src/main/java/com/x/calendar/assemble/control/jaxrs/calendar/ActionListWhatICanView.java
@@ -78,13 +78,21 @@ public class ActionListWhatICanView extends BaseAction {
 					woCalendar.setPublishable( ThisApplication.isCalendarPublisher( effectivePerson, unitNames, groupNames, calendar ) );	
 					//将所有的日历分为三类，放在三个不同的LIST里
 					//组织日历
-					if( "UNIT".equalsIgnoreCase( calendar.getType() ) ) {
-						wo.addUnitCalendar( woCalendar );
-					}else {
-						if( calendar.getCreateor().equalsIgnoreCase( effectivePerson.getDistinguishedName() )
-								|| StringUtils.equalsAnyIgnoreCase("SYSTEM", calendar.getCreateor() )) {
-							if( StringUtils.equalsAnyIgnoreCase("SYSTEM", calendar.getCreateor() )) {
+					if( ListTools.isNotEmpty( calendar.getFollowers() ) && calendar.getFollowers().contains( effectivePerson.getDistinguishedName() )){
+						//我关注的日历
+						wo.addFollowCalendar( woCalendar );
+					}else{
+						//个人或者组织日历
+						if( "UNIT".equalsIgnoreCase( calendar.getType() ) ) {
+							//组织日历
+							wo.addUnitCalendar( woCalendar );
+						}else {
+							//个人日历
+//							Boolean isMineCalendar = calendar.getCreateor().equalsIgnoreCase( effectivePerson.getDistinguishedName() );
+							Boolean isSystemCreate = StringUtils.equalsAnyIgnoreCase("SYSTEM", calendar.getCreateor() );
+							if( isSystemCreate ) {
 								if( !existsSystemDefaultCalendar ){
+									woCalendar.setCreateor( effectivePerson.getDistinguishedName() );
 									wo.addMyCalendar( woCalendar );
 									existsSystemDefaultCalendar = true;
 								}else{
@@ -94,9 +102,6 @@ public class ActionListWhatICanView extends BaseAction {
 							}else{
 								wo.addMyCalendar( woCalendar );
 							}
-
-						}else {
-							wo.addFollowCalendar( woCalendar );
 						}
 					}
 				}
@@ -173,7 +178,7 @@ public class ActionListWhatICanView extends BaseAction {
 		
 		static {
 			Excludes.add(  JpaObject.sequence_FIELDNAME );
-			Excludes.add( "updateTime" );
+//			Excludes.add( "updateTime" );
 			Excludes.add( "manageablePersonList" );
 			Excludes.add( "viewablePersonList" );
 			Excludes.add( "viewableUnitList" );
@@ -183,13 +188,13 @@ public class ActionListWhatICanView extends BaseAction {
 			Excludes.add( "publishableGroupList" );
 			Excludes.add( "status" );
 			Excludes.add( "distributeFactor" );
-			Excludes.add( "isPublic" );
+//			Excludes.add( "isPublic" );
 			Excludes.add( "followers" );
-			Excludes.add( "description" );
-			Excludes.add( "target" );
-			Excludes.add( "createor" );
-			Excludes.add( "source" );
-			Excludes.add( "createTime" );		
+//			Excludes.add( "description" );
+//			Excludes.add( "target" );
+//			Excludes.add( "createor" );
+//			Excludes.add( "source" );
+//			Excludes.add( "createTime" );
 		}
 		
 		@FieldDescribe("用户是否可以对该日历进行管理.")
@@ -215,6 +220,5 @@ public class ActionListWhatICanView extends BaseAction {
 		public void setPublishable(Boolean publishable) {
 			this.publishable = publishable;
 		}
-		
 	}
 }
