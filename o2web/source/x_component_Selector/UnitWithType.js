256791abbf6ee0c92ab7cd8492b59d6a193bf61b
蔡祥熠 <caixiangyi@zoneland.net>
2020-06-28 09:50:51 +0000

    Merge branch 'feature/Selector.select_all_range' into 'release'
    Merge of feature/【流程管理】人员选择器根据职务选择的时候，允许跨层级全选 to release
    
    See merge request o2oa/o2oa!883
    
    (cherry picked from commit da5180e97d67ecd8febb3f3b820e068e4672d456)
    
    3ac60bbb 允许职务跨层级全选

--- o2web/source/x_component_Selector/UnitWithType.js
+++ o2web/source/x_component_Selector/UnitWithType.js
@@ -165,10 +165,12 @@ MWF.xApplication.Selector.UnitWithType.Item = new Class({
                 //}.bind(this))
                 this.selectAllNode.addEvent( "click", function(ev) {
                     if (this.isSelectedAll) {
-                        this.unselectAll(ev);
+                        // this.unselectAll(ev);
+                        this.selector.options.selectAllRange === "all" ? this.unselectAllNested(ev) : this.unselectAll(ev);
                         this.selector.fireEvent("unselectCatgory", [this])
                     } else {
-                        this.selectAll(ev);
+                        // this.selectAll(ev);
+                        this.selector.options.selectAllRange === "all" ? this.selectAllNested(ev) : this.selectAll(ev);
                         this.selector.fireEvent("selectCatgory", [this])
                     }
                     ev.stopPropagation();
