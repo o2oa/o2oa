9134605b692d76d77f9b41d204844d4493a6f01a
st <zhangzhiping@o2oa.net>
2020-06-29 10:07:41 +0800

    [应用管理]修改导出bug

--- o2web/source/x_component_AppCenter/Main.js
+++ o2web/source/x_component_AppCenter/Main.js
@@ -381,7 +381,9 @@ MWF.xApplication.AppCenter.Exporter = new Class({
                     if (callback) callback();
                 }.bind(this),
                 "onPostClose": function() {
-                    _self.app.fireEvent("exporterClose");
+                    if(_self.app){
+                        _self.app.fireEvent("exporterClose");
+                    }
                     MWF.release(_self);
                 }
             });
@@ -442,7 +444,7 @@ MWF.xApplication.AppCenter.Exporter = new Class({
                         if (this.mask) this.mask.hide();
                         this.dlg.close();
                         window.open(o2.filterUrl(this.app.actions.action.address+uri));
-
+                        this.app.fireEvent("exporterClose");
                         MWF.release(this);
                     }.bind(this), function(xhr, text, error){
                         if (xhr.status!=0){
