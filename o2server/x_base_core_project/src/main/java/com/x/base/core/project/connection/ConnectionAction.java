80f53d387db1491f48be03a70452f0db08991758
o2sword <171715986@qq.com>
2020-07-08 17:14:55 +0800

    应用市场应用安装2

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/connection/ConnectionAction.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/connection/ConnectionAction.java
@@ -30,7 +30,6 @@ import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.apache.mina.core.session.IoSession;
 
 public class ConnectionAction {
 
--- o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/market/ActionGet.java
+++ o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/market/ActionGet.java
@@ -5,7 +5,6 @@ import com.x.base.core.container.factory.EntityManagerContainerFactory;
 import com.x.base.core.project.annotation.FieldDescribe;
 import com.x.base.core.project.bean.WrapCopier;
 import com.x.base.core.project.bean.WrapCopierFactory;
-import com.x.base.core.project.exception.ExceptionAccessDenied;
 import com.x.base.core.project.exception.ExceptionEntityNotExist;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
@@ -19,9 +18,6 @@ class ActionGet extends BaseAction {
 
 	ActionResult<Wo> execute(EffectivePerson effectivePerson, String id) throws Exception {
 		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-			if (effectivePerson.isAnonymous()) {
-				throw new ExceptionAccessDenied(effectivePerson);
-			}
 			ActionResult<Wo> result = new ActionResult<>();
 			Application app = emc.find(id, Application.class);
 			if (null == app) {
