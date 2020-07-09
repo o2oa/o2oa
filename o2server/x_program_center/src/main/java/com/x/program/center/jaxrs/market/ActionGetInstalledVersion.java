3264b81a077111bd738bce4bdfbc06e281391e74
o2sword <171715986@qq.com>
2020-07-09 14:41:08 +0800

    新版应用市场优化2

---
+++ o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/market/ActionGetInstalledVersion.java
@@ -0,0 +1,30 @@
+package com.x.program.center.jaxrs.market;
+
+import com.x.base.core.container.EntityManagerContainer;
+import com.x.base.core.container.factory.EntityManagerContainerFactory;
+import com.x.base.core.project.http.ActionResult;
+import com.x.base.core.project.http.EffectivePerson;
+import com.x.base.core.project.jaxrs.WrapString;
+import com.x.program.center.core.entity.InstallLog;
+
+class ActionGetInstalledVersion extends BaseAction {
+
+	ActionResult<Wo> execute(EffectivePerson effectivePerson, String id) throws Exception {
+		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+			ActionResult<Wo> result = new ActionResult<>();
+			InstallLog installLog = emc.find(id, InstallLog.class);
+			Wo wo = new Wo();
+			if(installLog==null){
+				wo.setValue("");
+			}else{
+				wo.setValue(installLog.getVersion());
+			}
+			result.setData(wo);
+			return result;
+		}
+	}
+
+	public static class Wo extends WrapString {
+
+	}
+}
