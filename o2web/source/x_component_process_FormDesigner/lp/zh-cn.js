393cbbb39d05478baaa2e8402618447fe224b928
unknown <caixiangyi@zoneland.net>
2020-07-11 23:18:03 +0800

    自定义表单样式

--- o2web/source/x_component_process_FormDesigner/lp/zh-cn.js
+++ o2web/source/x_component_process_FormDesigner/lp/zh-cn.js
@@ -146,6 +146,7 @@ MWF.xApplication.process.FormDesigner.LP = {
     "cancel": "取消",
     "newCategory": "新分类",
     "mustSelectFormStyle" : "必须选择一种表单样式",
+    "notValidJson" : "错误的json格式",
 
     "imageClipper" : "图片编辑",
 
