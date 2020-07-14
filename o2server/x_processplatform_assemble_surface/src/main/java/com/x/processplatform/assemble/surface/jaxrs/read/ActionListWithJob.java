37240c6126ff8d1c87849e229d00082744bf397f
zhourui <zhourui@o2oa.net>
2020-07-14 13:52:30 +0800

    待阅不再判断往前滚动,read/reference根据job直接列示work

--- o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/read/ActionListWithJob.java
+++ o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/read/ActionListWithJob.java
@@ -15,7 +15,6 @@ import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.processplatform.assemble.surface.Business;
 import com.x.processplatform.core.entity.content.Read;
-import com.x.processplatform.core.entity.content.TaskCompleted;
 
 class ActionListWithJob extends BaseAction {
 
