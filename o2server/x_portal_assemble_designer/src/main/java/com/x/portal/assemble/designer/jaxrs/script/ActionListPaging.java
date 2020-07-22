03d2ff29af0a6f2c63eb605e488ce43d9b091065
o2sword <171715986@qq.com>
2020-07-22 14:27:08 +0800

    脚本和数据字段增加分页查询接口

--- o2server/x_portal_assemble_designer/src/main/java/com/x/portal/assemble/designer/jaxrs/script/ActionListPaging.java
+++ o2server/x_portal_assemble_designer/src/main/java/com/x/portal/assemble/designer/jaxrs/script/ActionListPaging.java
@@ -3,12 +3,14 @@ package com.x.portal.assemble.designer.jaxrs.script;
 import com.x.base.core.container.EntityManagerContainer;
 import com.x.base.core.container.factory.EntityManagerContainerFactory;
 import com.x.base.core.entity.JpaObject;
+import com.x.base.core.project.annotation.FieldDescribe;
 import com.x.base.core.project.bean.WrapCopier;
 import com.x.base.core.project.bean.WrapCopierFactory;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.tools.ListTools;
 import com.x.portal.assemble.designer.Business;
+import com.x.portal.core.entity.Portal;
 import com.x.portal.core.entity.Script;
 
 import javax.persistence.EntityManager;
@@ -29,6 +31,15 @@ class ActionListPaging extends BaseAction {
 			CriteriaBuilder cb = em.getCriteriaBuilder();
 			Predicate p = cb.conjunction();;
 			List<Wo> wos = emc.fetchDescPaging(Script.class, Wo.copier, p, page, size, Script.sequence_FIELDNAME);
+			wos.stream().forEach(wo -> {
+				try {
+					Portal portal = emc.find(wo.getPortal(), Portal.class);
+					if(portal != null){
+						wo.setPortalName(portal.getName());
+					}
+				} catch (Exception e) {
+				}
+			});
 			result.setData(wos);
 			result.setCount(emc.count(Script.class, p));
 			return result;
@@ -42,5 +53,15 @@ class ActionListPaging extends BaseAction {
 		static WrapCopier<Script, Wo> copier = WrapCopierFactory.wo(Script.class, Wo.class, null,
 				ListTools.toList(JpaObject.FieldsInvisible, Script.dependScriptList_FIELDNAME, Script.text_FIELDNAME));
 
+		@FieldDescribe("门户应用名称.")
+		private String portalName;
+
+		public String getPortalName() {
+			return portalName;
+		}
+
+		public void setPortalName(String portalName) {
+			this.portalName = portalName;
+		}
 	}
 }
