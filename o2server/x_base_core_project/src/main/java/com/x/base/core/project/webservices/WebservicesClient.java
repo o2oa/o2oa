6af3451fef820fe1dba74d339b9736c01351dd8d
zhourui <zhourui@o2oa.net>
2020-07-27 12:22:01 +0800

    修复ws调用编码错误

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/webservices/WebservicesClient.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/webservices/WebservicesClient.java
@@ -1,7 +1,6 @@
 package com.x.base.core.project.webservices;
 
 import org.apache.cxf.endpoint.Client;
-import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 
 public class WebservicesClient {
 	public Object[] jaxws(String wsdl, String method, Object... objects) {
