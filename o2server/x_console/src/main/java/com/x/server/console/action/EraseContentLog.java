b62c210713346444fe09a8ca16959062b6e124b4
zhourui <zhourui@o2oa.net>
2020-08-07 13:33:26 +0800

    add erase content entity

--- o2server/x_console/src/main/java/com/x/server/console/action/EraseContentLog.java
+++ o2server/x_console/src/main/java/com/x/server/console/action/EraseContentLog.java
@@ -1,7 +1,5 @@
 package com.x.server.console.action;
 
-import com.x.base.core.project.logger.Logger;
-import com.x.base.core.project.logger.LoggerFactory;
 import com.x.processplatform.core.entity.element.FormVersion;
 import com.x.processplatform.core.entity.element.ProcessVersion;
 import com.x.processplatform.core.entity.element.ScriptVersion;
@@ -10,9 +8,7 @@ import com.x.program.center.core.entity.ScheduleLog;
 import com.x.program.center.core.entity.UnexpectedErrorLog;
 import com.x.program.center.core.entity.WarnLog;
 
-public class EraseContentLog extends EraseContentProcessPlatform {
-
-	private static Logger logger = LoggerFactory.getLogger(EraseContentLog.class);
+public class EraseContentLog extends EraseContent {
 
 	@Override
 	public boolean execute() throws Exception {
