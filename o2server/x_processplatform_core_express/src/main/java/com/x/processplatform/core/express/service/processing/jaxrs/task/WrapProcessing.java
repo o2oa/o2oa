a26cb7a0955a478352ff5581a1312961e57ed740
zhourui <zhourui@o2oa.net>
2020-08-07 10:17:31 +0800

    add prevTaskIdentity

--- o2server/x_processplatform_core_express/src/main/java/com/x/processplatform/core/express/service/processing/jaxrs/task/WrapProcessing.java
+++ o2server/x_processplatform_core_express/src/main/java/com/x/processplatform/core/express/service/processing/jaxrs/task/WrapProcessing.java
@@ -5,15 +5,17 @@ import com.x.base.core.project.gson.GsonPropertyObject;
 
 public class WrapProcessing extends GsonPropertyObject {
 
+	private static final long serialVersionUID = -2272062779655632688L;
+
 	public static final String PROCESSINGTYPE_TASK = "task";
 
 	public static final String PROCESSINGTYPE_RESET = "reset";
 
-	// @FieldDescribe("当前处理人")
-	// private String person;
-
-	// @FieldDescribe("当前处理身份")
-	// private String identity;
+//	@FieldDescribe("当前处理人")
+//	private String processingPerson;
+//
+//	@FieldDescribe("当前处理身份")
+//	private String processingIdentity;
 
 	@FieldDescribe("流转类型.")
 	private String processingType;
@@ -25,5 +27,21 @@ public class WrapProcessing extends GsonPropertyObject {
 	public void setProcessingType(String processingType) {
 		this.processingType = processingType;
 	}
+
+//	public String getProcessingPerson() {
+//		return processingPerson;
+//	}
+//
+//	public void setProcessingPerson(String processingPerson) {
+//		this.processingPerson = processingPerson;
+//	}
+//
+//	public String getProcessingIdentity() {
+//		return processingIdentity;
+//	}
+//
+//	public void setProcessingIdentity(String processingIdentity) {
+//		this.processingIdentity = processingIdentity;
+//	}
 
 }
