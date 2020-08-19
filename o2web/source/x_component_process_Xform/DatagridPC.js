b8b468b49bf414feba0d58927dff6e7f0a9e4451
unknown <caixiangyi@zoneland.net>
2020-08-19 14:43:04 +0800

    修复数据网格的下拉组件设置成只读时值可能出错的问题

--- o2web/source/x_component_process_Xform/DatagridPC.js
+++ o2web/source/x_component_process_Xform/DatagridPC.js
@@ -284,6 +284,7 @@ MWF.xApplication.process.Xform.DatagridPC = new Class({
 	},
 
 	_editLine:function(td){
+		debugger;
 		if (this.isEdit){
 			if (!this._completeLineEdit()) return false;
 		}
@@ -413,6 +414,7 @@ MWF.xApplication.process.Xform.DatagridPC = new Class({
 
 	},
 	_completeLineEdit: function( ev ){
+		debugger;
 		//this.currentEditLine.getElemets(td);
 		if (!this.editValidation()){
 			return false;
