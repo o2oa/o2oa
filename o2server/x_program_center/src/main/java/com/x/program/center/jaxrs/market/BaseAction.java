20a44c3f10c33542e4cb9635d5ab65a713d4cf49
o2sword <171715986@qq.com>
2020-08-27 17:32:56 +0800

    新版应用市场优化

--- o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/market/BaseAction.java
+++ o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/market/BaseAction.java
@@ -1,7 +1,9 @@
 package com.x.program.center.jaxrs.market;
 
+import com.x.base.core.project.gson.GsonPropertyObject;
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.jaxrs.StandardJaxrsAction;
+import com.x.program.center.WrapModule;
 
 abstract class BaseAction extends StandardJaxrsAction {
 
@@ -13,6 +15,38 @@ abstract class BaseAction extends StandardJaxrsAction {
 			return true;
 		}
 		return false;
+	}
+
+	public static class InstallData extends GsonPropertyObject {
+		private WrapModule WrapModule;
+
+		private String staticResource;
+
+		private String customApp;
+
+		public com.x.program.center.WrapModule getWrapModule() {
+			return WrapModule;
+		}
+
+		public void setWrapModule(com.x.program.center.WrapModule wrapModule) {
+			WrapModule = wrapModule;
+		}
+
+		public String getStaticResource() {
+			return staticResource;
+		}
+
+		public void setStaticResource(String staticResource) {
+			this.staticResource = staticResource;
+		}
+
+		public String getCustomApp() {
+			return customApp;
+		}
+
+		public void setCustomApp(String customApp) {
+			this.customApp = customApp;
+		}
 	}
 
 }
