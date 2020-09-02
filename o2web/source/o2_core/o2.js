818500a276ec6b8eb9b871b570be249644c54914
huqi <hq_1980@hotmail.com>
2020-09-02 14:18:04 +0800

    表单性能优化

--- o2web/source/o2_core/o2.js
+++ o2web/source/o2_core/o2.js
@@ -1239,11 +1239,22 @@
 
     var _cacheUrls = [
         /jaxrs\/form\/workorworkcompleted\/.+/ig,
+        /jaxrs\/form\/.+/ig,
         //    /jaxrs\/script/ig,
         /jaxrs\/script\/.+\/app\/.+\/imported/ig,
         /jaxrs\/script\/portal\/.+\/name\/.+\/imported/ig,
         /jaxrs\/script\/.+\/application\/.+\/imported/ig,
-        /jaxrs\/page\/.+\/portal\/.+/ig
+        /jaxrs\/page\/.+\/portal\/.+/ig,
+        /jaxrs\/document\/.+/ig,
+        /jaxrs\/applicationdict\/.+/ig,
+        /jaxrs\/custom\/.+/ig
+
+        ///jaxrs\/form\/workorworkcompleted\/.+/ig,
+        //    /jaxrs\/script/ig,
+        // /jaxrs\/script\/.+\/app\/.+\/imported/ig,
+        // /jaxrs\/script\/portal\/.+\/name\/.+\/imported/ig,
+        // /jaxrs\/script\/.+\/application\/.+\/imported/ig,
+        // /jaxrs\/page\/.+\/portal\/.+/ig
         // /jaxrs\/authentication/ig
         // /jaxrs\/statement\/.*\/execute\/page\/.*\/size\/.*/ig
     ];
