f35d95580401a24d2f5969de56695caf2f3fa8d9
zhourui <zhourui@o2oa.net>
2020-09-02 10:20:41 +0800

    修改启动脚本

--- o2server/x_console/src/main/java/com/x/server/console/server/application/ApplicationServerTools.java
+++ o2server/x_console/src/main/java/com/x/server/console/server/application/ApplicationServerTools.java
@@ -83,8 +83,8 @@ public class ApplicationServerTools extends JettySeverTools {
 
 	private static Logger logger = LoggerFactory.getLogger(ApplicationServerTools.class);
 
-	private static int APPLICATIONSERVER_THREAD_POOL_SIZE_MIN = 50;
-	private static int APPLICATIONSERVER_THREAD_POOL_SIZE_MAX = 500;
+	private static final int APPLICATIONSERVER_THREAD_POOL_SIZE_MIN = 50;
+	private static final int APPLICATIONSERVER_THREAD_POOL_SIZE_MAX = 500;
 
 	private static final List<String> OFFICIAL_MODULE_SORTED_TEMPLATE = ListTools.toList(
 			x_general_assemble_control.class.getName(), x_organization_assemble_authentication.class.getName(),
