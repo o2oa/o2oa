24b86bdb80c81a949af0cb7c7ad6460a8a89a9b9
zhourui <zhourui@o2oa.net>
2020-09-11 17:54:14 +0800

    修改启动脚本先行拷贝

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/tools/PathTools.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/tools/PathTools.java
@@ -1,10 +1,11 @@
 package com.x.base.core.project.tools;
 
-public class PathTools {
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-	private PathTools() {
-		// nothing
-	}
+import org.apache.commons.lang3.StringUtils;
+
+public class PathTools {
 
 	public static final String WEB_INF = "WEB-INF";
 	public static final String WEB_INF_WEB_XML = "WEB-INF/web.xml";
@@ -12,5 +13,37 @@ public class PathTools {
 	public static final String WEB_INF_LASTMODIFIED = "WEB-INF/lastModified";
 	public static final String META_INF = "META";
 	public static final String DOT_WAR = ".war";
+	public static final String DOT_JAR = ".jar";
+	public static final String DOT_ZIP = ".zip";
+	public static final String DOT_BAT = ".bat";
+	public static final String DOT_SH = ".sh";
+
+	public static boolean shOrBat(Path path) {
+		if (Files.exists(path) && (!Files.isDirectory(path))) {
+			return StringUtils.endsWithAny(path.getFileName().toString().toLowerCase(), DOT_BAT, DOT_SH);
+		}
+		return false;
+	}
+
+	public static boolean jarOrZip(Path path) {
+		if (Files.exists(path) && (!Files.isDirectory(path))) {
+			return StringUtils.endsWithAny(path.getFileName().toString().toLowerCase(), DOT_JAR, DOT_ZIP);
+		}
+		return false;
+	}
+
+	public static boolean jar(Path path) {
+		if (Files.exists(path) && (!Files.isDirectory(path))) {
+			return StringUtils.endsWith(path.getFileName().toString().toLowerCase(), DOT_JAR);
+		}
+		return false;
+	}
+
+	public static boolean war(Path path) {
+		if (Files.exists(path) && (!Files.isDirectory(path))) {
+			return StringUtils.endsWith(path.getFileName().toString().toLowerCase(), DOT_WAR);
+		}
+		return false;
+	}
 
 }
