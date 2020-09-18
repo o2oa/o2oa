488042ebc6bc41e2276dae12013c880d8ce69929
o2sword <171715986@qq.com>
2020-09-18 17:46:56 +0800

    1、增加批量查询组织信息-树形展现接口；2、增加批量查询群组信息-树形展现接口；3、增加根据身份查询群组

--- o2server/x_organization_assemble_express/src/main/java/com/x/organization/assemble/express/jaxrs/unit/UnitAction.java
+++ o2server/x_organization_assemble_express/src/main/java/com/x/organization/assemble/express/jaxrs/unit/UnitAction.java
@@ -567,4 +567,22 @@ public class UnitAction extends StandardJaxrsAction {
 		}
 		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
 	}
+
+	@JaxrsMethodDescribe(value = "批量查询组织的嵌套下级组织信息-树形模式.", action = ActionListWithUnitTree.class)
+	@POST
+	@Path("list/unit/tree")
+	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void ListWithUnitTree(@Suspended final AsyncResponse asyncResponse,
+								  @Context HttpServletRequest request, JsonElement jsonElement) {
+		ActionResult<List<ActionListWithUnitTree.Wo>> result = new ActionResult<>();
+		EffectivePerson effectivePerson = this.effectivePerson(request);
+		try {
+			result = new ActionListWithUnitTree().execute(effectivePerson, jsonElement);
+		} catch (Exception e) {
+			logger.error(e, effectivePerson, request, jsonElement);
+			result.error(e);
+		}
+		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
+	}
 }
