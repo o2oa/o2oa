a7e6ea2a6a9d557190f5e1b9cc3b2aaba85cadc9
zhourui <zhourui@o2oa.net>
2020-09-25 11:56:30 +0800

    context 转换到 abstractContext

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/jaxrs/thread/ThreadAction.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/jaxrs/thread/ThreadAction.java
@@ -2,17 +2,21 @@ package com.x.base.core.project.jaxrs.thread;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 
 import com.x.base.core.project.annotation.JaxrsDescribe;
 import com.x.base.core.project.annotation.JaxrsMethodDescribe;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
+import com.x.base.core.project.http.HttpMediaType;
 import com.x.base.core.project.jaxrs.ResponseFactory;
 import com.x.base.core.project.jaxrs.StandardJaxrsAction;
 import com.x.base.core.project.logger.Logger;
@@ -26,6 +30,8 @@ public class ThreadAction extends StandardJaxrsAction {
 
 	@JaxrsMethodDescribe(value = "是否运行中.", action = ActionAlive.class)
 	@GET
+	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("alive/{name}")
 	public void alive(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
 			@Context ServletContext servletContext, @PathParam("name") String name) {
@@ -42,6 +48,8 @@ public class ThreadAction extends StandardJaxrsAction {
 
 	@JaxrsMethodDescribe(value = "获取参数.", action = ActionParameter.class)
 	@GET
+	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("parameter/{name}")
 	public void parameter(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
 			@Context ServletContext servletContext, @PathParam("name") String name) {
@@ -58,6 +66,8 @@ public class ThreadAction extends StandardJaxrsAction {
 
 	@JaxrsMethodDescribe(value = "停止运行.", action = ActionStop.class)
 	@GET
+	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("stop/{name}")
 	public void stop(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
 			@Context ServletContext servletContext, @PathParam("name") String name) {
