dd434bf46886bfe826bb3b6fa7ab8fbd260a257b
o2sword <171715986@qq.com>
2020-09-29 09:39:09 +0000

    流程平台管理员分页查询接口修改，解决数据量大慢的问题
    (cherry picked from commit 43353ae464cb65cc6f2464dea7e344d429db2092)

--- o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/workcompleted/ActionManageListFilterPaging.java
+++ o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/workcompleted/ActionManageListFilterPaging.java
@@ -35,15 +35,10 @@ class ActionManageListFilterPaging extends BaseAction {
 			ActionResult<List<Wo>> result = new ActionResult<>();
 			if (business.canManageApplication(effectivePerson, null)) {
 				Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
-				if (wi == null) {
-					wi = new Wi();
-				}
-				Integer adjustPage = this.adjustPage(page);
-				Integer adjustPageSize = this.adjustSize(size);
-				List<WorkCompleted> os = this.list(effectivePerson, business, adjustPage, adjustPageSize, wi);
-				List<Wo> wos = Wo.copier.copy(os);
+				Predicate p = this.toFilterPredicate(effectivePerson, business, wi);
+				List<Wo> wos = emc.fetchDescPaging(WorkCompleted.class, Wo.copier, p, page, size, WorkCompleted.startTime_FIELDNAME);
 				result.setData(wos);
-				result.setCount(this.count(effectivePerson, business, wi));
+				result.setCount(emc.count(WorkCompleted.class, p));
 			}else{
 				result.setData(new ArrayList<Wo>());
 				result.setCount(0L);
@@ -52,8 +47,7 @@ class ActionManageListFilterPaging extends BaseAction {
 		}
 	}
 
-	private List<WorkCompleted> list(EffectivePerson effectivePerson, Business business, Integer adjustPage,
-			Integer adjustPageSize, Wi wi) throws Exception {
+	private Predicate toFilterPredicate(EffectivePerson effectivePerson, Business business,  Wi wi) throws Exception {
 		EntityManager em = business.entityManagerContainer().get(WorkCompleted.class);
 		List<String> person_ids = business.organization().person().list(wi.getCredentialList());
 		CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -127,89 +121,7 @@ class ActionManageListFilterPaging extends BaseAction {
 			p = cb.and(p,cb.equal(root.get(WorkCompleted_.stringValue10), wi.getStringValue10()));
 		}
 
-
-
-
-		cq.select(root).where(p).orderBy(cb.desc(root.get(WorkCompleted_.startTime)));
-		return em.createQuery(cq).setFirstResult((adjustPage - 1) * adjustPageSize).setMaxResults(adjustPageSize)
-				.getResultList();
-	}
-
-	private Long count(EffectivePerson effectivePerson, Business business, Wi wi) throws Exception {
-		EntityManager em = business.entityManagerContainer().get(WorkCompleted.class);
-		List<String> person_ids = business.organization().person().list(wi.getCredentialList());
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery<Long> cq = cb.createQuery(Long.class);
-		Root<WorkCompleted> root = cq.from(WorkCompleted.class);
-		Predicate p = cb.conjunction();
-		if (ListTools.isNotEmpty(wi.getApplicationList())) {
-			p = cb.and(p, root.get(WorkCompleted_.application).in(wi.getApplicationList()));
-		}
-		if (ListTools.isNotEmpty(wi.getProcessList())) {
-			if(BooleanUtils.isFalse(wi.getRelateEditionProcess())) {
-				p = cb.and(p, root.get(WorkCompleted_.process).in(wi.getProcessList()));
-			}else{
-				p = cb.and(p, root.get(WorkCompleted_.process).in(business.process().listEditionProcess(wi.getProcessList())));
-			}
-		}
-		if(DateTools.isDateTimeOrDate(wi.getStartTime())){
-			p = cb.and(p, cb.greaterThan(root.get(WorkCompleted_.startTime), DateTools.parse(wi.getStartTime())));
-		}
-		if(DateTools.isDateTimeOrDate(wi.getEndTime())){
-			p = cb.and(p, cb.lessThan(root.get(WorkCompleted_.startTime), DateTools.parse(wi.getEndTime())));
-		}
-		if (ListTools.isNotEmpty(person_ids)) {
-			p = cb.and(p, root.get(WorkCompleted_.creatorPerson).in(person_ids));
-		}
-		if (ListTools.isNotEmpty(wi.getCreatorUnitList())) {
-			p = cb.and(p, root.get(WorkCompleted_.creatorUnit).in(wi.getCreatorUnitList()));
-		}
-		if (ListTools.isNotEmpty(wi.getWorkList())) {
-			p = cb.and(p, root.get(WorkCompleted_.id).in(wi.getWorkList()));
-		}
-		if (ListTools.isNotEmpty(wi.getJobList())) {
-			p = cb.and(p, root.get(WorkCompleted_.job).in(wi.getJobList()));
-		}
-		if (ListTools.isNotEmpty(wi.getStartTimeMonthList())) {
-			p = cb.and(p, root.get(WorkCompleted_.startTimeMonth).in(wi.getStartTimeMonthList()));
-		}
-		if (StringUtils.isNoneBlank(wi.getKey())) {
-			String key = StringTools.escapeSqlLikeKey(wi.getKey());
-			p = cb.and(p,cb.like(root.get(WorkCompleted_.title), "%" + key + "%", StringTools.SQL_ESCAPE_CHAR));
-		}
-
-		if (StringUtils.isNotBlank(wi.getStringValue01())){
-			p = cb.and(p,cb.equal(root.get(WorkCompleted_.stringValue01), wi.getStringValue01()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue02())){
-			p = cb.and(p,cb.equal(root.get(WorkCompleted_.stringValue02), wi.getStringValue02()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue03())){
-			p = cb.and(p,cb.equal(root.get(WorkCompleted_.stringValue03), wi.getStringValue03()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue04())){
-			p = cb.and(p,cb.equal(root.get(WorkCompleted_.stringValue04), wi.getStringValue04()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue05())){
-			p = cb.and(p,cb.equal(root.get(WorkCompleted_.stringValue05), wi.getStringValue05()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue06())){
-			p = cb.and(p,cb.equal(root.get(WorkCompleted_.stringValue06), wi.getStringValue06()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue07())){
-			p = cb.and(p,cb.equal(root.get(WorkCompleted_.stringValue07), wi.getStringValue07()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue08())){
-			p = cb.and(p,cb.equal(root.get(WorkCompleted_.stringValue08), wi.getStringValue08()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue09())){
-			p = cb.and(p,cb.equal(root.get(WorkCompleted_.stringValue09), wi.getStringValue09()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue10())){
-			p = cb.and(p,cb.equal(root.get(WorkCompleted_.stringValue10), wi.getStringValue10()));
-		}
-
-		return em.createQuery(cq.select(cb.count(root)).where(p)).getSingleResult();
+		return p;
 	}
 
 	public class Wi extends GsonPropertyObject {
