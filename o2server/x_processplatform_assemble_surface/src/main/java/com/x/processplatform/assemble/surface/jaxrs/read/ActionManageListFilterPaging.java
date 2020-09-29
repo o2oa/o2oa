3731808aa941c347e97c9706d30c7e6236cc087b
o2sword <171715986@qq.com>
2020-09-29 09:39:09 +0000

    流程平台管理员分页查询接口修改，解决数据量大慢的问题
    (cherry picked from commit 155260fb85ea86c7821dcd48ae88f3defa0f47b1)

--- o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/read/ActionManageListFilterPaging.java
+++ o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/read/ActionManageListFilterPaging.java
@@ -34,15 +34,10 @@ class ActionManageListFilterPaging extends BaseAction {
 			ActionResult<List<Wo>> result = new ActionResult<>();
 			if (business.canManageApplication(effectivePerson, null)) {
 				Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
-				if (wi == null) {
-					wi = new Wi();
-				}
-				Integer adjustPage = this.adjustPage(page);
-				Integer adjustPageSize = this.adjustSize(size);
-				List<Read> os = this.list(effectivePerson, business, adjustPage, adjustPageSize, wi);
-				List<Wo> wos = Wo.copier.copy(os);
+				Predicate p = this.toFilterPredicate(effectivePerson, business, wi);
+				List<Wo> wos = emc.fetchDescPaging(Read.class, Wo.copier, p, page, size, Read.startTime_FIELDNAME);
 				result.setData(wos);
-				result.setCount(this.count(effectivePerson, business, wi));
+				result.setCount(emc.count(Read.class, p));
 			}else{
 				result.setData(new ArrayList<Wo>());
 				result.setCount(0L);
@@ -51,8 +46,7 @@ class ActionManageListFilterPaging extends BaseAction {
 		}
 	}
 
-	private List<Read> list(EffectivePerson effectivePerson, Business business, Integer adjustPage,
-			Integer adjustPageSize, Wi wi) throws Exception {
+	private Predicate toFilterPredicate(EffectivePerson effectivePerson, Business business,  Wi wi) throws Exception {
 		EntityManager em = business.entityManagerContainer().get(Read.class);
 		List<String> person_ids = business.organization().person().list(wi.getCredentialList());
 		CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -138,98 +132,8 @@ class ActionManageListFilterPaging extends BaseAction {
 								cb.like(root.get(Read_.creatorUnit), "%" + key + "%")));
 			}
 		}
-		cq.select(root).where(p).orderBy(cb.desc(root.get(Read_.startTime)));
-		return em.createQuery(cq).setFirstResult((adjustPage - 1) * adjustPageSize).setMaxResults(adjustPageSize)
-				.getResultList();
-	}
 
-	private Long count(EffectivePerson effectivePerson, Business business, Wi wi) throws Exception {
-		EntityManager em = business.entityManagerContainer().get(Read.class);
-		List<String> person_ids = business.organization().person().list(wi.getCredentialList());
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery<Long> cq = cb.createQuery(Long.class);
-		Root<Read> root = cq.from(Read.class);
-		Predicate p = cb.conjunction();
-		if (ListTools.isNotEmpty(wi.getApplicationList())) {
-			p = cb.and(p, root.get(Read_.application).in(wi.getApplicationList()));
-		}
-		if (StringUtils.isNotBlank(wi.getPerson())){
-			p = cb.and(p,cb.equal(root.get(Read_.person), wi.getPerson()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue01())){
-			p = cb.and(p,cb.equal(root.get(Read_.stringValue01), wi.getStringValue01()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue02())){
-			p = cb.and(p,cb.equal(root.get(Read_.stringValue02), wi.getStringValue02()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue03())){
-			p = cb.and(p,cb.equal(root.get(Read_.stringValue03), wi.getStringValue03()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue04())){
-			p = cb.and(p,cb.equal(root.get(Read_.stringValue04), wi.getStringValue04()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue05())){
-			p = cb.and(p,cb.equal(root.get(Read_.stringValue05), wi.getStringValue05()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue06())){
-			p = cb.and(p,cb.equal(root.get(Read_.stringValue06), wi.getStringValue06()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue07())){
-			p = cb.and(p,cb.equal(root.get(Read_.stringValue07), wi.getStringValue07()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue08())){
-			p = cb.and(p,cb.equal(root.get(Read_.stringValue08), wi.getStringValue08()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue09())){
-			p = cb.and(p,cb.equal(root.get(Read_.stringValue09), wi.getStringValue09()));
-		}
-		if (StringUtils.isNotBlank(wi.getStringValue10())){
-			p = cb.and(p,cb.equal(root.get(Read_.stringValue10), wi.getStringValue10()));
-		}
-		if (ListTools.isNotEmpty(wi.getProcessList())) {
-			if(BooleanUtils.isFalse(wi.getRelateEditionProcess())) {
-				p = cb.and(p, root.get(Read_.process).in(wi.getProcessList()));
-			}else{
-				p = cb.and(p, root.get(Read_.process).in(business.process().listEditionProcess(wi.getProcessList())));
-			}
-		}
-		if(DateTools.isDateTimeOrDate(wi.getStartTime())){
-			p = cb.and(p, cb.greaterThan(root.get(Read_.startTime), DateTools.parse(wi.getStartTime())));
-		}
-		if(DateTools.isDateTimeOrDate(wi.getEndTime())){
-			p = cb.and(p, cb.lessThan(root.get(Read_.startTime), DateTools.parse(wi.getEndTime())));
-		}
-		if (ListTools.isNotEmpty(person_ids)) {
-			p = cb.and(p, root.get(Read_.person).in(person_ids));
-		}
-		if (ListTools.isNotEmpty(wi.getCreatorUnitList())) {
-			p = cb.and(p, root.get(Read_.creatorUnit).in(wi.getCreatorUnitList()));
-		}
-		if (ListTools.isNotEmpty(wi.getWorkList())) {
-			p = cb.and(p, root.get(Read_.work).in(wi.getWorkList()));
-		}
-		if (ListTools.isNotEmpty(wi.getJobList())) {
-			p = cb.and(p, root.get(Read_.job).in(wi.getJobList()));
-		}
-		if (ListTools.isNotEmpty(wi.getStartTimeMonthList())) {
-			p = cb.and(p, root.get(Read_.startTimeMonth).in(wi.getStartTimeMonthList()));
-		}
-		if (ListTools.isNotEmpty(wi.getActivityNameList())) {
-			p = cb.and(p, root.get(Read_.activityName).in(wi.getActivityNameList()));
-		}
-		if (StringUtils.isNotEmpty(wi.getKey())) {
-			String key = StringUtils.trim(StringUtils.replace(wi.getKey(), "\u3000", " "));
-			if (StringUtils.isNotEmpty(key)) {
-				key = StringUtils.replaceEach(key, new String[] { "?", "%" }, new String[] { "", "" });
-				p = cb.and(p,
-						cb.or(cb.like(root.get(Read_.title), "%" + key + "%"),
-								cb.like(root.get(Read_.opinion), "%" + key + "%"),
-								cb.like(root.get(Read_.serial), "%" + key + "%"),
-								cb.like(root.get(Read_.creatorPerson), "%" + key + "%"),
-								cb.like(root.get(Read_.creatorUnit), "%" + key + "%")));
-			}
-		}
-		return em.createQuery(cq.select(cb.count(root)).where(p)).getSingleResult();
+		return p;
 	}
 
 	public class Wi extends GsonPropertyObject {
