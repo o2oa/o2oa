2cfa67c1d79098c182f851274d9449f965b2d1b0
unknown <caixiangyi@zoneland.net>
2020-10-09 14:29:07 +0800

    查询设计中增加视图功能

--- o2web/source/x_component_query_InquiryDesigner/lp/zh-cn.js
+++
@@ -1,7 +0,0 @@
-MWF.xApplication.query = MWF.xApplication.query || {};
-MWF.xApplication.query.InquiryDesigner = MWF.xApplication.query.InquiryDesigner || {};
-MWF.xApplication.query.ViewDesigner = MWF.xApplication.query.ViewDesigner || {};
-MWF.xDesktop.requireApp("process.ViewDesigner", "lp."+MWF.language, null, false);
-MWF.xApplication.query.InquiryDesigner.LP = Object.merge( MWF.xApplication.query.ViewDesigner.LP, {
-    "title" : "查询设计"
-});
