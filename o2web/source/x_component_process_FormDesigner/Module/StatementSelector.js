0ae6ab07e44ed6b783d28f0cb81685e2f4e92516
unknown <caixiangyi@zoneland.net>
2020-10-15 11:22:27 +0800

    查询中允许使用视图

---
+++ o2web/source/x_component_process_FormDesigner/Module/StatementSelector.js
@@ -0,0 +1,25 @@
+MWF.xApplication.process.FormDesigner.Module = MWF.xApplication.process.FormDesigner.Module || {};
+MWF.xDesktop.requireApp("process.FormDesigner", "Module.$Element", null, false);
+MWF.xApplication.process.FormDesigner.Module.StatementSelector = MWF.FCStatementSelector = new Class({
+	Extends: MWF.xApplication.process.FormDesigner.Module.Button,
+	Implements: [Options, Events],
+	options: {
+		"style": "default",
+		"propertyPath": "../x_component_process_FormDesigner/Module/StatementSelector/StatementSelector.html"
+	},
+	
+	initialize: function(form, options){
+		this.setOptions(options);
+		
+		this.path = "../x_component_process_FormDesigner/Module/StatementSelector/";
+		this.cssPath = "../x_component_process_FormDesigner/Module/StatementSelector/"+this.options.style+"/css.wcss";
+
+		this._loadCss();
+		this.moduleType = "element";
+		this.moduleName = "statementSelector";
+
+		this.form = form;
+		this.container = null;
+		this.containerNode = null;
+	}
+});
