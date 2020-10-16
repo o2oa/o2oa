f2722cde18dddfe96d0842425b7d7eb3924d0349
o2sword <171715986@qq.com>
2020-10-16 10:36:28 +0800

    增加判断当前用户是否是云文件管理员角色

--- o2server/x_file_assemble_control/src/main/java/com/x/file/assemble/control/jaxrs/config/ConfigAction.java
+++ o2server/x_file_assemble_control/src/main/java/com/x/file/assemble/control/jaxrs/config/ConfigAction.java
@@ -3,7 +3,6 @@ package com.x.file.assemble.control.jaxrs.config;
 import com.google.gson.JsonElement;
 import com.x.base.core.project.annotation.JaxrsDescribe;
 import com.x.base.core.project.annotation.JaxrsMethodDescribe;
-import com.x.base.core.project.annotation.JaxrsParameterDescribe;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.http.HttpMediaType;
@@ -11,7 +10,6 @@ import com.x.base.core.project.jaxrs.ResponseFactory;
 import com.x.base.core.project.jaxrs.StandardJaxrsAction;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
-import com.x.file.assemble.control.jaxrs.recycle.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
@@ -19,13 +17,29 @@ import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
 
 @Path("config")
 @JaxrsDescribe("云文件管理配置")
 public class ConfigAction extends StandardJaxrsAction {
 
 	private static Logger logger = LoggerFactory.getLogger(ConfigAction.class);
+
+	@JaxrsMethodDescribe(value = "判断当前用户是否是云文件管理员。", action = ActionIsManager.class)
+	@GET
+	@Path("is/file/manager")
+	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void isFileManager(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request) {
+		ActionResult<ActionIsManager.Wo> result = new ActionResult<>();
+		EffectivePerson effectivePerson = this.effectivePerson(request);
+		try {
+			result = new ActionIsManager().execute(effectivePerson);
+		} catch (Exception e) {
+			logger.error(e, effectivePerson, request, null);
+			result.error(e);
+		}
+		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
+	}
 
 	@JaxrsMethodDescribe(value = "获取云文件系统配置。", action = ActionGetSystemConfig.class)
 	@GET
