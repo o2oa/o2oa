ab05b47e21607ca0c832b036c37e04179130bf62
fancy <fancylou@outlook.com>
2020-10-26 16:12:05 +0800

    钉钉回调功能

--- o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/dingding/DingdingAction.java
+++ o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/dingding/DingdingAction.java
@@ -1,15 +1,12 @@
 package com.x.program.center.jaxrs.dingding;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.google.gson.JsonElement;
 import com.x.base.core.project.annotation.JaxrsDescribe;
@@ -21,6 +18,9 @@ import com.x.base.core.project.jaxrs.ResponseFactory;
 import com.x.base.core.project.jaxrs.StandardJaxrsAction;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("dingding")
 @JaxrsDescribe("钉钉接口")
@@ -61,6 +61,74 @@ public class DingdingAction extends StandardJaxrsAction {
 			result.error(e);
 		}
 		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
+	}
+
+
+	@JaxrsMethodDescribe(value = "获取钉钉回调的EncodingAESKey.", action = ActionCallbackAESKey.class)
+	@GET
+	@Path("get/callback/aes")
+	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void syncOrganizationCallbackEncodingAESKeyGet(@Suspended final AsyncResponse asyncResponse,
+														  @Context HttpServletRequest request) {
+		EffectivePerson effectivePerson = this.effectivePerson(request);
+		ActionResult<ActionCallbackAESKey.Wo> result = new ActionResult<>();
+		try {
+			result = new ActionCallbackAESKey().execute(effectivePerson);
+		}catch (Exception e) {
+			logger.error(e, effectivePerson, request, null);
+			result.error(e);
+		}
+		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
+	}
+
+	@JaxrsMethodDescribe(value = "到钉钉注册回调地址", action = ActionSyncOrganizationCallbackUrlRegister.class)
+	@GET
+	@Path("sync/organization/register/callback")
+	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void registerSyncOrgCallbackUrl(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request) {
+		EffectivePerson effectivePerson = this.effectivePerson(request);
+		ActionResult<ActionSyncOrganizationCallbackUrlRegister.Wo> result = new ActionResult<>();
+		try {
+			result = new ActionSyncOrganizationCallbackUrlRegister().execute(effectivePerson);
+		} catch (Exception e) {
+			logger.error(e, effectivePerson, request, null);
+			result.error(e);
+		}
+		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
+	}
+
+	/**
+	 * 钉钉回调用的post接口 需要返回固定格式的json字符串
+	 * @param asyncResponse
+	 * @param request
+	 * @param signature
+	 * @param timestamp
+	 * @param nonce
+	 * @param jsonElement
+	 */
+	@JaxrsMethodDescribe(value = "接收钉钉通讯录变更回调的接口", action = ActionSyncOrganizationCallbackPost.class)
+	@POST
+	@Path("sync/organization/callback")
+	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void syncOrganizationCallback(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
+										 @QueryParam("signature") String signature,
+										 @QueryParam("timestamp") String timestamp, @QueryParam("nonce") String nonce,
+										 JsonElement jsonElement) {
+		EffectivePerson effectivePerson = this.effectivePerson(request);
+		try {
+			String json = new ActionSyncOrganizationCallbackPost().execute(effectivePerson, signature, timestamp, nonce, jsonElement);
+			asyncResponse.resume(Response.ok(json).build());
+		} catch (Exception e) {
+			logger.error(e, effectivePerson, request, null);
+			asyncResponse.resume(Response.serverError().entity("fail").build());
+		}
+
+
+
+//		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
 	}
 
 }
