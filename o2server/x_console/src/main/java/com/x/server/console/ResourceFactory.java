3d405fd0c399443e1f4ced0181099535f26265bc
zhourui <zhourui@o2oa.net>
2020-10-26 13:30:15 +0800

    增加Kingase8R6适配

--- o2server/x_console/src/main/java/com/x/server/console/ResourceFactory.java
+++ o2server/x_console/src/main/java/com/x/server/console/ResourceFactory.java
@@ -32,7 +32,6 @@ import com.x.base.core.project.config.CenterServer;
 import com.x.base.core.project.config.Config;
 import com.x.base.core.project.config.DataServer;
 import com.x.base.core.project.config.ExternalDataSource;
-import com.x.base.core.project.gson.XGsonBuilder;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
 import com.x.base.core.project.tools.ClassLoaderTools;
