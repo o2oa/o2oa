054a79bbb0f55ddc4b8c1c2dbd9f7862bc100ddf
zhourui <zhourui@o2oa.net>
2020-10-27 22:29:08 +0800

    添加work/V2GetWorkOrWorkCompleted

--- o2server/x_processplatform_core_entity/src/main/java/com/x/processplatform/core/entity/element/Message.java
+++ o2server/x_processplatform_core_entity/src/main/java/com/x/processplatform/core/entity/element/Message.java
@@ -1,5 +1,6 @@
 package com.x.processplatform.core.entity.element;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Basic;
@@ -14,6 +15,7 @@ import javax.persistence.OrderColumn;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.openjpa.persistence.PersistentCollection;
 import org.apache.openjpa.persistence.jdbc.ContainerTable;
 import org.apache.openjpa.persistence.jdbc.ElementColumn;
@@ -27,6 +29,7 @@ import com.x.base.core.entity.annotation.ContainerEntity;
 import com.x.base.core.entity.annotation.Flag;
 import com.x.base.core.entity.annotation.IdReference;
 import com.x.base.core.project.annotation.FieldDescribe;
+import com.x.base.core.project.tools.ListTools;
 import com.x.processplatform.core.entity.PersistenceProperties;
 
 @Entity
@@ -58,6 +61,15 @@ public class Message extends Activity {
 
 	public void onPersist() throws Exception {
 		// nothing
+	}
+
+	@Override
+	public List<String> getRouteList() {
+		if (StringUtils.isNotEmpty(this.getRoute())) {
+			return ListTools.toList(this.getRoute());
+		} else {
+			return new ArrayList<>();
+		}
 	}
 
 	@FieldDescribe("分组")
@@ -325,7 +337,7 @@ public class Message extends Activity {
 	@Column(name = ColumnNamePrefix + allowRerouteTo_FIELDNAME)
 	@Index(name = TABLE + IndexNameMiddle + allowRerouteTo_FIELDNAME)
 	private Boolean allowRerouteTo;
-	
+
 	@FieldDescribe("允许挂起")
 	@CheckPersist(allowEmpty = true)
 	@Column(name = ColumnNamePrefix + allowSuspend_FIELDNAME)
