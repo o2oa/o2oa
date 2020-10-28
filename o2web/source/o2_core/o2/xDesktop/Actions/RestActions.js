9a8d6419ebc45904715a77398c85c62e5334f7af
huqi <hq_1980@hotmail.com>
2020-10-28 19:39:48 +0800

    请求方法的转换（put转post，delete转get）

--- o2web/source/o2_core/o2/xDesktop/Actions/RestActions.js
+++ o2web/source/o2_core/o2/xDesktop/Actions/RestActions.js
@@ -89,6 +89,16 @@ MWF.xDesktop.Actions.RestActions = new Class({
             }
             uri = this.address+uri;
 
+            //putToPost, deleteToGet
+            if (layout.config.mock && layout.config.mock[this.serviceName]){
+                var mock = layout.config.mock[this.serviceName][method.toLowerCase()];
+                if (mock){
+                    method = mock.to || method;
+                    var append = mock.append;
+                    url += ((url.substr(url.length-1, 1)=="/") ? append : "/"+append);
+                }
+            }
+
             var async = (option.async===false) ? false : true;
 
             var callback = new MWF.xDesktop.Actions.RestActions.Callback(option.success, option.failure);
