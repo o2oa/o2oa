9ededc66f2be091192576d5998f25f305b05dbd7
unknown <caixiangyi@zoneland.net>
2020-11-18 10:35:58 +0800

    内容管理评论加了编辑器的配置

--- o2web/source/x_component_cms_Xform/Comment.js
+++ o2web/source/x_component_cms_Xform/Comment.js
@@ -5,11 +5,28 @@ MWF.xApplication.cms.Xform.Comment = MWF.CMSComment =  new Class({
 	_loadUserInterface: function(){
 		this.node.empty();
         this.node.setStyle("-webkit-user-select", "text");
+
+        debugger;
+
+        var config = {};
+        if(this.json.editorProperties){
+            config = Object.clone(this.json.editorProperties);
+        }
+        if (this.json.config){
+            if (this.json.config.code){
+                var obj = this.form.Macro.exec(this.json.config.code, this);
+                Object.each(obj, function(v, k){
+                    config[k] = v;
+                });
+            }
+        }
+
         this.comment = new MWF.xApplication.cms.Xform.widget.Comment( this.form.app, this.node, {
             "documentId" : this.form.businessData.document.id,
             "countPerPage" : this.json.countPerPage || 10,
             "isAllowModified" : this.json.isAllowModified,
-            "isAdmin" : this.form.app.isAdmin
+            "isAdmin" : this.form.app.isAdmin,
+            "editorProperties" : config
         });
         this.comment.load();
 	}
