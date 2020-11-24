5503f0beff5e265d16023f2bc376fbde2e93c575
o2null <zhourui@o2oa.net>
2020-11-24 16:51:20 +0800

    删除general

--- o2server/x_general_assemble_control/src/main/java/com/x/general/assemble/control/jaxrs/generalfile/GeneralFileAction.java
+++ o2server/x_general_assemble_control/src/main/java/com/x/general/assemble/control/jaxrs/generalfile/GeneralFileAction.java
@@ -1,5 +1,15 @@
 package com.x.general.assemble.control.jaxrs.generalfile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
 import com.x.base.core.project.annotation.JaxrsDescribe;
 import com.x.base.core.project.annotation.JaxrsMethodDescribe;
 import com.x.base.core.project.annotation.JaxrsParameterDescribe;
@@ -9,13 +19,6 @@ import com.x.base.core.project.jaxrs.ResponseFactory;
 import com.x.base.core.project.jaxrs.StandardJaxrsAction;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.*;
-import javax.ws.rs.container.AsyncResponse;
-import javax.ws.rs.container.Suspended;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 
 @Path("generalfile")
 @JaxrsDescribe("获取附件")
