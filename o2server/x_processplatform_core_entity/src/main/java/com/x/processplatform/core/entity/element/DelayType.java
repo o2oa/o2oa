0aa1c6603919ac4a00d238f012399d0fb22082ff
o2null <zhourui@o2oa.net>
2020-12-10 18:44:36 +0800

    修改delayMode为delayType

--- o2server/x_processplatform_core_entity/src/main/java/com/x/processplatform/core/entity/element/DelayType.java
+++ o2server/x_processplatform_core_entity/src/main/java/com/x/processplatform/core/entity/element/DelayType.java
@@ -2,8 +2,7 @@ package com.x.processplatform.core.entity.element;
 
 import com.x.base.core.entity.JpaObject;
 
-public enum DelayMode {
-	/* 定义的时候使用了 @Enumerated(EnumType.ORDINAL),这里的顺序不能修改 */
+public enum DelayType {
 	until, minute;
 	public static final int length = JpaObject.length_16B;
 }
