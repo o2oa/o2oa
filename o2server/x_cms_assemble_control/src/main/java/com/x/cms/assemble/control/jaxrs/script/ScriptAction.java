eb422b81c291bfdcd7a2e4f31a11c7cd4bc40e54
o2sword <171715986@qq.com>
2020-12-16 16:13:28 +0800

    全局设计搜索之script脚本搜索2

--- o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/script/ScriptAction.java
+++ o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/script/ScriptAction.java
@@ -260,15 +260,16 @@ public class ScriptAction extends StandardJaxrsAction {
 	}
 
 	@JaxrsMethodDescribe(value = "列示Script对象（管理员权限）.", action = ActionManagerList.class)
-	@GET
+	@POST
 	@Path("list/manager")
 	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void managerList(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request) {
+	public void managerList(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
+							JsonElement jsonElement) {
 		ActionResult<List<ActionManagerList.Wo>> result = new ActionResult<>();
 		EffectivePerson effectivePerson = this.effectivePerson(request);
 		try {
-			result = new ActionManagerList().execute(effectivePerson);
+			result = new ActionManagerList().execute(effectivePerson, jsonElement);
 		} catch (Exception e) {
 			logger.error(e, effectivePerson, request, null);
 			result.error(e);
