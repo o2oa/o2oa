643005d52ecd97149597a3481f5eb908bea72abd
unknown <caixiangyi@zoneland.net>
2020-12-17 10:32:35 +0800

    jsdoc

--- o2web/source/x_component_cms_Xform/Textfield.js
+++ o2web/source/x_component_cms_Xform/Textfield.js
@@ -1,8 +1,4 @@
 MWF.xDesktop.requireApp("process.Xform", "Textfield", null, false);
-/** @class cms.Textfield 文本输入框。
- * @extends MWF.xApplication.process.Xform.Textfield
- * @hideconstructor
- */
 MWF.xApplication.cms.Xform.Textfield = MWF.CMSTextfield =  new Class({
 	Extends: MWF.APPTextfield
 }); 
