f034860fa873037354af7e360925b274fa63fca7
unknown <caixiangyi@zoneland.net>
2020-12-23 15:17:13 +0800

    jsdoc

--- o2web/source/x_component_process_Xform/Documenteditor.js
+++ o2web/source/x_component_process_Xform/Documenteditor.js
@@ -7,6 +7,7 @@ MWF.xDesktop.requireApp("process.Xform", "$Module", null, false);
  * //方法2
  * var attachment = this.target; //在组件事件脚本中获取
  * @extends MWF.xApplication.process.Xform.$Module
+ * @category FormComponents
  * @hideconstructor
  */
 MWF.xApplication.process.Xform.Documenteditor = MWF.APPDocumenteditor =  new Class(
