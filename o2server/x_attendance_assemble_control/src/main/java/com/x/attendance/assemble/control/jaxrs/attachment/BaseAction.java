450f9a01f3231a48dddb20d3251088da96a6558f
luojing <luojing@o2oa.net>
2020-12-25 09:46:41 +0800

    考勤，新增统计导出功能，查询新增时间段作为查询条件

--- o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/jaxrs/attachment/BaseAction.java
+++ o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/jaxrs/attachment/BaseAction.java
@@ -2,10 +2,12 @@ package com.x.attendance.assemble.control.jaxrs.attachment;
 
 import com.x.attendance.assemble.common.date.DateOperation;
 import com.x.attendance.assemble.control.service.AttendanceImportFileInfoServiceAdv;
+import com.x.attendance.assemble.control.service.AttendanceScheduleSettingServiceAdv;
 import com.x.base.core.project.jaxrs.StandardJaxrsAction;
 
 public class BaseAction extends StandardJaxrsAction {
 	protected AttendanceImportFileInfoServiceAdv importFileInfoServiceAdv = new AttendanceImportFileInfoServiceAdv();
+	protected AttendanceScheduleSettingServiceAdv attendanceScheduleSettingServiceAdv = new AttendanceScheduleSettingServiceAdv();
 	protected DateOperation dateOperation = new DateOperation();
 }
 
