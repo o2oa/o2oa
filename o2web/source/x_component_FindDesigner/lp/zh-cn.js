eebe8a0c1198b4cb9e9f963335361ac288a677a7
huqi <hq_1980@hotmail.com>
2020-12-29 11:50:34 +0800

    设计搜索功能

--- o2web/source/x_component_FindDesigner/lp/zh-cn.js
+++ o2web/source/x_component_FindDesigner/lp/zh-cn.js
@@ -13,9 +13,17 @@ MWF.xApplication.FindDesigner.LP = {
 	"statement": "脚本",
 	"stat": "统计",
 
+	"processPlatform": "流程平台",
+	"cms": "内容管理",
+	"portal": "门户管理",
+	"query": "数据中心",
+	"service": "服务管理",
+
 	"search_placeholder": "请输入搜索内容",
 	"dockToBottom": "固定到下方",
 	"dockToRight": "固定到右侧",
 	"nothingFind": "未找到任何结果\n请输入搜索关键字",
-	"nothingPreview": "选择查找结果列表，可在此处预览"
+	"nothingPreview": "选择查找结果列表，可在此处预览",
+
+	"selectRange": "选择查找范围"
 };
