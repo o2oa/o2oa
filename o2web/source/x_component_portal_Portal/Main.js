f1b12040c4a1421488132a3c78422ee1b03a8238
unknown <caixiangyi@zoneland.net>
2021-01-13 10:39:18 +0800

    修复门户部件打开可能出错的问题

--- o2web/source/x_component_portal_Portal/Main.js
+++ o2web/source/x_component_portal_Portal/Main.js
@@ -154,7 +154,8 @@ MWF.xApplication.portal.Portal.Main = new Class({
         }
     },
     openPage: function(pageJson, par, callback){
-        this.setTitle((this.portal && this.portal.name) ? this.portal.name+"-"+pageJson.data.page.name : pageJson.data.page.name);
+        var pageName = pageJson.data.page ? pageJson.data.page.name : pageJson.data.name;
+        this.setTitle((this.portal && this.portal.name) ? this.portal.name+"-"+pageName : pageName);
         if (pageJson.data.page){
             this.page = (pageJson.data.page.data) ? JSON.decode(MWF.decodeJsonString(pageJson.data.page.data)): null;
             this.relatedFormMap = pageJson.data.relatedWidgetMap;
@@ -205,7 +206,10 @@ MWF.xApplication.portal.Portal.Main = new Class({
         }
         this.action.getApplication(this.options.portalId, function(json){
             this.portal = json.data;
-            if (this.pageJson) this.setTitle(this.portal.name+"-"+this.pageJson.data.page.name);
+            if (this.pageJson){
+                var pageName = this.pageJson.data.page ? this.pageJson.data.page.name : this.pageJson.data.name;
+                this.setTitle(this.portal.name+"-"+pageName);
+            }
 
             if (this.portal.icon){
                 if (this.taskitem){
