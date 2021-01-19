9b3cb9553fa7361f66f37ecc2c6cf8e0b9e94725
zhourui <zhourui@o2oa.net>
2021-01-19 11:49:01 +0800

    改为通过sso配置进行验证

---
+++ o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/invoke/ExceptionReadToken.java
@@ -0,0 +1,12 @@
+package com.x.program.center.jaxrs.invoke;
+
+import com.x.base.core.project.exception.PromptException;
+
+class ExceptionReadToken extends PromptException {
+
+	private static final long serialVersionUID = 4132300948670472899L;
+
+	ExceptionReadToken(String client, String token) {
+		super("can not read sso token, client:{}, token:{}.", client, token);
+	}
+}
