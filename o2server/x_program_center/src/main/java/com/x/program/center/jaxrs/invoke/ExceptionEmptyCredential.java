834c598c52ed15c2d11feb728f109f7a7032e6e5
zhourui <zhourui@o2oa.net>
2021-01-19 11:49:01 +0800

    改为通过sso配置进行验证

---
+++ o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/invoke/ExceptionEmptyCredential.java
@@ -0,0 +1,12 @@
+package com.x.program.center.jaxrs.invoke;
+
+import com.x.base.core.project.exception.PromptException;
+
+class ExceptionEmptyCredential extends PromptException {
+
+	private static final long serialVersionUID = 4132300948670472899L;
+
+	ExceptionEmptyCredential() {
+		super("名称为空.");
+	}
+}
