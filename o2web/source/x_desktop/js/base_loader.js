0f4307737ec92cf27824f8559fc55a6d37ba6a7d
fancy <fancylou@outlook.com>
2021-02-07 13:36:29 +0800

    覆盖token

--- o2web/source/x_desktop/js/base_loader.js
+++ o2web/source/x_desktop/js/base_loader.js
@@ -95,11 +95,34 @@ o2.addReady(function () {
             }
         };
 
+        // 是否ip
+        var _isIp = function(ip) {
+            var reg = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/
+            return reg.test(ip);
+        };
+        
+
         //修改支持x-token
         var uri = new URI(window.location.href);
         var options = uri.get("data");
         if (options["x-token"]) {
-            Cookie.write("x-token", options["x-token"]);
+            // 删除
+            Cookie.dispose("x-token");
+            // 写入
+            var host = window.location.host; // 域名 
+            var domain = null;
+            if (_isIp(host)) {
+                domain = host;
+            }else {
+                if (host.indexOf(".") > 0) {
+                    domain = host.substring(host.indexOf(".")); // 上级域名 如 .o2oa.net
+                }
+            }
+            if (domain) {
+                Cookie.write("x-token", options["x-token"], {domain: domain, path:"/"});
+            }else {
+                Cookie.write("x-token", options["x-token"]);
+            }
         }
 
         layout.sessionPromise = new Promise(function(resolve, reject){
