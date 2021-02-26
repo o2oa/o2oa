5e0d6a2c097c164b448d66ad7c2acf4dfa483d82
zhourui <zhourui@o2oa.net>
2021-02-26 21:25:32 +0800

    修改SignalStack 支持异步操作

--- o2server/x_processplatform_core_entity/src/main/java/com/x/processplatform/core/entity/log/SignalStack.java
+++ o2server/x_processplatform_core_entity/src/main/java/com/x/processplatform/core/entity/log/SignalStack.java
@@ -1,8 +1,8 @@
 package com.x.processplatform.core.entity.log;
 
-import java.util.LinkedList;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-public class SignalStack extends LinkedList<Signal> {
+public class SignalStack extends CopyOnWriteArrayList<Signal> {
 
 	private static final long serialVersionUID = -3971898583590304010L;
 
