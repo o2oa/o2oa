7cf819573125003fa1ddad6b579d1064a87cc344
unknown <caixiangyi@zoneland.net>
2021-03-03 11:04:23 +0800

    导入导出模型

---
+++ o2web/source/x_component_query_ImporterDesigner/lp/zh-cn.js
@@ -0,0 +1,59 @@
+MWF.xApplication.query.ImporterDesigner.LP = {
+    "title": "导入模型编辑",
+    "newView": "新建导入模型",
+    "property": "属性",
+    "item": "项目",
+    "type": "类型",
+    "value": "值",
+    "importer": "导入模型",
+    "unnamed": "无标题列",
+    "unCategory": "未分类",
+
+    "id": "标识",
+    "name": "名称",
+    "alias": "别名",
+    "description": "描述",
+    "saveViewNotice" : "请先保存导入模型！",
+
+    "application" : "数据应用",
+    "newImporter" : "新导入模型",
+    "newViewName" : "新导入模型名称",
+    "copy" : "副本",
+    "ok" : "确定",
+    "cancel" : "取消",
+
+    "action": {
+        "move": "移动",
+        "delete": "删除",
+        "add": "添加"
+    },
+
+    "notice": {
+        "save_success": "导入模型保存成功！",
+        "saveAs_success" : "导入模型另存成功，您可以在导入模型设置中打开新的导入模型！",
+
+        "deleteFilterTitle": "删除筛选条件确认",
+        "deleteFilter": "是否确定删除筛选条件？",
+
+        "deleteColumnTitle": "删除列确认",
+        "deleteColumn": "是否确定删除当前列？",
+
+
+        "changeTypeTitle": "改变数据类型确认",
+        "changeTypeDeleteChildren": "改变数据类型将删除所有子数据，是否确定要执行？",
+        "changeType": "改变数据类型将改变数据的值，是否确定要执行？",
+
+        "inputTypeError": "您输入的数据类型有误，请重新输入",
+        "sameKey": "您输入的项目名称在对象中已存在，请重新输入",
+        "emptyKey": "项目名称不能为空，请重新输入",
+        "numberKey": "项目名称不能为数字，请重新输入",
+
+        "inputName": "请输入导入模型名称",
+        "noModifyName": "不能修改名称或者别名",
+
+        "deleteEventTitle": "删除事件确认",
+        "deleteEvent": "是否确定删除当前事件吗？"
+    },
+    "mastInputPath": "请输入数据路径",
+    "mastInputTitle": "请输入标题"
+};
