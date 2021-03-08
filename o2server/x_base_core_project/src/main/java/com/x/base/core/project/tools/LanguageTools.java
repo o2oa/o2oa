2520a7ba23c1c822dbfb6e74f7d7b65f0cdb5f2d
o2sword <171715986@qq.com>
2021-03-08 10:16:11 +0800

    国语言国际化2

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/tools/LanguageTools.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/tools/LanguageTools.java
@@ -7,6 +7,8 @@ import java.util.ResourceBundle;
 
 public class LanguageTools {
 
+    private final static String LANGUAGE_PLATFORM = "language.platform";
+
     public static String getValueByKey(String key) {
         return getValueByKey(key, null);
     }
@@ -17,13 +19,31 @@ public class LanguageTools {
         }
         ResourceBundle resourceBundle = null;
         if(StringUtils.isBlank(locale)){
-            resourceBundle = ResourceBundle.getBundle("language.platform", Locale.getDefault());
+            resourceBundle = ResourceBundle.getBundle(LANGUAGE_PLATFORM, Locale.getDefault());
         }else if("zh".equalsIgnoreCase(locale) || "zh_CN".equalsIgnoreCase(locale)) {
-            resourceBundle = ResourceBundle.getBundle("language.platform", Locale.SIMPLIFIED_CHINESE);
+            resourceBundle = ResourceBundle.getBundle(LANGUAGE_PLATFORM, Locale.SIMPLIFIED_CHINESE);
         }else if(locale.toLowerCase().startsWith("en")) {
-            resourceBundle = ResourceBundle.getBundle("language.platform", Locale.ENGLISH);
+            resourceBundle = ResourceBundle.getBundle(LANGUAGE_PLATFORM, Locale.ENGLISH);
         }else if("zh_HK".equalsIgnoreCase(locale) || "zh_TW".equalsIgnoreCase(locale)) {
-            resourceBundle = ResourceBundle.getBundle("language.platform", Locale.TRADITIONAL_CHINESE);
+            resourceBundle = ResourceBundle.getBundle(LANGUAGE_PLATFORM, Locale.TRADITIONAL_CHINESE);
+        }
+
+        return resourceBundle.getString(key);
+    }
+
+    public static String getValueByKey(String baseName ,String key, String locale) {
+        if(StringUtils.isBlank(key)){
+            return null;
+        }
+        ResourceBundle resourceBundle = null;
+        if(StringUtils.isBlank(locale)){
+            resourceBundle = ResourceBundle.getBundle(baseName, Locale.getDefault());
+        }else if("zh".equalsIgnoreCase(locale) || "zh_CN".equalsIgnoreCase(locale)) {
+            resourceBundle = ResourceBundle.getBundle(baseName, Locale.SIMPLIFIED_CHINESE);
+        }else if(locale.toLowerCase().startsWith("en")) {
+            resourceBundle = ResourceBundle.getBundle(baseName, Locale.ENGLISH);
+        }else if("zh_HK".equalsIgnoreCase(locale) || "zh_TW".equalsIgnoreCase(locale)) {
+            resourceBundle = ResourceBundle.getBundle(baseName, Locale.TRADITIONAL_CHINESE);
         }
 
         return resourceBundle.getString(key);
