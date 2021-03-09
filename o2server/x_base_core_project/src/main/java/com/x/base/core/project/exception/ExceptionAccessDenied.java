226c0402e036ac5725d2fbfe0cc37e07969cbe48
o2sword <171715986@qq.com>
2021-03-09 18:09:56 +0800

    多语言国际化3

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/exception/ExceptionAccessDenied.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/exception/ExceptionAccessDenied.java
@@ -4,26 +4,27 @@ import java.util.Objects;
 
 import com.x.base.core.entity.JpaObject;
 import com.x.base.core.project.http.EffectivePerson;
+import com.x.base.core.project.tools.LanguageTools;
 
 public class ExceptionAccessDenied extends PromptException {
 
 	private static final long serialVersionUID = -7354813827434276962L;
 
 	public ExceptionAccessDenied(String person) {
-		super("用户:{} 权限不足.", person);
+		super(LanguageTools.getValue("exception_accessDenied"), person);
 	}
 
 	public ExceptionAccessDenied(EffectivePerson effectivePerson) {
-		super("用户:{} 权限不足.", effectivePerson.getDistinguishedName());
+		super(LanguageTools.getValue("exception_accessDenied"), effectivePerson.getDistinguishedName());
 	}
 
 	public ExceptionAccessDenied(EffectivePerson effectivePerson, JpaObject jpa) {
-		super("用户:{} 访问对象 class:{}, id:{}, 权限不足.", effectivePerson.getDistinguishedName(),
+		super(LanguageTools.getValue("exception_accessDenied_1"), effectivePerson.getDistinguishedName(),
 				(null == jpa) ? null : jpa.getClass().getName(), (null == jpa) ? null : jpa.getId());
 	}
 
 	public ExceptionAccessDenied(EffectivePerson effectivePerson, String message) {
-		super("用户:{} 权限不足, {}.", effectivePerson.getDistinguishedName(), Objects.toString(message, ""));
+		super(LanguageTools.getValue("exception_accessDenied"), effectivePerson.getDistinguishedName(), Objects.toString(message, ""));
 	}
 
 }
