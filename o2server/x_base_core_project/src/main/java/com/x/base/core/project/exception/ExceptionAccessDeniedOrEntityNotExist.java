c76b092ccf23e253915f0cfc1cb8669814dd8dce
o2sword <171715986@qq.com>
2021-03-09 18:09:56 +0800

    多语言国际化3

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/exception/ExceptionAccessDeniedOrEntityNotExist.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/exception/ExceptionAccessDeniedOrEntityNotExist.java
@@ -3,21 +3,22 @@ package com.x.base.core.project.exception;
 import java.util.Objects;
 
 import com.x.base.core.project.http.EffectivePerson;
+import com.x.base.core.project.tools.LanguageTools;
 
 public class ExceptionAccessDeniedOrEntityNotExist extends PromptException {
 
 	private static final long serialVersionUID = -7354813827434276962L;
 
 	public ExceptionAccessDeniedOrEntityNotExist(String person) {
-		super("用户:{} 权限不足或者对象不存在.", person);
+		super(LanguageTools.getValue("exception_accessDeniedOrEntityNotExist"), person);
 	}
 
 	public ExceptionAccessDeniedOrEntityNotExist(EffectivePerson effectivePerson) {
-		super("用户:{} 权限不足或者对象不存在.", effectivePerson.getDistinguishedName());
+		super(LanguageTools.getValue("exception_accessDeniedOrEntityNotExist"), effectivePerson.getDistinguishedName());
 	}
 
 	public ExceptionAccessDeniedOrEntityNotExist(EffectivePerson effectivePerson, String message) {
-		super("用户:{} 权限不足或者对象不存在, {}.", effectivePerson.getDistinguishedName(), Objects.toString(message, ""));
+		super(LanguageTools.getValue("exception_accessDeniedOrEntityNotExist_1"), effectivePerson.getDistinguishedName(), Objects.toString(message, ""));
 	}
 
 }
