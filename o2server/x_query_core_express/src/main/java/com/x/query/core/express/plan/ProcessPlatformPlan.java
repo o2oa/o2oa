b8a8838ed995ee7b7c67f38eb27c23d04b80a25b
o2null <zhourui@zoneland.net>
2021-03-27 15:24:01 +0000

    Merge branch 'fix/qry_item减少联合索引长度' into 'develop_java8'
    [数据中心]qry_item减少联合索引长度，设置视图查询对象最大1000个
    
    See merge request o2oa/o2oa!3312
    
    (cherry picked from commit 1b28408f8c988bebe42f05ce2ee818b09daae557)
    
    365ec409 文档data的item表增加联合索引,减少索引长度
    6218998b 设置视图查询对象最大1000个

--- o2server/x_query_core_express/src/main/java/com/x/query/core/express/plan/ProcessPlatformPlan.java
+++ o2server/x_query_core_express/src/main/java/com/x/query/core/express/plan/ProcessPlatformPlan.java
@@ -207,9 +207,8 @@ public class ProcessPlatformPlan extends Plan {
 						CriteriaBuilder cb = em.getCriteriaBuilder();
 						CriteriaQuery<String> cq = cb.createQuery(String.class);
 						Root<Item> root = cq.from(Item.class);
-						Predicate p = f.toPredicate(cb, root, this.runtime, ItemCategory.pp);
-						logger.debug("predicate:{}.", p);
-						p = cb.and(p, cb.isMember(root.get(Item_.bundle), cb.literal(_batch)));
+						Predicate p = root.get(Item_.bundle).in(_batch);
+						p = f.toPredicate(cb, root, this.runtime, p);
 						cq.select(root.get(Item_.bundle)).where(p);
 						List<String> parts = em.createQuery(cq).getResultList();
 						return parts.stream().distinct().collect(Collectors.toList());
