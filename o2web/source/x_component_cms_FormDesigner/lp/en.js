48a86f1351fcd611a87e5c580a06e9709aeef767
蔡祥熠 <caixiangyi@zoneland.net>
2021-04-12 09:40:53 +0000

    Merge branch 'fix/CMS_translation_to_en' into 'wrdp'
    Merge of fix/CMS_translation_to_en 内容管理英文翻译 to wrdp
    
    See merge request o2oa/o2oa!3412
    
    (cherry picked from commit da1263d6507b15e68a05c4d820895cdb16de84ed)
    
    6751c96f 内容管理翻译到英文

---
+++ o2web/source/x_component_cms_FormDesigner/lp/en.js
@@ -0,0 +1,8 @@
+MWF.xApplication.process = MWF.xApplication.process || {};
+MWF.xApplication.process.FormDesigner = MWF.xApplication.process.FormDesigner || {};
+MWF.xDesktop.requireApp("process.FormDesigner", "lp."+MWF.language, null, false);
+MWF.xApplication.cms.FormDesigner.LP = Object.merge( MWF.xApplication.process.FormDesigner.LP, {
+    "validation" : {
+        "publish" : "When publishing"
+    }
+});
