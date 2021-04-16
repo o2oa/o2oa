124e84c83f2e36056db37f2ef38383e6827a62a6
o2sword <171715986@qq.com>
2021-04-16 15:55:39 +0800

    多语言国际化3

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/exception/ExceptionEntityNotExist.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/exception/ExceptionEntityNotExist.java
@@ -7,33 +7,37 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.x.base.core.entity.JpaObject;
 
-public class ExceptionEntityNotExist extends PromptException {
+public class ExceptionEntityNotExist extends LanguagePromptException {
 
 	private static final long serialVersionUID = -7354813827434276962L;
 
+	public static String defaultMessage = "标识为:{} 的 {} 对象不存在.";
+
 	public ExceptionEntityNotExist(String flag, JpaObject jpa) {
-		super(LanguageTools.getValue("exception_entityNotExist"), flag, (null == jpa) ? null : jpa.nameOfEntity());
+		super(defaultMessage, flag, (null == jpa) ? null : jpa.nameOfEntity());
 	}
 
 	public <T extends JpaObject> ExceptionEntityNotExist(String flag, Class<T> cls) {
-		super(LanguageTools.getValue("exception_entityNotExist"), flag, (null == cls) ? null : cls.getSimpleName());
+		super(defaultMessage, flag, (null == cls) ? null : cls.getSimpleName());
 	}
 
 	public <T extends JpaObject> ExceptionEntityNotExist(List<String> ids, Class<T> cls) {
-		super(LanguageTools.getValue("exception_entityNotExist"), (null == ids) ? null : StringUtils.join(ids, ","),
+		super(defaultMessage, (null == ids) ? null : StringUtils.join(ids, ","),
 				(null == cls) ? null : cls.getSimpleName());
 	}
 
 	public <T extends JpaObject> ExceptionEntityNotExist(String flag, String name) {
-		super(LanguageTools.getValue("exception_entityNotExist"), flag, name);
+		super(defaultMessage, flag, name);
 	}
 
 	public <T extends JpaObject> ExceptionEntityNotExist(String flag) {
-		super(LanguageTools.getValue("exception_entityNotExist_1"), flag);
+		super("标识为:{} 的对象不存在.", flag);
+		this.setLanguageKey(this.getClass().getName()+"_1");
 	}
 
 	public <T extends JpaObject> ExceptionEntityNotExist(Class<T> cls) {
-		super(LanguageTools.getValue("exception_entityNotExist_2"), cls.getSimpleName());
+		super("类型为: {} 的对象不存在.", cls.getSimpleName());
+		this.setLanguageKey(this.getClass().getName()+"_2");
 	}
 
 }
