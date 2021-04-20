ffbbd8d9f68f4a074cf5a478d63203ab15d428c9
o2sword <171715986@qq.com>
2021-04-20 10:43:59 +0800

    门户模块国际化语言

--- o2server/x_portal_assemble_designer/src/main/java/com/x/portal/assemble/designer/jaxrs/widget/ExceptionNameDuplicateWithAlias.java
+++ o2server/x_portal_assemble_designer/src/main/java/com/x/portal/assemble/designer/jaxrs/widget/ExceptionNameDuplicateWithAlias.java
@@ -1,12 +1,12 @@
 package com.x.portal.assemble.designer.jaxrs.widget;
 
-import com.x.base.core.project.exception.PromptException;
+import com.x.base.core.project.exception.LanguagePromptException;
 
-class ExceptionNameDuplicateWithAlias extends PromptException {
+class ExceptionNameDuplicateWithAlias extends LanguagePromptException {
 
 	private static final long serialVersionUID = -9089355008820123519L;
 
 	ExceptionNameDuplicateWithAlias(String name) {
-		super("页面名称与别名重复: {} .", name);
+		super("页面名称与别名重复: {}.", name);
 	}
 }
