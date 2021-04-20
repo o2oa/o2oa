c16165e31943a694844c6e2fec1e4ab1cb6adcee
unknown <caixiangyi@zoneland.net>
2021-04-20 10:47:48 +0800

    数据模板

--- o2web/source/x_component_process_FormDesigner/Module/Opinion.js
+++ o2web/source/x_component_process_FormDesigner/Module/Opinion.js
@@ -8,5 +8,19 @@ MWF.xApplication.process.FormDesigner.Module.Opinion = MWF.FCOpinion = new Class
 		"type": "opinion",
 		"path": "../x_component_process_FormDesigner/Module/Opinion/",
 		"propertyPath": "../x_component_process_FormDesigner/Module/Opinion/opinion.html"
+	},
+	_getDroppableNodes: function(){
+		var nodes = [this.form.node].concat(this.form.moduleElementNodeList, this.form.moduleContainerNodeList, this.form.moduleComponentNodeList);
+		this.form.moduleList.each( function(module){
+			//意见不能往数据模板里拖
+			if( module.moduleName === "datatemplate" ){
+				var subDoms = this.form.getModuleNodes(module.node);
+				nodes.erase( module.node );
+				subDoms.each(function (dom) {
+					nodes.erase( dom );
+				})
+			}
+		}.bind(this));
+		return nodes;
 	}
 });
