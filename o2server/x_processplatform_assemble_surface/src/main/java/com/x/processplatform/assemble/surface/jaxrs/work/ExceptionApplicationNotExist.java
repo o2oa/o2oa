e0a869804fea18072ac15f2a23223b9ed679dc1e
o2sword <171715986@qq.com>
2021-04-20 17:51:11 +0800

    流程平台模块国际化语言

--- o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/work/ExceptionApplicationNotExist.java
+++ o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/work/ExceptionApplicationNotExist.java
@@ -1,12 +1,12 @@
 package com.x.processplatform.assemble.surface.jaxrs.work;
 
-import com.x.base.core.project.exception.PromptException;
+import com.x.base.core.project.exception.LanguagePromptException;
 
-class ExceptionApplicationNotExist extends PromptException {
+class ExceptionApplicationNotExist extends LanguagePromptException {
 
 	private static final long serialVersionUID = -9089355008820123519L;
 
 	ExceptionApplicationNotExist(String str) {
-		super("application: {} not existed.", str);
+		super("指定的应用不存在:{}.", str);
 	}
 }
--- o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/work/ExceptionNoneIdentity.java
+++ o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/work/ExceptionNoneIdentity.java
@@ -1,12 +1,12 @@
 package com.x.processplatform.assemble.surface.jaxrs.work;
 
-import com.x.base.core.project.exception.PromptException;
+import com.x.base.core.project.exception.LanguagePromptException;
 
-class ExceptionNoneIdentity extends PromptException {
+class ExceptionNoneIdentity extends LanguagePromptException {
 
 	private static final long serialVersionUID = 1040883405179987063L;
 
 	ExceptionNoneIdentity(String person) {
-		super("can not get identity of person: {}.", person);
+		super("指定用户没有找到身份: {}.", person);
 	}
 }
