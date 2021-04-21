dcaa91c93864f00c577020c88e87c5ccf73c0495
o2sword <171715986@qq.com>
2021-04-21 15:35:25 +0800

    内容管理模块国际化语言

--- o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/appdict/ExceptionAppInfoNotExist.java
+++ o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/appdict/ExceptionAppInfoNotExist.java
@@ -1,13 +1,13 @@
 package com.x.cms.assemble.control.jaxrs.appdict;
 
-import com.x.base.core.project.exception.PromptException;
+import com.x.base.core.project.exception.LanguagePromptException;
 
-class ExceptionAppInfoNotExist extends PromptException {
+class ExceptionAppInfoNotExist extends LanguagePromptException {
 
 	private static final long serialVersionUID = -5954915325716358186L;
 
 	ExceptionAppInfoNotExist(String flag) {
-		super("标识为:" + flag + ", 的应用不存在");
+		super("标识为:{}, 的应用不存在.", flag);
 	}
 
 }
--- o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/fileinfo/ExceptionDocumentNotExists.java
+++ o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/fileinfo/ExceptionDocumentNotExists.java
@@ -1,12 +1,12 @@
 package com.x.cms.assemble.control.jaxrs.fileinfo;
 
-import com.x.base.core.project.exception.PromptException;
+import com.x.base.core.project.exception.LanguagePromptException;
 
-class ExceptionDocumentNotExists extends PromptException {
+class ExceptionDocumentNotExists extends LanguagePromptException {
 
 	private static final long serialVersionUID = 1859164370743532895L;
 
 	ExceptionDocumentNotExists( String id ) {
-		super("文档信息不存在，无法继续进行操作。Id:" + id );
+		super("指定的文档不存在:{}.", id );
 	}
 }
