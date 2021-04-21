b6a8852e262e7a5cb0e996af45596c2875009c09
o2sword <171715986@qq.com>
2021-04-21 15:35:25 +0800

    内容管理模块国际化语言

--- o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/appdictdesign/ExceptionAppInfoAccessDenied.java
+++ o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/appdictdesign/ExceptionAppInfoAccessDenied.java
@@ -1,13 +1,13 @@
 package com.x.cms.assemble.control.jaxrs.appdictdesign;
 
-import com.x.base.core.project.exception.PromptException;
+import com.x.base.core.project.exception.LanguagePromptException;
 
-class ExceptionAppInfoAccessDenied extends PromptException {
+class ExceptionAppInfoAccessDenied extends LanguagePromptException {
 
 	private static final long serialVersionUID = -5515077418025884395L;
 
 	ExceptionAppInfoAccessDenied(String person, String name, String id) {
-		super("person:{} access application name: {} id: {}, denied.", person, name, id);
+		super("用户:{} 没有权限访问应用 name: {} id: {}.", person, name, id);
 	}
 
 }
--- o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/fileinfo/ExceptionFileInfoContentRead.java
+++ o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/fileinfo/ExceptionFileInfoContentRead.java
@@ -1,11 +1,11 @@
 package com.x.cms.assemble.control.jaxrs.fileinfo;
 
-import com.x.base.core.project.exception.PromptException;
+import com.x.base.core.project.exception.LanguagePromptException;
 
-public class ExceptionFileInfoContentRead extends PromptException {
+public class ExceptionFileInfoContentRead extends LanguagePromptException {
 
 	private static final long serialVersionUID = 1859164370743532895L;
-	
+
 	public ExceptionFileInfoContentRead(Throwable e ) {
 		super("系统读取文件输出时发生异常.", e);
 	}
