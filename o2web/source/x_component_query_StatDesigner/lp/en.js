4361d158216b04eb8f98cc0cc6819aa4bc235a07
蔡祥熠 <caixiangyi@zoneland.net>
2021-04-25 14:17:34 +0000

    Merge branch 'feature/Query.translate_en' into 'release'
    Merge of feature/Query.translate_en 数据中心HTML文件翻译成英文 to release
    
    See merge request o2oa/o2oa!3565
    
    (cherry picked from commit dc38de74128ef95c26a08790684d194c27ffd554)
    
    59ca951a 数据中心翻译
    ff357855 数据中心翻译英文

---
+++ o2web/source/x_component_query_StatDesigner/lp/en.js
@@ -0,0 +1,117 @@
+MWF.xApplication.query.StatDesigner.LP = {
+    "title": "Statistics Editor",
+    "newStat": "New Statistics",
+    "property": "Property",
+    "item": "Item",
+    "type": "Type",
+    "value": "Value",
+    "stat": "Statistics",
+    "unnamed": "Untitled column",
+    "unCategory": "Uncategorized",
+
+    "id": "Id",
+    "name": "Name",
+    "alias": "Alias",
+    "description": "Description",
+
+    "application": "Data Application",
+    "copyStat": "Copy Statistics",
+    "newStatName": "New Statistics Name",
+    "copy": "Copy",
+    "ok": "OK",
+    "cancel": "Cancel",
+    "view": "View",
+
+    "action": {
+        "move": "Move",
+        "delete": "Delete",
+        "add": "Add"
+    },
+
+    "notice": {
+        "save_success": "Statistics saved successfully!",
+        "saveAs_success": "The statistic is saved as successfully. You can open the new statistic in the statistic settings!",
+
+        "deleteFilterTitle": "Delete FilterTitle Confirmation",
+        "deleteFilter": "Are you sure you want to delete the filter criteria?",
+
+        "deleteColumnTitle": "Delete column confirmation",
+        "deleteColumn": "Are you sure you want to delete the current column?",
+
+
+        "changeTypeTitle": "Change Data Type Confirmation",
+        "changeTypeDeleteChildren": "Changing the data type will delete all child data. Are you sure you want to execute it?",
+        "changeType": "Changing the data type will change the value of the data. Are you sure you want to execute it?",
+
+        "inputTypeError": "The data type you entered is wrong, please re-enter",
+        "sameKey": "The item name you entered already exists in the object, please re-enter",
+        "emptyKey": "Project name cannot be empty, please re-enter",
+        "numberKey": "The item name cannot be a number, please re-enter",
+
+        "inputName": "Please enter the statistical name",
+        "noModifyName": "Cannot modify the name or alias",
+        "inputView": "Please select a view first",
+        "errorViewColumn": "There is an error in the view column selection, please select again"
+    },
+    "category": "Category",
+    "amount": "Total",
+    "propertyTemplate": {
+        "base": "Basic",
+        "event": "Event",
+        "html": "HTML",
+        "json": "JSON",
+        "action": "Action",
+        "select": "Select",
+        "alias": "Alias",
+
+        "id": "Identification",
+        "name": "Name",
+        "description": "Description",
+        "appearance": "Appearance Type",
+        "style": "Style",
+        "attribute": "Attribute",
+
+        "view":"View",
+        "viewNote":"The view needs to set the classification column",
+        "column":"View Column",
+        "columnNote":"Please select a view for statistics first",
+        "calculateType":"Calculation Type",
+        "sum":"Sum",
+        "average":"Average",
+        "count":"Count",
+        "formatType":"Format",
+        "number":"number",
+        "percent":"Percent",
+        "currency":"Currency",
+        "decimal":"Decimal Places",
+        "defaultValue":"Default Value",
+        "code":"Display script",
+
+        "chart":"Chart",
+        "bar": "Bar Chart",
+        "pie":"Pie Chart",
+        "line":"Line chart",
+        "authority":"Execution Authority",
+        "availableIdentity":"executable person",
+        "availableUnit":"Executive Organization",
+
+        "category":"Category",
+        "isGroup":"Classified Statistics",
+        "yes":"Yes",
+        "no":"No",
+        "categoryTitle":"Category Title",
+        "isAmount":"Total",
+        "sort":"Sort",
+        "none":"None",
+        "desc":"Descending",
+        "asc":"Ascending",
+        "orderColumn":"Order Column",
+        "categoryValue":"Category value",
+        "categoryDisplay":"Category Display",
+        "item": "Calculate according to column view",
+        "specified":"Based on the specified category value",
+        "intersection": "The intersection of column view calculation and specified category value",
+        "sum1": "Collection of column view calculation and specified category value",
+        "groupSpecifiedList":"Specified category value"
+    }
+};
