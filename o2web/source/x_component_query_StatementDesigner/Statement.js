e144b09e3d89eb77f245d4f2c15a8752a8b8596d
蔡祥熠 <caixiangyi@zoneland.net>
2021-04-26 10:05:47 +0000

    Merge branch 'fix/Query.statement_exception' into 'release'
    Merge of fix/Query.statement_exception to release
    
    See merge request o2oa/o2oa!3597
    
    (cherry picked from commit d0b5a79619a824e5f8c5b73f96e17bfd6383d097)
    
    dad0c603 修复查询语句的问题

--- o2web/source/x_component_query_StatementDesigner/Statement.js
+++ o2web/source/x_component_query_StatementDesigner/Statement.js
@@ -302,7 +302,7 @@ MWF.xApplication.query.StatementDesigner.Statement = new Class({
     loadJpqlTypeSelect : function(){
       this.jpqlTypeSelect.empty();
       var optionList = [{text:"SELECT", value:"select"}];
-        if( this.data.entityCategory === "dynamic" || this.data.description.indexOf("update")>-1){
+        if( this.data.entityCategory === "dynamic" || (this.data.description && this.data.description.indexOf("update")>-1)){
           optionList = optionList.concat([
               {text:"UPDATE", value:"update"},
               {text:"DELETE", value:"delete"}
