efd61938b4f10641121e7808c0272df37af6078f
胡起 <hq_1980@hotmail.com>
2021-04-27 15:53:43 +0000

    Merge branch 'feature/lp_global' into 'wrdp'
    增加英文语言包
    
    See merge request o2oa/o2oa!3642
    
    (cherry picked from commit 17a7190bbbe1c3db3f2db744c1d7440b99a0152c)
    
    8a3d4c40 增加英文语言包

---
+++ o2web/source/x_component_portal_Portal/lp/en.js
@@ -0,0 +1,6 @@
+MWF.xApplication.portal = MWF.xApplication.portal || {};
+MWF.xApplication.portal.Portal = MWF.xApplication.portal.Portal || {};
+MWF.xApplication.portal.Portal.LP = {
+	"title": "Portal"
+};
+MWF.xApplication.portal.Portal["lp."+o2.language] = MWF.xApplication.portal.Portal.LP;
