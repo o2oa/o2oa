9c0c0e569aa8222c1768254daa3e501a5b2e66ed
胡起 <hq_1980@hotmail.com>
2021-04-28 13:55:16 +0000

    Merge branch 'feature/language_package' into 'wrdp'
    增加英文语言包
    
    See merge request o2oa/o2oa!3680
    
    (cherry picked from commit 4fd4bcfe191f7aeb4ad7cfb342d51c3a7b59a169)
    
    73ef14f9 增加英文语言包

---
+++ o2web/source/x_component_service_AgentDesigner/lp/en.js
@@ -0,0 +1,51 @@
+MWF.xApplication.service.AgentDesigner.LP = {
+    "title": "AgentEditor",
+    "newAgent": "Create Agent",
+    "agentLibrary": "Agent Library",
+    "property": "Property",
+    "include": "Include",
+
+    "id": "ID",
+    "name": "Name",
+    "alias": "Alias",
+    "description": "Description",
+
+    "validated": "Is the code format correct",
+    "isEnable": "Whether to enable",
+    "cron": "Timed task cron expression",
+    "lastStartTime": "Last Start Time",
+    "lastEndTime": "Last EndTime",
+    "appointmentTime": "Estimated next execution time",
+
+    "true" : "Yes",
+    "false" : "No",
+    "enable" : "Click to enable",
+    "disable" : "Click to disable",
+
+    "notice": {
+        "save_success": "Agent saved successfully!",
+
+        "deleteDataTitle": "Delete Data Confirmation",
+        "deleteData": "Are you sure to delete the current data and its sub-data?",
+
+        "changeTypeTitle": "Change Data Type Confirmation",
+        "changeTypeDeleteChildren": "Changing the data type will delete all child data. Are you sure you want to execute it?",
+        "changeType": "Changing the data type will change the value of the data. Are you sure you want to execute it?",
+
+        "inputTypeError": "The data type you entered is wrong, please re-enter",
+        "sameKey": "The item name you entered already exists in the object, please re-enter",
+        "emptyKey": "Project name cannot be empty, please re-enter",
+        "numberKey": "The item name cannot be a number, please re-enter",
+
+        "inputName": "Please enter the agent name",
+        "inputCron": "Timed task corn expression cannot be empty"
+    },
+    "comment" : {
+        "entityManager": "Entity Manager",
+        "applications": "Access to services in the system",
+        "organization": "Organization Visit",
+        "org": "Organize quick access method",
+        "service": "webSerivces client"
+    }
+
+};
