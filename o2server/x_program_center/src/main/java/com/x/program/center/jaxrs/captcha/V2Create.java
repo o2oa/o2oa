bf179a826d5026f1d915fea0cec058014f524bd2
o2null <zhourui@zoneland.net>
2021-05-08 06:34:42 +0000

    Merge branch 'feature/easycaptcha' into 'wrdp'
    使用easy-captcha作为验证码验证
    
    See merge request o2oa/o2oa!3754
    
    (cherry picked from commit 22cf7a0b154a1b0211c9eb81eaa40dca424f3a95)
    
    f859eab1 使用easy-captcha作为验证码验证

--- o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/captcha/V2Create.java
+++ o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/captcha/V2Create.java
@@ -1,26 +1,24 @@
 package com.x.program.center.jaxrs.captcha;
 
+import java.awt.Font;
 import java.io.ByteArrayOutputStream;
 
-import javax.imageio.ImageIO;
+import org.apache.commons.lang3.StringUtils;
 
-import org.apache.commons.codec.binary.Base64;
-
+import com.wf.captcha.SpecCaptcha;
 import com.x.base.core.container.EntityManagerContainer;
 import com.x.base.core.container.factory.EntityManagerContainerFactory;
 import com.x.base.core.entity.annotation.CheckPersistType;
 import com.x.base.core.project.annotation.FieldDescribe;
+import com.x.base.core.project.config.Config;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
 import com.x.program.center.core.entity.Captcha;
 
-import nl.captcha.text.producer.NumbersAnswerProducer;
+class V2Create extends BaseAction {
 
-@Deprecated
-class ActionCreate extends BaseAction {
-
-	private static Logger logger = LoggerFactory.getLogger(ActionCreate.class);
+	private static Logger logger = LoggerFactory.getLogger(V2Create.class);
 
 	ActionResult<Wo> execute(Integer width, Integer height) throws Exception {
 		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
@@ -31,18 +29,23 @@ class ActionCreate extends BaseAction {
 			if (width <= 30 && width > 800) {
 				width = 30;
 			}
-			nl.captcha.Captcha o = new nl.captcha.Captcha.Builder(width, height).addText(new NumbersAnswerProducer(4))
-					.addNoise().addNoise().build();
+
+			SpecCaptcha specCaptcha = new SpecCaptcha(width, height, 4);
+			specCaptcha.setCharType(com.wf.captcha.base.Captcha.TYPE_ONLY_NUMBER);
+
+			if (StringUtils.isNotBlank(Config.person().getCaptchaFont())) {
+				specCaptcha.setFont(new Font(Config.person().getCaptchaFont(), Font.PLAIN, 32));
+			}
+
 			emc.beginTransaction(Captcha.class);
 			Captcha captcha = new Captcha();
-			captcha.setAnswer(o.getAnswer());
+			captcha.setAnswer(specCaptcha.text());
 			emc.persist(captcha, CheckPersistType.all);
 			emc.commit();
 			Wo wo = new Wo();
 			wo.setId(captcha.getId());
 			try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-				ImageIO.write(o.getImage(), "png", baos);
-				wo.setImage(Base64.encodeBase64String(baos.toByteArray()));
+				wo.setImage(specCaptcha.toBase64(""));
 			}
 			result.setData(wo);
 			return result;
