21a2cfc98bf116f46112231e741277b073faefe8
o2sword <171715986@qq.com>
2021-05-26 16:54:55 +0800

    数据中心导入模块设计开发

--- o2server/x_query_assemble_designer/src/main/java/com/x/query/assemble/designer/jaxrs/TableJaxrsFilter.java
+++ o2server/x_query_assemble_designer/src/main/java/com/x/query/assemble/designer/jaxrs/TableJaxrsFilter.java
@@ -3,8 +3,9 @@ package com.x.query.assemble.designer.jaxrs;
 import javax.servlet.annotation.WebFilter;
 
 import com.x.base.core.project.jaxrs.AnonymousCipherManagerUserJaxrsFilter;
+import com.x.base.core.project.jaxrs.CipherManagerUserJaxrsFilter;
 
 @WebFilter(urlPatterns = "/jaxrs/table/*", asyncSupported = true)
-public class TableJaxrsFilter extends AnonymousCipherManagerUserJaxrsFilter {
+public class TableJaxrsFilter extends CipherManagerUserJaxrsFilter {
 
 }
