1cd4658042e9e874f3fa2c1780e2b7c89e5339a5
unknown <caixiangyi@zoneland.net>
2021-05-28 17:35:07 +0800

    数据表格api

--- o2web/source/x_component_process_Xform/Datatemplate.js
+++ o2web/source/x_component_process_Xform/Datatemplate.js
@@ -57,7 +57,7 @@ MWF.xDesktop.requireApp("process.Xform", "$Module", null, false);
  * var datatemplate = this.target; //在组件事件脚本中获取
  * @extends MWF.xApplication.process.Xform.$Module
  * @o2category FormComponents
- * @since v6.1.3
+ * @since v6.2
  * @o2range {Process|CMS|Protal}
  * @hideconstructor
  */
