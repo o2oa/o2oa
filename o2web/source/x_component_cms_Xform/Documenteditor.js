e5a0a1fae2990ff65e8b73a70babce6730fc77a1
蔡祥熠 <caixiangyi@zoneland.net>
2021-06-07 04:30:15 +0000

    Merge branch 'feature/CMS_documentEditor' into 'wrdp'
    Merge of feature/CMS_documentEditor 内容管理中加入版式文件 to wrdp
    
    See merge request o2oa/o2oa!3923
    
    (cherry picked from commit 28e95ff0dc10611852f3dc6986b759feea01c9ed)
    
    4e0726af 内容管理中加入板式文件
    9ccfe8a2 内容管理中加入板式文件

---
+++ o2web/source/x_component_cms_Xform/Documenteditor.js
@@ -0,0 +1,4 @@
+MWF.xDesktop.requireApp("process.Xform", "Documenteditor", null, false);
+MWF.xApplication.cms.Xform.Documenteditor = MWF.CMSDocumenteditor =  new Class({
+    Extends: MWF.APPDocumenteditor
+});
