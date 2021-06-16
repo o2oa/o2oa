ff2b8a2297bc5015c4b76b502b7b509f4d41baa0
fancy <fancylou@outlook.com>
2021-06-16 14:42:47 +0800

    移动端app在线打包功能

--- o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/apppack/ExceptionAppNameMax6.java
+++ o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/apppack/ExceptionAppNameMax6.java
@@ -6,9 +6,9 @@ import com.x.base.core.project.exception.LanguagePromptException;
  * Created by fancyLou on 6/15/21.
  * Copyright © 2021 O2. All rights reserved.
  */
-public class ExceptionFileNotPng extends LanguagePromptException {
+public class ExceptionAppNameMax6 extends LanguagePromptException {
 
     private static final long serialVersionUID = -8045803301464102031L;
 
-    ExceptionFileNotPng(){ super("Logo图片只支持png格式！"); }
+    ExceptionAppNameMax6(){ super("App名称长度最多6个字！"); }
 }
