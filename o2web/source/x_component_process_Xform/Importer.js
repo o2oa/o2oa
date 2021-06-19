e2e65df349542aaef1737bc10af7afd0d0402ec8
unknown <caixiangyi@zoneland.net>
2021-06-19 10:38:26 +0800

    表单上加导入

---
+++ o2web/source/x_component_process_Xform/Importer.js
@@ -0,0 +1,75 @@
+MWF.xDesktop.requireApp("process.Xform", "$Module", null, false);
+MWF.xDesktop.requireApp("process.Xform", "Button", null, false);
+/** @class Importer 导入数据组件。
+ * @example
+ * //可以在脚本中获取该组件
+ * //方法1：
+ * var importer = this.form.get("fieldId"); //获取组件
+ * //方法2
+ * var importer = this.target; //在组件本身的脚本中获取
+ * @extends MWF.xApplication.process.Xform.Importer
+ * @o2category FormComponents
+ * @o2range {Process|CMS}
+ * @hideconstructor
+ */
+MWF.xApplication.process.Xform.Importer = MWF.APPImporter =  new Class({
+	Implements: [Events],
+    Extends: MWF.xApplication.process.Xform.Button,
+    _loadUserInterface: function(){
+        var button = this.node.getElement("button");
+        if (!button) button = new Element("button");
+        button.inject(this.node, "after");
+        this.node.destroy();
+        this.node = button;
+        this.node.set({
+            "id": this.json.id,
+            "text": this.json.name || this.json.id,
+            "styles": this.form.css.buttonStyles,
+            "MWFType": this.json.type
+        });
+        this.node.addEvent("click", function(){
+            this.upload();
+        }.bind(this));
+        if( this.json.allowDownloadTempalte && this.json.downloadTempalteFieldId ){
+            this.setDownloadEvent();
+        }
+    },
+    upload: function () {
+	    debugger;
+	    this.json.queryImportModel;
+        MWF.xDesktop.requireApp("query.Query", "Importer", function () {
+            var importer = new MWF.xApplication.query.Query.Importer(this.form.app.content, {
+                "application": this.json.queryImportModel.application || this.json.queryImportModel.appName,
+                "name": this.json.queryImportModel.id || this.json.queryImportModel.alias || this.json.queryImportModel.name
+            }, {}, this.form.app, this.form.Macro);
+            importer.load();
+        }.bind(this));
+    },
+    setDownloadEvent: function () {
+        this.bindEvent = function () {
+            var module = this._getModuleByPath(this.json.downloadTempalteFieldId);
+
+            if(module)module.node.addEvent("click", function () {
+                this.downloadTemplate();
+            }.bind(this))
+
+            this.fireEvent("afterLoad");
+
+            //加载完成以后，删除事件
+            this.form.removeEvent("afterModulesLoad", this.bindEvent );
+        }.bind(this);
+
+        //去要表单的所有组件加载完成以后再去获取外部组件
+        this.form.addEvent("afterModulesLoad", this.bindEvent );
+    },
+    downloadTemplate: function(){
+        MWF.xDesktop.requireApp("query.Query", "Importer", function () {
+            var importer = new MWF.xApplication.query.Query.Importer(this.form.app.content, {
+                "application": this.json.queryImportModel.application || this.json.queryImportModel.appName,
+                "name": this.json.queryImportModel.id || this.json.queryImportModel.alias || this.json.queryImportModel.name
+            }, {}, this.form.app, this.form.Macro);
+            importer.downloadTemplate(fileName);
+        }.bind(this));
+    }
+	
+}); 
