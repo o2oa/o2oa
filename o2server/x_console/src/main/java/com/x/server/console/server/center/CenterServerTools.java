cfc9cec5dda93211fa74e861ac11db611a1537b1
o2null <zhourui@zoneland.net>
2021-07-01 07:17:51 +0000

    Merge branch 'cherry-pick-de58090e' into 'develop_java8'
    Merge branch 'feature/requestLog' into 'wrdp'
    
    See merge request o2oa/o2oa!4330
    
    (cherry picked from commit 5640946248b9f686a1977ecd48d8480b147dcdf4)
    
    05ebf167 Merge branch 'feature/requestLog' into 'wrdp'

--- o2server/x_console/src/main/java/com/x/server/console/server/center/CenterServerTools.java
+++ o2server/x_console/src/main/java/com/x/server/console/server/center/CenterServerTools.java
@@ -125,6 +125,7 @@ public class CenterServerTools extends JettySeverTools {
 		AsyncRequestLogWriter asyncRequestLogWriter = new AsyncRequestLogWriter();
 		asyncRequestLogWriter.setFilenameDateFormat("yyyy_MM_dd");
 		asyncRequestLogWriter.setAppend(true);
+		asyncRequestLogWriter.setRetainDays(centerServer.getRequestLogRetainDays());
 		asyncRequestLogWriter.setFilename(
 				Config.dir_logs().toString() + File.separator + "yyyy_MM_dd." + Config.node() + ".center.request.log");
 		String format = "%{client}a - %u %{yyyy-MM-dd HH:mm:ss.SSS ZZZ|" + DateFormatUtils.format(new Date(), "z")
