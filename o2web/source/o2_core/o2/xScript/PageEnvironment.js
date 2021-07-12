0031be787ebfcc29eee121b3c80273a2bec0e954
unknown <caixiangyi@zoneland.net>
2021-07-12 14:37:46 +0800

    窗口模式，内容管理打开文档增加句柄

--- o2web/source/o2_core/o2/xScript/PageEnvironment.js
+++ o2web/source/o2_core/o2/xScript/PageEnvironment.js
@@ -1769,7 +1769,7 @@ MWF.xScript.PageEnvironment = function (ev) {
             var op = options || {};
             op.documentId = id;
             op.docTitle = title || "";
-            layout.desktop.openApplication(this.event, "cms.Document", op);
+            return layout.desktop.openApplication(this.event, "cms.Document", op);
         },
         /**打开一个门户页面
          * @method openPortal
@@ -1839,7 +1839,7 @@ MWF.xScript.PageEnvironment = function (ev) {
          * @see module:form.openApplication
          */
         "openApplication": function (name, options) {
-            layout.desktop.openApplication(null, name, options);
+            return layout.desktop.openApplication(null, name, options);
         },
         /**创建一个内容管理文档
          * @method createDocument
