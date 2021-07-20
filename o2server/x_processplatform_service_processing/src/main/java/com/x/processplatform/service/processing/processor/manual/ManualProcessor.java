bd4902924560c768e0819cd7e247e91d0118c08a
o2null <zhourui@o2oa.net>
2021-07-20 14:26:08 +0800

    soleDirect不再绑定sole

--- o2server/x_processplatform_service_processing/src/main/java/com/x/processplatform/service/processing/processor/manual/ManualProcessor.java
+++ o2server/x_processplatform_service_processing/src/main/java/com/x/processplatform/service/processing/processor/manual/ManualProcessor.java
@@ -450,9 +450,11 @@ public class ManualProcessor extends AbstractManualProcessor {
 		// 取得本环节已经处理的已办
 		List<TaskCompleted> taskCompleteds = this.listJoinInquireTaskCompleted(aeiObjects, identities);
 		// 存在优先路由,如果有人选择了优先路由那么直接流转.需要判断是否启用了soleDirect
-		Route soleRoute = aeiObjects.getRoutes().stream()
-				.filter(r -> BooleanUtils.isTrue(r.getSole()) && BooleanUtils.isTrue(r.getSoleDirect())).findFirst()
-				.orElse(null);
+//		Route soleRoute = aeiObjects.getRoutes().stream()
+//				.filter(r -> BooleanUtils.isTrue(r.getSole()) && BooleanUtils.isTrue(r.getSoleDirect())).findFirst()
+//				.orElse(null);
+		Route soleRoute = aeiObjects.getRoutes().stream().filter(r -> BooleanUtils.isTrue(r.getSoleDirect()))
+				.findFirst().orElse(null);
 		if (null != soleRoute) {
 			TaskCompleted soleTaskCompleted = taskCompleteds.stream()
 					.filter(t -> BooleanUtils.isTrue(t.getJoinInquire())
