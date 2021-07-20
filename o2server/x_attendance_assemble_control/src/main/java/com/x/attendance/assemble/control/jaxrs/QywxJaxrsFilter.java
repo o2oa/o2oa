d2239c73fd000778d2f43ccd23f60e0e900d9960
楼国栋 <fancylou@outlook.com>
2021-07-20 05:33:45 +0000

    Merge branch 'fix/qywx_attendance_bug' into 'wrdp'
    企业微信支持考勤数据导入后查询的一些bug修复
    
    See merge request o2oa/o2oa!4594
    
    (cherry picked from commit 8347c01ea87a5d466786bb86b8eeb915025b7323)
    
    c7f66439 企业微信支持考勤数据导入后查询的一些bug修复

--- o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/jaxrs/QywxJaxrsFilter.java
+++ o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/jaxrs/QywxJaxrsFilter.java
@@ -1,10 +1,10 @@
 package com.x.attendance.assemble.control.jaxrs;
 
-import com.x.base.core.project.jaxrs.CipherManagerJaxrsFilter;
+import com.x.base.core.project.jaxrs.ManagerUserJaxrsFilter;
 
 import javax.servlet.annotation.WebFilter;
 
 
 @WebFilter(urlPatterns = "/jaxrs/qywx/*", asyncSupported = true)
-public class QywxJaxrsFilter extends CipherManagerJaxrsFilter {
+public class QywxJaxrsFilter extends ManagerUserJaxrsFilter {
 }
