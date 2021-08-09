f1b9d2305e4c21b2be694627e705166e124459fa
zhourui <zhourui@o2oa.net>
2021-08-09 18:02:54 +0800

    Charset在java8中不支持

---
+++ o2server/x_console/src/main/java/com/x/server/console/server/Ex.java
@@ -0,0 +1,12 @@
+package com.x.server.console.server;
+
+import java.nio.charset.StandardCharsets;
+
+public class Ex {
+
+	public static void main(String[] args) {
+		System.out.println(StandardCharsets.UTF_8.toString());
+		System.out.println(StandardCharsets.UTF_8.displayName());
+	}
+
+}
