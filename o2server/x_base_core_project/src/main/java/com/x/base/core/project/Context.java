b73e00e3a87f0c9234be6df4be28eaf821468dcc
o2null <zhourui@o2oa.net>
2021-08-09 17:41:30 +0800

    清理调试代码

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/Context.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/Context.java
@@ -132,10 +132,10 @@ public class Context extends AbstractContext {
 
 	private AbstractQueue<WrapClearCacheRequest> clearCacheRequestQueue;
 
-	@Override
-	public AbstractQueue<WrapClearCacheRequest> clearCacheRequestQueue() {
-		return this.clearCacheRequestQueue;
-	}
+//	@Override
+//	public AbstractQueue<WrapClearCacheRequest> clearCacheRequestQueue() {
+//		return this.clearCacheRequestQueue;
+//	}
 
 //	public ThreadFactory threadFactory() {
 //		return this.threadFactory;
