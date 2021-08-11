0021e809ef02913fe32d23947220e12a1b76f02e
unknown <caixiangyi@zoneland.net>
2021-08-11 16:58:37 +0800

    ckeditor升级到4161

---
+++ o2web/source/o2_lib/htmleditor/ckeditor4161/plugins/o2image/lang/zh.js
@@ -0,0 +1,32 @@
+/*
+Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+*/
+CKEDITOR.plugins.setLang( 'o2image', 'zh', {
+	alt: '替代文字',
+	border: '框線',
+	btnUpload: '傳送到伺服器',
+	button2Img: '請問您確定要將「圖片按鈕」轉換成「圖片」嗎？',
+	hSpace: 'HSpace',
+	img2Button: '請問您確定要將「圖片」轉換成「圖片按鈕」嗎？',
+	infoTab: '影像資訊',
+	linkTab: '連結',
+	lockRatio: '固定比例',
+	menu: '影像屬性',
+	resetSize: '重設大小',
+	title: '影像屬性',
+	titleButton: '影像按鈕屬性',
+	upload: '上傳',
+	urlMissing: '遺失圖片來源之 URL ',
+	vSpace: 'VSpace',
+	validateBorder: '框線必須是整數。',
+	validateHSpace: 'HSpace 必須是整數。',
+	validateVSpace: 'VSpace 必須是整數。',
+
+	selectLocalImage: '選擇本地圖片',
+	selectLocalImageNote: "請確保文件為圖像文件",
+	selectCloudImage: '選擇雲文件圖片',
+	selectCurDocImage: '選擇本文檔圖片',
+	allPreview: '允許瀏覽大圖',
+	saveAsBase64: '存為Base64編碼'
+} );
