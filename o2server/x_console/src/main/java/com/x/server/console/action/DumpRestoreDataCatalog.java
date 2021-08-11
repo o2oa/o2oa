66085aa5422e394faf1e5d6c224c86c53d1d2592
zhourui <zhourui@o2oa.net>
2021-08-11 13:44:29 +0800

    修复dump的配置文件由于多线程导致写入错误

--- o2server/x_console/src/main/java/com/x/server/console/action/DumpRestoreDataCatalog.java
+++ o2server/x_console/src/main/java/com/x/server/console/action/DumpRestoreDataCatalog.java
@@ -1,8 +1,8 @@
 package com.x.server.console.action;
 
-import java.util.LinkedHashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class DumpRestoreDataCatalog extends LinkedHashMap<String, Integer> {
+public class DumpRestoreDataCatalog extends ConcurrentHashMap<String, Integer> {
 
 	private static final long serialVersionUID = 5759921581192045332L;
 
