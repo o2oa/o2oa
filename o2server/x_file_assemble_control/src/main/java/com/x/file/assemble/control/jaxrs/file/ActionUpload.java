b9fdc33da2a6f56eafdd41b35d4928f093a35e6b
o2wwx <95523761@qq.com>
2021-08-12 17:24:52 +0800

    '增加保留原图'

--- o2server/x_file_assemble_control/src/main/java/com/x/file/assemble/control/jaxrs/file/ActionUpload.java
+++ o2server/x_file_assemble_control/src/main/java/com/x/file/assemble/control/jaxrs/file/ActionUpload.java
@@ -18,8 +18,10 @@ import org.imgscalr.Scalr.Mode;
 import com.x.base.core.container.EntityManagerContainer;
 import com.x.base.core.container.factory.EntityManagerContainerFactory;
 import com.x.base.core.entity.annotation.CheckPersistType;
+import com.x.base.core.project.annotation.FieldDescribe;
 import com.x.base.core.project.cache.CacheManager;
 import com.x.base.core.project.config.StorageMapping;
+import com.x.base.core.project.gson.GsonPropertyObject;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.jaxrs.WoId;
@@ -32,57 +34,108 @@ class ActionUpload extends BaseAction {
 
 	ActionResult<Wo> execute(EffectivePerson effectivePerson, String referenceType, String reference, Integer scale,
 			byte[] bytes, FormDataContentDisposition disposition) throws Exception {
+		String fileName = "";
+		StorageMapping mapping = ThisApplication.context().storageMappings().random(File.class);
+		File file = null;
+		ReferenceType type = null;
+		Wo wo = new Wo();
+		ActionResult<Wo> result = new ActionResult<>();
+
 		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create();
 				ByteArrayInputStream in = new ByteArrayInputStream(bytes)) {
-			ActionResult<Wo> result = new ActionResult<>();
-			ReferenceType type = EnumUtils.getEnum(ReferenceType.class, referenceType);
-			StorageMapping mapping = ThisApplication.context().storageMappings().random(File.class);
+			type = EnumUtils.getEnum(ReferenceType.class, referenceType);
 			if (null == mapping) {
 				throw new ExceptionAllocateStorageMaaping();
 			}
 			/** 由于这里需要根据craeteTime创建path,先进行赋值,再进行校验,最后保存 */
 			/** 禁止不带扩展名的文件上传 */
 			/** 文件名编码转换 */
-			String fileName = new String(disposition.getFileName().getBytes(DefaultCharset.charset_iso_8859_1),
+			fileName = new String(disposition.getFileName().getBytes(DefaultCharset.charset_iso_8859_1),
 					DefaultCharset.charset);
 			fileName = FilenameUtils.getName(fileName);
 
 			if (StringUtils.isEmpty(FilenameUtils.getExtension(fileName))) {
 				throw new ExceptionEmptyExtension(fileName);
 			}
-			File file = new File(mapping.getName(), fileName, effectivePerson.getDistinguishedName(), type, reference);
+
+			/* 先保存原图 */
+			file = new File(mapping.getName(), fileName, effectivePerson.getDistinguishedName(), type, reference);
 			emc.check(file, CheckPersistType.all);
+			file.saveContent(mapping, in, fileName);
+			emc.beginTransaction(File.class);
+			emc.persist(file);
+			emc.commit();
+			wo.setId(file.getId());
+		}
+		
+         /*保存压缩图*/
+		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create();
+				ByteArrayInputStream in = new ByteArrayInputStream(bytes)) {
 			if ((scale > 0) && ArrayUtils.contains(IMAGE_EXTENSIONS, file.getExtension())) {
+				String fileNameThumbnail = fileName.substring(0, fileName.lastIndexOf(".")) + "_t" + "."+ file.getExtension();
+				File fileThumbnail = new File(mapping.getName(), fileNameThumbnail,effectivePerson.getDistinguishedName(), type, reference);
 				/** 如果是需要压缩的附件 */
 				try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
 					BufferedImage image = ImageIO.read(in);
 					if (image.getWidth() > scale) {
 						/** 图像的实际大小比scale大的要进行压缩 */
 						BufferedImage scalrImage = Scalr.resize(image, Method.QUALITY, Mode.FIT_TO_WIDTH, scale);
-						ImageIO.write(scalrImage, file.getExtension(), baos);
+						ImageIO.write(scalrImage, fileThumbnail.getExtension(), baos);
 					} else {
 						/** 图像的实际大小比scale小,保存原图不进行压缩 */
-						ImageIO.write(image, file.getExtension(), baos);
+						ImageIO.write(image, fileThumbnail.getExtension(), baos);
 					}
 					try (ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray())) {
-						file.saveContent(mapping, bais, fileName);
+						fileThumbnail.saveContent(mapping, bais, fileNameThumbnail);
 					}
+
+					emc.beginTransaction(File.class);
+					emc.persist(fileThumbnail);
+					emc.commit();
+					wo.setOrigId(fileThumbnail.getId());
 				}
+
 			} else {
-				file.saveContent(mapping, in, fileName);
+				//不进行压缩,保存原图id
+				wo.setOrigId(file.getId());
 			}
-			emc.beginTransaction(File.class);
-			emc.persist(file);
-			emc.commit();
-			CacheManager.notify(File.class);
-			Wo wo = new Wo();
-			wo.setId(file.getId());
-			result.setData(wo);
-			return result;
 		}
+
+		CacheManager.notify(File.class);
+		result.setData(wo);
+		return result;
 	}
 
-	public static class Wo extends WoId {
+	public static class Wo extends GsonPropertyObject {
 
+		public Wo() {
+
+		}
+
+		public Wo(String id) throws Exception {
+			this.id = id;
+		}
+
+		@FieldDescribe("缩略图id")
+		private String id;
+
+		@FieldDescribe("原图id")
+		private String origId;
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getOrigId() {
+			return origId;
+		}
+
+		public void setOrigId(String origId) {
+			this.origId = origId;
+		}
 	}
 }
