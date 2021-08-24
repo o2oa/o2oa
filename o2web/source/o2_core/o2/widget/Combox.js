4fa46327def1b84e6cbd2295d8ca22d56b64d995
胡起 <hq_1980@hotmail.com>
2021-08-24 09:18:41 +0000

    Merge branch 'fix/combox' into 'wrdp'
    Fix/combox
    
    See merge request o2oa/o2oa!5023
    
    (cherry picked from commit 42c9bcb331c0f9b65ff5505906f85dc02b86844c)
    
    0af39779 Update Combox.js
    6f82eeb3 Update Combox.js

--- o2web/source/o2_core/o2/widget/Combox.js
+++ o2web/source/o2_core/o2/widget/Combox.js
@@ -564,11 +564,12 @@ o2.widget.Combox.Input = new Class({
                 }
             }.bind(this),
             "blur": function(e){
-                //if (!this.noBlur){
-                if ((this.combox.editItem == this.bind) || (!this.combox.editItem)){
-                    this.bind.commitInput();
+                if (this.combox){
+                    if ((this.combox.editItem == this.bind) || (!this.combox.editItem)){
+                        this.bind.commitInput();
+                    }
+                    this.hideOptionList();
                 }
-                this.hideOptionList();
                 e.stopPropagation();
                 //}
             }.bind(this)
