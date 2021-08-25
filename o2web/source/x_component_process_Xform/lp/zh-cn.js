dce48198f6a8762fa4b6bf4f8e0fde67da5d5972
蔡祥熠 <caixiangyi@zoneland.net>
2021-08-25 16:18:55 +0000

    Merge branch 'feature/Process.sendRead' into 'wrdp'
    Merge of feature/Process.sendRead 增加开始菜单的群组判断,API中增加发送待阅和参阅 to wrdp
    
    See merge request o2oa/o2oa!5065
    
    (cherry picked from commit ec9f7ef308d15e265f3899e39238c80f108dff6b)
    
    a1d0fc64 发送待阅
    07c584a0 增加开始菜单的群组判断
    4d3b6515 api中增加发送待阅和参阅
    31d4cd92 api中增加发送待阅和参阅

--- o2web/source/x_component_process_Xform/lp/zh-cn.js
+++ o2web/source/x_component_process_Xform/lp/zh-cn.js
@@ -347,7 +347,23 @@ MWF.xApplication.process.Xform.LP = {
         "selectUnit": "选择组织或人员",
         "excludeBranches": "排除已有分支",
         "yes": "是",
-        "no": "否"
-    }
+        "no": "否",
+
+        "readIdentity": "待阅人",
+        "sendReadNotify": "发送待阅通知",
+
+        "reviewIdentity": "阅读人"
+    },
+
+    "sendRead": "发送待阅",
+    "sendReadOk": "已经发送待阅",
+    "noPermissionOrWorkNotExisted": "您没有权限或者工作不存在",
+    "inputSendReadPeople": "请选择待阅人",
+    "sendReadPeopleCanNotEmpty": "待阅人不能为空",
+
+    "sendReview": "添加阅读人",
+    "sendReviewOk": "已添加阅读人",
+    "inputSendReviewPeople": "请选择阅读人",
+    "sendReviewPeopleCanNotEmpty": "阅读人不能为空"
 };
 MWF.xApplication.process.Xform["lp."+o2.language] = MWF.xApplication.process.Xform.LP;
