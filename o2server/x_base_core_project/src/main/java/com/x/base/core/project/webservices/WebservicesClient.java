ce8bb38145e33cf04f84a7873a7add525a5e5cac
o2wwx <95523761@qq.com>
2021-09-02 11:44:21 +0800

    'WebserviceClient增加raw方法'

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/webservices/WebservicesClient.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/webservices/WebservicesClient.java
@@ -1,7 +1,23 @@
 package com.x.base.core.project.webservices;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
+
+import javax.ws.rs.core.MediaType;
+import com.x.base.core.project.bean.NameValuePair;
+import com.x.base.core.project.connection.ConnectionAction;
+import com.x.base.core.project.connection.HttpConnection;
 
 public class WebservicesClient {
 	public Object[] jaxws(String wsdl, String method, Object... objects) throws Exception {
@@ -12,4 +28,11 @@ public class WebservicesClient {
 		}
 		return result;
 	}
+	
+	public static String raw(String wsdlURL, String soapXML) throws Exception {
+	      List<NameValuePair> heads = new ArrayList<>();
+	      heads.add(new NameValuePair(ConnectionAction.CONTENT_TYPE,MediaType.TEXT_XML));
+		  return HttpConnection.postAsString( wsdlURL , heads, soapXML);
+	}
+	
 }
