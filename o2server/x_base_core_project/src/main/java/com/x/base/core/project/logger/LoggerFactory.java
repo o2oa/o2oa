f2456e588e7af0384c76cc66a21847d773b92f21
o2null <zhourui@o2oa.net>
2021-09-03 15:23:31 +0800

    更新 logger

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/logger/LoggerFactory.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/logger/LoggerFactory.java
@@ -10,56 +10,39 @@ public class LoggerFactory {
 	}
 
 	public static Logger getLogger(Class<?> clz) {
-		Logger logger = new Logger(clz.getName());
-		return logger;
-	}
-
-	public static void setLevelTrace() {
-		Logger.level = Logger.TRACE_INT;
-	}
-
-	public static void setLevelDebug() {
-		Logger.level = Logger.DEBUG_INT;
-	}
-
-	public static void setLevelInfo() {
-		Logger.level = Logger.INFO_INT;
-	}
-
-	public static void setLevelWarn() {
-		Logger.level = Logger.WARN_INT;
+		return new Logger(clz.getName());
 	}
 
 	public static void setLevel(String str) {
-		if (StringUtils.equalsIgnoreCase(Logger.ERROR, str)) {
-			Logger.level = Logger.ERROR_INT;
-		}
-		if (StringUtils.equalsIgnoreCase(Logger.WARN, str)) {
-			Logger.level = Logger.WARN_INT;
-		}
-		if (StringUtils.equalsIgnoreCase(Logger.INFO, str)) {
-			Logger.level = Logger.INFO_INT;
-		}
-		if (StringUtils.equalsIgnoreCase(Logger.DEBUG, str)) {
-			Logger.level = Logger.DEBUG_INT;
-		}
-		if (StringUtils.equalsIgnoreCase(Logger.TRACE, str)) {
-			Logger.level = Logger.TRACE_INT;
-		}
+//		if (StringUtils.equalsIgnoreCase(Logger.ERROR, str)) {
+//			Logger.level = Logger.ERROR_INT;
+//		}
+//		if (StringUtils.equalsIgnoreCase(Logger.WARN, str)) {
+//			Logger.level = Logger.WARN_INT;
+//		}
+//		if (StringUtils.equalsIgnoreCase(Logger.INFO, str)) {
+//			Logger.level = Logger.INFO_INT;
+//		}
+//		if (StringUtils.equalsIgnoreCase(Logger.DEBUG, str)) {
+//			Logger.level = Logger.DEBUG_INT;
+//		}
+//		if (StringUtils.equalsIgnoreCase(Logger.TRACE, str)) {
+//			Logger.level = Logger.TRACE_INT;
+//		}
 	}
-
-	public static String getLevel() {
-		switch (Logger.level) {
-		case Logger.TRACE_INT:
-			return Logger.TRACE;
-		case Logger.DEBUG_INT:
-			return Logger.DEBUG;
-		case Logger.WARN_INT:
-			return Logger.WARN;
-		default:
-			return Logger.INFO;
-		}
-	}
+//
+//	public static String getLevel() {
+//		switch (Logger.level) {
+//		case Logger.TRACE_INT:
+//			return Logger.TRACE;
+//		case Logger.DEBUG_INT:
+//			return Logger.DEBUG;
+//		case Logger.WARN_INT:
+//			return Logger.WARN;
+//		default:
+//			return Logger.INFO;
+//		}
+//	}
 
 	public static void print(Class<?> cls, String message, Object... os) {
 		StringBuilder o = new StringBuilder();
