fed4b25d3f59fa1275ef6df9acbb5f7d465a2d57
o2null <zhourui@o2oa.net>
2021-09-09 13:16:33 +0800

    增加默认路由及升级接口

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/x_general_assemble_control.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/x_general_assemble_control.java
@@ -6,7 +6,8 @@ import com.x.base.core.project.annotation.ModuleCategory;
 import com.x.base.core.project.annotation.ModuleType;
 
 @Module(type = ModuleType.ASSEMBLE, category = ModuleCategory.OFFICIAL, name = "公共模块", packageName = "com.x.general.assemble.control", containerEntities = {
-		"com.x.general.core.entity.area.District", "com.x.general.core.entity.GeneralFile" }, storageTypes = {
-				StorageType.file, StorageType.general }, storeJars = { "x_general_core_entity" })
+		"com.x.general.core.entity.area.District", "com.x.general.core.entity.GeneralFile",
+		"com.x.processplatform.core.entity.element.Route" }, storageTypes = { StorageType.file,
+				StorageType.general }, storeJars = { "x_general_core_entity", "x_processplatform_core_entity" })
 public class x_general_assemble_control extends Deployable {
 }
