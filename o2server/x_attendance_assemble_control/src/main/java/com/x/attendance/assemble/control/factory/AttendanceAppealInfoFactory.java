f84e460bd229421ff624a8d2adf09dd4583c12ca
luojing <luojing@o2oa.net>
2021-09-09 14:29:35 +0800

    考勤-修复员工申诉审批列表权限问题

--- o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/factory/AttendanceAppealInfoFactory.java
+++ o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/factory/AttendanceAppealInfoFactory.java
@@ -1,7 +1,9 @@
 package com.x.attendance.assemble.control.factory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -9,15 +11,14 @@ import javax.persistence.criteria.*;
 
 import com.x.attendance.assemble.control.CriteriaQueryTools;
 import com.x.attendance.assemble.control.service.UserManagerService;
-import com.x.attendance.entity.AttendanceSelfHoliday;
-import com.x.attendance.entity.AttendanceSelfHoliday_;
+import com.x.attendance.entity.*;
+import com.x.base.core.project.http.EffectivePerson;
+import com.x.base.core.project.tools.ListTools;
 import org.apache.commons.lang3.StringUtils;
 
 import com.x.attendance.assemble.control.AbstractFactory;
 import com.x.attendance.assemble.control.Business;
 import com.x.attendance.assemble.control.jaxrs.attendanceappealinfo.WrapInFilterAppeal;
-import com.x.attendance.entity.AttendanceAppealInfo;
-import com.x.attendance.entity.AttendanceAppealInfo_;
 import com.x.base.core.project.bean.NameValueCountPair;
 import com.x.base.core.project.exception.ExceptionWhen;
 /**
@@ -355,6 +356,84 @@ public class AttendanceAppealInfoFactory extends AbstractFactory {
 	}
 
 	/**
+	 * 查询下一页的信息数据--按管理员组织
+	 * @param id
+	 * @param count
+	 * @param sequence
+	 * @param wrapIn
+	 * @return
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<AttendanceAppealInfo> listIdsNextWithFilterWithManager( String id, Integer count, Object sequence, WrapInFilterAppeal wrapIn ,Map<String,List<String>> unitMap,Boolean isManager) throws Exception {
+		//先获取上一页最后一条的sequence值，如果有值的话，以此sequence值作为依据取后续的count条数据
+		Boolean unitFlag = false;
+		EntityManager em = this.entityManagerContainer().get( AttendanceAppealInfo.class );
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<AttendanceAppealInfo> cq = cb.createQuery(AttendanceAppealInfo.class);
+		Root<AttendanceAppealInfo> root = cq.from(AttendanceAppealInfo.class);
+
+		String order = wrapIn.getOrder();//排序方式
+		if( order == null || order.isEmpty() ){
+			order = "DESC";
+		}
+		String orderFieldName = "";
+		if(StringUtils.isNotEmpty( wrapIn.getKey())){
+			orderFieldName = wrapIn.getKey();
+		}else{
+			orderFieldName = "sequence";
+		}
+		Order _order = CriteriaQueryTools.setOrder(cb, root, AttendanceAppealInfo_.class, orderFieldName,order);
+		Predicate p = cb.isNotNull(root.get(AttendanceAppealInfo_.detailId));
+		if ((null != sequence) ) {
+			if(StringUtils.equalsIgnoreCase(order, "DESC")){
+				p = cb.and(p,cb.lessThan(root.get(AttendanceAppealInfo_.sequence),sequence.toString()));
+			}else{
+				p = cb.and(p,cb.greaterThan(root.get(AttendanceAppealInfo_.sequence),sequence.toString()));
+			}
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getDetailId())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.detailId),wrapIn.getDetailId()));
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getEmpName())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.empName),wrapIn.getEmpName()));
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getUnitName())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.unitName),wrapIn.getUnitName()));
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getTopUnitName())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.topUnitName),wrapIn.getTopUnitName()));
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getYearString())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.yearString),wrapIn.getYearString()));
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getMonthString())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.monthString),wrapIn.getMonthString()));
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getAppealReason())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.appealReason),wrapIn.getAppealReason()));
+		}
+		if (wrapIn.getStatus()!=999) {
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.status),wrapIn.getStatus()));
+		}
+
+		if(!isManager){
+			if(unitMap.isEmpty()){
+				p = cb.and(p,cb.isNull(root.get(AttendanceAppealInfo_.detailId)));
+			}else{
+				if(unitMap.containsKey("COMPANY")){
+					p = cb.and(p,root.get(AttendanceAppealInfo_.topUnitName).in(unitMap.get("COMPANY")));
+				}
+				if(unitMap.containsKey("DEPT")){
+					p = cb.and(p,root.get(AttendanceAppealInfo_.unitName).in(unitMap.get("DEPT")));
+				}
+			}
+		}
+		Query query = em.createQuery(cq.select(root).where(p).orderBy(_order) );
+		return query.setMaxResults(count).getResultList();
+	}
+
+	/**
 	 * 查询上一页的文档信息数据
 	 * @param id
 	 * @param count
@@ -484,7 +563,7 @@ public class AttendanceAppealInfoFactory extends AbstractFactory {
 	 * @return
 	 * @throws Exception
 	 */
-	public long getCountWithFilterWithCurrentProcessor( WrapInFilterAppeal wrapIn ,boolean isManager) throws Exception {
+	public long getCountWithFilterWithCurrentProcessor(WrapInFilterAppeal wrapIn , boolean isManager) throws Exception {
 		//先获取上一页最后一条的sequence值，如果有值的话，以此sequence值作为依据取后续的count条数据
 		EntityManager em = this.entityManagerContainer().get( AttendanceAppealInfo.class );
 		CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -523,5 +602,89 @@ public class AttendanceAppealInfoFactory extends AbstractFactory {
 		}
 		cq.select(cb.count(root)).where(p);
 		return em.createQuery(cq).getSingleResult();
+	}
+
+	/**
+	 * 查询符合的文档信息总数
+	 * @param wrapIn
+	 * @return
+	 * @throws Exception
+	 */
+	public long getCountWithFilterWithManager(WrapInFilterAppeal wrapIn , Map<String,List<String>> unitMap, boolean isManager) throws Exception {
+		//先获取上一页最后一条的sequence值，如果有值的话，以此sequence值作为依据取后续的count条数据
+		EntityManager em = this.entityManagerContainer().get( AttendanceAppealInfo.class );
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+		Root<AttendanceAppealInfo> root = cq.from(AttendanceAppealInfo.class);
+		Predicate p = cb.isNotNull(root.get(AttendanceAppealInfo_.detailId));
+
+		if(StringUtils.isNotEmpty(wrapIn.getDetailId())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.detailId),wrapIn.getDetailId()));
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getEmpName())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.empName),wrapIn.getEmpName()));
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getUnitName())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.unitName),wrapIn.getUnitName()));
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getTopUnitName())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.topUnitName),wrapIn.getTopUnitName()));
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getYearString())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.yearString),wrapIn.getYearString()));
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getMonthString())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.monthString),wrapIn.getMonthString()));
+		}
+		if(StringUtils.isNotEmpty(wrapIn.getAppealReason())){
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.appealReason),wrapIn.getAppealReason()));
+		}
+		if (wrapIn.getStatus()!=999) {
+			p = cb.and(p,cb.equal(root.get(AttendanceAppealInfo_.status),wrapIn.getStatus()));
+		}
+		if(!isManager){
+			if(unitMap.isEmpty()){
+				p = cb.and(p,cb.isNull(root.get(AttendanceAppealInfo_.detailId)));
+			}else{
+				if(unitMap.containsKey("COMPANY")){
+					p = cb.and(p,root.get(AttendanceAppealInfo_.topUnitName).in(unitMap.get("COMPANY")));
+				}
+				if(unitMap.containsKey("DEPT")){
+					p = cb.and(p,root.get(AttendanceAppealInfo_.unitName).in(unitMap.get("DEPT")));
+				}
+			}
+		}
+		cq.select(cb.count(root)).where(p);
+		return em.createQuery(cq).getSingleResult();
+	}
+
+	public Map<String,List<String>> listUnits(EffectivePerson effectivePerson)  throws Exception {
+		Map<String,List<String>> unitMap=new HashMap<>();
+		List<AttendanceAdmin> unitlist = new ArrayList<>();
+		List<String> companyList = new ArrayList<String>();
+		List<String> deptList = new ArrayList<String>();
+		EntityManager em = this.entityManagerContainer().get( AttendanceAdmin.class );
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<AttendanceAdmin> cq = cb.createQuery(AttendanceAdmin.class);
+		Root<AttendanceAdmin> root = cq.from( AttendanceAdmin.class);
+		Predicate p = cb.equal(root.get(AttendanceAdmin_.admin),effectivePerson.getDistinguishedName());
+		unitlist =  em.createQuery(cq.where(p)).getResultList();
+		if(ListTools.isNotEmpty(unitlist)){
+			for(AttendanceAdmin attendanceAdmin :unitlist){
+				if(StringUtils.equals(attendanceAdmin.getAdminLevel(),"DEPT")){
+					deptList.add(attendanceAdmin.getUnitName());
+				}
+				if(StringUtils.equals(attendanceAdmin.getAdminLevel(),"COMPANY")){
+					companyList.add(attendanceAdmin.getUnitName());
+				}
+			}
+		}
+		if(ListTools.isNotEmpty(deptList)){
+			unitMap.put("DEPT",deptList);
+		}
+		if(ListTools.isNotEmpty(companyList)){
+			unitMap.put("COMPANY",companyList);
+		}
+		return  unitMap;
 	}
 }
