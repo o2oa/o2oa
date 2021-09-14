f2aac65c54a4d97ae14e93246e27f47724d766d7
unknown <caixiangyi@zoneland.net>
2021-09-14 14:19:47 +0800

    支持黏贴html片段到ckeditor的时候，获取url上传到服务器

--- o2web/source/o2_lib/htmleditor/ckeditor4161/plugins/o2uploadremoteimage/plugin.js
+++ o2web/source/o2_lib/htmleditor/ckeditor4161/plugins/o2uploadremoteimage/plugin.js
@@ -30,8 +30,6 @@
 		return 'image-' + CKEDITOR.tools.array.map( dateParts, padNumber ).join( '' ) + '-' + uniqueNameCounter + '.' + type;
 	}
 
-
-
 	function isCurrentServerFile(url){
 		if( url.indexOf("/x_file_assemble_control/") === -1 )return false;
 		var fileHost = o2.Actions.getHost( "x_file_assemble_control" );
@@ -41,12 +39,20 @@
 		return fileHost === imgHost;
 	}
 
-	function isImageUploadUrl(uploadUrl){
-		return uploadUrl.indexOf("/x_file_assemble_control/jaxrs/file/upload/with/url/referencetype/") > -1;
-	}
-
 	function getImageMaxWidth(editor){
 		return editor.config.localImageMaxWidth || 2000;
+	}
+
+	function getFileName(url){
+		var uri = new URI(url);
+		var file = uri.parsed.file;
+		if( !file ){
+			return "untitle.jpg"
+		}else if( file.indexOf(".") === -1 ){
+			return file+".jpg"
+		}else{
+			return file;
+		}
 	}
 
 	CKEDITOR.plugins.add( 'o2uploadremoteimage', {
@@ -64,24 +70,8 @@
 			return CKEDITOR.plugins.clipboard.isFileApiSupported;
 		},
 
-		getImageUploadUrl: function( editor ){
-			if (layout.config.app_protocol=="auto"){
-				layout.config.app_protocol = window.location.protocol;
-			}
-
-			var addressObj = layout.serviceAddressList["x_file_assemble_control"];
-			if (addressObj){
-				var address = layout.config.app_protocol+"//"+addressObj.host+(addressObj.port==80 ? "" : ":"+addressObj.port)+addressObj.context;
-			}else{
-				var host = layout.config.center.host || window.location.hostname;
-				var port = layout.config.center.port;
-				var address = layout.config.app_protocol+"//"+host+(port=="80" ? "" : ":"+port)+"/x_program_center";
-			}
-			var url = "/jaxrs/file/upload/with/url/referencetype/"+editor.config.referenceType+"/reference/"+editor.config.reference+"/scale/"+ getImageMaxWidth(editor);
-			return o2.filterUrl(address+url);
-		},
-
 		init: function( editor ) {
+			var _self = this;
 			// Do not execute this paste listener if it will not be possible to upload file.
 			if ( !this.isSupportedEnvironment() ) {
 				return;
@@ -91,13 +81,6 @@
 			editor.filter.allow( 'img[data-cke-img-unique-id]' );
 
 			if( !editor.config.reference || !editor.config.referenceType )return;
-
-			var uploadUrl = this.getImageUploadUrl( editor, 'image' );
-
-			if ( !uploadUrl ) {
-				return;
-			}
-
 
 			editor.on( 'paste', function( evt ) {
 				debugger;
@@ -131,74 +114,107 @@
 						// Image have to contain src=data:...
 						isDataInSrc = imgSrc && imgSrc.substring( 0, 5 ) == 'data:',
 						isRealObject = img.data( 'cke-realelement' ) === null,
-						inCurrentServer = isCurrentServerFile(imgSrc || "" ),
+						//inCurrentServer = isCurrentServerFile(imgSrc || "" ),
 						isLocalFileSr = !isDataInSrc && imgSrc && imgSrc.substring( 0, 5 ) == 'file:';
 
 					// We are not uploading images in non-editable blocs and fake objects (https://dev.ckeditor.com/ticket/13003).
-					if ( imgSrc && !inCurrentServer && !isDataInSrc && !isLocalFileSr && isRealObject && !img.data( 'cke-upload-id' ) && !img.isReadOnly( 1 ) ) {
+					if ( imgSrc && !isDataInSrc && !isLocalFileSr && isRealObject && !img.data( 'cke-upload-id' ) && !img.isReadOnly( 1 ) ) {
 
-						var imgFormat = imgSrc.match( /image\/([a-z]+?);/i ),
-						imgFormat = ( imgFormat && imgFormat[ 1 ] ) || 'jpg',
-						uniqueName = getUniqueImageFileName(imgFormat);
+						_self.upload(editor, img, imgSrc)
 
-						img.setAttributes( {
-							'data-cke-img-unique-id': uniqueName
-						});
-
-
-						debugger;
-
-						window.setTimeout(function(){
-							var newImg = CKEDITOR.dom.element.createFromHtml( '<strong class="anyclass">My element</strong>' );
-							var oldImg = editor.editable().findOne( '[data-cke-img-unique-id="' + this.uniqueName + '"]');
-							newImg.insertBefore( oldImg );
-						}.bind({uniqueName:uniqueName}), 100);
-
-
-
-						continue;
-
-						o2.Actions.load("x_file_assemble_control").uploadWithUrl(imgSrc, function (responseData) {
-							var id = responseData.data ? responseData.data.id : responseData.id;
-							var orgid = responseData.data ? responseData.data.origId : responseData.origId;
-							var src = MWF.xDesktop.getImageSrc( id );
-
-							// Width and height could be returned by server (https://dev.ckeditor.com/ticket/13519).
-							var $img = this.img.$,
-								width = $img.naturalWidth,
-								height = $img.naturalHeight;
-
-							//按最大宽度比率缩小
-							var maxWidth = getImageMaxWidth(editor);
-							if( maxWidth && maxWidth < width ){
-								height = parseInt( height * (maxWidth / width) );
-							}
-
-							width = Math.min(width, maxWidth);
-
-							var imgString = '<img src="' + src + '" ';
-							imgString += 'data-id="' + id + '" ';
-							imgString += 'data-orgid="' + orgid + '" ';
-							imgString += 'data-height="' + height + 'px" ';
-							imgString += 'data-width="' + width + 'px" ';
-							imgString += 'style="max-width:100%; width:' + width + 'px" ';
-							imgString += 'onerror="MWF.xDesktop.setImageSrc()" ';
-							if(upload.fileName){
-								imgString += 'alt="' + upload.fileName + '" ';
-							}
-							imgString += editor.config.enablePreview ? 'data-prv="true" ' : 'data-prv="false" ';
-							imgString += '">';
-
-							// Set width and height to prevent blinking.
-							// this.replaceWith( imgString );
-
-							editor.fire( 'change' );
-						}.bind({img: img}))
 					}
 				}
 
 				data.dataValue = temp.getHtml();
 			} );
+		},
+		upload: function (editor, img, imgSrc) {
+			var imgFormat = imgSrc.match( /image\/([a-z]+?);/i ),
+				imgFormat = ( imgFormat && imgFormat[ 1 ] ) || 'jpg',
+				uniqueName = getUniqueImageFileName(imgFormat);
+
+			img.setAttributes( {
+				'data-cke-img-unique-id': uniqueName
+			});
+
+			var _upload = function ( width, height) {
+
+				var fileName = getFileName(imgSrc);
+				var	alt = img.getAttribute("alt");
+
+				o2.Actions.load("x_file_assemble_control").FileAction.uploadWithUrl({
+					referenceType: editor.config.referenceType,
+					reference: editor.config.reference,
+					scale: getImageMaxWidth(editor),
+					fileName: fileName,
+					fileUrl: imgSrc
+				}, function (responseData) {
+					var id = responseData.data ? responseData.data.id : responseData.id;
+					var orgid = responseData.data ? responseData.data.origId : responseData.origId;
+					var src = MWF.xDesktop.getImageSrc( id );
+
+					// Width and height could be returned by server (https://dev.ckeditor.com/ticket/13519).
+					//按最大宽度比率缩小
+					var maxWidth = getImageMaxWidth(editor);
+					if( maxWidth && maxWidth < width ){
+						height = parseInt( height * (maxWidth / width) );
+					}
+
+					width = Math.min(width, maxWidth);
+
+					var imgString = '<img src="' + src + '" ';
+					imgString += 'data-id="' + id + '" ';
+					imgString += 'data-orgid="' + orgid + '" ';
+					imgString += 'data-height="' + height + 'px" ';
+					imgString += 'data-width="' + width + 'px" ';
+					imgString += 'style="max-width:100%; width:' + width + 'px" ';
+					imgString += 'onerror="MWF.xDesktop.setImageSrc()" ';
+					imgString += 'alt="' + (alt || fileName) + '" ';
+					imgString += editor.config.enablePreview ? 'data-prv="true" ' : 'data-prv="false" ';
+					imgString += '">';
+
+					var newImg = CKEDITOR.dom.element.createFromHtml( imgString );
+					var oldImg = editor.editable().findOne( '[data-cke-img-unique-id="' + uniqueName + '"]');
+					newImg.insertBefore( oldImg );
+					oldImg.remove();
+
+					editor.fire( 'change' );
+				}, function () {
+					return true;
+				})
+			};
+
+			var width = img.getStyle("width");
+			var $img = img.$;
+			if( width && !isNaN(parseInt(width)) ){
+				width = parseInt(width);
+			}
+			if( !width || isNaN(parseInt(width)) ){
+				width = $img.naturalWidth;
+			}
+
+			var height = img.getStyle("height");
+			if( height && !isNaN(parseInt(height)) ){
+				height = parseInt(height);
+			}
+			if( !height || isNaN(parseInt(height)) ){
+				height = $img.naturalHeight;
+			}
+
+			if( !width || !height ){
+				new Element("img", {
+					src: imgSrc,
+					events: {
+						"load": function () {
+							if( this.naturalWidth && this.naturalHeight ){
+								_upload(this.naturalWidth, this.naturalHeight);
+							}
+						}
+					}
+				})
+			}else{
+				_upload(width, height);
+			}
 		}
 	} );
 
