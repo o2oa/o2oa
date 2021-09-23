2a9f051ed5126baab03671b889b65574d2331c9e
o2null <zhourui@o2oa.net>
2021-09-23 05:48:26 +0000

    Merge branch 'feature/scripting' into 'wrdp'
    新版本Scripting
    
    See merge request o2oa/o2oa!5560
    
    (cherry picked from commit 0c33702fec08288f5bb707f424723eceb9fc1265)
    
    992fc799 add scripting
    0c9b1765 新版本Scripting

---
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/scripting/ScriptingFactory.java
@@ -0,0 +1,98 @@
+package com.x.base.core.project.scripting;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Objects;
+
+import javax.script.Compilable;
+import javax.script.CompiledScript;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import com.google.gson.reflect.TypeToken;
+import com.x.base.core.project.config.Config;
+import com.x.base.core.project.logger.Logger;
+import com.x.base.core.project.logger.LoggerFactory;
+
+public class ScriptingFactory {
+
+	private static Logger logger = LoggerFactory.getLogger(ScriptingFactory.class);
+
+	private ScriptingFactory() {
+
+	}
+
+	public static final ScriptEngine scriptEngine = (new ScriptEngineManager())
+			.getEngineByName(Config.SCRIPTING_ENGINE_NAME);
+
+	private static CompiledScript compiledScriptInitialServiceScriptText;
+	private static CompiledScript compiledScriptInitialScriptText;
+
+	public static final String BINDING_NAME_RESOURCES = "resources";
+	public static final String BINDING_NAME_EFFECTIVEPERSON = "effectivePerson";
+	public static final String BINDING_NAME_WORKCONTEXT = "workContext";
+	public static final String BINDING_NAME_GSON = "gson";
+	public static final String BINDING_NAME_DATA = "data";
+	public static final String BINDING_NAME_ORGANIZATION = "organization";
+	public static final String BINDING_NAME_WEBSERVICESCLIENT = "webservicesClient";
+	public static final String BINDING_NAME_DICTIONARY = "dictionary";
+	public static final String BINDING_NAME_ROUTES = "routes";
+	public static final String BINDING_NAME_ROUTE = "routes";
+	public static final String BINDING_NAME_APPLICATIONS = "applications";
+
+	public static final String BINDING_NAME_ASSIGNDATA = "assignData";
+
+	public static final String BINDING_NAME_IDENTITY = "identity";
+
+	public static final String BINDING_NAME_PARAMETERS = "parameters";
+	public static final String BINDING_NAME_JAXRSRESPONSE = "jaxrsResponse";
+	public static final String BINDING_NAME_JAXWSRESPONSE = "jaxwsResponse";
+
+	public static final String BINDING_NAME_JAXRSBODY = "jaxrsBody";
+	public static final String BINDING_NAME_JAXRSHEAD = "jaxrsHead";
+
+	public static final String BINDING_NAME_SERVICEVALUE = "serviceValue";
+	public static final String BINDING_NAME_TASK = "task";
+	public static final String BINDING_NAME_EXPIRE = "expire";
+	public static final String BINDING_NAME_SERIAL = "serial";
+	public static final String BINDING_NAME_PROCESS = "process";
+
+	private static Type stringsType = new TypeToken<ArrayList<String>>() {
+	}.getType();
+
+	public static ScriptEngine newScriptEngine() {
+		return (new ScriptEngineManager()).getEngineByName(Config.SCRIPTING_ENGINE_NAME);
+	}
+
+	public static synchronized CompiledScript initialServiceScriptText() throws Exception {
+		if (compiledScriptInitialServiceScriptText == null) {
+			String text = Config.initialServiceScriptText();
+			compiledScriptInitialServiceScriptText = ((Compilable) scriptEngine).compile(text);
+		}
+		return compiledScriptInitialServiceScriptText;
+	}
+
+	public static synchronized CompiledScript initialScriptText() throws Exception {
+		if (compiledScriptInitialScriptText == null) {
+			String text = Config.initialScriptText();
+			compiledScriptInitialScriptText = ((Compilable) scriptEngine).compile(text);
+		}
+		return compiledScriptInitialScriptText;
+	}
+
+	public static CompiledScript compile(String text) throws ScriptException {
+		return ((Compilable) scriptEngine).compile(text);
+	}
+
+	public static String functionalization(String text) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("(function(){").append(System.lineSeparator());
+		sb.append(Objects.toString(text, "")).append(System.lineSeparator());
+		sb.append("})();");
+		return sb.toString();
+	}
+
+	
+
+}
