a5974e3aa31ccdd6ed461b5e67e1de6da9ba3437
huqi <hq_1980@hotmail.com>
2021-09-24 16:04:35 +0800

    修复this.data直接赋值后，getter、setter方法可以通过check重新设置

--- o2web/source/o2_core/o2/xScript/CMSEnvironment.js
+++ o2web/source/o2_core/o2/xScript/CMSEnvironment.js
@@ -2091,6 +2091,11 @@ MWF.xScript.CMSJSONData = function(data, callback, key, parent, _form){
                                 }
                                 if (!noreset) this[newKey] = newValue;
                             }else{
+                                if (!Object.getOwnPropertyDescriptor(this, newKey).get){
+                                    var o = {};
+                                    o[newKey] = {"configurable": true, "enumerable": true, "get": getter.apply(this, [data, callback, newKey, this]),"set": setter.apply(this, [data, callback, newKey, this])};
+                                    MWF.defineProperties(this, o);
+                                }
                                 if (overwrite){
                                     data[newKey] = newValue;
                                     if (!noreset)  this[newKey] = newValue;
