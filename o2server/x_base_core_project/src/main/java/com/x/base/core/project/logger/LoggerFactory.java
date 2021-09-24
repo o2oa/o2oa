baabc276183f38fcbecb6deec3be9fcee1fd3115
o2null <zhourui@o2oa.net>
2021-09-24 09:35:42 +0000

    兼容custom的setLevel
    (cherry picked from commit 316629eeaa4d06988da295410ef0fedb27b2e035)

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/logger/LoggerFactory.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/logger/LoggerFactory.java
@@ -9,4 +9,13 @@ public class LoggerFactory {
 		return new Logger(clz.getName());
 	}
 
+	@Deprecated
+	/**
+	 * 
+	 * 此方法已经废弃没有任何用处,有部分custom模块编写时错误的使用了这个方法,作为兼容custom模块的空方法.
+	 */
+	public static void setLevel(Object o) {
+		// nothing
+	}
+
 }
