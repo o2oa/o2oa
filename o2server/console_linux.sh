ffb725e29655e4c472bbf1d8ec6c6bd2b92e2041
o2null <zhourui@o2oa.net>
2021-09-28 17:27:33 +0800

    修正console命令

--- o2server/console_linux.sh
+++ o2server/console_linux.sh
@@ -1,1 +1,1 @@
-$(cd "$(dirname "$0")"; pwd)/jvm/linux_java11/bin/java -javaagent:$(cd "$(dirname "$0")"; pwd)/console.jar -cp $(cd "$(dirname "$0")"; pwd)/console.jar com.x.server.console.Shadow
+$(cd "$(dirname "$0")"; pwd)/jvm/linux_java11/bin/java -javaagent:$(cd "$(dirname "$0")"; pwd)/console.jar=shadow -cp $(cd "$(dirname "$0")"; pwd)/console.jar com.x.server.console.Shadow
