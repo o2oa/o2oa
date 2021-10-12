1992e5bca161f25ac1f5bf8f1b56e3604247282c
unknown <caixiangyi@zoneland.net>
2021-10-12 11:13:28 +0800

    内容管理加入tiny

--- o2web/source/x_component_cms_FormDesigner/Module/Package.js
+++ o2web/source/x_component_cms_FormDesigner/Module/Package.js
@@ -38,6 +38,7 @@ MWF.xDesktop.requireApp("cms.FormDesigner", "Module.Tab", null, false);
 MWF.xDesktop.requireApp("cms.FormDesigner", "Module.Tree", null, false);
 MWF.xDesktop.requireApp("cms.FormDesigner", "Module.Iframe", null, false);
 MWF.xDesktop.requireApp("cms.FormDesigner", "Module.Htmleditor", null, false);
+MWF.xDesktop.requireApp("cms.FormDesigner", "Module.TinyMCEEditor", null, false);
 MWF.xDesktop.requireApp("cms.FormDesigner", "Module.Common", null, false);
 MWF.xDesktop.requireApp("cms.FormDesigner", "Module.Documenteditor", null, false);
 MWF.xDesktop.requireApp("cms.FormDesigner", "Module.Office", null, false);
