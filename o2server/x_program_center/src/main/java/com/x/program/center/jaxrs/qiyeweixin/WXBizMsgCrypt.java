38beb5cc9eb0300f48fc9fd8367c0f3eee3b2bf1
o2null <zhourui@o2oa.net>
2021-10-14 07:15:10 +0000

    更新令牌过期,通过center进行刷新
    (cherry picked from commit ba70570e482b7260a9d44bf2ab61fd2d8daeefdd)

--- o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/qiyeweixin/WXBizMsgCrypt.java
+++ o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/qiyeweixin/WXBizMsgCrypt.java
@@ -14,6 +14,8 @@
 package com.x.program.center.jaxrs.qiyeweixin;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -22,24 +24,28 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 提供接收和推送给企业微信消息的加解密接口(UTF8编码的字符串).
  * <ol>
- * 	<li>第三方回复加密消息给企业微信</li>
- * 	<li>第三方收到企业微信发送的消息，验证消息的安全性，并对消息进行解密。</li>
+ * <li>第三方回复加密消息给企业微信</li>
+ * <li>第三方收到企业微信发送的消息，验证消息的安全性，并对消息进行解密。</li>
  * </ol>
  * 说明：异常java.security.InvalidKeyException:illegal Key Size的解决方案
  * <ol>
- * 	<li>在官方网站下载JCE无限制权限策略文件（JDK7的下载地址：
- *      http://www.oracle.com/technetwork/java/javase/downloads/jce-7-download-432124.html</li>
- * 	<li>下载后解压，可以看到local_policy.jar和US_export_policy.jar以及readme.txt</li>
- * 	<li>如果安装了JRE，将两个jar文件放到%JRE_HOME%\lib\security目录下覆盖原来的文件</li>
- * 	<li>如果安装了JDK，将两个jar文件放到%JDK_HOME%\jre\lib\security目录下覆盖原来文件</li>
+ * <li>在官方网站下载JCE无限制权限策略文件（JDK7的下载地址：
+ * http://www.oracle.com/technetwork/java/javase/downloads/jce-7-download-432124.html</li>
+ * <li>下载后解压，可以看到local_policy.jar和US_export_policy.jar以及readme.txt</li>
+ * <li>如果安装了JRE，将两个jar文件放到%JRE_HOME%\lib\security目录下覆盖原来的文件</li>
+ * <li>如果安装了JDK，将两个jar文件放到%JDK_HOME%\jre\lib\security目录下覆盖原来文件</li>
  * </ol>
  */
 public class WXBizMsgCrypt {
-	static Charset CHARSET = Charset.forName("utf-8");
+
+	private static final Random random = new SecureRandom();
+
+	static final Charset CHARSET = StandardCharsets.UTF_8;
 	Base64 base64 = new Base64();
 	byte[] aesKey;
 	String token;
@@ -47,9 +53,10 @@ public class WXBizMsgCrypt {
 
 	/**
 	 * 构造函数
-	 * @param token 企业微信后台，开发者设置的token
+	 * 
+	 * @param token          企业微信后台，开发者设置的token
 	 * @param encodingAesKey 企业微信后台，开发者设置的EncodingAESKey
-	 * @param receiveid, 不同场景含义不同，详见文档
+	 * @param receiveid,     不同场景含义不同，详见文档
 	 * 
 	 * @throws AesException 执行失败，请查看该异常的错误码和具体的错误信息
 	 */
@@ -86,11 +93,9 @@ public class WXBizMsgCrypt {
 	// 随机生成16位字符串
 	String getRandomStr() {
 		String base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		Random random = new Random();
-		StringBuffer sb = new StringBuffer();
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < 16; i++) {
-			int number = random.nextInt(base.length());
-			sb.append(base.charAt(number));
+			sb.append(base.charAt(random.nextInt(base.length())));
 		}
 		return sb.toString();
 	}
@@ -179,8 +184,7 @@ public class WXBizMsgCrypt {
 			int xmlLength = recoverNetworkBytesOrder(networkOrder);
 
 			xmlContent = new String(Arrays.copyOfRange(bytes, 20, 20 + xmlLength), CHARSET);
-			from_receiveid = new String(Arrays.copyOfRange(bytes, 20 + xmlLength, bytes.length),
-					CHARSET);
+			from_receiveid = new String(Arrays.copyOfRange(bytes, 20 + xmlLength, bytes.length), CHARSET);
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new AesException(AesException.IllegalBuffer);
@@ -197,14 +201,14 @@ public class WXBizMsgCrypt {
 	/**
 	 * 将企业微信回复用户的消息加密打包.
 	 * <ol>
-	 * 	<li>对要发送的消息进行AES-CBC加密</li>
-	 * 	<li>生成安全签名</li>
-	 * 	<li>将消息密文和安全签名打包成xml格式</li>
+	 * <li>对要发送的消息进行AES-CBC加密</li>
+	 * <li>生成安全签名</li>
+	 * <li>将消息密文和安全签名打包成xml格式</li>
 	 * </ol>
 	 * 
-	 * @param replyMsg 企业微信待回复用户的消息，xml格式的字符串
+	 * @param replyMsg  企业微信待回复用户的消息，xml格式的字符串
 	 * @param timeStamp 时间戳，可以自己生成，也可以用URL参数的timestamp
-	 * @param nonce 随机串，可以自己生成，也可以用URL参数的nonce
+	 * @param nonce     随机串，可以自己生成，也可以用URL参数的nonce
 	 * 
 	 * @return 加密后的可以直接回复用户的密文，包括msg_signature, timestamp, nonce, encrypt的xml格式的字符串
 	 * @throws AesException 执行失败，请查看该异常的错误码和具体的错误信息
@@ -214,7 +218,7 @@ public class WXBizMsgCrypt {
 		String encrypt = encrypt(getRandomStr(), replyMsg);
 
 		// 生成安全签名
-		if (timeStamp == "") {
+		if (StringUtils.isEmpty(timeStamp)) {
 			timeStamp = Long.toString(System.currentTimeMillis());
 		}
 
@@ -229,21 +233,20 @@ public class WXBizMsgCrypt {
 	/**
 	 * 检验消息的真实性，并且获取解密后的明文.
 	 * <ol>
-	 * 	<li>利用收到的密文生成安全签名，进行签名验证</li>
-	 * 	<li>若验证通过，则提取xml中的加密消息</li>
-	 * 	<li>对消息进行解密</li>
+	 * <li>利用收到的密文生成安全签名，进行签名验证</li>
+	 * <li>若验证通过，则提取xml中的加密消息</li>
+	 * <li>对消息进行解密</li>
 	 * </ol>
 	 * 
 	 * @param msgSignature 签名串，对应URL参数的msg_signature
-	 * @param timeStamp 时间戳，对应URL参数的timestamp
-	 * @param nonce 随机串，对应URL参数的nonce
-	 * @param postData 密文，对应POST请求的数据
+	 * @param timeStamp    时间戳，对应URL参数的timestamp
+	 * @param nonce        随机串，对应URL参数的nonce
+	 * @param postData     密文，对应POST请求的数据
 	 * 
 	 * @return 解密后的原文
 	 * @throws AesException 执行失败，请查看该异常的错误码和具体的错误信息
 	 */
-	public String DecryptMsg(String msgSignature, String timeStamp, String nonce, String postData)
-			throws AesException {
+	public String DecryptMsg(String msgSignature, String timeStamp, String nonce, String postData) throws AesException {
 
 		// 密钥，公众账号的app secret
 		// 提取密文
@@ -266,16 +269,16 @@ public class WXBizMsgCrypt {
 
 	/**
 	 * 验证URL
+	 * 
 	 * @param msgSignature 签名串，对应URL参数的msg_signature
-	 * @param timeStamp 时间戳，对应URL参数的timestamp
-	 * @param nonce 随机串，对应URL参数的nonce
-	 * @param echoStr 随机串，对应URL参数的echostr
+	 * @param timeStamp    时间戳，对应URL参数的timestamp
+	 * @param nonce        随机串，对应URL参数的nonce
+	 * @param echoStr      随机串，对应URL参数的echostr
 	 * 
 	 * @return 解密之后的echostr
 	 * @throws AesException 执行失败，请查看该异常的错误码和具体的错误信息
 	 */
-	public String VerifyURL(String msgSignature, String timeStamp, String nonce, String echoStr)
-			throws AesException {
+	public String VerifyURL(String msgSignature, String timeStamp, String nonce, String echoStr) throws AesException {
 		String signature = SHA1.getSHA1(token, timeStamp, nonce, echoStr);
 
 		if (!signature.equals(msgSignature)) {
