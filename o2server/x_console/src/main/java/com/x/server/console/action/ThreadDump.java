92eeb0c2dab715d940be4b514c9b9f514f903a54
o2null <zhourui@o2oa.net>
2021-10-18 15:26:16 +0800

    P3C修复

--- o2server/x_console/src/main/java/com/x/server/console/action/ThreadDump.java
+++ o2server/x_console/src/main/java/com/x/server/console/action/ThreadDump.java
@@ -4,26 +4,40 @@ import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 import java.nio.file.Path;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.SystemUtils;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 
 import com.x.base.core.project.config.Config;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
 import com.x.base.core.project.tools.DateTools;
 
+/**
+ * 
+ * @author ray
+ *
+ */
 public class ThreadDump {
 
 	private static Logger logger = LoggerFactory.getLogger(ThreadDump.class);
 
-	public void execute(Integer count) throws Exception {
-		new Thread() {
-			public void run() {
-				try {
-					RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
-					String jvmName = runtimeBean.getName();
-					long pid = Long.parseLong(jvmName.split("@")[0]);
-					Path path = SystemUtils.getJavaHome().toPath();
+	public void execute(Integer count) {
+
+		ExecutorService service = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(),
+				new BasicThreadFactory.Builder().namingPattern(ThreadDump.class.getName()).daemon(true).build());
+
+		service.execute(() -> {
+			try {
+				RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
+				String jvmName = runtimeBean.getName();
+				long pid = Long.parseLong(jvmName.split("@")[0]);
+				Path path = SystemUtils.getJavaHome().toPath();
+				if (null != path) {
 					path = path.getParent().resolve("bin/jcmd").toAbsolutePath();
 					for (int i = 0; i < count; i++) {
 						Path out = Config.dir_logs(true).toPath()
@@ -42,10 +56,13 @@ public class ThreadDump {
 						Thread.sleep(1000);
 					}
 					logger.print("{} thread dump completed.", count);
-				} catch (Exception e) {
-					e.printStackTrace();
 				}
+			} catch (InterruptedException e) {
+				logger.error(e);
+				Thread.currentThread().interrupt();
+			} catch (Exception e) {
+				logger.error(e);
 			}
-		}.start();
+		});
 	}
 }
