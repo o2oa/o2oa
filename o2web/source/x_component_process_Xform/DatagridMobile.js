2d5ff9fb7b10b296406eb3509031d5fc310670ef
unknown <caixiangyi@zoneland.net>
2021-10-26 14:27:03 +0800

    修复数据网格、数据表格、数据模板，queryLoad和load会执行两次的问题

--- o2web/source/x_component_process_Xform/DatagridMobile.js
+++ o2web/source/x_component_process_Xform/DatagridMobile.js
@@ -61,9 +61,22 @@ MWF.xApplication.process.Xform.DatagridMobile = new Class(
         this.field = true;
         this.fieldModuleLoaded = false;
     },
+    load: function(){
+        this._loadModuleEvents();
+        if (this.fireEvent("queryLoad")){
+            this._queryLoaded();
+            this._loadUserInterface();
+            this._loadStyles();
+            this._loadDomEvents();
+            //this._loadEvents();
 
+            this._afterLoaded();
+            this.fireEvent("afterLoad");
+            // this.fireEvent("load");
+        }
+    },
     _loadUserInterface: function(){
-        this.fireEvent("queryLoad");
+        // this.fireEvent("queryLoad");
 
         this.editModules = [];
         this.node.setStyle("overflow-x", "hidden");
