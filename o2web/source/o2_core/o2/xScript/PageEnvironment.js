389124646257d3b2728877b861c2053da9658490
huqi <hq_1980@hotmail.com>
2021-11-01 09:31:04 +0000

    增加Table自键表操作的api
    (cherry picked from commit c84be0b80af57bb04b77530941c9255e93fe032a)

--- o2web/source/o2_core/o2/xScript/PageEnvironment.js
+++ o2web/source/o2_core/o2/xScript/PageEnvironment.js
@@ -2159,7 +2159,7 @@ if (!MWF.xScript || !MWF.xScript.PageEnvironment) {
 if (!MWF.xScript.createTable) MWF.xScript.createTable = function(){
     return function(name){
         this.name = name;
-        this.action = o2.Actions.get("x_query_assemble_surface");
+        this.action = o2.Actions.load("x_query_assemble_surface").TableAction;
 
         this.listRowNext = function(id, count, success, error, async){
             this.action.listRowNext(this.name, id, count, success, error, async);
@@ -2167,8 +2167,8 @@ if (!MWF.xScript.createTable) MWF.xScript.createTable = function(){
         this.listRowPrev = function(id, count, success, error, async){
             this.action.listRowPrev(this.name, id, count, success, error, async);
         };
-        this.listRowPrev = function(id, count, success, error, async){
-            this.action.listRowPrev(this.name, id, count, success, error, async);
+        this.listRowSelect = function(where, orderBy, size, success, error, async){
+            this.action.listRowSelect(this.name, {"where": where, "orderBy": orderBy, "size": size || ""}, success, error, async);
         };
         this.listRowSelectWhere = function(where, success, error, async){
             this.action.listRowSelectWhere(this.name, where, success, error, async);
@@ -2177,19 +2177,19 @@ if (!MWF.xScript.createTable) MWF.xScript.createTable = function(){
             this.action.listRowCountWhere(this.name, where, success, error, async);
         };
         this.deleteRow = function(id, success, error, async){
-            this.action.deleteRow(this.name, id, success, error, async);
+            this.action.rowDelete(this.name, id, success, error, async);
         };
         this.deleteAllRow = function(success, error, async){
-            this.action.deleteAllRow(this.name, success, error, async);
+            this.action.rowDeleteAll(this.name, success, error, async);
         };
         this.getRow = function(id, success, error, async){
-            this.action.getRow(this.name, id, success, error, async);
+            this.action.rowGet(this.name, id, success, error, async);
         };
         this.insertRow = function(data, success, error, async){
-            this.action.insertRow(this.name, data, success, error, async);
+            this.action.rowInsert(this.name, data, success, error, async);
         };
         this.updateRow = function(id, data, success, error, async){
-            this.action.updateRow(this.name, id, data, success, error, async);
+            this.action.rowUpdate(this.name, id, data, success, error, async);
         };
     }
 };
--- o2web/source/o2_core/o2/xScript/ViewEnvironment.js
+++ o2web/source/o2_core/o2/xScript/ViewEnvironment.js
@@ -4784,7 +4784,45 @@ MWF.xScript.ViewEnvironment = function (ev) {
             o2.Actions.get("x_query_assemble_surface").executeStatement(option.name, json, success, error, options.async);
         }
     };
-    // this.Table = MWF.xScript.createTable();
+    this.Table = MWF.xScript.createTable();
+};
+
+MWF.xScript.createTable = function(){
+    return function(name){
+        this.name = name;
+        this.action = o2.Actions.load("x_query_assemble_surface").TableAction;
+
+        this.listRowNext = function(id, count, success, error, async){
+            this.action.listRowNext(this.name, id, count, success, error, async);
+        };
+        this.listRowPrev = function(id, count, success, error, async){
+            this.action.listRowPrev(this.name, id, count, success, error, async);
+        };
+        this.listRowSelect = function(where, orderBy, size, success, error, async){
+            this.action.listRowSelect(this.name, {"where": where, "orderBy": orderBy, "size": size || ""}, success, error, async);
+        };
+        this.listRowSelectWhere = function(where, success, error, async){
+            this.action.listRowSelectWhere(this.name, where, success, error, async);
+        };
+        this.listRowCountWhere = function(where, success, error, async){
+            this.action.listRowCountWhere(this.name, where, success, error, async);
+        };
+        this.deleteRow = function(id, success, error, async){
+            this.action.rowDelete(this.name, id, success, error, async);
+        };
+        this.deleteAllRow = function(success, error, async){
+            this.action.rowDeleteAll(this.name, success, error, async);
+        };
+        this.getRow = function(id, success, error, async){
+            this.action.rowGet(this.name, id, success, error, async);
+        };
+        this.insertRow = function(data, success, error, async){
+            this.action.rowInsert(this.name, data, success, error, async);
+        };
+        this.updateRow = function(id, data, success, error, async){
+            this.action.rowUpdate(this.name, id, data, success, error, async);
+        };
+    }
 };
 
 
