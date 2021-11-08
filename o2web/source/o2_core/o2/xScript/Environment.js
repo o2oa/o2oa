0a29faf186c2fcc8046a45be33e54a30917658b9
huqi <hq_1980@hotmail.com>
2021-11-08 03:48:11 +0000

    修改脚本中的箭头函数，以支持IE11
    (cherry picked from commit 10d5cd9b4ac084948d04e94526e50496e44eb2ce)

--- o2web/source/o2_core/o2/xScript/Environment.js
+++ o2web/source/o2_core/o2/xScript/Environment.js
@@ -3214,7 +3214,7 @@ MWF.xScript.Environment = function(ev){
             }
             if (!app || !process){
                 var cmpt = this.getApp();
-                o2.requireApp([["process.TaskCenter", "lp."+o2.language], ["process.TaskCenter", ""]],"", ()=>{
+                o2.requireApp([["process.TaskCenter", "lp."+o2.language], ["process.TaskCenter", ""]],"", function(){
                     var obj = {
                         "lp": o2.xApplication.process.TaskCenter.LP,
                         "content": cmpt.content,
