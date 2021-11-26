8074507fbb751e3135fef48a3062f3dc93fbca42
o2sword <171715986@qq.com>
2021-11-26 10:46:38 +0800

    自建表分应用编译

--- o2server/x_query_assemble_designer/src/main/java/com/x/query/assemble/designer/jaxrs/table/ActionBuildTableDispatch.java
+++ o2server/x_query_assemble_designer/src/main/java/com/x/query/assemble/designer/jaxrs/table/ActionBuildTableDispatch.java
@@ -1,28 +1,30 @@
 package com.x.query.assemble.designer.jaxrs.table;
 
-import java.util.Date;
-import java.util.List;
-
 import com.x.base.core.container.EntityManagerContainer;
 import com.x.base.core.container.factory.EntityManagerContainerFactory;
 import com.x.base.core.project.Application;
-import com.x.base.core.project.x_query_assemble_designer;
 import com.x.base.core.project.connection.CipherConnectionAction;
 import com.x.base.core.project.exception.ExceptionAccessDenied;
+import com.x.base.core.project.exception.ExceptionEntityNotExist;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.jaxrs.WrapBoolean;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
 import com.x.base.core.project.tools.ListTools;
+import com.x.base.core.project.x_query_assemble_designer;
 import com.x.query.assemble.designer.Business;
 import com.x.query.assemble.designer.ThisApplication;
+import com.x.query.core.entity.Query;
+import com.x.query.core.entity.schema.Table;
 
-class ActionBuildAll extends BaseAction {
+import java.util.List;
 
-	private static Logger logger = LoggerFactory.getLogger(ActionBuildAll.class);
+class ActionBuildTableDispatch extends BaseAction {
 
-	ActionResult<Wo> execute(EffectivePerson effectivePerson) throws Exception {
+	private static Logger logger = LoggerFactory.getLogger(ActionBuildTableDispatch.class);
+
+	ActionResult<Wo> execute(EffectivePerson effectivePerson, String queryId) throws Exception {
 		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
 			ActionResult<Wo> result = new ActionResult<>();
 			Wo wo = new Wo();
@@ -30,11 +32,15 @@ class ActionBuildAll extends BaseAction {
 			if (!business.controllable(effectivePerson)) {
 				throw new ExceptionAccessDenied(effectivePerson);
 			}
+			Query query = emc.fetch(queryId, Query.class);
+			if (null == query) {
+				throw new ExceptionEntityNotExist(queryId, Query.class);
+			}
 			List<Application> apps = ThisApplication.context().applications().get(x_query_assemble_designer.class);
 			if (ListTools.isNotEmpty(apps)) {
 				apps.stream().forEach(o -> {
-					String url = o.getUrlJaxrsRoot() + "table/build?tt="+System.currentTimeMillis();
-					logger.print("{} do dispatch build table request to : {}", effectivePerson.getDistinguishedName(), url);
+					String url = o.getUrlJaxrsRoot() + "table/"+ queryId +"/build?tt="+System.currentTimeMillis();
+					logger.info("{} do dispatch build query {} table request to : {}", effectivePerson.getDistinguishedName(), queryId, url);
 					try {
 						CipherConnectionAction.get(effectivePerson.getDebugger(), url);
 					} catch (Exception e) {
