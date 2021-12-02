7ef648239b0a916d899e92acea71d01dd575993b
unknown <caixiangyi@zoneland.net>
2021-12-02 18:36:30 +0800

    允许数据表格单元格拖入多个组件

--- o2web/source/x_component_process_Xform/$Module.js
+++ o2web/source/x_component_process_Xform/$Module.js
@@ -10,7 +10,7 @@ MWF.xApplication.process.Xform.$Module = MWF.APP$Module =  new Class(
     Implements: [Events],
     options: {
         /**
-         * 组件加载前触发。当前组件的queryLoad事件运行时还没有在form里注册，通过this.form.get("fieldId")不能获取到当前组件，需要用this.target获取。
+         * 组件加载前触发。queryLoad执行的时候，当前组件没有在form里注册，通过this.form.get("fieldId")不能获取到当前组件，需要用this.target获取。
          * @event MWF.xApplication.process.Xform.$Module#queryLoad
          * @event MWF.xApplication.process.Xform.$Module#queryLoad
          * @see {@link https://www.yuque.com/o2oa/ixsnyt/hm5uft#i0zTS|组件事件说明}
