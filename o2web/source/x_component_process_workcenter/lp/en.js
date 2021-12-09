0d437f8f914270e94806534fb9f0b3966c31322d
huqi <hq_1980@hotmail.com>
2021-12-09 18:05:11 +0800

    新版办公中心

---
+++ o2web/source/x_component_process_workcenter/lp/en.js
@@ -0,0 +1,15 @@
+MWF.xApplication.process.workcenter.LP = {
+	"title": "Work Center",
+	"welcome": "Welcome to O2OA Component",
+	"taskListTitle": "Here are your top 5 task",
+
+	"taskTitle": "Title",
+	"taskProcess": "Process",
+	"taskTime": "Time",
+
+	"openCalendar": "Open Calendar",
+	"openOrganization": "Open Organization",
+	"startProcess": "Start Process",
+	"createDocument": "Create Document",
+	"openInBrowser": "Open In Browser"
+};
---
+++ o2web/source/x_component_process_workcenter/lp/zh-cn.js
@@ -0,0 +1,9 @@
+MWF.xApplication.process.workcenter.LP = {
+	"title": "办公中心",
+	"task": "待办",
+	"read": "待阅",
+	"taskCompleted": "已办",
+	"readCompleted": "已阅",
+	"draft": "草稿",
+	"createProcess": "新建流程"
+};
