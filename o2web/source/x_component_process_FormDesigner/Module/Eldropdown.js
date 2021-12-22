2eadf178c8910072dcce62bdf443cce1cd82697f
unknown <caixiangyi@zoneland.net>
2021-12-22 16:39:38 +0800

    eldropdown

---
+++ o2web/source/x_component_process_FormDesigner/Module/Eldropdown.js
@@ -0,0 +1,64 @@
+MWF.xApplication.process.FormDesigner.Module = MWF.xApplication.process.FormDesigner.Module || {};
+MWF.xDesktop.requireApp("process.FormDesigner", "Module.$ElElement", null, false);
+MWF.xApplication.process.FormDesigner.Module.Eldropdown = MWF.FCEldropdown = new Class({
+	Extends: MWF.FC$ElElement,
+	Implements: [Options, Events],
+	options: {
+		"style": "default",
+		"propertyPath": "../x_component_process_FormDesigner/Module/Eldropdown/eldropdown.html"
+	},
+
+	_initModuleType: function(){
+		this.className = "Eldropdown";
+		this.moduleType = "element";
+		this.moduleName = "eldropdown";
+	},
+	_createElementHtml: function(){
+
+		var html = "<el-dropdown";
+		html += " :type=\"selectType\"";
+		html += " :size=\"size\"";
+		html += " :prefix-icon=\"prefixIcon\"";
+		html += " :range-separator=\"rangeSeparator\"";
+		html += " readonly";
+		html += " :placeholder=\"id\"";
+		html += " :start-placeholder=\"id\"";
+		html += " :end-placeholder=\"id\"";
+
+		if (this.json.elProperties){
+			Object.keys(this.json.elProperties).forEach(function(k){
+				if (this.json.elProperties[k]) html += " "+k+"=\""+this.json.elProperties[k]+"\"";
+			}, this);
+		}
+
+		html += " :style=\"elStyles\">";
+		if (this.json.vueSlot) html += this.json.vueSlot;
+		html += "</el-dropdown>";
+		return html;
+	},
+	_createCopyNode: function(){
+		this.copyNode = new Element("div", {
+			"styles": this.css.moduleNodeShow
+		});
+		this.copyNode.addEvent("selectstart", function(){
+			return false;
+		});
+	},
+	_getCopyNode: function(){
+		if (!this.copyNode) this._createCopyNode();
+		this.copyNode.setStyle("display", "inline-block");
+		return this.copyNode;
+	},
+	setPropertyName: function(){
+		// if (this.json.name){
+		// 	var input = this.node.getElement("input");
+		// 	if (input) input.set("value", this.json.name);
+		// }
+	},
+	setPropertyId: function(){
+		// if (!this.json.name){
+		// 	var input = this.node.getElement("input");
+		// 	if (input) input.set("value", this.json.id);
+		// }
+	}
+});
