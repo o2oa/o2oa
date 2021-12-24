2c480145989adc8233e4b7fe2b4af9381245b1e7
o2sword <171715986@qq.com>
2021-12-24 15:31:26 +0800

    定时清除云文件回收站数据

--- o2server/x_file_assemble_control/src/main/java/com/x/file/assemble/control/jaxrs/recycle/ActionEmpty.java
+++ o2server/x_file_assemble_control/src/main/java/com/x/file/assemble/control/jaxrs/recycle/ActionEmpty.java
@@ -7,13 +7,17 @@ import javax.persistence.EntityManager;
 
 import com.x.base.core.container.EntityManagerContainer;
 import com.x.base.core.container.factory.EntityManagerContainerFactory;
+import com.x.base.core.project.config.StorageMapping;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.jaxrs.WrapBoolean;
 import com.x.file.assemble.control.Business;
+import com.x.file.assemble.control.ThisApplication;
+import com.x.file.core.entity.open.OriginFile;
 import com.x.file.core.entity.personal.Attachment2;
 import com.x.file.core.entity.personal.Folder2;
 import com.x.file.core.entity.personal.Recycle;
+import com.x.file.core.entity.personal.Share;
 
 class ActionEmpty extends BaseAction {
 
@@ -24,10 +28,11 @@ class ActionEmpty extends BaseAction {
 			List<Recycle> recycleList = business.recycle().listWithPerson(effectivePerson.getDistinguishedName());
 			if(recycleList!=null && !recycleList.isEmpty()){
 				for(Recycle recycle : recycleList){
-					if("attachment".equals(recycle.getFileType())){
-						EntityManager aem = emc.beginTransaction(Attachment2.class);
-						emc.delete(Attachment2.class, recycle.getFileId());
-						aem.getTransaction().commit();
+					if(Share.FILE_TYPE_ATTACHMENT.equals(recycle.getFileType())){
+						Attachment2 att = emc.find(recycle.getFileId(), Attachment2.class);
+						if(att!=null){
+							this.deleteFile(business, att);
+						}
 					}else{
 						Folder2 folder = emc.find(recycle.getFileId(), Folder2.class);
 						if(folder!=null) {
@@ -37,25 +42,43 @@ class ActionEmpty extends BaseAction {
 							for (int i = ids.size() - 1; i >= 0; i--) {
 								List<Attachment2> attachments = business.attachment2().listWithFolder2(ids.get(i),null);
 								for (Attachment2 att : attachments) {
-									EntityManager aem = emc.beginTransaction(Attachment2.class);
-									aem.remove(att);
-									aem.getTransaction().commit();
+									this.deleteFile(business, att);
 								}
-								EntityManager fem = emc.beginTransaction(Folder2.class);
+								emc.beginTransaction(Folder2.class);
 								emc.delete(Folder2.class, ids.get(i));
-								fem.getTransaction().commit();
+								emc.commit();
 							}
 						}
 					}
-					EntityManager em = emc.beginTransaction(Recycle.class);
+					emc.beginTransaction(Recycle.class);
 					emc.delete(Recycle.class, recycle.getId());
-					em.getTransaction().commit();
+					emc.commit();
 				}
 			}
 			Wo wo = new Wo();
 			wo.setValue(true);
 			result.setData(wo);
 			return result;
+		}
+	}
+
+	private void deleteFile(Business business, Attachment2 att) throws Exception{
+		EntityManagerContainer emc = business.entityManagerContainer();
+		Long count = emc.countEqual(Attachment2.class, Attachment2.originFile_FIELDNAME, att.getOriginFile());
+		if(count.equals(1L)){
+			OriginFile originFile = emc.find(att.getOriginFile(), OriginFile.class);
+			if(originFile!=null){
+				StorageMapping mapping = ThisApplication.context().storageMappings().get(OriginFile.class,
+						originFile.getStorage());
+				if(mapping!=null){
+					originFile.deleteContent(mapping);
+				}
+				emc.beginTransaction(Attachment2.class);
+				emc.beginTransaction(OriginFile.class);
+				emc.remove(att);
+				emc.remove(originFile);
+				emc.commit();
+			}
 		}
 	}
 
