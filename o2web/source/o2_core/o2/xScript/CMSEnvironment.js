55926a68ce84374908c9baa9fad97a79a54cbc6c
huqi <hq_1980@hotmail.com>
2021-12-30 16:53:10 +0800

    优化脚本中的service对象

--- o2web/source/o2_core/o2/xScript/CMSEnvironment.js
+++ o2web/source/o2_core/o2/xScript/CMSEnvironment.js
@@ -1049,10 +1049,10 @@ MWF.xScript.CMSEnvironment = function(ev){
         }
     })();
 
-    this.service = {
-        "jaxwsClient": {},
-        "jaxrsClient":{}
-    };
+    // this.service = {
+    //     "jaxwsClient": {},
+    //     "jaxrsClient":{}
+    // };
 
     var lookupAction = null;
     var getLookupAction = function(callback){
