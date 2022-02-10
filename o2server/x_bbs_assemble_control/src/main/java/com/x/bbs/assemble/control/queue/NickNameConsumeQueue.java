94e2c5a36b76e5faa0d1e8d3b56738bdd20dee38
o2sword <171715986@qq.com>
2022-02-10 21:00:46 +0800

    论坛允许使用昵称发布2

--- o2server/x_bbs_assemble_control/src/main/java/com/x/bbs/assemble/control/queue/NickNameConsumeQueue.java
+++ o2server/x_bbs_assemble_control/src/main/java/com/x/bbs/assemble/control/queue/NickNameConsumeQueue.java
@@ -5,8 +5,16 @@ import com.x.base.core.container.factory.EntityManagerContainerFactory;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
 import com.x.base.core.project.queue.AbstractQueue;
+import com.x.bbs.assemble.control.Business;
+import com.x.bbs.entity.BBSReplyInfo;
+import com.x.bbs.entity.BBSSubjectInfo;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * 批量更新指定用户帖子和回复的昵称
  * @author sword
  */
 public class NickNameConsumeQueue extends AbstractQueue<String> {
@@ -15,9 +23,57 @@ public class NickNameConsumeQueue extends AbstractQueue<String> {
 
 	@Override
 	protected void execute(String person) throws Exception {
+		if(StringUtils.isBlank(person)){
+			return;
+		}
+		String nickName = person;
+		List<String> subjectIds = new ArrayList<>();
+		List<String> replyIds = new ArrayList<>();
 		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
 			logger.debug("change nick name:{}.", person);
+			Business business = new Business(emc);
+			nickName = business.organization().person().getNickName(person);
+			if(nickName.equals(person)){
+				return;
+			}
+			subjectIds = business.subjectInfoFactory().listSubjectIdsByCreator(person);
+			replyIds = business.replyInfoFactory().listReplyIdsByCreator(person);
+		} catch (Exception e){
+			logger.error(e);
+		}
+		for(String id : subjectIds){
+			try {
+				this.updateSubjectNickName(id, nickName);
+			} catch (Exception e) {
+				logger.warn("更新帖子{}的昵称{}异常：{}",id, nickName, e.getMessage());
+			}
+		}
 
+		for(String id : replyIds){
+			try {
+				this.updateReplayNickName(id, nickName);
+			} catch (Exception e) {
+				logger.warn("更新回帖{}的昵称{}异常：{}",id, nickName, e.getMessage());
+			}
+		}
+
+	}
+
+	private void updateSubjectNickName(String id, String nickName) throws Exception{
+		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+			BBSSubjectInfo bbsSubjectInfo = emc.find(id, BBSSubjectInfo.class);
+			emc.beginTransaction(BBSSubjectInfo.class);
+			bbsSubjectInfo.setNickName(nickName);
+			emc.commit();
+		}
+	}
+
+	private void updateReplayNickName(String id, String nickName) throws Exception{
+		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+			BBSReplyInfo bbsReplyInfo = emc.find(id, BBSReplyInfo.class);
+			emc.beginTransaction(BBSReplyInfo.class);
+			bbsReplyInfo.setNickName(nickName);
+			emc.commit();
 		}
 	}
 }
