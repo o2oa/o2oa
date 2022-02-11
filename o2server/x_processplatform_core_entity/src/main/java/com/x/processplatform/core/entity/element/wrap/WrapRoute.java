0cd5399f3751af1bbf5d47034af802002a0509af
o2null <zhourui@o2oa.net>
2022-02-11 05:50:21 +0000

    Merge branch 'fix/properties' into 'develop'
    Fix/properties
    
    See merge request o2oa/o2oa!97
    
    (cherry picked from commit 449ff5b90d062f476e9d868cc9019b1963c23f7e)
    
    573056a1 修正properties通过copyTo的执行顺序
    33db10d2 删除调试

--- o2server/x_processplatform_core_entity/src/main/java/com/x/processplatform/core/entity/element/wrap/WrapRoute.java
+++ o2server/x_processplatform_core_entity/src/main/java/com/x/processplatform/core/entity/element/wrap/WrapRoute.java
@@ -11,8 +11,8 @@ public class WrapRoute extends Route {
 	private static final long serialVersionUID = 6333586002792120317L;
 
 	public static final WrapCopier<Route, WrapRoute> outCopier = WrapCopierFactory.wo(Route.class, WrapRoute.class,
-			null, ListTools.toList(JpaObject.FieldsInvisible, Route.properties_FIELDNAME));
+			null, JpaObject.FieldsInvisible);
 
 	public static final WrapCopier<WrapRoute, Route> inCopier = WrapCopierFactory.wi(WrapRoute.class, Route.class, null,
-			ListTools.toList(JpaObject.FieldsUnmodifyExcludeId, Route.properties_FIELDNAME), false);
+			JpaObject.FieldsUnmodifyExcludeId, false);
 }
