528d1d89f2766d86b3f6ab963ee77a67ace602fe
huqi <hq_1980@hotmail.com>
2022-02-15 16:38:52 +0800

    新版办公中心-2022-02-15

--- o2web/source/x_component_process_workcenter/lp/en.js
+++ o2web/source/x_component_process_workcenter/lp/en.js
@@ -14,5 +14,99 @@ MWF.xApplication.process.workcenter.LP = {
 	"firstPage": "First Page",
 	"lastPage": "Last Page",
 
-	"process": "Flow"
+	"process": "Flow",
+
+
+	"title": "Work Center",
+	"task": "Task",
+	"read": "Read",
+	"taskCompleted": "TaskCompleted",
+	"readCompleted": "ReadCompleted",
+	"draft": "Draft",
+	"createProcess": "Create Process",
+	"all": "All",
+
+	"expire1": "Task deadline: {time}",
+	"expire2": "The task is about to time out. Deadline:{time}",
+	"expire3": "The task has timed out. Deadline:{time}",
+
+	"firstPage": "First Page",
+	"lastPage": "Last Page",
+
+	"process": "Flow",
+
+	"processStarted": "Process Started",
+	"taskProcessed": "文件已提交",
+	"taskProcessedMessage": "您处理了一个待办任务：",
+	"nextActivity": "下一个活动：",
+	"nextUser": "处理人：",
+	"processStartedMessage": "您启动了一个新的工作：",
+	"deal": "处理",
+
+	"processing": "正在处理",
+	"workCompleted": "文件流转完成",
+	"completed": "流转完成",
+	"workProcess": "继续流转",
+	"next_etc": "等{count}人",
+	"processTaskCompleted": "待办已处理",
+	"arrivedActivity": "工作已到达活动：",
+
+	"rapidEditor": "快速处理待办",
+	"setReadCompleted": "设置为已阅",
+	"setReaded" : "设置为已阅",
+	"setReadedConfirmContent" : "您确定要将“{title}”标记为已阅吗？",
+	"setReadedConfirmTitle" : "标记已阅确认",
+
+	"readOpinion": "阅读意见",
+	"processInfo": "流转详情",
+
+	"opinion": "意见",
+	"time": "时间",
+	"starttime": "到达时间",
+
+	"workFlowTo": "文件流转至：",
+	"taskPerson": "处理人：",
+	"open": "打开",
+	"select": "选择",
+	"processActivity": "办理活动",
+	"processActivityInfo": "办理此文件时的活动状态",
+	"readActivity": "待阅活动",
+	"readActivityInfo": "收到待阅时的活动状态",
+
+	"filter": "筛选",
+	"search": "搜索",
+	"filterPlaceholder": "请输入关键字",
+	"commonUseProcess":"常用流程",
+	"searchProcessResault": "”{key}“ 的搜索结果",
+
+	"filterCategoryList": [
+		{"key": "applicationList", "name": "应用名称"},
+		{"key": "processList", "name": "流程名称"},
+		{"key": "activityNameList", "name": "办理活动"},
+		{"key": "creatorUnitList", "name": "创建部门"},
+		{"key": "startTimeMonthList", "name": "收到月份"},
+		{"key": "completedTimeMonthList", "name": "办理月份"},
+		{"key": "completedList", "name": "是否流转完成"},
+		{"key": "key", "name": "关键字"}
+	],
+	"filterCategoryShortList": [
+		{"key": "applicationList", "name": "应用"},
+		{"key": "processList", "name": "流程"},
+		{"key": "activityNameList", "name": "活动"},
+		{"key": "creatorUnitList", "name": "部门"},
+		{"key": "startTimeMonthList", "name": "收到月份"},
+		{"key": "completedTimeMonthList", "name": "办理月份"},
+		{"key": "completedList", "name": "完成"},
+		{"key": "key", "name": "关键字"}
+	],
+	"noTask": "没有要处理的待办",
+	"noTaskCompleted": "没有已处理的文件",
+	"noRead": "没有需要阅读的文件",
+	"noReadCompleted": "没有已经阅读的文件",
+	"noDraft": "没有草稿文件",
+	"createWork": "新建流程文件",
+
+	"batch": "批量处理",
+	"selectBatch": "选中多个相同环节的待办进行批量处理",
+	"cannotSelectBatch": "不同环节的待办不能进行批量处理"
 };
