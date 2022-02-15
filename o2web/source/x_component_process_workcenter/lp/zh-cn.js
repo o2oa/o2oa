8e5ebe02b24a52c809591df67b7f972b82912f1c
huqi <hq_1980@hotmail.com>
2022-02-15 17:54:25 +0800

    Merge branch 'feature/work_center' into develop
    (cherry picked from commit be8fbe0b95fec06017acef29d35b57cef6920626)
    
    0d964891 新版办公中心
    0d437f8f 新版办公中心
    709d215a 新版办公中心
    68a7410e 新版办公中心
    09de137e 新版办公中心
    859d6411 新版办公中心
    df915c13 新版办公中心
    0c0aabb8 新版办公中心
    319d2021 新版办公中心
    ade39f9d 新版办公中心
    3a474a9d 新版办公中心
    5a95a8fb 新版办公中心
    b4ff7661 新版办公中心
    fda347e6 新版办公中心
    8044c8ea 新版办公中心
    56b27481 新版办公中心
    c0dcca01 新版办公中心
    99e676e6 新版办公中心-2022-01-15
    bcdfafa1 新版办公中心-2022-01-15
    5a587e51 新版办公中心-2022-01-26
    3a85a995 新版办公中心-2022-01-27
    fa752e3d 新版办公中心-2022-02-07
    c16b583f 修复前端自定义component，使用vue或react打包后，css中引用资源的路径错误
    ada049fd 新版办公中心-2022-02-08
    d20dc5f8 新版办公中心-2022-02-09
    402c0a37 新版办公中心-2022-02-10
    ffb3f7b3 新版办公中心-2022-02-11
    253a5c34 新版办公中心-2022-02-11
    9b6008e5 新版办公中心2022-02-13
    2c55284b 新版办公中心-2022-02-14
    fb596aaa 新版办公中心2022-02-15
    d8c17efa 新版办公中心-2022-02-15
    5660fbb2 新版办公中心-2022-02-15
    9784870b 新版办公中心,英文语言包
    00346f8d 新版办公中心,切换新版提示

---
+++ o2web/source/x_component_process_workcenter/lp/zh-cn.js
@@ -0,0 +1,95 @@
+MWF.xApplication.process.workcenter.LP = {
+	"title": "办公中心",
+	"task": "待办",
+	"read": "待阅",
+	"taskCompleted": "已办",
+	"readCompleted": "已阅",
+	"draft": "草稿",
+	"createProcess": "新建流程",
+	"all": "全部",
+
+	"expire1": "任务截至时间：{time}",
+	"expire2": "任务即将超时，截至时间：{time}",
+	"expire3": "任务已超时，截至时间：{time}",
+
+	"firstPage": "第一页",
+	"lastPage": "最后一页",
+
+	"process": "继续流转",
+
+	"processStarted": "文件已启动",
+	"taskProcessed": "文件已提交",
+	"taskProcessedMessage": "您处理了一个待办任务：",
+	"nextActivity": "下一个活动：",
+	"nextUser": "处理人：",
+	"processStartedMessage": "您启动了一个新的工作：",
+	"deal": "处理",
+
+	"processing": "正在处理",
+	"workCompleted": "文件流转完成",
+	"completed": "流转完成",
+	"workProcess": "继续流转",
+	"next_etc": "等{count}人",
+	"processTaskCompleted": "待办已处理",
+	"arrivedActivity": "工作已到达活动：",
+
+	"rapidEditor": "快速处理待办",
+	"setReadCompleted": "设置为已阅",
+	"setReaded" : "设置为已阅",
+	"setReadedConfirmContent" : "您确定要将“{title}”标记为已阅吗？",
+	"setReadedConfirmTitle" : "标记已阅确认",
+
+	"readOpinion": "阅读意见",
+	"processInfo": "流转详情",
+
+	"opinion": "意见",
+	"time": "时间",
+	"starttime": "到达时间",
+
+	"workFlowTo": "文件流转至：",
+	"taskPerson": "处理人：",
+	"open": "打开",
+	"select": "选择",
+	"processActivity": "办理活动",
+	"processActivityInfo": "办理此文件时的活动状态",
+	"readActivity": "待阅活动",
+	"readActivityInfo": "收到待阅时的活动状态",
+
+	"filter": "筛选",
+	"search": "搜索",
+	"filterPlaceholder": "请输入关键字",
+	"commonUseProcess":"常用流程",
+	"searchProcessResault": "”{key}“ 的搜索结果",
+
+	"filterCategoryList": [
+		{"key": "applicationList", "name": "应用名称"},
+		{"key": "processList", "name": "流程名称"},
+		{"key": "activityNameList", "name": "办理活动"},
+		{"key": "creatorUnitList", "name": "创建部门"},
+		{"key": "startTimeMonthList", "name": "收到月份"},
+		{"key": "completedTimeMonthList", "name": "办理月份"},
+		{"key": "completedList", "name": "是否流转完成"},
+		{"key": "key", "name": "关键字"}
+	],
+	"filterCategoryShortList": [
+		{"key": "applicationList", "name": "应用"},
+		{"key": "processList", "name": "流程"},
+		{"key": "activityNameList", "name": "活动"},
+		{"key": "creatorUnitList", "name": "部门"},
+		{"key": "startTimeMonthList", "name": "收到月份"},
+		{"key": "completedTimeMonthList", "name": "办理月份"},
+		{"key": "completedList", "name": "完成"},
+		{"key": "key", "name": "关键字"}
+	],
+	"noTask": "没有要处理的待办",
+	"noTaskCompleted": "没有已处理的文件",
+	"noRead": "没有需要阅读的文件",
+	"noReadCompleted": "没有已经阅读的文件",
+	"noDraft": "没有草稿文件",
+	"createWork": "新建流程文件",
+
+	"batch": "批量处理",
+	"selectBatch": "选中多个相同环节的待办进行批量处理",
+	"cannotSelectBatch": "不同环节的待办不能进行批量处理"
+
+};
