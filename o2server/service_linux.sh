5deb6a363aa2a7dcb1c05903d61b4c7a34bb876f
Ray <zhourui@o2oa.net>
2022-02-23 20:51:06 +0800

    fix service_linux.sh

--- o2server/service_linux.sh
+++ o2server/service_linux.sh
@@ -3,29 +3,29 @@ current_dir="$(
     pwd
 )"
 if [ -z "$1" ]; then
-    echo "usage: ./service_linux_install.sh name [start.sh default is start_linux.sh]"
+    echo "usage: ./service_linux.sh name [start.sh default is start_linux.sh]"
     exit
 fi
 name="$1"
-scriptFile="start_linux.sh"
+scriptName="start_linux.sh"
 if [ -z "$2" ]; then
-    echo "use ${current_dir}/${scriptFile} as start script."
+    echo "use ${current_dir}/${scriptName} as start script."
 else
-    scriptFile="$2"
+    scriptName="$2"
 fi
-if [ ! -f ${current_dir}/${scriptFile} ]; then
-    echo "start script ${current_dir}/${scriptFile} not exist."
+if [ ! -f ${current_dir}/${scriptName} ]; then
+    echo "start script ${current_dir}/${scriptName} not exist."
     exit
 fi
-servicefile="/etc/systemd/system/${name}.service"
-echo "[Unit]" >${servicefile}
-echo "Description=o2server name:${name}" >>${servicefile}
-echo "Wants=network-online.target" >>${servicefile}
-echo "After=network.target" >>${servicefile}
-echo "[Service]" >>${servicefile}
-echo "Type=simple" >>${servicefile}
-echo "ExecStart=${current_dir}/${servicefile}" >>${servicefile}
-echo "ExecReload=${current_dir}/restart_linux.sh" >>${servicefile}
-echo "ExecStop=${current_dir}/stop_linux.sh" >>${servicefile}
-echo "[Install]" >>${servicefile}
-echo "WantedBy=multi-user.target" >>${servicefile}
+servicePath="/etc/systemd/system/${name}.service"
+echo "[Unit]" >${servicePath}
+echo "Description=o2server name:${name}" >>${servicePath}
+echo "Wants=network-online.target" >>${servicePath}
+echo "After=network.target" >>${servicePath}
+echo "[Service]" >>${servicePath}
+echo "Type=simple" >>${servicePath}
+echo "ExecStart=${current_dir}/${scriptName}" >>${servicePath}
+echo "ExecReload=${current_dir}/restart_linux.sh" >>${servicePath}
+echo "ExecStop=${current_dir}/stop_linux.sh" >>${servicePath}
+echo "[Install]" >>${servicePath}
+echo "WantedBy=multi-user.target" >>${servicePath}
