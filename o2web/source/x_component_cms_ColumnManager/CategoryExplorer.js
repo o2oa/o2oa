4e6aedc1d343ccd0c538da33bdc01c337f66b27b
unknown <caixiangyi@zoneland.net>
2022-03-02 11:27:34 +0800

    文档发布范围为空时是否全员通

--- o2web/source/x_component_cms_ColumnManager/CategoryExplorer.js
+++ o2web/source/x_component_cms_ColumnManager/CategoryExplorer.js
@@ -782,6 +782,13 @@ MWF.xApplication.cms.ColumnManager.CategoryExplorer.Category = new Class({
             this.app.notice( MWF.xApplication.cms.ColumnManager.LP.setSendNotifySuccess );
         }.bind(this))
     },
+    setBlankToAllNotify: function( value ){
+        var d = this.data;
+        d.blankToAllNotify = value;
+        this.app.restActions.saveCategory(  d, function( json ){
+            this.app.notice( MWF.xApplication.cms.ColumnManager.LP.setBlankToAllNotifySuccess );
+        }.bind(this))
+    },
     setEditForm : function( formId, formName ){
         var d = this.data;
         d.formId = formId;
@@ -2665,6 +2672,7 @@ MWF.xApplication.cms.ColumnManager.CategoryExplorer.CategoryProperty = new Class
         //     html += "<tr><td class='formTitle'>"+this.app.lp.application.icon+"</td><td id='formApplicationIcon'></td></tr>";
         html += "<tr><td class='formTitle'>"+this.app.lp.category.excelImportView +"</td><td class='formValue'><div id='formImportViewId' style='cursor:pointer;width:90%;min-height:22px;overflow:hidden;border:1px solid #ccc;border-radius: 3px;'></div></td></td></tr>"; //(this.category.data.documentType || "" )+
         html += "<tr><td class='formTitle'>"+this.app.lp.category.sendNotify +"</td><td id='formCategorySendNotify' class='formValue'></td></tr>"; //"+this.category.data.categoryAlias+"
+        html += "<tr><td class='formTitle'>"+this.app.lp.category.blankToAllNotify +"</td><td id='formCategoryBlankToAllNotify' class='formValue'></td></tr>"; //"+this.category.data.categoryAlias+"
         html += "</table>";
         this.propertyContentNode.set("html", html);
         this.propertyContentNode.getElements("td.formTitle").setStyles(this.app.css.propertyBaseContentTdTitle);
@@ -2706,6 +2714,22 @@ MWF.xApplication.cms.ColumnManager.CategoryExplorer.CategoryProperty = new Class
             }
         });
         this.sendNotify.load();
+
+        var blankToAllNotifyValue = o2.typeOf(this.category.data.blankToAllNotify) === "boolean" ? this.category.data.blankToAllNotify : true;
+        this.blankToAllNotify = new MDomItem( this.propertyContentNode.getElement("#formCategoryBlankToAllNotify"), {
+            type : "select",
+            style: this.app.css.processSelect,
+            value : blankToAllNotifyValue.toString(),
+            selectValue : lp.blankToAllNotifySelectValue,
+            selectText: lp.blankToAllNotifySelectText,
+            event : {
+                change : function( item ){
+                    this.category.setBlankToAllNotify( item.getValue() !== "false" );
+                }.bind(this)
+            }
+        });
+        this.blankToAllNotify.load();
+
 
         var value = this.category.data.importViewId ? [{
             id : this.category.data.importViewId,
