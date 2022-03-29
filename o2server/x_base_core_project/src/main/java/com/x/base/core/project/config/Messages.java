cdee5b1c288c17c29ad27fbbd703332eedf8aa44
Ray <zhourui@o2oa.net>
2022-03-29 10:41:42 +0800

    update max thread pool

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/config/Messages.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/config/Messages.java
@@ -14,153 +14,130 @@ public class Messages extends ConcurrentSkipListMap<String, Message> {
 
 	public static final Boolean DEFAULT_WEBSOCKETENABLE = true;
 
-	public Messages() throws Exception {
+	public Messages() {
 		super();
 	}
 
-	public static Messages defaultInstance() throws Exception {
+	public static Messages defaultInstance() {
 		Messages o = new Messages();
-
-		/* 示例 */
-		Map<String,String> map = new HashMap<>();
+		// 示例
+		Map<String, String> map = new HashMap<>();
 		map.put(MessageConnector.CONSUME_QIYEWEIXIN, "excute");
-		map.put("describe","excute表示脚本messageSendRule.js中的方法名称，该js文件需放在与messages.json同目录下，更改脚本需重启服务");
+		map.put("describe", "excute表示脚本messageSendRule.js中的方法名称，该js文件需放在与messages.json同目录下，更改脚本需重启服务");
 		o.put("##sample##", new Message(map));
-
-		/* 文件通知 */
+		// 文件通知
 		o.put(MessageConnector.TYPE_ATTACHMENT_SHARE,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
 						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
-						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,MessageConnector.CONSUME_MQ));
-
+						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,
+						MessageConnector.CONSUME_MQ));
 		o.put(MessageConnector.TYPE_ATTACHMENT_EDITOR,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
 						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
-						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,MessageConnector.CONSUME_MQ));
+						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,
+						MessageConnector.CONSUME_MQ));
 
 		o.put(MessageConnector.TYPE_ATTACHMENT_SHARECANCEL,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
 						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
-						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,MessageConnector.CONSUME_MQ));
+						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,
+						MessageConnector.CONSUME_MQ));
 
 		o.put(MessageConnector.TYPE_ATTACHMENT_EDITORCANCEL,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
 						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
-						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,MessageConnector.CONSUME_MQ));
+						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,
+						MessageConnector.CONSUME_MQ));
 
 		o.put(MessageConnector.TYPE_ATTACHMENT_EDITORMODIFY,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
 						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
-						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,MessageConnector.CONSUME_MQ));
-		/* 文件通知结束 */
-
-		/* 会议通知 */
+						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,
+						MessageConnector.CONSUME_MQ));
+		// 会议通知
 		o.put(MessageConnector.TYPE_MEETING_INVITE,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
 						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
-						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,MessageConnector.CONSUME_MQ));
-
+						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,
+						MessageConnector.CONSUME_MQ));
 		o.put(MessageConnector.TYPE_MEETING_DELETE,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
 						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
-						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,MessageConnector.CONSUME_MQ));
-		/* 会议通知结束 */
+						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,
+						MessageConnector.CONSUME_MQ));
+		// 待办已办通知
+		o.put(MessageConnector.TYPE_TASK_CREATE, new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
+				MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
+				MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK, MessageConnector.CONSUME_MQ));
 
-		/* 待办已办通知 */
-		o.put(MessageConnector.TYPE_TASK_CREATE,
-				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
-						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
-						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,MessageConnector.CONSUME_MQ));
-
-		/* 待办转已办通知 */
+		// 待办转已办通知
 		o.put(MessageConnector.TYPE_TASK_TO_TASKCOMPLETED,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
 						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
-						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,MessageConnector.CONSUME_MQ));
-
-		/* 待办提醒通知 */
-		o.put(MessageConnector.TYPE_TASK_PRESS,
-				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
-						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
-						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,MessageConnector.CONSUME_MQ));
+						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,
+						MessageConnector.CONSUME_MQ));
+		// 待办提醒通知
+		o.put(MessageConnector.TYPE_TASK_PRESS, new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
+				MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
+				MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK, MessageConnector.CONSUME_MQ));
 
 		o.put(MessageConnector.TYPE_TASK_DELETE, new Message());
 
 		o.put(MessageConnector.TYPE_TASKCOMPLETED_CREATE, new Message());
 
 		o.put(MessageConnector.TYPE_TASKCOMPLETED_DELETE, new Message());
-		/* 待办已办通知结束 */
-
-		/* 待阅已阅通知 */
-		o.put(MessageConnector.TYPE_READ_CREATE,
-				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
-						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
-						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,MessageConnector.CONSUME_MQ));
-
-		/* 待阅转已阅通知 */
+		// 待阅已阅通知
+		o.put(MessageConnector.TYPE_READ_CREATE, new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
+				MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
+				MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK, MessageConnector.CONSUME_MQ));
+		// 待阅转已阅通知
 		o.put(MessageConnector.TYPE_READ_TO_READCOMPLETED,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
 						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
-						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,MessageConnector.CONSUME_MQ));
-
+						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK,
+						MessageConnector.CONSUME_MQ));
 		o.put(MessageConnector.TYPE_READ_DELETE, new Message());
-
 		o.put(MessageConnector.TYPE_READCOMPLETED_CREATE, new Message());
-
 		o.put(MessageConnector.TYPE_READCOMPLETED_DELETE, new Message());
-		/* 待阅已阅通知结束 */
-
-		/* 日程管理消息通知 */
+		// 日程管理消息通知
 		o.put(MessageConnector.TYPE_CALENDAR_ALARM,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS,
 						MessageConnector.CONSUME_DINGDING, MessageConnector.CONSUME_ZHENGWUDINGDING,
 						MessageConnector.CONSUME_QIYEWEIXIN, MessageConnector.CONSUME_WELINK));
-
-		/* 文档发布消息通知 */
-//		o.put(MessageConnector.TYPE_CMS_PUBLISH,
-//				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS ));
-
-		/* 社区新贴发布消息通知 */
+		// 社区新贴发布消息通知
 		o.put(MessageConnector.TYPE_BBS_SUBJECTCREATE,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS));
-
-		/* 社区新回复发布消息通知 */
+		// 社区新回复发布消息通知
 		o.put(MessageConnector.TYPE_BBS_REPLYCREATE,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS));
-		/* 脑图分享消息通知 */
+		// 脑图分享消息通知
 		o.put(MessageConnector.TYPE_MIND_FILESHARE,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS));
-
-		/* 脑图发送消息通知 */
+		// 脑图发送消息通知
 		o.put(MessageConnector.TYPE_MIND_FILESEND,
 				new Message(MessageConnector.CONSUME_WS, MessageConnector.CONSUME_PMS));
-
-		/* im聊天消息发送 */
-		o.put(MessageConnector.TYPE_IM_CREATE,
-				new Message(MessageConnector.CONSUME_WS));
-
+		// im聊天消息发送
+		o.put(MessageConnector.TYPE_IM_CREATE, new Message(MessageConnector.CONSUME_WS));
 		return o;
 	}
 
 	public List<String> getConsumers(String type) {
 		Message o = this.get(type);
 		if (o != null) {
-			/* 这里必须复制内容,在消息处理中会对列表进行删除操作 */
+			// 这里必须复制内容,在消息处理中会对列表进行删除操作
 			List<String> list = new ArrayList<>();
 			list.addAll(o.getConsumers());
 			return list;
 		}
-		return new ArrayList<String>();
+		return new ArrayList<>();
 	}
 
-	public Map<String,String> getConsumersV2(String type) {
+	public Map<String, String> getConsumersV2(String type) {
 		Message o = this.get(type);
-		Map<String,String> map = new HashMap<>();
-		if (o != null) {
-			/* 这里必须复制内容,在消息处理中会对列表进行删除操作 */
-			if(o.getConsumersV2()!=null){
-				map.putAll(o.getConsumersV2());
-			}
+		Map<String, String> map = new HashMap<>();
+		// 这里必须复制内容,在消息处理中会对列表进行删除操作
+		if ((o != null) && (o.getConsumersV2() != null)) {
+			map.putAll(o.getConsumersV2());
 		}
 		return map;
 	}
