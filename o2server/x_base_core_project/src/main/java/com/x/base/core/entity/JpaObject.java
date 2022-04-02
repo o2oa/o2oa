45f4c6ce19bbed0062265f0d976fbbf2d93cb9ae
Ray <zhourui@o2oa.net>
2022-04-02 18:43:29 +0800

    meta data check

--- o2server/x_base_core_project/src/main/java/com/x/base/core/entity/JpaObject.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/entity/JpaObject.java
@@ -230,7 +230,6 @@ public abstract class JpaObject extends GsonPropertyObject implements Serializab
 	public static final String TYPE_FLOATLIST = "floatList";
 	public static final String TYPE_DOUBLELIST = "doubleList";
 	public static final String TYPE_DATETIMELIST = "dateTimeList";
-	public static final String TYPE_DATELIST = "dateList";
 	public static final String TYPE_BOOLEANLIST = "booleanList";
 
 	public static final String TYPE_STRINGLOB = "stringLob";
@@ -241,7 +240,6 @@ public abstract class JpaObject extends GsonPropertyObject implements Serializab
 	public static final String TYPE_FLOATMAP = "floatMap";
 	public static final String TYPE_DOUBLEMAP = "doubleMap";
 	public static final String TYPE_DATETIMEMAP = "dateTimeMap";
-	public static final String TYPE_DATEMAP = "dateMap";
 	public static final String TYPE_BOOLEANMAP = "booleanMap";
 
 	public static final String[] ID_DISTRIBUTEFACTOR = new String[] { id_FIELDNAME, distributeFactor_FIELDNAME };
