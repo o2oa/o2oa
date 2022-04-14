d7b8cf7254845aca7aa44f1b277cd3595685060f
Ray <zhourui@o2oa.net>
2022-04-14 10:52:33 +0800

    update custom message

--- o2server/x_message_assemble_communicate/src/main/java/com/x/message/assemble/communicate/MPWeixinConsumeQueue.java
+++ o2server/x_message_assemble_communicate/src/main/java/com/x/message/assemble/communicate/MPWeixinConsumeQueue.java
@@ -30,7 +30,7 @@ import com.x.organization.core.entity.Person;
  */
 public class MPWeixinConsumeQueue extends AbstractQueue<Message> {
 
-	private static Logger logger = LoggerFactory.getLogger(MPWeixinConsumeQueue.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(MPWeixinConsumeQueue.class);
 
 	private static final Gson gson = XGsonBuilder.instance();
 
@@ -42,95 +42,119 @@ public class MPWeixinConsumeQueue extends AbstractQueue<Message> {
 		List<MPweixinMessageTemp> list = Config.mPweixin().getFieldList();
 		if (Config.mPweixin().getEnable() && Config.mPweixin().getMessageEnable() && StringUtils.isNotEmpty(tempId)
 				&& (list != null && !list.isEmpty())) {
-			try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-				Business business = new Business(emc);
-				// openid 查询用户
-				Person person = business.message().getPersonWithCredential(message.getPerson());
-				if (person != null) {
-					String openId = person.getMpwxopenId();
-					logger.info("openId : " + openId);
-					if (StringUtils.isNotEmpty(openId)) {
-						JsonObject object = gson.fromJson(message.getBody(), JsonObject.class);
-						Map<String, WeixinTempMessageFieldObj> data = new HashMap<>();
-						WeixinTempMessageFieldObj wobj = new WeixinTempMessageFieldObj();
-						wobj.setValue(message.getTitle());
-						data.put("first", wobj);
-						for (int i = 0; i < list.size(); i++) {
-							MPweixinMessageTemp filed = list.get(i);
-							String name = filed.getName();
-							String tempName = filed.getTempName();
-							String value = object.get(name).getAsString();
-							logger.info("解析出的结果 name：" + name + " value:" + value + "  放入tempName：" + tempName);
-							if ("title".equalsIgnoreCase(name)) { // 工作标题为空就用消息的标题
-								if (StringUtils.isEmpty(value)) {
-									value = "无标题";
-								}
-							} else {
-								if ("creatorPerson".equalsIgnoreCase(name) && StringUtils.isNotEmpty(value)) {
-									value = value.split("@")[0]; // 截取姓名
-								}
-								if (StringUtils.isEmpty(value)) {
-									value = "unknown";
-								}
-							}
-							WeixinTempMessageFieldObj obj = new WeixinTempMessageFieldObj();
-							obj.setValue(value);
-							obj.setColor("#173177");
-							data.put(tempName, obj);
-						}
-						WeixinTempMessageFieldObj robj = new WeixinTempMessageFieldObj();
-						robj.setValue("请注意查收！");
-						data.put("remark", robj);
-						String workId = object.get("work").getAsString();
-						String workUrl = getOpenUrl(workId);
-						WeixinTempMessage wxMessage = new WeixinTempMessage();
-						wxMessage.setTouser(openId);
-						wxMessage.setUrl(workUrl);
-						wxMessage.setTemplate_id(tempId);
-						wxMessage.setTopcolor("#fb4747");
-						wxMessage.setData(data);
-						logger.info("发送的消息对象：" + wxMessage.toString());
-						String url = MPweixin.default_apiAddress + "/cgi-bin/message/template/send?access_token="
-								+ Config.mPweixin().accessToken();
-						WeixinResponse response = HttpConnection.postAsObject(url, null, wxMessage.toString(),
-								WeixinResponse.class);
-						logger.info("返回：" + response);
-						if (response.getErrcode() != 0) {
-							ExceptionQiyeweixinMessage e = new ExceptionQiyeweixinMessage(response.getErrcode(),
-									response.getErrmsg());
-							logger.error(e);
-						} else {
-							Message messageEntityObject = emc.find(message.getId(), Message.class);
-							if (null != messageEntityObject) {
-								emc.beginTransaction(Message.class);
-								messageEntityObject.setConsumed(true);
-								emc.commit();
-							}
-						}
+			Person person = getPerson(message.getPerson());
+			if (person != null) {
+				String openId = person.getMpwxopenId();
+				LOGGER.debug("openId:{}.", () -> openId);
+				if (StringUtils.isNotEmpty(openId)) {
+					JsonObject object = gson.fromJson(message.getBody(), JsonObject.class);
+					Map<String, WeixinTempMessageFieldObj> data = concreteData(message, object, list);
+					String workId = object.get("work").getAsString();
+					String workUrl = getOpenUrl(workId);
+					WeixinTempMessage wxMessage = new WeixinTempMessage();
+					wxMessage.setTouser(openId);
+					wxMessage.setUrl(workUrl);
+					wxMessage.setTemplate_id(tempId);
+					wxMessage.setTopcolor("#fb4747");
+					wxMessage.setData(data);
+					LOGGER.debug("发送的消息对象:{}.", wxMessage::toString);
+					String url = MPweixin.default_apiAddress + "/cgi-bin/message/template/send?access_token="
+							+ Config.mPweixin().accessToken();
+					WeixinResponse response = HttpConnection.postAsObject(url, null, wxMessage.toString(),
+							WeixinResponse.class);
+					LOGGER.debug("返回:{}.", () -> response);
+					if (response.getErrcode() != 0) {
+						ExceptionQiyeweixinMessage e = new ExceptionQiyeweixinMessage(response.getErrcode(),
+								response.getErrmsg());
+						LOGGER.error(e);
 					} else {
-						logger.info("没有绑定微信公众号 ：" + message.getPerson());
+						success(message.getId());
 					}
 				} else {
-					logger.info("没有找到用户！");
+					LOGGER.warn("没有绑定微信公众号:{}.", message::getPerson);
 				}
-
+			} else {
+				LOGGER.warn("没有找到用户.");
 			}
+
 		} else {
-			logger.info("配置文件配置条件不足！！");
+			LOGGER.warn("配置文件配置条件不足.");
 		}
 
+	}
+
+	private Map<String, WeixinTempMessageFieldObj> concreteData(Message message, JsonObject object,
+			List<MPweixinMessageTemp> list) {
+		Map<String, WeixinTempMessageFieldObj> data = new HashMap<>();
+		WeixinTempMessageFieldObj wobj = new WeixinTempMessageFieldObj();
+		wobj.setValue(message.getTitle());
+		data.put("first", wobj);
+		for (int i = 0; i < list.size(); i++) {
+			MPweixinMessageTemp filed = list.get(i);
+			String name = filed.getName();
+			String tempName = filed.getTempName();
+			String value = object.get(name).getAsString();
+			LOGGER.debug("解析出的结果 name：{}, value:{}, tempName:{}.", name, value, tempName);
+			if ("title".equalsIgnoreCase(name)) { // 工作标题为空就用消息的标题
+				if (StringUtils.isEmpty(value)) {
+					value = "无标题";
+				}
+			} else {
+				if ("creatorPerson".equalsIgnoreCase(name) && StringUtils.isNotEmpty(value)) {
+					value = value.split("@")[0]; // 截取姓名
+				}
+				if (StringUtils.isEmpty(value)) {
+					value = "unknown";
+				}
+			}
+			WeixinTempMessageFieldObj obj = new WeixinTempMessageFieldObj();
+			obj.setValue(value);
+			obj.setColor("#173177");
+			data.put(tempName, obj);
+		}
+		WeixinTempMessageFieldObj robj = new WeixinTempMessageFieldObj();
+		robj.setValue("请注意查收！");
+		data.put("remark", robj);
+		return data;
+	}
+
+	private Person getPerson(String credential) throws Exception {
+		Person person;
+		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+			Business business = new Business(emc);
+			// openid 查询用户
+			person = business.message().getPersonWithCredential(credential);
+		}
+		return person;
+	}
+
+	/**
+	 * 标志消息消费成功
+	 * 
+	 * @param id
+	 * @throws Exception
+	 */
+	private void success(String id) throws Exception {
+		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+			Message message = emc.find(id, Message.class);
+			if (null != message) {
+				emc.beginTransaction(Message.class);
+				message.setConsumed(true);
+				emc.commit();
+			}
+		}
 	}
 
 	private String getOpenUrl(String workId) {
 		try {
 			String httpProtocol = Config.currentNode().getCenter().getHttpProtocol();
 			if (StringUtils.isEmpty(httpProtocol)) {
-				logger.error(new Exception("没有获取到http访问协议"));
+				LOGGER.error(new Exception("没有获取到http访问协议"));
 				return null;
 			}
 			String host = Config.currentNode().getWeb().getProxyHost();
 			if (StringUtils.isEmpty(host)) {
-				logger.error(new Exception("没有获取到代理地址"));
+				LOGGER.error(new Exception("没有获取到代理地址"));
 				return null;
 			}
 			String workUrl = "workmobilewithaction.html?workid=" + workId;
@@ -141,16 +165,14 @@ public class MPWeixinConsumeQueue extends AbstractQueue<Message> {
 			workUrl = URLEncoder.encode(workUrl, DefaultCharset.name);
 			String o2oaUrl = httpProtocol + "://" + host + "/x_desktop/";
 			o2oaUrl = o2oaUrl + "mpweixinsso.html?redirect=" + workUrl + "&type=login";
-			logger.info("o2oa 地址：" + o2oaUrl);
 			o2oaUrl = URLEncoder.encode(o2oaUrl, DefaultCharset.name);
-			logger.info("encode url :" + o2oaUrl);
 			String appId = Config.mPweixin().getAppid();
 			String url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appId + "&redirect_uri="
 					+ o2oaUrl + "&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
-			logger.info("final url :" + url);
+			LOGGER.debug("final url:{}.", () -> url);
 			return url;
 		} catch (Exception e) {
-			logger.error(e);
+			LOGGER.error(e);
 			return null;
 		}
 
