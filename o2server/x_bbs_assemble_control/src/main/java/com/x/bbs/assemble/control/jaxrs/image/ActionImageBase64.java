33b8250aea4e7de46d46504c0017e937e519608f
o2sword <171715986@qq.com>
2022-08-01 10:30:02 +0800

    论坛图片转base64接口问题修复

--- o2server/x_bbs_assemble_control/src/main/java/com/x/bbs/assemble/control/jaxrs/image/ActionImageBase64.java
+++ o2server/x_bbs_assemble_control/src/main/java/com/x/bbs/assemble/control/jaxrs/image/ActionImageBase64.java
@@ -1,18 +1,5 @@
 package com.x.bbs.assemble.control.jaxrs.image;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Optional;
-
-import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.codec.binary.Base64;
-import org.imgscalr.Scalr;
-
 import com.google.gson.JsonElement;
 import com.x.base.core.project.annotation.FieldDescribe;
 import com.x.base.core.project.cache.Cache;
@@ -23,43 +10,36 @@ import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
 import com.x.bbs.assemble.control.jaxrs.image.exception.ExceptionURLEmpty;
-import com.x.bbs.assemble.control.jaxrs.image.exception.ExceptionWrapInConvert;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+import org.imgscalr.Scalr;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.net.URL;
+import java.util.Optional;
+
+/**
+ * 图片转base64
+ * @author sword
+ */
 public class ActionImageBase64 extends BaseAction {
 
 	private static Logger logger = LoggerFactory.getLogger(ActionImageBase64.class);
-	private String catchNamePrefix = this.getClass().getName();
 
 	protected ActionResult<String> execute(HttpServletRequest request, EffectivePerson effectivePerson,
 			JsonElement jsonElement) throws Exception {
 		ActionResult<String> result = new ActionResult<>();
-		Wi wrapIn = null;
-		EffectivePerson currentPerson = this.effectivePerson(request);
-		String wrap = null;
-		URL url = null;
-		BufferedImage image = null;
-		Boolean check = true;
+		Wi wrapIn = this.convertToWrapIn(jsonElement, Wi.class);
+		String wrap;
 
-		try {
-			wrapIn = this.convertToWrapIn(jsonElement, Wi.class);
-		} catch (Exception e) {
-			check = false;
-			Exception exception = new ExceptionWrapInConvert(e, jsonElement);
-			result.error(exception);
-			logger.error(e, currentPerson, request, null);
+		if(StringUtils.isBlank(wrapIn.getUrl())){
+			throw new ExceptionURLEmpty();
 		}
-
-		if (check) {
-			if (wrapIn.getUrl() != null || wrapIn.getUrl().isEmpty()) {
-				check = false;
-				Exception exception = new ExceptionURLEmpty();
-				result.error(exception);
-			}
-		}
-		if (check) {
-			if (wrapIn.getSize() != null || wrapIn.getSize() == 0) {
-				wrapIn.setSize(800);
-			}
+		if(wrapIn.getSize() == null || wrapIn.getSize() < 1){
+			wrapIn.setSize(800);
 		}
 
 		Cache.CacheKey cacheKey = new Cache.CacheKey( this.getClass(), wrapIn.getUrl(), wrapIn.getSize());
@@ -68,49 +48,18 @@ public class ActionImageBase64 extends BaseAction {
 			wrap = (String) optional.get();
 			result.setData(wrap);
 		} else {
-			if (check) {
-				try {
-					url = new URL(wrapIn.getUrl());
-				} catch (MalformedURLException e) {
-					check = false;
-					Exception exception = new ExceptionURLEmpty(e, wrapIn.getUrl());
-					result.error(exception);
-					logger.error(e, currentPerson, request, null);
-				}
+			URL url = new URL(wrapIn.getUrl());
+			BufferedImage image = ImageIO.read(url);
+			int width = image.getWidth();
+			int height = image.getHeight();
+			if (width * height > wrapIn.getSize() * wrapIn.getSize()) {
+				image = Scalr.resize(image, wrapIn.getSize());
 			}
-			if (check) {
-				try {
-					image = ImageIO.read(url);
-					if (image == null) {
-						check = false;
-						result.error(new Exception("system can not read image in url."));
-					}
-				} catch (IOException e) {
-					check = false;
-					result.error(e);
-					logger.warn("system read picture with url got an exception!url:" + url);
-					logger.error(e);
-				}
-			}
-			if (check) {
-				int width = image.getWidth();
-				int height = image.getHeight();
-				if (width * height > wrapIn.getSize() * wrapIn.getSize()) {
-					image = Scalr.resize(image, wrapIn.getSize());
-				}
-			}
-			if (check) {
-				try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-					ImageIO.write(image, "png", baos);
-					wrap = Base64.encodeBase64String(baos.toByteArray());
-					CacheManager.put( cacheCategory, cacheKey, wrap );
-					result.setData(wrap);
-				} catch (Exception e) {
-					check = false;
-					result.error(e);
-					logger.warn("system encode picture in base64 got an exception!");
-					logger.error(e);
-				}
+			try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+				ImageIO.write(image, "png", out);
+				wrap = Base64.encodeBase64String(out.toByteArray());
+				CacheManager.put( cacheCategory, cacheKey, wrap );
+				result.setData(wrap);
 			}
 		}
 		return result;
