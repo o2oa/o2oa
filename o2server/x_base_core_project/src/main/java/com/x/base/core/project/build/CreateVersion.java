7b64118a5dbc6a072b224babfa324e1083d4b0e3
zhourui <zhourui@o2oa.net>
2022-08-03 16:52:00 +0800

    update version with git.

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/build/CreateVersion.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/build/CreateVersion.java
@@ -2,33 +2,60 @@ package com.x.base.core.project.build;
 
 import java.io.File;
 import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.eclipse.jgit.api.DescribeCommand;
+import org.eclipse.jgit.api.Git;
 
 import com.x.base.core.project.gson.XGsonBuilder;
 import com.x.base.core.project.tools.DateTools;
-import com.x.base.core.project.tools.DefaultCharset;
 
 public class CreateVersion {
 	public static void main(String... args) throws Exception {
 		File base = new File(args[0]);
 		File file = new File(base, "version.o2");
-		File pom = new File(base, "pom.xml");
+		Map<String, String> map = fromGit(base);
+		if (map.isEmpty()) {
+			map = fromMaven(base);
+		}
+		FileUtils.write(file, XGsonBuilder.toJson(map), StandardCharsets.UTF_8);
+		System.exit(0);
+	}
 
-		MavenXpp3Reader reader = new MavenXpp3Reader();
-		Model model = reader.read(new FileReader(pom));
+	private static Map<String, String> fromGit(File base) {
+		Map<String, String> map = new LinkedHashMap<>();
+		File dir = base.getParentFile().getAbsoluteFile();
+		try (Git git = Git.init().setDirectory(dir).call()) {
+			DescribeCommand describeCommand = git.describe().setTags(true).setAlways(true);
+			String version = describeCommand.call();
+			map.put("version", version);
+			map.put("date", DateTools.format(new Date()));
+			return map;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new LinkedHashMap<>();
+	}
 
-		LinkedHashMap<String, String> map = new LinkedHashMap<>();
-
-		map.put("version", model.getVersion() + "");
-		map.put("date", DateTools.format(new Date()));
-
-		FileUtils.write(file, XGsonBuilder.toJson(map), DefaultCharset.charset_utf_8);
-
+	private static Map<String, String> fromMaven(File base) {
+		Map<String, String> map = new LinkedHashMap<>();
+		try {
+			File pom = new File(base, "pom.xml");
+			MavenXpp3Reader reader = new MavenXpp3Reader();
+			Model model = reader.read(new FileReader(pom));
+			map.put("version", model.getVersion() + "");
+			map.put("date", DateTools.format(new Date()));
+			return map;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new LinkedHashMap<>();
 	}
 
 }
