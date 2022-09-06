b86f78d4c38677b00fa0ee9730f1e86dddea7327
zhourui <zhouuri@o2oa.net>
2022-09-06 16:36:45 +0800

    标记8版本删除

--- o2server/x_processplatform_service_processing/src/main/java/com/x/processplatform/service/processing/processor/manual/ManualProcessor.java
+++ o2server/x_processplatform_service_processing/src/main/java/com/x/processplatform/service/processing/processor/manual/ManualProcessor.java
@@ -382,6 +382,7 @@ public class ManualProcessor extends AbstractManualProcessor {
 	}
 
 	@SuppressWarnings("unchecked")
+	@Deprecated(forRemoval = true,since = "7.3")
 	private ManualTaskIdentityMatrix executingManualTaskIdentityMatrix(AeiObjects aeiObjects, Manual manual)
 			throws Exception {
 		ManualTaskIdentityMatrix matrix = aeiObjects.getWork().getManualTaskIdentityMatrix();
