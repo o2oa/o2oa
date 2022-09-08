8886f6e51c9941b6bedcb22c6231285898582c2d
zhourui <zhouuri@o2oa.net>
2022-09-08 12:12:34 +0800

    add tuple

---
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/bean/tuple/Quintuple.java
@@ -0,0 +1,51 @@
+package com.x.base.core.project.bean.tuple;
+
+public class Quintuple<A, B, C, D, E> {
+
+	protected A first;
+
+	protected B second;
+
+	protected C third;
+
+	protected D fourth;
+
+	protected E fifth;
+
+	public static <A, B, C, D, E> Quintuple<A, B, C, D, E> of(final A first, final B second, final C third,
+			final D fourth, final E fifth) {
+		return new ImmutableQuintuple<>(first, second, third, fourth, fifth);
+	}
+
+	public A first() {
+		return this.first;
+	}
+
+	public B second() {
+		return this.second;
+	}
+
+	public C third() {
+		return this.third;
+	}
+
+	public D fourth() {
+		return this.fourth;
+	}
+
+	public E fifth() {
+		return this.fifth;
+	}
+
+	public static class ImmutableQuintuple<A, B, C, D, E> extends Quintuple<A, B, C, D, E> {
+
+		public ImmutableQuintuple(final A first, final B second, final C third, final D fourth, E fifth) {
+			this.first = first;
+			this.second = second;
+			this.third = third;
+			this.fourth = fourth;
+			this.fifth = fifth;
+		}
+	}
+
+}
