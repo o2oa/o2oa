1eb3b2c7d2156cb837b8f51f26831d521f71f623
o2sword <171715986@qq.com>
2022-10-12 10:09:17 +0800

    人员组织发送验证码用户不存在报发送错误；

--- o2server/x_organization_assemble_authentication/src/main/java/com/x/organization/assemble/authentication/jaxrs/authentication/ActionCode.java
+++ o2server/x_organization_assemble_authentication/src/main/java/com/x/organization/assemble/authentication/jaxrs/authentication/ActionCode.java
@@ -1,12 +1,8 @@
 package com.x.organization.assemble.authentication.jaxrs.authentication;
 
-import org.apache.commons.lang3.BooleanUtils;
-import org.apache.commons.lang3.StringUtils;
-
 import com.x.base.core.container.EntityManagerContainer;
 import com.x.base.core.container.factory.EntityManagerContainerFactory;
 import com.x.base.core.project.config.Config;
-import com.x.base.core.project.exception.ExceptionPersonNotExist;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.jaxrs.WrapBoolean;
@@ -14,8 +10,9 @@ import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
 import com.x.organization.assemble.authentication.Business;
 import com.x.organization.core.entity.Person;
-
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
 
 class ActionCode extends BaseAction {
 
@@ -36,13 +33,17 @@ class ActionCode extends BaseAction {
 			Business business = new Business(emc);
 			String id = business.person().getWithCredential(credential);
 			if (StringUtils.isEmpty(id)) {
-				return result;
+				throw new ExceptionSendCodeError();
 			}
 			Person o = emc.find(id, Person.class);
 			if (!Config.person().isMobile(o.getMobile())) {
 				throw new ExceptionInvalidMobile(o.getMobile());
 			}
-			business.instrument().code().create(o.getMobile());
+			try {
+				business.instrument().code().create(o.getMobile());
+			} catch (Exception e) {
+				throw new ExceptionSendCodeError(e);
+			}
 			return result;
 		}
 	}
