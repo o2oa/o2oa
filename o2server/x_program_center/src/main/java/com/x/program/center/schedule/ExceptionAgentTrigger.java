8fda2d3d307d0ccc68a492d52b6b9352c1f0baaa
zhourui <zhouuri@o2oa.net>
2022-10-25 11:02:38 +0800

    LanguagePromptException构造方法错误,导致错误提示错误.

--- o2server/x_program_center/src/main/java/com/x/program/center/schedule/ExceptionAgentTrigger.java
+++ o2server/x_program_center/src/main/java/com/x/program/center/schedule/ExceptionAgentTrigger.java
@@ -4,9 +4,9 @@ import com.x.base.core.project.exception.LanguagePromptException;
 
 class ExceptionAgentTrigger extends LanguagePromptException {
 
-	private static final long serialVersionUID = -8597019540568284908L;
+    private static final long serialVersionUID = -8597019540568284908L;
 
-	ExceptionAgentTrigger(Throwable cause, String id, String name, String cron) {
-		super(cause, "id:{}, name:{}, cron:{}.");
-	}
+    ExceptionAgentTrigger(Throwable cause, String id, String name, String cron) {
+        super(cause, "id:{}, name:{}, cron:{}.", id, name, cron);
+    }
 }
