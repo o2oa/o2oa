f0620c8f8ee6cd018c694e0e04515697f68b92e8
zhourui <zhouuri@o2oa.net>
2022-10-28 17:55:42 +0800

    更新tuple

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/bean/tuple/Octuple.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/bean/tuple/Octuple.java
@@ -1,73 +1,80 @@
 package com.x.base.core.project.bean.tuple;
 
-public class Octuple<A, B, C, D, E, F, G, H> {
+public abstract class Octuple<A, B, C, D, E, F, G, H> {
 
-	protected A first;
+    protected A first;
 
-	protected B second;
+    protected B second;
 
-	protected C third;
+    protected C third;
 
-	protected D fourth;
+    protected D fourth;
 
-	protected E fifth;
+    protected E fifth;
 
-	protected F sixth;
+    protected F sixth;
 
-	protected G seventh;
-	
-	protected H eighth;
+    protected G seventh;
 
-	public static <A, B, C, D, E, F, G, H> Octuple<A, B, C, D, E, F, G, H> of(final A first, final B second,
-			final C third, final D fourth, final E fifth, final F sixth, final G seventh, final H eighth) {
-		return new ImmutableOctuple<>(first, second, third, fourth, fifth, sixth, seventh, eighth);
-	}
+    protected H eighth;
 
-	public A first() {
-		return this.first;
-	}
+    public static <A, B, C, D, E, F, G, H> Octuple<A, B, C, D, E, F, G, H> of(final A first, final B second,
+            final C third, final D fourth, final E fifth, final F sixth, final G seventh, final H eighth) {
+        return new ImmutableOctuple<>(first, second, third, fourth, fifth, sixth, seventh, eighth);
+    }
 
-	public B second() {
-		return this.second;
-	}
+    public static <A, B, C, D, E, F, G, H> Octuple<A, B, C, D, E, F, G, H> of(
+            final Septuple<A, B, C, D, E, F, G> septuple,
+            final H eighth) {
+        return new ImmutableOctuple<>(septuple.first(), septuple.second(), septuple.third(), septuple.fourth(),
+                septuple.fifth(), septuple.sixth(), septuple.seventh(), eighth);
+    }
 
-	public C third() {
-		return this.third;
-	}
+    public A first() {
+        return this.first;
+    }
 
-	public D fourth() {
-		return this.fourth;
-	}
+    public B second() {
+        return this.second;
+    }
 
-	public E fifth() {
-		return this.fifth;
-	}
+    public C third() {
+        return this.third;
+    }
 
-	public F sixth() {
-		return this.sixth;
-	}
+    public D fourth() {
+        return this.fourth;
+    }
 
-	public G seventh() {
-		return this.seventh;
-	}
+    public E fifth() {
+        return this.fifth;
+    }
 
-	public H eighth() {
-		return this.eighth;
-	}
+    public F sixth() {
+        return this.sixth;
+    }
 
-	public static class ImmutableOctuple<A, B, C, D, E, F, G, H> extends Octuple<A, B, C, D, E, F, G, H> {
+    public G seventh() {
+        return this.seventh;
+    }
 
-		public ImmutableOctuple(final A first, final B second, final C third, final D fourth, E fifth, F sixth,
-				G seventh, H eighth) {
-			this.first = first;
-			this.second = second;
-			this.third = third;
-			this.fourth = fourth;
-			this.fifth = fifth;
-			this.sixth = sixth;
-			this.seventh = seventh;
-			this.eighth = eighth;
-		}
-	}
+    public H eighth() {
+        return this.eighth;
+    }
+
+    public static class ImmutableOctuple<A, B, C, D, E, F, G, H> extends Octuple<A, B, C, D, E, F, G, H> {
+
+        public ImmutableOctuple(final A first, final B second, final C third, final D fourth, E fifth, F sixth,
+                G seventh, H eighth) {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+            this.fourth = fourth;
+            this.fifth = fifth;
+            this.sixth = sixth;
+            this.seventh = seventh;
+            this.eighth = eighth;
+        }
+    }
 
 }
--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/bean/tuple/Sextuple.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/bean/tuple/Sextuple.java
@@ -1,58 +1,64 @@
 package com.x.base.core.project.bean.tuple;
 
-public class Sextuple<A, B, C, D, E, F> {
+public abstract class Sextuple<A, B, C, D, E, F> {
 
-	protected A first;
+    protected A first;
 
-	protected B second;
+    protected B second;
 
-	protected C third;
+    protected C third;
 
-	protected D fourth;
+    protected D fourth;
 
-	protected E fifth;
+    protected E fifth;
 
-	protected F sixth;
+    protected F sixth;
 
-	public static <A, B, C, D, E, F> Sextuple<A, B, C, D, E, F> of(final A first, final B second, final C third,
-			final D fourth, final E fifth, final F sixth) {
-		return new ImmutableSextuple<>(first, second, third, fourth, fifth, sixth);
-	}
+    public static <A, B, C, D, E, F> Sextuple<A, B, C, D, E, F> of(final A first, final B second, final C third,
+            final D fourth, final E fifth, final F sixth) {
+        return new ImmutableSextuple<>(first, second, third, fourth, fifth, sixth);
+    }
 
-	public A first() {
-		return this.first;
-	}
+    public static <A, B, C, D, E, F> Sextuple<A, B, C, D, E, F> of(final Quintuple<A, B, C, D, E> quintuple,
+            final F sixth) {
+        return new ImmutableSextuple<>(quintuple.first(), quintuple.second(), quintuple.third(), quintuple.fourth(),
+                quintuple.fifth(), sixth);
+    }
 
-	public B second() {
-		return this.second;
-	}
+    public A first() {
+        return this.first;
+    }
 
-	public C third() {
-		return this.third;
-	}
+    public B second() {
+        return this.second;
+    }
 
-	public D fourth() {
-		return this.fourth;
-	}
+    public C third() {
+        return this.third;
+    }
 
-	public E fifth() {
-		return this.fifth;
-	}
+    public D fourth() {
+        return this.fourth;
+    }
 
-	public F sixth() {
-		return this.sixth;
-	}
+    public E fifth() {
+        return this.fifth;
+    }
 
-	public static class ImmutableSextuple<A, B, C, D, E, F> extends Sextuple<A, B, C, D, E, F> {
+    public F sixth() {
+        return this.sixth;
+    }
 
-		public ImmutableSextuple(final A first, final B second, final C third, final D fourth, E fifth, F sixth) {
-			this.first = first;
-			this.second = second;
-			this.third = third;
-			this.fourth = fourth;
-			this.fifth = fifth;
-			this.sixth = sixth;
-		}
-	}
+    public static class ImmutableSextuple<A, B, C, D, E, F> extends Sextuple<A, B, C, D, E, F> {
+
+        public ImmutableSextuple(final A first, final B second, final C third, final D fourth, E fifth, F sixth) {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+            this.fourth = fourth;
+            this.fifth = fifth;
+            this.sixth = sixth;
+        }
+    }
 
 }
