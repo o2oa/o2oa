e1d58c3743960662813304f93a66351c2f5973c5
unknown <caixiangyi@zoneland.net>
2022-11-08 18:13:30 +0800

    ftsearch

--- o2web/source/x_component_cms_Xform/Relatedlink.js
+++ o2web/source/x_component_cms_Xform/Relatedlink.js
@@ -1,4 +1,10 @@
 MWF.xDesktop.requireApp("process.Xform", "Relatedlink", null, false);
 MWF.xApplication.cms.Xform.Relatedlink = MWF.CMSRelatedlink =  new Class({
-	Extends: MWF.APPRelatedlink
+	Extends: MWF.APPRelatedlink,
+	getFlag: function(){
+		return this.form.businessData.document.id;
+	},
+	getCategory: function(){
+		return "cms";
+	}
 }); 
