ebe559988161cb309c9f1901a6efe410d50dab7d
unknown <caixiangyi@zoneland.net>
2022-12-10 13:51:56 +0800

    提交界面手写后保存，手写图标修改一下

--- o2web/source/x_component_process_Work/Processor.js
+++ o2web/source/x_component_process_Work/Processor.js
@@ -768,7 +768,13 @@ MWF.xApplication.process.Work.Processor = new Class({
                 "contentWidth": this.options.tabletWidth || 0,
                 "contentHeight": this.options.tabletHeight || 0,
                 "onSave": function (base64code, base64Image, imageFile) {
-                    this.handwritingFile = imageFile;
+                    if( !this.tablet.isBlank() ){
+                        this.handwritingFile = imageFile;
+                        this.handwritingAction.setStyles( this.css.inputOpinionHandwritingOkAction )
+                    }else{
+                        this.handwritingFile = null
+                        this.handwritingAction.setStyles( this.css.inputOpinionHandwritingAction );
+                    }
                     this.handwritingNode.hide();
                     this.handwritingNodeMask.hide();
                     // this.page.get("div_image").node.set("src",base64Image);
@@ -776,6 +782,7 @@ MWF.xApplication.process.Work.Processor = new Class({
                 }.bind(this),
                 "onCancel": function () {
                     this.handwritingFile = null;
+                    this.handwritingAction.setStyles( this.css.inputOpinionHandwritingAction );
                     this.handwritingNode.hide();
                     this.handwritingNodeMask.hide();
                 }.bind(this)
