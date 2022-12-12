63987c82c14bba795b009de6f340b5fffeded877
zhourui <zhouuri@o2oa.net>
2022-12-12 15:43:14 +0800

    修复ctl ec 以及允许多选优先路由

--- o2server/x_console/src/main/java/com/x/server/console/action/EraseContentProcessPlatform.java
+++ o2server/x_console/src/main/java/com/x/server/console/action/EraseContentProcessPlatform.java
@@ -6,33 +6,36 @@ import com.x.base.core.project.logger.LoggerFactory;
 
 public class EraseContentProcessPlatform extends EraseContent {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(EraseContentProcessPlatform.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EraseContentProcessPlatform.class);
 
-	public boolean execute() {
-		try {
-			ClassLoader classLoader = EntityClassLoaderTools.concreteClassLoader();
-			Thread.currentThread().setContextClassLoader(classLoader);
-			this.init("processPlatform", ItemCategory.pp, classLoader);
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Attachment"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.DocumentVersion"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Draft"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Read"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.ReadCompleted"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Record"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Review"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.SerialNumber"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Task"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.TaskCompleted"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Work"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.WorkCompleted"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.WorkLog"));
-			addClass(classLoader.loadClass("com.x.processplatform.core.entity.log.SignalStackLog"));
-			addClass(classLoader.loadClass("com.x.query.core.entity.Item"));
-			this.run();
-			return true;
-		} catch (Exception e) {
-			LOGGER.error(e);
-		}
-		return false;
-	}
+    public boolean execute() {
+        try {
+            ClassLoader classLoader = EntityClassLoaderTools.concreteClassLoader();
+            Thread.currentThread().setContextClassLoader(classLoader);
+            this.init("processPlatform", ItemCategory.pp, classLoader);
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Attachment"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.DocSign"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.DocSignScrawl"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.DocumentVersion"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Draft"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.KeyLock"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Read"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.ReadCompleted"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Record"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Review"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.SerialNumber"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Snap"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Task"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.TaskCompleted"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.Work"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.WorkCompleted"));
+            addClass(classLoader.loadClass("com.x.processplatform.core.entity.content.WorkLog"));
+            addClass(classLoader.loadClass("com.x.query.core.entity.Item"));
+            this.run();
+            return true;
+        } catch (Exception e) {
+            LOGGER.error(e);
+        }
+        return false;
+    }
 }
