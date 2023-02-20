14772ee7ca396ae4cde7a877a1db35ae47e3906e
zhourui <zhouuri@o2oa.net>
2023-02-20 15:00:07 +0800

    修复后加签多人情况下逆序的问题

--- o2server/x_processplatform_service_processing/src/main/java/com/x/processplatform/service/processing/jaxrs/work/V2AddManualTaskIdentityMatrix.java
+++ o2server/x_processplatform_service_processing/src/main/java/com/x/processplatform/service/processing/jaxrs/work/V2AddManualTaskIdentityMatrix.java
@@ -1,11 +1,15 @@
 package com.x.processplatform.service.processing.jaxrs.work;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
+import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import com.google.gson.JsonElement;
 import com.x.base.core.container.EntityManagerContainer;
@@ -27,99 +31,109 @@ import com.x.processplatform.service.processing.Business;
 
 class V2AddManualTaskIdentityMatrix extends BaseAction {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(V2AddManualTaskIdentityMatrix.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(V2AddManualTaskIdentityMatrix.class);
 
-	ActionResult<Wo> execute(EffectivePerson effectivePerson, String id, JsonElement jsonElement) throws Exception {
+    ActionResult<Wo> execute(EffectivePerson effectivePerson, String id, JsonElement jsonElement) throws Exception {
 
-		final Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
+        final Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
 
-		if (LOGGER.isDebugEnabled()) {
-			LOGGER.debug("execute:{}.", effectivePerson::getDistinguishedName);
-		}
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("execute:{}, id:{}, jsonElement:{}.", effectivePerson::getDistinguishedName, () -> id,
+                    () -> jsonElement);
+        }
 
-		Work work = null;
+        Work work = null;
 
-		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-			Business business = new Business(emc);
-			work = getWork(business, id);
-			if (null == work) {
-				throw new ExceptionEntityNotExist(id, Work.class);
-			}
-		}
+        try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+            Business business = new Business(emc);
+            work = getWork(business, id);
+            if (null == work) {
+                throw new ExceptionEntityNotExist(id, Work.class);
+            }
+        }
 
-		return ProcessPlatformExecutorFactory.get(work.getJob())
-				.submit(new CallableImpl(work.getId(), wi.getIdentity(), wi.getOptionList(), wi.getRemove()))
-				.get(300, TimeUnit.SECONDS);
+        return ProcessPlatformExecutorFactory.get(work.getJob())
+                .submit(new CallableImpl(work.getId(), wi.getIdentity(), wi.getOptionList(), wi.getRemove()))
+                .get(300, TimeUnit.SECONDS);
 
-	}
+    }
 
-	private Work getWork(Business business, String id) throws Exception {
-		List<String> attributes = new ArrayList<>();
-		attributes.add(Work.job_FIELDNAME);
-		attributes.add(JpaObject.id_FIELDNAME);
-		return business.entityManagerContainer().fetch(id, Work.class, attributes);
-	}
+    private Work getWork(Business business, String id) throws Exception {
+        List<String> attributes = new ArrayList<>();
+        attributes.add(Work.job_FIELDNAME);
+        attributes.add(JpaObject.id_FIELDNAME);
+        return business.entityManagerContainer().fetch(id, Work.class, attributes);
+    }
 
-	private class CallableImpl implements Callable<ActionResult<Wo>> {
+    private class CallableImpl implements Callable<ActionResult<Wo>> {
 
-		private String id;
-		private String identity;
-		private List<V2AddManualTaskIdentityMatrixWi.Option> optionList;
-		private Boolean remove;
+        private String id;
+        private String identity;
+        private List<V2AddManualTaskIdentityMatrixWi.Option> optionList;
+        private Boolean remove;
 
-		CallableImpl(String id, String identity, List<V2AddManualTaskIdentityMatrixWi.Option> optionList,
-				Boolean remove) {
-			this.id = id;
-			this.identity = identity;
-			this.optionList = optionList;
-			this.remove = remove;
-		}
+        CallableImpl(String id, String identity, List<V2AddManualTaskIdentityMatrixWi.Option> optionList,
+                Boolean remove) {
+            this.id = id;
+            this.identity = identity;
+            this.optionList = optionList;
+            this.remove = remove;
+        }
 
-		@Override
-		public ActionResult<Wo> call() throws Exception {
-			ManualTaskIdentityMatrix matrix = null;
-			try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-				Business business = new Business(emc);
-				emc.beginTransaction(Work.class);
-				Work work = emc.find(this.id, Work.class);
-				if (null == work) {
-					throw new ExceptionEntityNotExist(id, Work.class);
-				}
-				matrix = work.getManualTaskIdentityMatrix();
-				for (V2AddManualTaskIdentityMatrixWi.Option option : optionList) {
-					List<String> identities = business.organization().identity().list(option.getIdentityList());
-					if (!ListTools.isEmpty(identities)) {
-						matrix.add(identity, option.getPosition(), identities);
-					}
-				}
-				if (BooleanUtils.isTrue(remove)) {
-					matrix.remove(identity);
-				}
-				work.setManualTaskIdentityMatrix(matrix);
-				emc.check(work, CheckPersistType.all);
-				emc.commit();
-			} catch (Exception e) {
-				LOGGER.error(e);
-			}
-			ActionResult<Wo> result = new ActionResult<>();
-			Wo wo = new Wo();
-			wo.setManualTaskIdentityMatrix(matrix);
-			result.setData(wo);
-			return result;
-		}
+        @Override
+        public ActionResult<Wo> call() throws Exception {
+            ManualTaskIdentityMatrix matrix = null;
+            try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+                Business business = new Business(emc);
+                emc.beginTransaction(Work.class);
+                Work work = emc.find(this.id, Work.class);
+                if (null == work) {
+                    throw new ExceptionEntityNotExist(id, Work.class);
+                }
+                matrix = work.getManualTaskIdentityMatrix();
+                // 需要将ADD_POSITION_AFTER挑选出来进行逆序执行
+                List<V2AddManualTaskIdentityMatrixWi.Option> afterOptionList = optionList.stream()
+                        .filter(o -> StringUtils.equals(ManualTaskIdentityMatrix.ADD_POSITION_AFTER, o.getPosition()))
+                        .collect(Collectors.toList());
+                List<V2AddManualTaskIdentityMatrixWi.Option> otherOptionList = optionList.stream()
+                        .filter(o -> !StringUtils.equals(ManualTaskIdentityMatrix.ADD_POSITION_AFTER, o.getPosition()))
+                        .collect(Collectors.toList());
+                Collections.reverse(afterOptionList);
+                for (V2AddManualTaskIdentityMatrixWi.Option option : ListUtils.sum(afterOptionList, otherOptionList)) {
+                    List<String> identities = business.organization().identity().list(option.getIdentityList());
+                    LOGGER.debug("add identites:{}.", identities);
+                    if (!ListTools.isEmpty(identities)) {
+                        matrix.add(identity, option.getPosition(), identities);
+                    }
+                }
+                if (BooleanUtils.isTrue(remove)) {
+                    matrix.remove(identity);
+                }
+                work.setManualTaskIdentityMatrix(matrix);
+                emc.check(work, CheckPersistType.all);
+                emc.commit();
+            } catch (Exception e) {
+                LOGGER.error(e);
+            }
+            ActionResult<Wo> result = new ActionResult<>();
+            Wo wo = new Wo();
+            wo.setManualTaskIdentityMatrix(matrix);
+            result.setData(wo);
+            return result;
+        }
 
-	}
+    }
 
-	public static class Wi extends V2AddManualTaskIdentityMatrixWi {
+    public static class Wi extends V2AddManualTaskIdentityMatrixWi {
 
-		private static final long serialVersionUID = 7870902860170655791L;
+        private static final long serialVersionUID = 7870902860170655791L;
 
-	}
+    }
 
-	public static class Wo extends V2AddManualTaskIdentityMatrixWo {
+    public static class Wo extends V2AddManualTaskIdentityMatrixWo {
 
-		private static final long serialVersionUID = -1377290527826280418L;
+        private static final long serialVersionUID = -1377290527826280418L;
 
-	}
+    }
 
 }
