ee0231718d2b5b23b8bfa9df50c797c58ddd7dcb
fancy <fancylou@outlook.com>
2023-03-08 13:43:44 +0800

    新版考勤 修改前端包名称，统计api

---
+++ o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/jaxrs/v2/detail/ActionStatisticWithFilter.java
@@ -0,0 +1,5 @@
+package com.x.attendance.assemble.control.jaxrs.v2.detail;/**
+ * Created by fancyLou on 2023/3/8.
+ * Copyright © 2023 O2. All rights reserved.
+ */public class ActionStatisticWithFilter {
+}
