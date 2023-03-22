e280a25d53f59fd1d4724f0f2b692c24a83ba52e
zhourui <zhouuri@o2oa.net>
2023-03-22 16:15:35 +0800

    update

--- o2server/x_organization_assemble_personal/src/main/java/com/x/organization/assemble/personal/jaxrs/empowerlog/EmpowerLogAction.java
+++ o2server/x_organization_assemble_personal/src/main/java/com/x/organization/assemble/personal/jaxrs/empowerlog/EmpowerLogAction.java
@@ -31,118 +31,137 @@ import com.x.base.core.project.logger.LoggerFactory;
 @JaxrsDescribe("授权日志操作")
 public class EmpowerLogAction extends StandardJaxrsAction {
 
-	private static Logger logger = LoggerFactory.getLogger(EmpowerLogAction.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmpowerLogAction.class);
 
-	@JaxrsMethodDescribe(value = "管理员列示授权日志对象,下一页.", action = ActionListNext.class)
-	@GET
-	@Path("list/{id}/next/{count}")
-	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public void listNext(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
-			@JaxrsParameterDescribe("标识") @PathParam("id") String id,
-			@JaxrsParameterDescribe("数量") @PathParam("count") Integer count) {
-		ActionResult<List<ActionListNext.Wo>> result = new ActionResult<>();
-		EffectivePerson effectivePerson = this.effectivePerson(request);
-		try {
-			result = new ActionListNext().execute(effectivePerson, id, count);
-		} catch (Exception e) {
-			logger.error(e, effectivePerson, request, null);
-			result.error(e);
-		}
-		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
-	}
+    @JaxrsMethodDescribe(value = "管理员列示授权日志对象,下一页.", action = ActionListNext.class)
+    @GET
+    @Path("list/{id}/next/{count}")
+    @Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void listNext(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
+            @JaxrsParameterDescribe("标识") @PathParam("id") String id,
+            @JaxrsParameterDescribe("数量") @PathParam("count") Integer count) {
+        ActionResult<List<ActionListNext.Wo>> result = new ActionResult<>();
+        EffectivePerson effectivePerson = this.effectivePerson(request);
+        try {
+            result = new ActionListNext().execute(effectivePerson, id, count);
+        } catch (Exception e) {
+            LOGGER.error(e, effectivePerson, request, null);
+            result.error(e);
+        }
+        asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
+    }
 
-	@JaxrsMethodDescribe(value = "管理员列示授权日志对象,上一页.", action = ActionListPrev.class)
-	@GET
-	@Path("list/{id}/prev/{count}")
-	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public void listPrev(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
-			@JaxrsParameterDescribe("标识") @PathParam("id") String id,
-			@JaxrsParameterDescribe("数量") @PathParam("count") Integer count) {
-		ActionResult<List<ActionListPrev.Wo>> result = new ActionResult<>();
-		EffectivePerson effectivePerson = this.effectivePerson(request);
-		try {
-			result = new ActionListPrev().execute(effectivePerson, id, count);
-		} catch (Exception e) {
-			logger.error(e, effectivePerson, request, null);
-			result.error(e);
-		}
-		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
-	}
+    @JaxrsMethodDescribe(value = "管理员列示授权日志对象,上一页.", action = ActionListPrev.class)
+    @GET
+    @Path("list/{id}/prev/{count}")
+    @Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void listPrev(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
+            @JaxrsParameterDescribe("标识") @PathParam("id") String id,
+            @JaxrsParameterDescribe("数量") @PathParam("count") Integer count) {
+        ActionResult<List<ActionListPrev.Wo>> result = new ActionResult<>();
+        EffectivePerson effectivePerson = this.effectivePerson(request);
+        try {
+            result = new ActionListPrev().execute(effectivePerson, id, count);
+        } catch (Exception e) {
+            LOGGER.error(e, effectivePerson, request, null);
+            result.error(e);
+        }
+        asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
+    }
 
-	@JaxrsMethodDescribe(value = "获取当前人员的授权日志.", action = ActionListWithCurrentPersonPaging.class)
-	@POST
-	@Path("list/currentperson/paging/{page}/size/{size}")
-	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public void listWithCurrentPersonPaging(@Suspended final AsyncResponse asyncResponse,
-			@Context HttpServletRequest request, @JaxrsParameterDescribe("分页") @PathParam("page") Integer page,
-			@JaxrsParameterDescribe("数量") @PathParam("size") Integer size, JsonElement jsonElement) {
-		ActionResult<List<ActionListWithCurrentPersonPaging.Wo>> result = new ActionResult<>();
-		EffectivePerson effectivePerson = this.effectivePerson(request);
-		try {
-			result = new ActionListWithCurrentPersonPaging().execute(effectivePerson, page, size, jsonElement);
-		} catch (Exception e) {
-			logger.error(e, effectivePerson, request, jsonElement);
-			result.error(e);
-		}
-		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result, jsonElement));
-	}
+    @JaxrsMethodDescribe(value = "获取当前人员的授权日志.", action = ActionListWithCurrentPersonPaging.class)
+    @POST
+    @Path("list/currentperson/paging/{page}/size/{size}")
+    @Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void listWithCurrentPersonPaging(@Suspended final AsyncResponse asyncResponse,
+            @Context HttpServletRequest request, @JaxrsParameterDescribe("分页") @PathParam("page") Integer page,
+            @JaxrsParameterDescribe("数量") @PathParam("size") Integer size, JsonElement jsonElement) {
+        ActionResult<List<ActionListWithCurrentPersonPaging.Wo>> result = new ActionResult<>();
+        EffectivePerson effectivePerson = this.effectivePerson(request);
+        try {
+            result = new ActionListWithCurrentPersonPaging().execute(effectivePerson, page, size, jsonElement);
+        } catch (Exception e) {
+            LOGGER.error(e, effectivePerson, request, jsonElement);
+            result.error(e);
+        }
+        asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result, jsonElement));
+    }
 
-	@JaxrsMethodDescribe(value = "获取当前人员的被授权日志.", action = ActionListToCurrentPersonPaging.class)
-	@POST
-	@Path("list/to/currentperson/paging/{page}/size/{size}")
-	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public void listToCurrentPersonPaging(@Suspended final AsyncResponse asyncResponse,
-			@Context HttpServletRequest request, @JaxrsParameterDescribe("分页") @PathParam("page") Integer page,
-			@JaxrsParameterDescribe("数量") @PathParam("size") Integer size, JsonElement jsonElement) {
-		ActionResult<List<ActionListToCurrentPersonPaging.Wo>> result = new ActionResult<>();
-		EffectivePerson effectivePerson = this.effectivePerson(request);
-		try {
-			result = new ActionListToCurrentPersonPaging().execute(effectivePerson, page, size, jsonElement);
-		} catch (Exception e) {
-			logger.error(e, effectivePerson, request, jsonElement);
-			result.error(e);
-		}
-		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result, jsonElement));
-	}
+    @JaxrsMethodDescribe(value = "获取当前人员的被授权日志.", action = ActionListToCurrentPersonPaging.class)
+    @POST
+    @Path("list/to/currentperson/paging/{page}/size/{size}")
+    @Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void listToCurrentPersonPaging(@Suspended final AsyncResponse asyncResponse,
+            @Context HttpServletRequest request, @JaxrsParameterDescribe("分页") @PathParam("page") Integer page,
+            @JaxrsParameterDescribe("数量") @PathParam("size") Integer size, JsonElement jsonElement) {
+        ActionResult<List<ActionListToCurrentPersonPaging.Wo>> result = new ActionResult<>();
+        EffectivePerson effectivePerson = this.effectivePerson(request);
+        try {
+            result = new ActionListToCurrentPersonPaging().execute(effectivePerson, page, size, jsonElement);
+        } catch (Exception e) {
+            LOGGER.error(e, effectivePerson, request, jsonElement);
+            result.error(e);
+        }
+        asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result, jsonElement));
+    }
 
-	@JaxrsMethodDescribe(value = "删除授权日志.", action = ActionDelete.class)
-	@DELETE
-	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public void delete(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
-			@JaxrsParameterDescribe("标识") @PathParam("id") String id) {
-		ActionResult<ActionDelete.Wo> result = new ActionResult<>();
-		EffectivePerson effectivePerson = this.effectivePerson(request);
-		try {
-			result = new ActionDelete().execute(effectivePerson, id);
-		} catch (Exception e) {
-			logger.error(e, effectivePerson, request, null);
-			result.error(e);
-		}
-		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
-	}
+    @JaxrsMethodDescribe(value = "删除授权日志.", action = ActionDelete.class)
+    @DELETE
+    @Path("{id}")
+    @Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void delete(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
+            @JaxrsParameterDescribe("标识") @PathParam("id") String id) {
+        ActionResult<ActionDelete.Wo> result = new ActionResult<>();
+        EffectivePerson effectivePerson = this.effectivePerson(request);
+        try {
+            result = new ActionDelete().execute(effectivePerson, id);
+        } catch (Exception e) {
+            LOGGER.error(e, effectivePerson, request, null);
+            result.error(e);
+        }
+        asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
+    }
+    
+    @JaxrsMethodDescribe(value = "删除授权日志 MockDeleteToGet", action = ActionDelete.class)
+    @GET
+    @Path("{id}/mockdeletetoget")
+    @Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void deleteMockDeleteToGet(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
+            @JaxrsParameterDescribe("标识") @PathParam("id") String id) {
+        ActionResult<ActionDelete.Wo> result = new ActionResult<>();
+        EffectivePerson effectivePerson = this.effectivePerson(request);
+        try {
+            result = new ActionDelete().execute(effectivePerson, id);
+        } catch (Exception e) {
+            LOGGER.error(e, effectivePerson, request, null);
+            result.error(e);
+        }
+        asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
+    }
 
-	@JaxrsMethodDescribe(value = "管理员根据条件查询授权日志.", action = ActionManagerListPaging.class)
-	@POST
-	@Path("manager/list/paging/{page}/size/{size}")
-	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public void managerlistPaging(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
-			@JaxrsParameterDescribe("分页") @PathParam("page") Integer page,
-			@JaxrsParameterDescribe("数量") @PathParam("size") Integer size, JsonElement jsonElement) {
-		ActionResult<List<ActionManagerListPaging.Wo>> result = new ActionResult<>();
-		EffectivePerson effectivePerson = this.effectivePerson(request);
-		try {
-			result = new ActionManagerListPaging().execute(effectivePerson, page, size, jsonElement);
-		} catch (Exception e) {
-			logger.error(e, effectivePerson, request, jsonElement);
-			result.error(e);
-		}
-		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result, jsonElement));
-	}
+    @JaxrsMethodDescribe(value = "管理员根据条件查询授权日志.", action = ActionManagerListPaging.class)
+    @POST
+    @Path("manager/list/paging/{page}/size/{size}")
+    @Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void managerlistPaging(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
+            @JaxrsParameterDescribe("分页") @PathParam("page") Integer page,
+            @JaxrsParameterDescribe("数量") @PathParam("size") Integer size, JsonElement jsonElement) {
+        ActionResult<List<ActionManagerListPaging.Wo>> result = new ActionResult<>();
+        EffectivePerson effectivePerson = this.effectivePerson(request);
+        try {
+            result = new ActionManagerListPaging().execute(effectivePerson, page, size, jsonElement);
+        } catch (Exception e) {
+            LOGGER.error(e, effectivePerson, request, jsonElement);
+            result.error(e);
+        }
+        asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result, jsonElement));
+    }
 
 }
