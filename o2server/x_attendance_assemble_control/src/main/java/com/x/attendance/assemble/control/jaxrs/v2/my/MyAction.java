ec7883e8be3b696eb091f22791929d645a3b870a
fancy <fancylou@outlook.com>
2023-04-07 18:02:34 +0800

    新版考勤 新版本api

--- o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/jaxrs/v2/my/MyAction.java
+++ o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/jaxrs/v2/my/MyAction.java
@@ -2,6 +2,7 @@ package com.x.attendance.assemble.control.jaxrs.v2.my;
 
 import com.google.gson.JsonElement;
 import com.x.attendance.assemble.control.jaxrs.v2.detail.model.StatisticWo;
+import com.x.attendance.assemble.control.jaxrs.v2.mobile.ActionPreCheck;
 import com.x.base.core.project.annotation.JaxrsDescribe;
 import com.x.base.core.project.annotation.JaxrsMethodDescribe;
 import com.x.base.core.project.http.ActionResult;
@@ -13,10 +14,7 @@ import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
@@ -35,6 +33,23 @@ public class MyAction extends StandardJaxrsAction {
     private static Logger logger = LoggerFactory.getLogger(MyAction.class);
 
 
+
+    @JaxrsMethodDescribe(value = "考勤版本查询.", action = ActionVersion.class)
+    @GET
+    @Path("version")
+    @Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void versionCheck(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request) {
+        ActionResult<ActionVersion.Wo> result = new ActionResult<>();
+        EffectivePerson effectivePerson = this.effectivePerson(request);
+        try {
+            result = new ActionVersion().execute();
+        } catch (Exception e) {
+            logger.error(e, effectivePerson, request, null);
+            result.error(e);
+        }
+        asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
+    }
 
     @JaxrsMethodDescribe(value = "根据日期查询我的考勤数据.", action = ActionListDetailWithDate.class)
     @POST
