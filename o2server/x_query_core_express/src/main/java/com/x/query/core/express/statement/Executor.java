bd37fb7f0376bc05fbb8207f397d31f8e5c2be84
zhourui <zhourui@o2oa.net>
2023-04-09 16:41:15 +0800

    resultSet As Map

--- o2server/x_query_core_express/src/main/java/com/x/query/core/express/statement/Executor.java
+++ o2server/x_query_core_express/src/main/java/com/x/query/core/express/statement/Executor.java
@@ -1,5 +1,6 @@
 package com.x.query.core.express.statement;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.persistence.EntityManager;
@@ -21,136 +22,135 @@ import com.x.query.core.entity.schema.Table;
 
 public class Executor {
 
-    private Executor() {
-        // nothing
-    }
+	private Executor() {
+		// nothing
+	}
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Executor.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(Executor.class);
 
-    public static Object executeData(Statement statement, Runtime runtime, ExecuteTarget executeTarget)
-            throws Exception {
-        if (StringUtils.equalsAnyIgnoreCase(statement.getFormat(), Statement.FORMAT_SQL, Statement.FORMAT_SQLSCRIPT)) {
-            return executeDataSql(runtime, executeTarget);
-        } else {
-            return executeDataJpql(statement, runtime, executeTarget);
-        }
-    }
+	public static Object executeData(Statement statement, Runtime runtime, ExecuteTarget executeTarget)
+			throws Exception {
+		if (StringUtils.equalsAnyIgnoreCase(statement.getFormat(), Statement.FORMAT_SQL, Statement.FORMAT_SQLSCRIPT)) {
+			return executeDataSql(runtime, executeTarget);
+		} else {
+			return executeDataJpql(statement, runtime, executeTarget);
+		}
+	}
 
-    private static Object executeDataSql(Runtime runtime, ExecuteTarget executeTarget)
-            throws Exception {
-        try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-            EntityManager em = emc.get(DynamicBaseEntity.class);
-            LOGGER.debug("executeDataSql:{}, param:{}.", executeTarget::getSql, executeTarget::getQuestionMarkParam);
-            Query query = em.createNativeQuery(executeTarget.getSql());
-            for (Map.Entry<String, Object> entry : executeTarget.getQuestionMarkParam().entrySet()) {
-                int idx = Integer.parseInt(entry.getKey().substring(1));
-                query.setParameter(idx, entry.getValue());
-            }
-            if (executeTarget.getParsedStatement() instanceof net.sf.jsqlparser.statement.select.Select) {
-                if (NumberTools.greaterThan(runtime.page, 0) && NumberTools.greaterThan(runtime.size, 0)) {
-                    query.setFirstResult((runtime.page - 1) * runtime.size);
-                    query.setMaxResults(runtime.size);
-                }
-                return query.getResultList();
-            } else {
-                emc.beginTransaction(DynamicBaseEntity.class);
-                Object data = Integer.valueOf(query.executeUpdate());
-                emc.commit();
-                return data;
-            }
-        }
-    }
+	private static Object executeDataSql(Runtime runtime, ExecuteTarget executeTarget) throws Exception {
+		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+			EntityManager em = emc.get(DynamicBaseEntity.class);
+			LOGGER.debug("executeDataSql:{}, param:{}.", executeTarget::getSql, executeTarget::getQuestionMarkParam);
+			Query query = em.createNativeQuery(executeTarget.getSql(), LinkedHashMap.class);
+			for (Map.Entry<String, Object> entry : executeTarget.getQuestionMarkParam().entrySet()) {
+				int idx = Integer.parseInt(entry.getKey().substring(1));
+				query.setParameter(idx, entry.getValue());
+			}
+			if (executeTarget.getParsedStatement() instanceof net.sf.jsqlparser.statement.select.Select) {
+				if (NumberTools.greaterThan(runtime.page, 0) && NumberTools.greaterThan(runtime.size, 0)) {
+					query.setFirstResult((runtime.page - 1) * runtime.size);
+					query.setMaxResults(runtime.size);
+				}
+				return query.getResultList();
+			} else {
+				emc.beginTransaction(DynamicBaseEntity.class);
+				Object data = Integer.valueOf(query.executeUpdate());
+				emc.commit();
+				return data;
+			}
+		}
+	}
 
-    private static Object executeDataJpql(Statement statement, Runtime runtime, ExecuteTarget executeTarget)
-            throws Exception {
-        try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-            Class<? extends JpaObject> cls = clazz(emc, statement);
-            EntityManager em;
-            if (StringUtils.equalsIgnoreCase(statement.getEntityCategory(), Statement.ENTITYCATEGORY_DYNAMIC)
-                    && executeTarget.getParsedStatement() instanceof net.sf.jsqlparser.statement.select.Select) {
-                em = emc.get(DynamicBaseEntity.class);
-            } else {
-                em = emc.get(cls);
-            }
-            LOGGER.debug("executeDataJpql:{}, param:{}.", executeTarget::getSql, executeTarget::getQuestionMarkParam);
-            Query query = em.createQuery(executeTarget.getSql());
-            for (Map.Entry<String, Object> entry : executeTarget.getQuestionMarkParam().entrySet()) {
-                int idx = Integer.parseInt(entry.getKey().substring(1));
-                query.setParameter(idx, entry.getValue());
-            }
-            if (executeTarget.getParsedStatement() instanceof net.sf.jsqlparser.statement.select.Select) {
-                if (NumberTools.greaterThan(runtime.page, 0) && NumberTools.greaterThan(runtime.size, 0)) {
-                    query.setFirstResult((runtime.page - 1) * runtime.size);
-                    query.setMaxResults(runtime.size);
-                }
-                return query.getResultList();
-            } else {
-                emc.beginTransaction(cls);
-                Object data = Integer.valueOf(query.executeUpdate());
-                emc.commit();
-                return data;
-            }
-        }
-    }
+	private static Object executeDataJpql(Statement statement, Runtime runtime, ExecuteTarget executeTarget)
+			throws Exception {
+		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+			Class<? extends JpaObject> cls = clazz(emc, statement);
+			EntityManager em;
+			if (StringUtils.equalsIgnoreCase(statement.getEntityCategory(), Statement.ENTITYCATEGORY_DYNAMIC)
+					&& executeTarget.getParsedStatement() instanceof net.sf.jsqlparser.statement.select.Select) {
+				em = emc.get(DynamicBaseEntity.class);
+			} else {
+				em = emc.get(cls);
+			}
+			LOGGER.debug("executeDataJpql:{}, param:{}.", executeTarget::getSql, executeTarget::getQuestionMarkParam);
+			Query query = em.createQuery(executeTarget.getSql());
+			for (Map.Entry<String, Object> entry : executeTarget.getQuestionMarkParam().entrySet()) {
+				int idx = Integer.parseInt(entry.getKey().substring(1));
+				query.setParameter(idx, entry.getValue());
+			}
+			if (executeTarget.getParsedStatement() instanceof net.sf.jsqlparser.statement.select.Select) {
+				if (NumberTools.greaterThan(runtime.page, 0) && NumberTools.greaterThan(runtime.size, 0)) {
+					query.setFirstResult((runtime.page - 1) * runtime.size);
+					query.setMaxResults(runtime.size);
+				}
+				return query.getResultList();
+			} else {
+				emc.beginTransaction(cls);
+				Object data = Integer.valueOf(query.executeUpdate());
+				emc.commit();
+				return data;
+			}
+		}
+	}
 
-    public static Long executeCount(Statement statement, ExecuteTarget executeTarget) throws Exception {
-        if (StringUtils.equalsAnyIgnoreCase(statement.getFormat(), Statement.FORMAT_SQL, Statement.FORMAT_SQLSCRIPT)) {
-            return executeCountSql(executeTarget);
-        } else {
-            return executeCountJpql(statement, executeTarget);
-        }
-    }
+	public static Long executeCount(Statement statement, ExecuteTarget executeTarget) throws Exception {
+		if (StringUtils.equalsAnyIgnoreCase(statement.getFormat(), Statement.FORMAT_SQL, Statement.FORMAT_SQLSCRIPT)) {
+			return executeCountSql(executeTarget);
+		} else {
+			return executeCountJpql(statement, executeTarget);
+		}
+	}
 
-    private static Long executeCountSql(ExecuteTarget executeTarget) throws Exception {
-        try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-            EntityManager em = emc.get(DynamicBaseEntity.class);
-            LOGGER.debug("executeCountSql:{}, param:{}.", executeTarget::getSql, executeTarget::getQuestionMarkParam);
-            Query query = em.createNativeQuery(executeTarget.getSql());
-            for (Map.Entry<String, Object> entry : executeTarget.getQuestionMarkParam().entrySet()) {
-                int idx = Integer.parseInt(entry.getKey().substring(1));
-                query.setParameter(idx, entry.getValue());
-            }
-            return (Long) query.getSingleResult();
-        }
-    }
+	private static Long executeCountSql(ExecuteTarget executeTarget) throws Exception {
+		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+			EntityManager em = emc.get(DynamicBaseEntity.class);
+			LOGGER.debug("executeCountSql:{}, param:{}.", executeTarget::getSql, executeTarget::getQuestionMarkParam);
+			Query query = em.createNativeQuery(executeTarget.getSql());
+			for (Map.Entry<String, Object> entry : executeTarget.getQuestionMarkParam().entrySet()) {
+				int idx = Integer.parseInt(entry.getKey().substring(1));
+				query.setParameter(idx, entry.getValue());
+			}
+			return (Long) query.getSingleResult();
+		}
+	}
 
-    private static Long executeCountJpql(Statement statement, ExecuteTarget executeTarget) throws Exception {
-        try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-            Class<? extends JpaObject> cls = clazz(emc, statement);
-            EntityManager em;
-            if (StringUtils.equalsIgnoreCase(statement.getEntityCategory(), Statement.ENTITYCATEGORY_DYNAMIC)
-                    && executeTarget.getParsedStatement() instanceof net.sf.jsqlparser.statement.select.Select) {
-                em = emc.get(DynamicBaseEntity.class);
-            } else {
-                em = emc.get(cls);
-            }
-            LOGGER.debug("executeCountJpql:{}, param:{}.", executeTarget::getSql, executeTarget::getQuestionMarkParam);
-            Query query = em.createQuery(executeTarget.getSql());
-            for (Map.Entry<String, Object> entry : executeTarget.getQuestionMarkParam().entrySet()) {
-                int idx = Integer.parseInt(entry.getKey().substring(1));
-                query.setParameter(idx, entry.getValue());
-            }
-            return (Long) query.getSingleResult();
-        }
-    }
+	private static Long executeCountJpql(Statement statement, ExecuteTarget executeTarget) throws Exception {
+		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+			Class<? extends JpaObject> cls = clazz(emc, statement);
+			EntityManager em;
+			if (StringUtils.equalsIgnoreCase(statement.getEntityCategory(), Statement.ENTITYCATEGORY_DYNAMIC)
+					&& executeTarget.getParsedStatement() instanceof net.sf.jsqlparser.statement.select.Select) {
+				em = emc.get(DynamicBaseEntity.class);
+			} else {
+				em = emc.get(cls);
+			}
+			LOGGER.debug("executeCountJpql:{}, param:{}.", executeTarget::getSql, executeTarget::getQuestionMarkParam);
+			Query query = em.createQuery(executeTarget.getSql());
+			for (Map.Entry<String, Object> entry : executeTarget.getQuestionMarkParam().entrySet()) {
+				int idx = Integer.parseInt(entry.getKey().substring(1));
+				query.setParameter(idx, entry.getValue());
+			}
+			return (Long) query.getSingleResult();
+		}
+	}
 
-    @SuppressWarnings("unchecked")
-    private static Class<? extends JpaObject> clazz(EntityManagerContainer entityManagerContainer, Statement statement)
-            throws Exception {
-        Class<? extends JpaObject> cls = null;
-        if (StringUtils.equals(Statement.ENTITYCATEGORY_OFFICIAL, statement.getEntityCategory())
-                || StringUtils.equals(Statement.ENTITYCATEGORY_CUSTOM, statement.getEntityCategory())) {
-            cls = (Class<? extends JpaObject>) Thread.currentThread().getContextClassLoader()
-                    .loadClass(statement.getEntityClassName());
-        } else {
-            Table table = entityManagerContainer.flag(statement.getTable(), Table.class);
-            if (null == table) {
-                throw new ExceptionEntityNotExist(statement.getTable(), Table.class);
-            }
-            DynamicEntity dynamicEntity = new DynamicEntity(table.getName());
-            cls = (Class<? extends JpaObject>) Thread.currentThread().getContextClassLoader()
-                    .loadClass(dynamicEntity.className());
-        }
-        return cls;
-    }
+	@SuppressWarnings("unchecked")
+	private static Class<? extends JpaObject> clazz(EntityManagerContainer entityManagerContainer, Statement statement)
+			throws Exception {
+		Class<? extends JpaObject> cls = null;
+		if (StringUtils.equals(Statement.ENTITYCATEGORY_OFFICIAL, statement.getEntityCategory())
+				|| StringUtils.equals(Statement.ENTITYCATEGORY_CUSTOM, statement.getEntityCategory())) {
+			cls = (Class<? extends JpaObject>) Thread.currentThread().getContextClassLoader()
+					.loadClass(statement.getEntityClassName());
+		} else {
+			Table table = entityManagerContainer.flag(statement.getTable(), Table.class);
+			if (null == table) {
+				throw new ExceptionEntityNotExist(statement.getTable(), Table.class);
+			}
+			DynamicEntity dynamicEntity = new DynamicEntity(table.getName());
+			cls = (Class<? extends JpaObject>) Thread.currentThread().getContextClassLoader()
+					.loadClass(dynamicEntity.className());
+		}
+		return cls;
+	}
 }
--- o2server/x_query_core_express/src/main/java/com/x/query/core/express/statement/Runtime.java
+++ o2server/x_query_core_express/src/main/java/com/x/query/core/express/statement/Runtime.java
@@ -1,56 +1,117 @@
 package com.x.query.core.express.statement;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import com.x.base.core.project.annotation.FieldDescribe;
 import com.x.base.core.project.gson.GsonPropertyObject;
+import com.x.base.core.project.tools.ListTools;
+import com.x.cms.core.entity.query.Comparison;
+import com.x.query.core.express.plan.FilterEntry;
 
 public class Runtime extends GsonPropertyObject {
 
-    public static final String PARAMETER_PERSON = "person";
-    public static final String PARAMETER_IDENTITYLIST = "identityList";
-    public static final String PARAMETER_UNITLIST = "unitList";
-    public static final String PARAMETER_UNITALLLIST = "unitAllList";
-    public static final String PARAMETER_GROUPLIST = "groupList";
-    public static final String PARAMETER_ROLELIST = "roleList";
+	private static final long serialVersionUID = 2119629142378875023L;
 
-    public static final List<String> ALL_BUILT_IN_PARAMETER = ListUtils
-            .unmodifiableList(Arrays.asList(PARAMETER_PERSON,
-                    PARAMETER_IDENTITYLIST, PARAMETER_UNITLIST, PARAMETER_UNITALLLIST, PARAMETER_GROUPLIST,
-                    PARAMETER_ROLELIST));
+	public static final String PARAMETER_PERSON = "person";
+	public static final String PARAMETER_IDENTITYLIST = "identityList";
+	public static final String PARAMETER_UNITLIST = "unitList";
+	public static final String PARAMETER_UNITALLLIST = "unitAllList";
+	public static final String PARAMETER_GROUPLIST = "groupList";
+	public static final String PARAMETER_ROLELIST = "roleList";
 
-    @FieldDescribe("参数")
-    public Map<String, Object> parameters = new HashMap<>();
+	public static final List<String> ALL_BUILT_IN_PARAMETER = ListUtils
+			.unmodifiableList(Arrays.asList(PARAMETER_PERSON, PARAMETER_IDENTITYLIST, PARAMETER_UNITLIST,
+					PARAMETER_UNITALLLIST, PARAMETER_GROUPLIST, PARAMETER_ROLELIST));
 
-    public Map<String, Object> getParameters() {
-        return parameters;
-    }
+	@FieldDescribe("参数.")
+	public Map<String, Object> parameters = new HashMap<>();
 
-    @FieldDescribe("页码")
-    public Integer page = 0;
+	@FieldDescribe("过滤条件.")
+	public List<FilterEntry> filterList = new ArrayList<>();
 
-    @FieldDescribe("每页大小")
-    public Integer size = 20;
+	public Map<String, Object> getParameters() {
+		return parameters;
+	}
 
-    public boolean hasParameter(String name) {
-        if (StringUtils.isEmpty(name)) {
-            return false;
-        }
+	@FieldDescribe("页码")
+	public Integer page = 0;
 
-        return this.parameters.containsKey(name);
-    }
+	@FieldDescribe("每页大小")
+	public Integer size = 20;
 
-    public void setParameter(String name, Object obj) {
-        this.parameters.put(name, obj);
-    }
+	public boolean hasParameter(String name) {
+		if (StringUtils.isEmpty(name)) {
+			return false;
+		}
 
-    public Object getParameter(String name) {
-        return this.parameters.get(name);
-    }
+		return this.parameters.containsKey(name);
+	}
+
+	public void setParameter(String name, Object obj) {
+		this.parameters.put(name, obj);
+	}
+
+	public Object getParameter(String name) {
+		return this.parameters.get(name);
+	}
+
+	public List<FilterEntry> getFilterList() {
+		return filterList;
+	}
+
+	public void setFilterList(List<FilterEntry> filterList) {
+		this.filterList = filterList;
+	}
+
+	public Optional<String> additionFilter() throws Exception {
+		if (ListTools.isNotEmpty(getFilterList())) {
+			StringBuilder builder = new StringBuilder();
+			builder.append("(");
+			for (FilterEntry entry : this.getFilterList()) {
+				if (builder.length() > 1) {
+					builder.append(" ").append(entry.logic).append(" ");
+				}
+				builder.append(entry.path).append(" ").append(comparison(entry)).append("").append(entry.value);
+			}
+			builder.append(")");
+			return Optional.of(builder.toString());
+		}
+		return Optional.empty();
+	}
+
+	private String comparison(FilterEntry entry) throws Exception {
+		if (Comparison.isNotEquals(entry.comparison)) {
+			return "!=";
+		}
+
+		if (Comparison.isGreaterThan(entry.comparison)) {
+			return ">";
+		}
+
+		if (Comparison.isGreaterThanOrEqualTo(entry.comparison)) {
+			return ">=";
+		}
+
+		if (Comparison.isLessThan(entry.comparison)) {
+			return "<";
+		}
+
+		if (Comparison.isLessThanOrEqualTo(entry.comparison)) {
+			return "<=";
+		}
+
+		if (Comparison.isLike(entry.comparison)) {
+			return "like";
+		}
+		return "=";
+	}
+
 }
