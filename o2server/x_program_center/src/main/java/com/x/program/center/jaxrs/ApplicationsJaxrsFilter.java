36bf809e9184801cc42ad83715d614a0fb146cd2
o2sword <171715986@qq.com>
2023-04-21 17:39:31 +0800

    修复获取平台运行的应用列表未限制权限的问题

--- o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/ApplicationsJaxrsFilter.java
+++ o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/ApplicationsJaxrsFilter.java
@@ -2,9 +2,9 @@ package com.x.program.center.jaxrs;
 
 import javax.servlet.annotation.WebFilter;
 
-import com.x.base.core.project.jaxrs.AnonymousCipherManagerUserJaxrsFilter;
+import com.x.base.core.project.jaxrs.CipherManagerUserJaxrsFilter;
 
 @WebFilter(urlPatterns = "/jaxrs/applications/*", asyncSupported = true)
-public class ApplicationsJaxrsFilter extends AnonymousCipherManagerUserJaxrsFilter {
+public class ApplicationsJaxrsFilter extends CipherManagerUserJaxrsFilter {
 
 }
