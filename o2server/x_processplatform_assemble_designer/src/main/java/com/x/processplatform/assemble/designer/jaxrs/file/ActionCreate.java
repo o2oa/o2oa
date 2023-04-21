cde51e42f44c534d1e4ef487b349aa96844e0bf4
胡起 <hq_1980@hotmail.com>
2023-04-21 05:56:25 +0000

    Merge branch 'fix/releaseStatement' into 'release'
    流程版本脚本版本保存导致id重复.
    
    See merge request o2oa/o2oa!1710
    
    (cherry picked from commit b2406aa54729e51e820b3cca9f2e099fd5d3d9cf)
    
    46a3fb1d 流程版本脚本版本保存导致id重复.
    f3ee3979 代码清理,删除desinger中的projection

--- o2server/x_processplatform_assemble_designer/src/main/java/com/x/processplatform/assemble/designer/jaxrs/file/ActionCreate.java
+++ o2server/x_processplatform_assemble_designer/src/main/java/com/x/processplatform/assemble/designer/jaxrs/file/ActionCreate.java
@@ -13,6 +13,7 @@ import com.x.base.core.project.cache.CacheManager;
 import com.x.base.core.project.exception.ExceptionAccessDenied;
 import com.x.base.core.project.exception.ExceptionDuplicateFlag;
 import com.x.base.core.project.exception.ExceptionDuplicateRestrictFlag;
+import com.x.base.core.project.exception.ExceptionEntityExist;
 import com.x.base.core.project.exception.ExceptionEntityNotExist;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
@@ -23,57 +24,60 @@ import com.x.processplatform.core.entity.element.Application;
 import com.x.processplatform.core.entity.element.File;
 
 class ActionCreate extends BaseAction {
-	ActionResult<Wo> execute(EffectivePerson effectivePerson, JsonElement jsonElement) throws Exception {
-		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-			ActionResult<Wo> result = new ActionResult<>();
-			Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
-			Business business = new Business(emc);
-			Application application = emc.flag(wi.getApplication(), Application.class);
-			if (null == application) {
-				throw new ExceptionEntityNotExist(wi.getApplication(), Application.class);
-			}
-			if (!business.editable(effectivePerson, application)) {
-				throw new ExceptionAccessDenied(effectivePerson);
-			}
-			File file = new File();
-			Wi.copier.copy(wi, file);
-			/** 设置file 的Id由前端生成提供 */
-			file.setId(wi.getId());
-			file.setApplication(application.getId());
-			this.updateCreator(file, effectivePerson);
-			emc.beginTransaction(File.class);
-			if (StringUtils.isNotEmpty(file.getAlias())) {
-				if (emc.duplicateWithFlags(file.getId(), File.class, file.getAlias())) {
-					throw new ExceptionDuplicateFlag(File.class, file.getAlias());
-				}
-			}
-			if (StringUtils.isEmpty(file.getName())) {
-				throw new ExceptionEmptyName();
-			}
-			if (emc.duplicateWithRestrictFlags(File.class, File.application_FIELDNAME, file.getApplication(),
-					file.getId(), ListTools.toList(file.getName()))) {
-				throw new ExceptionDuplicateRestrictFlag(File.class, file.getName());
-			}
-			emc.persist(file, CheckPersistType.all);
-			emc.commit();
-			CacheManager.notify(File.class);
-			Wo wo = new Wo();
-			wo.setId(file.getId());
-			result.setData(wo);
-			return result;
-		}
-	}
+    ActionResult<Wo> execute(EffectivePerson effectivePerson, JsonElement jsonElement) throws Exception {
+        try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+            ActionResult<Wo> result = new ActionResult<>();
+            Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
+            Business business = new Business(emc);
+            Application application = emc.flag(wi.getApplication(), Application.class);
+            if (null == application) {
+                throw new ExceptionEntityNotExist(wi.getApplication(), Application.class);
+            }
+            if (!business.editable(effectivePerson, application)) {
+                throw new ExceptionAccessDenied(effectivePerson);
+            }
+            if (emc.duplicateWithFlags(File.class, wi.getId())) {
+                throw new ExceptionEntityExist(wi.getId());
+            }
+            File file = new File();
+            Wi.copier.copy(wi, file);
+            /** 设置file 的Id由前端生成提供 */
+            file.setId(wi.getId());
+            file.setApplication(application.getId());
+            this.updateCreator(file, effectivePerson);
+            emc.beginTransaction(File.class);
+            if (StringUtils.isNotEmpty(file.getAlias())) {
+                if (emc.duplicateWithFlags(file.getId(), File.class, file.getAlias())) {
+                    throw new ExceptionDuplicateFlag(File.class, file.getAlias());
+                }
+            }
+            if (StringUtils.isEmpty(file.getName())) {
+                throw new ExceptionEmptyName();
+            }
+            if (emc.duplicateWithRestrictFlags(File.class, File.application_FIELDNAME, file.getApplication(),
+                    file.getId(), ListTools.toList(file.getName()))) {
+                throw new ExceptionDuplicateRestrictFlag(File.class, file.getName());
+            }
+            emc.persist(file, CheckPersistType.all);
+            emc.commit();
+            CacheManager.notify(File.class);
+            Wo wo = new Wo();
+            wo.setId(file.getId());
+            result.setData(wo);
+            return result;
+        }
+    }
 
-	public static class Wo extends WoId {
-	}
+    public static class Wo extends WoId {
+    }
 
-	public static class Wi extends File {
+    public static class Wi extends File {
 
-		private static final long serialVersionUID = 4289841165185269299L;
+        private static final long serialVersionUID = 4289841165185269299L;
 
-		static WrapCopier<Wi, File> copier = WrapCopierFactory.wi(Wi.class, File.class, null,
-				JpaObject.FieldsUnmodifyExcludeId);
+        static WrapCopier<Wi, File> copier = WrapCopierFactory.wi(Wi.class, File.class, null,
+                JpaObject.FieldsUnmodifyExcludeId);
 
-	}
+    }
 
 }
