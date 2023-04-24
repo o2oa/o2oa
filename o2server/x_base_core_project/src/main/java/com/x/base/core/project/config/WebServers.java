cae2491c7fc75d965031b302190d8b7795ee54cc
zhourui <zhouuri@o2oa.net>
2023-04-24 16:15:49 +0800

    query配置文件中增加enable,并写入到config

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/config/WebServers.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/config/WebServers.java
@@ -22,150 +22,151 @@ import com.x.base.core.project.tools.DefaultCharset;
 
 public class WebServers extends ConcurrentSkipListMap<String, WebServer> {
 
-	private static final long serialVersionUID = -706102090064680898L;
+    private static final long serialVersionUID = -706102090064680898L;
 
-	private static final Random RANDOM = new SecureRandom();
+    private static final Random RANDOM = new SecureRandom();
 
-	public WebServers() {
-		super();
-	}
+    public WebServers() {
+        super();
+    }
 
-	public WebServers(Nodes nodeConfigs) {
-		for (Entry<String, Node> o : nodeConfigs.entrySet()) {
-			WebServer server = o.getValue().getWeb();
-			if ((null != server) && BooleanUtils.isTrue(server.getEnable())) {
-				this.put(o.getKey(), server);
-			}
-		}
-	}
+    public WebServers(Nodes nodeConfigs) {
+        for (Entry<String, Node> o : nodeConfigs.entrySet()) {
+            WebServer server = o.getValue().getWeb();
+            if ((null != server) && BooleanUtils.isTrue(server.getEnable())) {
+                this.put(o.getKey(), server);
+            }
+        }
+    }
 
-	public Entry<String, WebServer> getRandom() throws IllegalStateException {
-		List<Entry<String, WebServer>> list = new ArrayList<>();
-		for (Entry<String, WebServer> o : this.entrySet()) {
-			if (BooleanUtils.isTrue(o.getValue().getEnable())) {
-				list.add(o);
-			}
-		}
-		if (!list.isEmpty()) {
-			return list.get(RANDOM.nextInt(list.size()));
-		}
-		throw new IllegalStateException("randomWithWeight error.");
-	}
+    public Entry<String, WebServer> getRandom() throws IllegalStateException {
+        List<Entry<String, WebServer>> list = new ArrayList<>();
+        for (Entry<String, WebServer> o : this.entrySet()) {
+            if (BooleanUtils.isTrue(o.getValue().getEnable())) {
+                list.add(o);
+            }
+        }
+        if (!list.isEmpty()) {
+            return list.get(RANDOM.nextInt(list.size()));
+        }
+        throw new IllegalStateException("randomWithWeight error.");
+    }
 
-	public static void updateWebServerConfigJson() throws Exception {
-		File dir = new File(Config.base(), "servers/webServer/x_desktop/res/config");
-		FileUtils.forceMkdir(dir);
-		File file = new File(dir, "config.json");
-		Gson gson = XGsonBuilder.instance();
-		LinkedHashMap<String, Object> map = new LinkedHashMap<>();
-		// 三服务器合一运行不需要输出centers,否则需要输出centers
-		if ((!Objects.equals(Config.currentNode().getWeb().getPort(), Config.currentNode().getApplication().getPort()))
-				|| (!Objects.equals(Config.currentNode().getWeb().getPort(),
-						Config.currentNode().getCenter().getPort()))) {
-			List<Map<String, String>> centers = new ArrayList<>();
-			map.put("center", centers);
-			Map<String, String> center = new HashMap<>();
-			center.put("host", "");
-			center.put("port", Config.currentNode().getCenter().getPort().toString());
-			centers.add(center);
-			if (!Objects.equals(Config.currentNode().getCenter().getProxyPort(),
-					Config.currentNode().getCenter().getPort())) {
-				center = new HashMap<>();
-				center.put("host", "");
-				center.put("port", Config.currentNode().getCenter().getProxyPort().toString());
-				centers.add(center);
-			}
-		}
-		// 写入systemName
-		map.put("footer", Config.collect().getFooter());
-		map.put("title", Config.collect().getTitle());
-		map.put("version", Config.version());
-		map.put("appUrl", Config.collect().getAppUrl());
-		map.put("app_protocol", "auto");
+    public static void updateWebServerConfigJson() throws Exception {
+        File dir = new File(Config.base(), "servers/webServer/x_desktop/res/config");
+        FileUtils.forceMkdir(dir);
+        File file = new File(dir, "config.json");
+        Gson gson = XGsonBuilder.instance();
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+        // 三服务器合一运行不需要输出centers,否则需要输出centers
+        if ((!Objects.equals(Config.currentNode().getWeb().getPort(), Config.currentNode().getApplication().getPort()))
+                || (!Objects.equals(Config.currentNode().getWeb().getPort(),
+                        Config.currentNode().getCenter().getPort()))) {
+            List<Map<String, String>> centers = new ArrayList<>();
+            map.put("center", centers);
+            Map<String, String> center = new HashMap<>();
+            center.put("host", "");
+            center.put("port", Config.currentNode().getCenter().getPort().toString());
+            centers.add(center);
+            if (!Objects.equals(Config.currentNode().getCenter().getProxyPort(),
+                    Config.currentNode().getCenter().getPort())) {
+                center = new HashMap<>();
+                center.put("host", "");
+                center.put("port", Config.currentNode().getCenter().getProxyPort().toString());
+                centers.add(center);
+            }
+        }
+        // 写入systemName
+        map.put("footer", Config.collect().getFooter());
+        map.put("title", Config.collect().getTitle());
+        map.put("version", Config.version());
+        map.put("appUrl", Config.collect().getAppUrl());
+        map.put("app_protocol", "auto");
 
-		map.put("loginPage", Config.portal().getLoginPage());
-		map.put("indexPage", Config.portal().getIndexPage());
-		map.put("urlMapping", Config.portal().getUrlMapping());
+        map.put("loginPage", Config.portal().getLoginPage());
+        map.put("indexPage", Config.portal().getIndexPage());
+        map.put("urlMapping", Config.portal().getUrlMapping());
+        map.put("searchEnable", Config.query().index().getEnable() && Config.query().index().getSearchEnable());
 
-		writeWebServerConfigPasswordPolicy(map);
-		writeWebServerConfigLanguage(map);
-		writeWebServerConfigTokenName(map);
-		writeWebServerConfigRsa(map);
-		writeWebServerConfigSafeLogout(map);
-		writeWebServerConfigWebSocketEnable(map);
+        writeWebServerConfigPasswordPolicy(map);
+        writeWebServerConfigLanguage(map);
+        writeWebServerConfigTokenName(map);
+        writeWebServerConfigRsa(map);
+        writeWebServerConfigSafeLogout(map);
+        writeWebServerConfigWebSocketEnable(map);
 
-		for (Entry<String, JsonElement> en : Config.web().entrySet()) {
-			map.put(en.getKey(), en.getValue());
-		}
+        for (Entry<String, JsonElement> en : Config.web().entrySet()) {
+            map.put(en.getKey(), en.getValue());
+        }
 
-		for (Entry<String, JsonElement> en : Config.mock().entrySet()) {
-			map.put(en.getKey(), en.getValue());
-		}
+        for (Entry<String, JsonElement> en : Config.mock().entrySet()) {
+            map.put(en.getKey(), en.getValue());
+        }
 
-		FileUtils.writeStringToFile(file, gson.toJson(map), DefaultCharset.charset);
-	}
+        FileUtils.writeStringToFile(file, gson.toJson(map), DefaultCharset.charset);
+    }
 
-	/**
-	 * 是否启用安全注销
-	 * 
-	 * @param map
-	 * @throws Exception
-	 */
-	private static void writeWebServerConfigSafeLogout(LinkedHashMap<String, Object> map) throws Exception {
-		map.put("enableSafeLogout", Config.person().getEnableSafeLogout());
-	}
+    /**
+     * 是否启用安全注销
+     * 
+     * @param map
+     * @throws Exception
+     */
+    private static void writeWebServerConfigSafeLogout(LinkedHashMap<String, Object> map) throws Exception {
+        map.put("enableSafeLogout", Config.person().getEnableSafeLogout());
+    }
 
-	/**
-	 * 如果启用了rsa加密,输出public.key
-	 * 
-	 * @param map
-	 * @throws Exception
-	 * @throws IOException
-	 */
-	private static void writeWebServerConfigRsa(LinkedHashMap<String, Object> map) throws Exception {
-		map.put("publicKey", BooleanUtils.isTrue(Config.token().getRsaEnable()) ? Config.publicKey() : "");
-	}
+    /**
+     * 如果启用了rsa加密,输出public.key
+     * 
+     * @param map
+     * @throws Exception
+     * @throws IOException
+     */
+    private static void writeWebServerConfigRsa(LinkedHashMap<String, Object> map) throws Exception {
+        map.put("publicKey", BooleanUtils.isTrue(Config.token().getRsaEnable()) ? Config.publicKey() : "");
+    }
 
-	/**
-	 * 平台TokenName
-	 * 
-	 * @param map
-	 * @throws Exception
-	 */
-	private static void writeWebServerConfigTokenName(LinkedHashMap<String, Object> map) throws Exception {
-		map.put("tokenName", Config.person().getTokenName());
-	}
+    /**
+     * 平台TokenName
+     * 
+     * @param map
+     * @throws Exception
+     */
+    private static void writeWebServerConfigTokenName(LinkedHashMap<String, Object> map) throws Exception {
+        map.put("tokenName", Config.person().getTokenName());
+    }
 
-	/**
-	 * 平台语言
-	 * 
-	 * @param map
-	 * @throws Exception
-	 */
-	private static void writeWebServerConfigLanguage(LinkedHashMap<String, Object> map) throws Exception {
-		map.put("language", Config.person().getLanguage());
-	}
+    /**
+     * 平台语言
+     * 
+     * @param map
+     * @throws Exception
+     */
+    private static void writeWebServerConfigLanguage(LinkedHashMap<String, Object> map) throws Exception {
+        map.put("language", Config.person().getLanguage());
+    }
 
-	/**
-	 * 是否启用webSocket链接
-	 * 
-	 * @param map
-	 * @throws Exception
-	 */
-	private static void writeWebServerConfigWebSocketEnable(LinkedHashMap<String, Object> map) throws Exception {
-		map.put("webSocketEnable", Config.general().getWebSocketEnable());
-	}
+    /**
+     * 是否启用webSocket链接
+     * 
+     * @param map
+     * @throws Exception
+     */
+    private static void writeWebServerConfigWebSocketEnable(LinkedHashMap<String, Object> map) throws Exception {
+        map.put("webSocketEnable", Config.general().getWebSocketEnable());
+    }
 
-	/**
-	 * 写入密码规则
-	 * 
-	 * @param map
-	 * @throws Exception
-	 */
-	private static void writeWebServerConfigPasswordPolicy(LinkedHashMap<String, Object> map) throws Exception {
-		map.put("passwordRegex", Config.person().getPasswordRegex());
-		map.put("passwordRegexHint", Config.person().getPasswordRegexHint());
-	}
+    /**
+     * 写入密码规则
+     * 
+     * @param map
+     * @throws Exception
+     */
+    private static void writeWebServerConfigPasswordPolicy(LinkedHashMap<String, Object> map) throws Exception {
+        map.put("passwordRegex", Config.person().getPasswordRegex());
+        map.put("passwordRegexHint", Config.person().getPasswordRegexHint());
+    }
 
 //	/**
 //	 * 写入是否启用了center和application的代理
