377d0503e7f3326a3420e9281b26685f50cad940
o2null <zhourui@o2oa.net>
2023-05-15 17:44:41 +0800

    update

--- o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/work/V2ListActivityGoBack.java
+++ o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/work/V2ListActivityGoBack.java
@@ -11,7 +11,6 @@ import java.util.stream.Collectors;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import com.google.gson.JsonElement;
 import com.x.base.core.container.EntityManagerContainer;
 import com.x.base.core.container.factory.EntityManagerContainerFactory;
 import com.x.base.core.project.exception.ExceptionAccessDenied;
@@ -38,13 +37,9 @@ class V2ListActivityGoBack extends BaseAction {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(V2ListActivityGoBack.class);
 
-	ActionResult<List<Wo>> execute(EffectivePerson effectivePerson, String id, JsonElement jsonElement)
-			throws Exception {
+	ActionResult<List<Wo>> execute(EffectivePerson effectivePerson, String id) throws Exception {
 
-		LOGGER.debug("execute:{}, id:{}, jsonElement:{}.", effectivePerson::getDistinguishedName, () -> id,
-				() -> jsonElement);
-
-		Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
+		LOGGER.debug("execute:{}, id:{}.", effectivePerson::getDistinguishedName, () -> id);
 
 		ActionResult<List<Wo>> result = new ActionResult<>();
 
@@ -77,24 +72,16 @@ class V2ListActivityGoBack extends BaseAction {
 				Node node = workLogTree.location(work);
 				if (null != node) {
 					List<WorkLog> workLogs = new ArrayList<>();
-					if (BooleanUtils.isNotFalse(wi.getLastOnly())) {
-						// 过滤掉未链接的,过滤掉不是manual活动的,每个活动只取最近一次的workLog
-						workLogs = workLogTree.up(node).stream().map(Node::getWorkLog)
-								.filter(o -> Objects.equals(o.getArrivedActivityType(), ActivityType.manual)
-										&& BooleanUtils.isTrue(o.getConnected()))
-								.collect(Collectors.groupingBy(WorkLog::getFromActivity)).entrySet().stream()
-								.map(o -> o.getValue().get(0)).collect(Collectors.toList());
-					} else {
-						workLogs = workLogTree.up(node).stream().map(Node::getWorkLog)
-								.filter(o -> Objects.equals(o.getArrivedActivityType(), ActivityType.manual)
-										&& BooleanUtils.isTrue(o.getConnected()))
-								.collect(Collectors.toList());
-					}
+					// 过滤掉未链接的,过滤掉不是manual活动的,每个活动只取最近一次的workLog
+					workLogs = workLogTree.up(node).stream().map(Node::getWorkLog)
+							.filter(o -> Objects.equals(o.getArrivedActivityType(), ActivityType.manual)
+									&& BooleanUtils.isTrue(o.getConnected()))
+							.collect(Collectors.groupingBy(WorkLog::getFromActivity)).entrySet().stream()
+							.map(o -> o.getValue().get(0)).collect(Collectors.toList());
 					List<Wo> wos = this.list(manual, workLogs);
 					wos = this.supplement(business, manual, wos);
 				}
 			}
-
 		}
 		return result;
 
@@ -110,7 +97,7 @@ class V2ListActivityGoBack extends BaseAction {
 			if (opt.isPresent()) {
 				Wo wo = new Wo();
 				wo.setActivity(opt.get().getFromActivity());
-				wo.setLastUpdateTime(opt.get().getFromTime());
+				wo.setLastModifyTime(opt.get().getFromTime());
 				wo.setActivityToken(opt.get().getFromActivityToken());
 				wo.setWay(manual.getGoBackConfig().getWay());
 				list.add(wo);
@@ -123,7 +110,7 @@ class V2ListActivityGoBack extends BaseAction {
 					Wo wo = new Wo();
 					wo.setActivity(opt.get().getActivity());
 					wo.setWay(opt.get().getWay());
-					wo.setLastUpdateTime(o.getFromTime());
+					wo.setLastModifyTime(o.getFromTime());
 					wo.setActivityToken(o.getFromActivityToken());
 					list.add(wo);
 				}
@@ -132,7 +119,7 @@ class V2ListActivityGoBack extends BaseAction {
 			workLogs.stream().forEach(o -> {
 				Wo wo = new Wo();
 				wo.setActivity(o.getFromActivity());
-				wo.setLastUpdateTime(o.getFromTime());
+				wo.setLastModifyTime(o.getFromTime());
 				wo.setActivityToken(o.getFromActivityToken());
 				wo.setWay(manual.getGoBackConfig().getWay());
 				list.add(wo);
@@ -180,25 +167,17 @@ class V2ListActivityGoBack extends BaseAction {
 		private static final long serialVersionUID = -7690519507479509393L;
 
 		// 上次处理时间
-		private Date lastUpdateTime;
+		private Date lastModifyTime;
 		// 返回方式
 		private String way;
-		// 返回方式
-		private String activityToken;
 		// 活动标识
 		private String activity;
+		// 活动标识
+		private String activityToken;
 		// 活动名称
 		private String name;
 		// 上次处理人
 		private List<String> lastIdentityList = new ArrayList<>();
-
-		public String getActivityToken() {
-			return activityToken;
-		}
-
-		public void setActivityToken(String activityToken) {
-			this.activityToken = activityToken;
-		}
 
 		public String getName() {
 			return name;
@@ -224,12 +203,12 @@ class V2ListActivityGoBack extends BaseAction {
 			this.way = way;
 		}
 
-		public Date getLastUpdateTime() {
-			return lastUpdateTime;
+		public Date getLastModifyTime() {
+			return lastModifyTime;
 		}
 
-		public void setLastUpdateTime(Date lastUpdateTime) {
-			this.lastUpdateTime = lastUpdateTime;
+		public void setLastModifyTime(Date lastModifyTime) {
+			this.lastModifyTime = lastModifyTime;
 		}
 
 		public List<String> getLastIdentityList() {
@@ -240,22 +219,14 @@ class V2ListActivityGoBack extends BaseAction {
 			this.lastIdentityList = lastIdentityList;
 		}
 
-	}
-
-	public static class Wi extends GsonPropertyObject {
-
-		private static final long serialVersionUID = -6178473080370914961L;
-
-		// 重复经过的活动仅取最近的,默认为true
-		private Boolean lastOnly;
-
-		public Boolean getLastOnly() {
-			return lastOnly;
+		public String getActivityToken() {
+			return activityToken;
 		}
 
-		public void setLastOnly(Boolean lastOnly) {
-			this.lastOnly = lastOnly;
+		public void setActivityToken(String activityToken) {
+			this.activityToken = activityToken;
 		}
 
 	}
+
 }
