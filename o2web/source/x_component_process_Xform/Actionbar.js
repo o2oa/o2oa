125273ee237ed84531c8b0b68e2aeb24f6bf8867
huqi <hq_1980@hotmail.com>
2023-05-17 14:42:39 +0800

    增加人工活动前端退回

--- o2web/source/x_component_process_Xform/Actionbar.js
+++ o2web/source/x_component_process_Xform/Actionbar.js
@@ -148,8 +148,18 @@ MWF.xApplication.process.Xform.Actionbar = MWF.APPActionbar =  new Class(
                         }
                     }
                 }
+
+
+                if (this.toolbarWidget.items["action_goBack"]){
+                    // 异步判断是否有可退回的活动
+                    o2.Actions.load('x_processplatform_assemble_surface').WorkAction.V2ListActivityGoBack(this.form.businessData.task.work, function(json){
+                        if (json.data.length){
+                            this.toolbarWidget.items["action_goBack"].show();
+                        }
+                    }.bind(this));
+                }
+
             }.bind(this));
-            // }
         },
 
         setCustomToolbars: function(tools, node){
@@ -229,10 +239,6 @@ MWF.xApplication.process.Xform.Actionbar = MWF.APPActionbar =  new Class(
                 flag = flag && (!hideFlag);
             }
 
-            if (tool.id === "action_goBack"){
-                flag = false;
-            }
-
             if (tool.id == "action_downloadAll" || tool.id == "action_print"){
                 if (!this.form.businessData.work.startTime){
                     flag = false;
@@ -244,7 +250,6 @@ MWF.xApplication.process.Xform.Actionbar = MWF.APPActionbar =  new Class(
                 }
             }
 
-
             if (tool.id == "action_rollback") tool.read = true;
             if (readonly) if (!tool.read) flag = false;
             if (flag){
@@ -255,7 +260,8 @@ MWF.xApplication.process.Xform.Actionbar = MWF.APPActionbar =  new Class(
                     "MWFButtonImage": this.getImagePath(tool.img, tool.customImg),
                     "title": tool.title,
                     "MWFButtonAction": tool.action,
-                    "MWFButtonText": tool.text
+                    "MWFButtonText": tool.text,
+                    "MWFHide": (tool.id === "action_goBack") ? 'true' : ''
                 }).inject(node);
                 if( this.json.iconOverStyle ){
                     actionNode.set("MWFButtonImageOver" , this.getImageOverPath(tool.img, tool.customImg) );
