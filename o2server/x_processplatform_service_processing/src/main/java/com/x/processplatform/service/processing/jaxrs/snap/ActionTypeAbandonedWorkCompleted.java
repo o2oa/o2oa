9ba501bb000388559837f913ad64f24b45735708
o2null <zhourui@o2oa.net>
2023-06-16 12:51:19 +0800

    修复删除工作和已完成工作时没有同步执行删除索引文件.

--- o2server/x_processplatform_service_processing/src/main/java/com/x/processplatform/service/processing/jaxrs/snap/ActionTypeAbandonedWorkCompleted.java
+++ o2server/x_processplatform_service_processing/src/main/java/com/x/processplatform/service/processing/jaxrs/snap/ActionTypeAbandonedWorkCompleted.java
@@ -33,75 +33,76 @@ import com.x.query.core.entity.Item;
 
 class ActionTypeAbandonedWorkCompleted extends BaseAction {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ActionTypeAbandonedWorkCompleted.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(ActionTypeAbandonedWorkCompleted.class);
 
-    ActionResult<Wo> execute(EffectivePerson effectivePerson, String workCompletedId) throws Exception {
+	ActionResult<Wo> execute(EffectivePerson effectivePerson, String workCompletedId) throws Exception {
 
-        LOGGER.debug("execute:{}, workCompletedId:{}.", effectivePerson::getDistinguishedName, () -> workCompletedId);
+		LOGGER.debug("execute:{}, workCompletedId:{}.", effectivePerson::getDistinguishedName, () -> workCompletedId);
 
-        String job = null;
-        try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-            WorkCompleted workCompleted = emc.find(workCompletedId, WorkCompleted.class);
-            if (null == workCompleted) {
-                throw new ExceptionEntityNotExist(workCompletedId, WorkCompleted.class);
-            }
-            job = workCompleted.getJob();
-        }
-        return ProcessPlatformExecutorFactory.get(job).submit(new CallableImpl(workCompletedId)).get(300,
-                TimeUnit.SECONDS);
-    }
+		String job = null;
+		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+			WorkCompleted workCompleted = emc.find(workCompletedId, WorkCompleted.class);
+			if (null == workCompleted) {
+				throw new ExceptionEntityNotExist(workCompletedId, WorkCompleted.class);
+			}
+			job = workCompleted.getJob();
+		}
+		return ProcessPlatformExecutorFactory.get(job).submit(new CallableImpl(workCompletedId)).get(300,
+				TimeUnit.SECONDS);
+	}
 
-    public class CallableImpl implements Callable<ActionResult<Wo>> {
+	public class CallableImpl implements Callable<ActionResult<Wo>> {
 
-        private String id;
+		private String id;
 
-        public CallableImpl(String id) {
-            this.id = id;
-        }
+		public CallableImpl(String id) {
+			this.id = id;
+		}
 
-        public ActionResult<Wo> call() throws Exception {
-            ActionResult<Wo> result = new ActionResult<>();
-            try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-                Business business = new Business(emc);
-                WorkCompleted workCompleted = emc.find(id, WorkCompleted.class);
-                if (null == workCompleted) {
-                    throw new ExceptionEntityNotExist(id, WorkCompleted.class);
-                }
-                Snap snap = new Snap(workCompleted);
-                List<Item> items = new ArrayList<>();
-                List<TaskCompleted> taskCompleteds = new ArrayList<>();
-                List<Read> reads = new ArrayList<>();
-                List<ReadCompleted> readCompleteds = new ArrayList<>();
-                List<Review> reviews = new ArrayList<>();
-                List<WorkLog> workLogs = new ArrayList<>();
-                List<Record> records = new ArrayList<>();
-                List<Attachment> attachments = new ArrayList<>();
-                List<DocSign> docSigns = new ArrayList<>();
-                List<DocSignScrawl> docSignScrawls = new ArrayList<>();
-                snap.setProperties(snap(business, workCompleted.getJob(), items, workCompleted, taskCompleteds, reads,
-                        readCompleteds, reviews, workLogs, records, attachments, docSigns, docSignScrawls));
-                snap.setType(Snap.TYPE_ABANDONEDWORKCOMPLETED);
-                emc.beginTransaction(Snap.class);
-                emc.persist(snap, CheckPersistType.all);
-                emc.commit();
-                clean(business, items, workCompleted, taskCompleteds, reads, readCompleteds, reviews, workLogs, records,
-                        attachments, docSigns, docSignScrawls);
-                emc.commit();
-                emc.beginTransaction(WorkEvent.class);
-                emc.persist(WorkCompletedEvent.deleteEventInstance(workCompleted), CheckPersistType.all);
-                emc.commit();
-                Wo wo = new Wo();
-                wo.setId(snap.getId());
-                result.setData(wo);
-                return result;
-            }
-        }
-    }
+		public ActionResult<Wo> call() throws Exception {
+			ActionResult<Wo> result = new ActionResult<>();
+			try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+				Business business = new Business(emc);
+				WorkCompleted workCompleted = emc.find(id, WorkCompleted.class);
+				if (null == workCompleted) {
+					throw new ExceptionEntityNotExist(id, WorkCompleted.class);
+				}
+				Snap snap = new Snap(workCompleted);
+				List<Item> items = new ArrayList<>();
+				List<TaskCompleted> taskCompleteds = new ArrayList<>();
+				List<Read> reads = new ArrayList<>();
+				List<ReadCompleted> readCompleteds = new ArrayList<>();
+				List<Review> reviews = new ArrayList<>();
+				List<WorkLog> workLogs = new ArrayList<>();
+				List<Record> records = new ArrayList<>();
+				List<Attachment> attachments = new ArrayList<>();
+				List<DocSign> docSigns = new ArrayList<>();
+				List<DocSignScrawl> docSignScrawls = new ArrayList<>();
+				snap.setProperties(snap(business, workCompleted.getJob(), items, workCompleted, taskCompleteds, reads,
+						readCompleteds, reviews, workLogs, records, attachments, docSigns, docSignScrawls));
+				snap.setType(Snap.TYPE_ABANDONEDWORKCOMPLETED);
+				emc.beginTransaction(Snap.class);
+				emc.persist(snap, CheckPersistType.all);
+				emc.commit();
+				clean(business, items, workCompleted, taskCompleteds, reads, readCompleteds, reviews, workLogs, records,
+						attachments, docSigns, docSignScrawls);
+				emc.commit();
+				// 创建已完成工作删除事件
+				emc.beginTransaction(WorkCompletedEvent.class);
+				emc.persist(WorkCompletedEvent.deleteEventInstance(workCompleted), CheckPersistType.all);
+				emc.commit();
+				Wo wo = new Wo();
+				wo.setId(snap.getId());
+				result.setData(wo);
+				return result;
+			}
+		}
+	}
 
-    public static class Wo extends WoId {
+	public static class Wo extends WoId {
 
-        private static final long serialVersionUID = -2577413577740827608L;
+		private static final long serialVersionUID = -2577413577740827608L;
 
-    }
+	}
 
 }
