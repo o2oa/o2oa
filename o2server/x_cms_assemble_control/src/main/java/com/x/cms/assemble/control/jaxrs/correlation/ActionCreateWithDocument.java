cb6561e8ca827daaa8f5b406243457a5422a05fd
o2sword <171715986@qq.com>
2023-06-28 16:43:20 +0800

    内容管理关联文档

---
+++ o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/correlation/ActionCreateWithDocument.java
@@ -0,0 +1,141 @@
+package com.x.cms.assemble.control.jaxrs.correlation;
+
+import com.google.gson.JsonElement;
+import com.x.base.core.container.EntityManagerContainer;
+import com.x.base.core.container.factory.EntityManagerContainerFactory;
+import com.x.base.core.entity.JpaObject;
+import com.x.base.core.project.Applications;
+import com.x.base.core.project.annotation.FieldDescribe;
+import com.x.base.core.project.exception.ExceptionAccessDenied;
+import com.x.base.core.project.exception.ExceptionEntityExist;
+import com.x.base.core.project.exception.ExceptionEntityNotExist;
+import com.x.base.core.project.gson.GsonPropertyObject;
+import com.x.base.core.project.http.ActionResult;
+import com.x.base.core.project.http.EffectivePerson;
+import com.x.base.core.project.logger.Logger;
+import com.x.base.core.project.logger.LoggerFactory;
+import com.x.base.core.project.tools.ListTools;
+import com.x.base.core.project.x_correlation_service_processing;
+import com.x.cms.assemble.control.Business;
+import com.x.cms.assemble.control.ThisApplication;
+import com.x.cms.core.entity.Document;
+import com.x.correlation.core.entity.content.Correlation;
+import com.x.correlation.core.express.service.processing.jaxrs.correlation.ActionCreateTypeProcessPlatformWi;
+import com.x.correlation.core.express.service.processing.jaxrs.correlation.ActionCreateTypeProcessPlatformWo;
+import com.x.correlation.core.express.service.processing.jaxrs.correlation.TargetWi;
+import com.x.processplatform.core.entity.content.Work;
+import com.x.processplatform.core.entity.content.WorkCompleted;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ActionCreateWithDocument extends BaseAction {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ActionCreateWithDocument.class);
+
+	ActionResult<Wo> execute(EffectivePerson effectivePerson, String docId, JsonElement jsonElement) throws Exception {
+
+		LOGGER.debug("execute:{}, jsonElement:{}.", effectivePerson::getDistinguishedName, () -> jsonElement);
+
+		ActionResult<Wo> result = new ActionResult<>();
+
+		Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
+
+		ActionCreateTypeProcessPlatformWi req = new ActionCreateTypeProcessPlatformWi();
+
+		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+			Business business = new Business(emc);
+			Document document = emc.find(docId, Document.class);
+			if (null == document) {
+				throw new ExceptionEntityNotExist(docId);
+			}
+			if (!business.isDocumentEditor(effectivePerson, null, null, document)) {
+				throw new ExceptionAccessDenied(effectivePerson);
+			}
+			req.setTargetList(readTarget(effectivePerson, business, wi.getTargetList()));
+			req.setPerson(effectivePerson.getDistinguishedName());
+		}
+
+		if (ListTools.isNotEmpty(wi.getTargetList())) {
+			Wo wo = ThisApplication.context().applications()
+					.postQuery(effectivePerson.getDebugger(), x_correlation_service_processing.class,
+							Applications.joinQueryUri("correlation", "type", "cms", "document", docId), req, docId)
+					.getData(Wo.class);
+			result.setData(wo);
+		}
+		return result;
+	}
+
+	private List<TargetWi> readTarget(EffectivePerson effectivePerson, Business business, List<TargetWi> targets)
+			throws Exception {
+		List<TargetWi> list = new ArrayList<>();
+		if (ListTools.isNotEmpty(targets)) {
+			for (TargetWi targetWi : targets) {
+				if (StringUtils.equalsIgnoreCase(targetWi.getType(), Correlation.TYPE_PROCESSPLATFORM)) {
+					list.add(readTargetProcessPlatform(effectivePerson, business, targetWi.getBundle(),
+							targetWi.getSite()));
+				} else if (StringUtils.equalsIgnoreCase(targetWi.getType(), Correlation.TYPE_CMS)) {
+					list.add(readTargetCms(effectivePerson, business, targetWi.getBundle(), targetWi.getSite()));
+				} else {
+					throw new ExceptionAccessDenied(effectivePerson);
+				}
+			}
+		}
+		return list;
+	}
+
+	private TargetWi readTargetProcessPlatform(EffectivePerson effectivePerson, Business business, String bundle,
+			String site) throws Exception {
+		Work work = business.entityManagerContainer().firstEqual(Work.class, Work.job_FIELDNAME, bundle);
+		if (null == work) {
+			WorkCompleted workCompleted = business.entityManagerContainer().firstEqual(WorkCompleted.class,
+					WorkCompleted.job_FIELDNAME, bundle);
+			if (null == workCompleted) {
+				throw new ExceptionEntityExist(bundle);
+			}
+		}
+		TargetWi targetWi = new TargetWi();
+		targetWi.setType(Correlation.TYPE_PROCESSPLATFORM);
+		targetWi.setBundle(bundle);
+		targetWi.setSite(site);
+		return targetWi;
+	}
+
+	private TargetWi readTargetCms(EffectivePerson effectivePerson, Business business, String bundle, String site)
+			throws Exception {
+		Document document = business.entityManagerContainer().find(bundle, Document.class);
+		if (null == document) {
+			throw new ExceptionEntityExist(bundle);
+		}
+		TargetWi targetWi = new TargetWi();
+		targetWi.setType(Correlation.TYPE_CMS);
+		targetWi.setBundle(bundle);
+		targetWi.setSite(site);
+		return targetWi;
+	}
+
+	public static class Wi extends GsonPropertyObject {
+
+		private static final long serialVersionUID = 8167538341492974963L;
+
+		@FieldDescribe("关联目标.")
+		private List<TargetWi> targetList;
+
+		public List<TargetWi> getTargetList() {
+			return targetList;
+		}
+
+		public void setTargetList(List<TargetWi> targetList) {
+			this.targetList = targetList;
+		}
+
+	}
+
+	public static class Wo extends ActionCreateTypeProcessPlatformWo {
+
+		private static final long serialVersionUID = -6712098733513365965L;
+
+	}
+
+}
