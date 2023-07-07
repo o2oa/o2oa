8573d0f889c26c3bab3d67ca078ec6978f375c12
o2null <zhourui@o2oa.net>
2023-07-07 14:58:15 +0800

    冲突解决

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/x_organization_assemble_personal.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/x_organization_assemble_personal.java
@@ -7,10 +7,10 @@ import com.x.base.core.project.annotation.ModuleType;
 @Module(type = ModuleType.ASSEMBLE, category = ModuleCategory.OFFICIAL, name = "组织管理个人", packageName = "com.x.organization.assemble.personal", containerEntities = {
 		"com.x.organization.core.entity.Group", "com.x.organization.core.entity.Role",
 		"com.x.organization.core.entity.Person", "com.x.organization.core.entity.PersonAttribute",
-		"com.x.organization.core.entity.Identity", "com.x.organization.core.entity.Unit",
-		"com.x.organization.core.entity.UnitAttribute", "com.x.organization.core.entity.UnitDuty",
-		"com.x.organization.core.entity.Custom", "com.x.organization.core.entity.Definition",
-		"com.x.organization.core.entity.accredit.Empower",
+		"com.x.organization.core.entity.PersonExtend", "com.x.organization.core.entity.Identity",
+		"com.x.organization.core.entity.Unit", "com.x.organization.core.entity.UnitAttribute",
+		"com.x.organization.core.entity.UnitDuty", "com.x.organization.core.entity.Custom",
+		"com.x.organization.core.entity.Definition", "com.x.organization.core.entity.accredit.Empower",
 		"com.x.organization.core.entity.accredit.EmpowerLog" }, storeJars = { "x_organization_core_entity",
 				"x_organization_core_express" })
 public class x_organization_assemble_personal extends Deployable {
