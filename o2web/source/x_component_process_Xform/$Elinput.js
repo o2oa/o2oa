51d658dcd9c88f11cfebf5b6bd1eded18eb849f9
unknown <caixiangyi@zoneland.net>
2023-08-04 16:17:01 +0800

    修复视图和查询视图导出excel时可以修改文件名

--- o2web/source/x_component_process_Xform/$Elinput.js
+++ o2web/source/x_component_process_Xform/$Elinput.js
@@ -11,6 +11,7 @@ Object.assign(o2.APP$Elinput.prototype, {
     isReadonly : function(){
         return !!(this.readonly || this.json.isReadonly || this.form.json.isReadonly || this.isSectionMergeRead() );
     },
+    reload: function(){},
     __setValue: function(value){
         this.moduleValueAG = null;
         this._setBusinessData(value);
