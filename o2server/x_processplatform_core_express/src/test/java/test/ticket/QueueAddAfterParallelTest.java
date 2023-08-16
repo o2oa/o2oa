bb4c1654c744e1f9d8d158e4374145c1f9fc36c6
o2null <zhourui@o2oa.net>
2023-08-16 12:44:30 +0800

    update

---
+++ o2server/x_processplatform_core_express/src/test/java/test/ticket/QueueAddAfterParallelTest.java
@@ -0,0 +1,45 @@
+package test.ticket;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.x.processplatform.core.express.ticket.Ticket;
+import com.x.processplatform.core.express.ticket.Tickets;
+
+class SingleAddAfterParallelTest {
+
+	private static final List<Ticket> p1 = Arrays.asList(new Ticket("A", "LA"), new Ticket("B", "LB"),
+			new Ticket("C", "LC"));
+	private static final List<Ticket> p2 = Arrays.asList(new Ticket("E", "LE"), new Ticket("F", "LF"),
+			new Ticket("G", "LG"));
+	private static final List<Ticket> p3 = Arrays.asList(new Ticket("I", "LI"), new Ticket("J", "LJ"),
+			new Ticket("K", "LK"));
+
+	@DisplayName("B加签EFG,EFG并行处理")
+	@Test
+	void test01() {
+		Tickets tickets = Tickets.single(p1);
+		String value = tickets.bubble().stream().<String>map(Ticket::target).collect(Collectors.joining(","));
+		Assertions.assertEquals("A,B,C", value);
+		Optional<Ticket> opt = tickets.findTicketWithLabel("LB");
+		tickets.add(opt.get(), p2, false, Tickets.MODE_PARALLEL);
+		value = tickets.bubble().stream().<String>map(Ticket::target).collect(Collectors.joining(","));
+		Assertions.assertEquals("E,F,G", value);
+		tickets.completed("LF");
+		value = tickets.bubble().stream().<String>map(Ticket::target).collect(Collectors.joining(","));
+		Assertions.assertEquals("E,G", value);
+		tickets.completed("LE");
+		value = tickets.bubble().stream().<String>map(Ticket::target).collect(Collectors.joining(","));
+		Assertions.assertEquals("G", value);
+		tickets.completed("LG");
+		value = tickets.bubble().stream().<String>map(Ticket::target).collect(Collectors.joining(","));
+		Assertions.assertEquals("", value);
+	}
+
+}
