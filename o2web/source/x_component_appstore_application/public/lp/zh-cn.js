a1f1c77fc93f89105c83435f04a8c6622d29b0ce
huqi <hq_1980@hotmail.com>
2023-08-21 17:12:57 +0800

    增加应用市场应用更新时间
    (cherry picked from commit 5b2f3aa607c3f77dd3f87787f0637bcff50514d9)

--- o2web/source/x_component_appstore_application/public/lp/zh-cn.js
+++ o2web/source/x_component_appstore_application/public/lp/zh-cn.js
@@ -27,5 +27,6 @@ MWF.xApplication.appstore.application.LP = {
     "describe": "功能介绍",
     "installSteps": "安装及使用",
     "video": "视频",
-    "appPic": "应用图片"
+    "appPic": "应用图片",
+    "updateTime": "更新时间"
 }
