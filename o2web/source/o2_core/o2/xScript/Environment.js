337247fcc1a889e71f604370bdf5343efc9fe35b
unknown <caixiangyi@zoneland.net>
2023-08-30 14:30:00 +0800

    修改下拉组件配置中的首选项改为第一个选项
    (cherry picked from commit 280fb4a6be6b41b2812bde75803a85b6e8cbadaf)

--- o2web/source/o2_core/o2/xScript/Environment.js
+++ o2web/source/o2_core/o2/xScript/Environment.js
@@ -4015,7 +4015,7 @@ MWF.xScript.Environment = function(ev){
          * @param {Boolean} [target]  - 为true时，在当前页面打开启动的流程实例；否则打开新窗口。默认false。（当前表单或页面在浏览器单独打开的时候该参数有效。）
          * @param {Boolean} [latest]  - 为true时，如果当前用户已经创建了此流程的实例，并且没有流转过，直接调用此实例为新流程实例；否则创建一个新实例。默认false。
          * @param {Function} [afterCreated]  - 流程创建后的回调，可以获取到创建的流程Work对象（桌面模式）或者Window对象(浏览器模式)。
-         * @param {Boolean} [skipDraftCheck]  - 是否跳过新建检查(默认根据流程的新建检查配置，设置true则不进行新建检查。
+         * @param {Boolean} [skipDraftCheck]  - 是否跳过新建检查（默认根据流程的新建检查配置），设置true则不进行新建检查。
          * @example
          //启动一个发文管理实例
          this.form.startProcess("公文管理", "发文管理");
