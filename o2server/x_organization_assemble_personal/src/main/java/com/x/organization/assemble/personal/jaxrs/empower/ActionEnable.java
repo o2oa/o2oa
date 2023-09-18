b5dd68972158c4bc1c3fa868e60cdce46483f7c7
o2null <zhourui@o2oa.net>
2023-09-18 11:19:01 +0800

    修复可以绕过权限对其他人执行授权的漏洞.

--- o2server/x_organization_assemble_personal/src/main/java/com/x/organization/assemble/personal/jaxrs/empower/ActionEnable.java
+++ o2server/x_organization_assemble_personal/src/main/java/com/x/organization/assemble/personal/jaxrs/empower/ActionEnable.java
@@ -1,9 +1,12 @@
 package com.x.organization.assemble.personal.jaxrs.empower;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.x.base.core.container.EntityManagerContainer;
 import com.x.base.core.container.factory.EntityManagerContainerFactory;
 import com.x.base.core.entity.annotation.CheckPersistType;
 import com.x.base.core.project.cache.CacheManager;
+import com.x.base.core.project.exception.ExceptionAccessDenied;
 import com.x.base.core.project.exception.ExceptionEntityNotExist;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
@@ -27,6 +30,9 @@ class ActionEnable extends BaseAction {
 			Empower empower = emc.find(id, Empower.class);
 			if (null == empower) {
 				throw new ExceptionEntityNotExist(id, Empower.class);
+			}
+			if (effectivePerson.isNotManager() && effectivePerson.isNotPerson(empower.getFromPerson())) {
+				throw new ExceptionAccessDenied(effectivePerson, empower);
 			}
 			emc.beginTransaction(Empower.class);
 			empower.setEnable(true);
