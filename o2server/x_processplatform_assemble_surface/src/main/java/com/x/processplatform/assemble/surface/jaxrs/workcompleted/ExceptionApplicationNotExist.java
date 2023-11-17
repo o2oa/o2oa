6e085a3a58b2ddd719df5dac95d2634cc0c07b70
o2null <zhourui@o2oa.net>
2023-11-17 16:20:48 +0800

    update

---
+++ o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/workcompleted/ExceptionApplicationNotExist.java
@@ -0,0 +1,12 @@
+package com.x.processplatform.assemble.surface.jaxrs.workcompleted;
+
+import com.x.base.core.project.exception.LanguagePromptException;
+
+class ExceptionApplicationNotExist extends LanguagePromptException {
+
+	private static final long serialVersionUID = -9089355008820123519L;
+
+	ExceptionApplicationNotExist(String str) {
+		super("指定的应用不存在:{}.", str);
+	}
+}
