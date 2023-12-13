33896f61b0b28254ee990c7422034d0a5c21147f
o2null <zhourui@o2oa.net>
2023-12-13 18:35:48 +0800

    在cms fileInfo的ActionGet接口,在输出中增加control对象

--- o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/fileinfo/BaseAction.java
+++ o2server/x_cms_assemble_control/src/main/java/com/x/cms/assemble/control/jaxrs/fileinfo/BaseAction.java
@@ -1,17 +1,28 @@
 package com.x.cms.assemble.control.jaxrs.fileinfo;
 
+import java.util.List;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+
+import com.x.base.core.project.annotation.FieldDescribe;
 import com.x.base.core.project.cache.Cache;
 import com.x.base.core.project.config.Cms;
 import com.x.base.core.project.config.StorageMapping;
 import com.x.base.core.project.connection.CipherConnectionAction;
+import com.x.base.core.project.gson.GsonPropertyObject;
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.jaxrs.StandardJaxrsAction;
+import com.x.base.core.project.tools.ListTools;
+import com.x.cms.assemble.control.Business;
 import com.x.cms.assemble.control.ThisApplication;
-import com.x.cms.assemble.control.service.*;
+import com.x.cms.assemble.control.service.AppInfoServiceAdv;
+import com.x.cms.assemble.control.service.CategoryInfoServiceAdv;
+import com.x.cms.assemble.control.service.DocumentQueryService;
+import com.x.cms.assemble.control.service.FileInfoServiceAdv;
+import com.x.cms.assemble.control.service.LogService;
 import com.x.cms.core.entity.Document;
 import com.x.cms.core.entity.FileInfo;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.lang3.StringUtils;
 
 public class BaseAction extends StandardJaxrsAction {
 
@@ -82,6 +93,85 @@ public class BaseAction extends StandardJaxrsAction {
 			this.fileBase64 = fileBase64;
 		}
 
+	}
+
+	protected boolean read(EffectivePerson effectivePerson, FileInfo fileInfo, List<String> identities,
+			List<String> units) throws Exception {
+		boolean value = false;
+		if (effectivePerson.isPerson(fileInfo.getCreatorUid())
+				|| (ListTools.isEmpty(fileInfo.getReadIdentityList()) && ListTools.isEmpty(fileInfo.getReadUnitList()))
+				|| (ListTools.containsAny(identities, fileInfo.getReadIdentityList())
+						|| ListTools.containsAny(units, fileInfo.getReadUnitList()))) {
+			value = true;
+		}
+		return value;
+	}
+
+	protected boolean edit(EffectivePerson effectivePerson, FileInfo fileInfo, List<String> identities,
+			List<String> units) throws Exception {
+		boolean value = false;
+		if (effectivePerson.isPerson(fileInfo.getCreatorUid())
+				|| (ListTools.isEmpty(fileInfo.getEditIdentityList()) && ListTools.isEmpty(fileInfo.getEditUnitList()))
+				|| (ListTools.containsAny(identities, fileInfo.getEditIdentityList())
+						|| ListTools.containsAny(units, fileInfo.getEditUnitList()))) {
+			value = true;
+		}
+		return value;
+	}
+
+	protected boolean control(EffectivePerson effectivePerson, Business business, FileInfo fileInfo,
+			List<String> identities, List<String> units) throws Exception {
+		boolean value = false;
+		if (business.isManager(effectivePerson)) {
+			value = true;
+		} else if (effectivePerson.isPerson(fileInfo.getCreatorUid())) {
+			value = true;
+		} else if (ListTools.isEmpty(fileInfo.getControllerUnitList())
+				&& ListTools.isEmpty(fileInfo.getControllerIdentityList())) {
+			value = true;
+		} else {
+			if (ListTools.containsAny(identities, fileInfo.getControllerIdentityList())
+					|| ListTools.containsAny(units, fileInfo.getControllerUnitList())) {
+				value = true;
+			}
+		}
+		return value;
+	}
+
+	public abstract static class AbstractWoControl extends GsonPropertyObject {
+
+		private static final long serialVersionUID = -6077066445665811296L;
+
+		@FieldDescribe("可读.")
+		private Boolean allowRead = false;
+		@FieldDescribe("可写.")
+		private Boolean allowEdit = false;
+		@FieldDescribe("可管理.")
+		private Boolean allowControl = false;
+
+		public Boolean getAllowRead() {
+			return allowRead;
+		}
+
+		public void setAllowRead(Boolean allowRead) {
+			this.allowRead = allowRead;
+		}
+
+		public Boolean getAllowEdit() {
+			return allowEdit;
+		}
+
+		public void setAllowEdit(Boolean allowEdit) {
+			this.allowEdit = allowEdit;
+		}
+
+		public Boolean getAllowControl() {
+			return allowControl;
+		}
+
+		public void setAllowControl(Boolean allowControl) {
+			this.allowControl = allowControl;
+		}
 	}
 
 }
