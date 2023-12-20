2c85acebee6470e86b287734f17be6c62dbc28bc
o2null <zhourui@o2oa.net>
2023-12-20 09:57:12 +0800

    update

--- o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/test/ActionTest2.java
+++ o2server/x_program_center/src/main/java/com/x/program/center/jaxrs/test/ActionTest2.java
@@ -10,7 +10,7 @@ import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
-import com.x.program.center.script.GrralScriptingFactory;
+import com.x.program.center.script.GraalScriptingFactory;
 
 class ActionTest2 extends BaseAction {
 
@@ -20,11 +20,11 @@ class ActionTest2 extends BaseAction {
 		LOGGER.debug("execute:{}.", effectivePerson::getDistinguishedName);
 		Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
 		ActionResult<Wo> result = new ActionResult<>();
-		Source source = GrralScriptingFactory.functionalization(wi.getText());
+		Source source = GraalScriptingFactory.functionalization(wi.getText());
 		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!222222222");
 		System.out.println(source.getCharacters());
 		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!2222222222");
-		Value value = GrralScriptingFactory.eval(source);
+		Value value = GraalScriptingFactory.eval(source);
 		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		System.out.println(value);
 		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
--- o2server/x_program_center/src/main/java/com/x/program/center/script/GraalScriptingFactory.java
+++ o2server/x_program_center/src/main/java/com/x/program/center/script/GraalScriptingFactory.java
@@ -1,48 +1,104 @@
 package com.x.program.center.script;
 
+import java.lang.reflect.Type;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.locks.ReentrantLock;
+
+import javax.script.CompiledScript;
+import javax.script.ScriptContext;
 
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Engine;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
 import com.x.base.core.project.config.Config;
+import com.x.base.core.project.gson.XGsonBuilder;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
 
-public class GrralScriptingFactory {
+public class GraalScriptingFactory {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(GrralScriptingFactory.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(GraalScriptingFactory.class);
 
-	private GrralScriptingFactory() {
+	private static Gson gson = XGsonBuilder.instance();
+
+	private GraalScriptingFactory() {
 
 	}
 
+	private static final String LANGUAGE_ID_JS = "js";
+	private static final String NATIVE_OBJECT_BINDING_TO_STRINGIFY = "nativeObjectBindingToStringify";
+
 	private static ReentrantLock engineLock = new ReentrantLock();
 	private static ReentrantLock initialScriptSourceLock = new ReentrantLock();
+	private static ReentrantLock stringifySourceLock = new ReentrantLock();
 
 	private static Engine engine;
 
 	private static Source initialScriptSource;
+	private static Source stringifySource;
 
-	public static Value eval(Source source) {
+	public static JsonElement eval(Source source) {
 		try (Context context = Context.newBuilder().engine(getEngine()).build()) {
 			context.eval(getInitialScriptSource());
 			Value value = context.eval(source);
-			System.out.println("!!!!!!!!!!!!!!!@@@@@@@@@@@@@");
-			System.out.println(value);
-			System.out.println("!!!!!!!!!!!!!!!@@@@@@@@@@@@@");
-			return value;
+			if (value.isHostObject()) {
+				return gson.toJsonTree(value.asHostObject());
+			} else {
+				context.getBindings(LANGUAGE_ID_JS).putMember(NATIVE_OBJECT_BINDING_TO_STRINGIFY, value);
+				String text = context.eval(getStringifySource()).asString();
+				return gson.fromJson(text, JsonElement.class);
+			}
 		}
+	}
+
+	public static Optional<Boolean> evalAsBoolean(Source source) {
+		JsonElement jsonElement = eval(source);
+		if (jsonElement != null && jsonElement.isJsonPrimitive()) {
+			JsonPrimitive jsonPrimitive = jsonElement.getAsJsonPrimitive();
+			if (jsonPrimitive.isBoolean()) {
+				return Optional.of(jsonElement.getAsBoolean());
+			}
+		}
+		return Optional.empty();
+	}
+
+	public static Optional<String> evalAsString(Source source) {
+		JsonElement jsonElement = eval(source);
+		if (jsonElement != null && jsonElement.isJsonPrimitive()) {
+			JsonPrimitive jsonPrimitive = jsonElement.getAsJsonPrimitive();
+			if (jsonPrimitive.isString()) {
+				return Optional.of(jsonElement.getAsString());
+			}
+		}
+		return Optional.empty();
+	}
+
+	public static <T> T eval(Source source, Class<T> clz) {
+		JsonElement jsonElement = eval(source);
+		return gson.fromJson(jsonElement, clz);
+	}
+
+	public static <T> T eval(Source source, Type type) {
+		JsonElement jsonElement = eval(source);
+		return gson.fromJson(jsonElement, type);
+	}
+
+	public static <T> T eval(Source source, Supplier<T> supplier) {
+		JsonElement jsonElement = eval(source);
+		return supplier.get(jsonElement);
 	}
 
 	private static Engine getEngine() {
 		if (null == engine) {
 			engineLock.lock();
 			try {
-				engine = Engine.newBuilder("js").build();
+				engine = Engine.newBuilder(LANGUAGE_ID_JS).build();
 			} catch (Exception e) {
 				LOGGER.error(e);
 			} finally {
@@ -56,11 +112,26 @@ public class GrralScriptingFactory {
 		if (null == initialScriptSource) {
 			initialScriptSourceLock.lock();
 			try {
-				initialScriptSource = Source.create("js", Config.initialScriptText());
+				initialScriptSource = Source.create(LANGUAGE_ID_JS, Config.initialScriptText());
 			} catch (Exception e) {
 				LOGGER.error(e);
 			} finally {
 				initialScriptSourceLock.unlock();
+			}
+		}
+		return initialScriptSource;
+	}
+
+	private static Source getStringifySource() {
+		if (null == stringifySource) {
+			stringifySourceLock.lock();
+			try {
+				stringifySource = Source.create(LANGUAGE_ID_JS,
+						"JSON.stringify(" + NATIVE_OBJECT_BINDING_TO_STRINGIFY + ")");
+			} catch (Exception e) {
+				LOGGER.error(e);
+			} finally {
+				stringifySourceLock.unlock();
 			}
 		}
 		return initialScriptSource;
@@ -114,7 +185,7 @@ public class GrralScriptingFactory {
 		sb.append(Objects.toString(text, "")).append(System.lineSeparator());
 		sb.append("}.apply(this));").append(System.lineSeparator());
 		sb.append("o;");
-		return Source.create("js", sb.toString());
+		return Source.create(LANGUAGE_ID_JS, sb.toString());
 	}
 
 }
