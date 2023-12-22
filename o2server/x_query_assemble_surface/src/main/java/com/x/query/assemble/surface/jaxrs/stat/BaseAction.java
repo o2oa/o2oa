67eeac1f81ef21c56c962ca6efd911cc6865373b
o2null <zhourui@o2oa.net>
2023-12-22 12:09:10 +0800

    修复统计没有包含流程其他版本work和workCompleted

--- o2server/x_query_assemble_surface/src/main/java/com/x/query/assemble/surface/jaxrs/stat/BaseAction.java
+++ o2server/x_query_assemble_surface/src/main/java/com/x/query/assemble/surface/jaxrs/stat/BaseAction.java
@@ -1,9 +1,19 @@
 package com.x.query.assemble.surface.jaxrs.stat;
 
+import java.util.List;
+
+import com.google.gson.reflect.TypeToken;
+import com.x.base.core.container.EntityManagerContainer;
+import com.x.base.core.container.factory.EntityManagerContainerFactory;
+import com.x.base.core.entity.JpaObject;
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.jaxrs.StandardJaxrsAction;
+import com.x.base.core.project.tools.ListTools;
+import com.x.processplatform.core.entity.element.Process;
 import com.x.query.assemble.surface.Business;
+import com.x.query.core.express.plan.ProcessPlatformPlan;
 import com.x.query.core.express.plan.Runtime;
+import com.x.query.core.express.plan.StatPlan;
 
 abstract class BaseAction extends StandardJaxrsAction {
 
