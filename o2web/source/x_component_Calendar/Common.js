95181b7394f48f026a3de9be45a678553df03592
unknown <caixiangyi@zoneland.net>
2023-12-26 10:08:37 +0800

    修复日程安排问题

--- o2web/source/x_component_Calendar/Common.js
+++ o2web/source/x_component_Calendar/Common.js
@@ -84,12 +84,11 @@ MWFCalendar.EventForm = new Class({
                     this.calendarIds = [];
                     this.calendarNames = [];
                     this.calendarList = [];
-                    debugger;
                     (json.data.myCalendars || []).each(function (c) {
-                        if ((c.publishable || c.manageable) && c.status !== "CLOSE") this.calendarList.push(c)
+                        if ((c.publishable || c.manageable) && c.status !== "CLOSE") this.calendarList.push(c);
                     }.bind(this));
                     (json.data.unitCalendars || []).each(function (c) {
-                        if ((c.publishable || c.manageable)  && c.status !== "CLOSE") this.calendarList.push(c)
+                        if ((c.publishable || c.manageable)  && c.status !== "CLOSE") this.calendarList.push(c);
                     }.bind(this));
                     this.calendarList.each(function (d) {
                         this.calendarIds.push(d.id);
