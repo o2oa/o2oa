ac3530368d5c21e08d076a5bbdcedc386ad8b1c6
unknown <caixiangyi@zoneland.net>
2023-12-29 15:43:00 +0800

    修复论坛匿名不能访问的问题

--- o2web/source/x_component_ForumCategory/lp/en.js
+++ o2web/source/x_component_ForumCategory/lp/en.js
@@ -1,4 +1,6 @@
+MWF.xApplication = MWF.xApplication || {};
 MWF.xApplication.ForumCategory = MWF.xApplication.ForumCategory || {};
-MWF.xApplication.ForumCategory.LP = {
+MWF.xApplication.ForumCategory.LP={
 	"title": "Forum Category"
-};
+}
+MWF.xApplication.ForumCategory["lp."+o2.language] = MWF.xApplication.ForumCategory.LP
