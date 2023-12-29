b363905e45a64466bb63a45ca9e00a8cc30954e1
fancy <fancylou@outlook.com>
2023-12-29 10:45:52 +0800

    极光推送添加各个通道的参数

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/config/JpushConfig.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/config/JpushConfig.java
@@ -1,17 +1,15 @@
 package com.x.base.core.project.config;
 
-import java.io.File;
-
+import com.x.base.core.project.annotation.FieldDescribe;
 import org.apache.commons.lang3.BooleanUtils;
 
-import com.x.base.core.project.annotation.FieldDescribe;
+import java.util.Map;
 
 public class JpushConfig extends ConfigObject {
 
     private static final String O2_app_key_default = "9aca7cc20fe0cc987cd913ca";
     private static final String O2_master_secret_default = "96ee7e2e0daffd51bac57815";
-    private static final String O2_apns_path_default = "configSample/o2oa_apns.p12";
-    private static final String O2_apns_password_default = "1209";
+
 
     // 自助打包的outer包使用的key
     private static final String O2_app_key_outer = "24a4af5965d2c325b33c243d";
@@ -25,11 +23,6 @@ public class JpushConfig extends ConfigObject {
         this.enable = true;
         this.appKey = O2_app_key_default;
         this.masterSecret = O2_master_secret_default;
-        this.huaweiPushEnable = false;
-        this.huaweiPushConfig = HuaweiPushConfig.defaultInstance();
-        this.apnsKeystorePassword = O2_apns_password_default;
-        this.apnsKeystorePath = O2_apns_path_default;
-
     }
 
 
@@ -39,14 +32,23 @@ public class JpushConfig extends ConfigObject {
     private String appKey;
     @FieldDescribe("极光推送应用的Master Secret")
     private String masterSecret;
-    @FieldDescribe("是否开启华为推送，已废弃")
-    private Boolean huaweiPushEnable;
-    @FieldDescribe("华为推送的配置，已废弃")
-    private HuaweiPushConfig huaweiPushConfig;
-    @FieldDescribe("苹果推送证书路径")
-    private String apnsKeystorePath;
-    @FieldDescribe("苹果推送证书的密码")
-    private String apnsKeystorePassword;
+
+
+    @FieldDescribe("华为通道参数配置")
+    private Map<String, String> huawei;
+
+
+    @FieldDescribe("小米通道参数配置")
+    private Map<String, String> xiaomi;
+
+
+    @FieldDescribe("VIVO通道参数配置")
+    private Map<String, String> vivo;
+
+
+    @FieldDescribe("OPPO通道参数配置")
+    private Map<String, String> oppo;
+
 
 
     /**
@@ -58,35 +60,9 @@ public class JpushConfig extends ConfigObject {
         config.setAppKey(O2_app_key_outer);
         config.setMasterSecret(O2_master_secret_outer);
         config.setEnable(true);
-        config.setHuaweiPushConfig(HuaweiPushConfig.outerPackInstance());
         return config;
     }
 
-
-    /**
-     * 获取苹果推送证书文件
-     * @return
-     * @throws Exception
-     */
-    public File getAPNSKeystoreFilePath() throws Exception {
-        return new File(Config.base(), apnsKeystorePath);
-    }
-
-    public String getApnsKeystorePath() {
-        return apnsKeystorePath;
-    }
-
-    public void setApnsKeystorePath(String apnsKeystorePath) {
-        this.apnsKeystorePath = apnsKeystorePath;
-    }
-
-    public String getApnsKeystorePassword() {
-        return apnsKeystorePassword;
-    }
-
-    public void setApnsKeystorePassword(String apnsKeystorePassword) {
-        this.apnsKeystorePassword = apnsKeystorePassword;
-    }
 
     public Boolean getEnable() {
         return BooleanUtils.isTrue(this.enable);
@@ -112,19 +88,35 @@ public class JpushConfig extends ConfigObject {
         this.masterSecret = masterSecret;
     }
 
-    public Boolean getHuaweiPushEnable() {
-        return huaweiPushEnable == null ? false : huaweiPushEnable;
+    public Map<String, String> getHuawei() {
+        return huawei;
     }
 
-    public void setHuaweiPushEnable(Boolean huaweiPushEnable) {
-        this.huaweiPushEnable = huaweiPushEnable;
+    public void setHuawei(Map<String, String> huawei) {
+        this.huawei = huawei;
     }
 
-    public HuaweiPushConfig getHuaweiPushConfig() {
-        return huaweiPushConfig;
+    public Map<String, String> getXiaomi() {
+        return xiaomi;
     }
 
-    public void setHuaweiPushConfig(HuaweiPushConfig huaweiPushConfig) {
-        this.huaweiPushConfig = huaweiPushConfig;
+    public void setXiaomi(Map<String, String> xiaomi) {
+        this.xiaomi = xiaomi;
+    }
+
+    public Map<String, String> getVivo() {
+        return vivo;
+    }
+
+    public void setVivo(Map<String, String> vivo) {
+        this.vivo = vivo;
+    }
+
+    public Map<String, String> getOppo() {
+        return oppo;
+    }
+
+    public void setOppo(Map<String, String> oppo) {
+        this.oppo = oppo;
     }
 }
--- o2server/x_jpush_assemble_control/src/main/java/com/x/jpush/assemble/control/jaxrs/message/ActionSendMessage.java
+++ o2server/x_jpush_assemble_control/src/main/java/com/x/jpush/assemble/control/jaxrs/message/ActionSendMessage.java
@@ -1,41 +1,5 @@
 package com.x.jpush.assemble.control.jaxrs.message;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import javax.servlet.http.HttpServletRequest;
-
-import com.google.gson.JsonObject;
-import com.x.jpush.assemble.control.JpushConst;
-import org.apache.commons.lang3.StringUtils;
-
-import com.google.gson.JsonElement;
-import com.x.base.core.container.EntityManagerContainer;
-import com.x.base.core.container.factory.EntityManagerContainerFactory;
-import com.x.base.core.project.annotation.FieldDescribe;
-import com.x.base.core.project.bean.NameValuePair;
-import com.x.base.core.project.config.Config;
-import com.x.base.core.project.config.HuaweiPushConfig;
-import com.x.base.core.project.connection.HttpConnection;
-import com.x.base.core.project.gson.GsonPropertyObject;
-import com.x.base.core.project.gson.XGsonBuilder;
-import com.x.base.core.project.http.ActionResult;
-import com.x.base.core.project.http.EffectivePerson;
-import com.x.base.core.project.jaxrs.StandardJaxrsAction;
-import com.x.base.core.project.jaxrs.WrapBoolean;
-import com.x.base.core.project.logger.Logger;
-import com.x.base.core.project.logger.LoggerFactory;
-import com.x.base.core.project.tools.ListTools;
-import com.x.jpush.assemble.control.Business;
-import com.x.jpush.assemble.control.huawei.model.Importance;
-import com.x.jpush.assemble.control.huawei.model.Urgency;
-import com.x.jpush.assemble.control.huawei.model.Visibility;
-import com.x.jpush.core.entity.PushDevice;
-
 import cn.jpush.api.JPushClient;
 import cn.jpush.api.push.PushResult;
 import cn.jpush.api.push.model.Options;
@@ -45,9 +9,29 @@ import cn.jpush.api.push.model.audience.Audience;
 import cn.jpush.api.push.model.notification.AndroidNotification;
 import cn.jpush.api.push.model.notification.IosNotification;
 import cn.jpush.api.push.model.notification.Notification;
-import javapns.Push;
-import javapns.notification.PushNotificationBigPayload;
-import javapns.notification.PushedNotification;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.x.base.core.container.EntityManagerContainer;
+import com.x.base.core.container.factory.EntityManagerContainerFactory;
+import com.x.base.core.project.annotation.FieldDescribe;
+import com.x.base.core.project.config.Config;
+import com.x.base.core.project.gson.GsonPropertyObject;
+import com.x.base.core.project.http.ActionResult;
+import com.x.base.core.project.http.EffectivePerson;
+import com.x.base.core.project.jaxrs.StandardJaxrsAction;
+import com.x.base.core.project.jaxrs.WrapBoolean;
+import com.x.base.core.project.logger.Logger;
+import com.x.base.core.project.logger.LoggerFactory;
+import com.x.jpush.assemble.control.Business;
+import com.x.jpush.assemble.control.JpushConst;
+import com.x.jpush.core.entity.PushDevice;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ActionSendMessage extends StandardJaxrsAction {
 
@@ -55,161 +39,33 @@ public class ActionSendMessage extends StandardJaxrsAction {
 
     protected ActionResult<Wo> execute(HttpServletRequest request, EffectivePerson effectivePerson,
                                        JsonElement jsonElement) throws Exception {
-        logger.info("execute action 'ActionSendMessage'......");
+        logger.info("execute action 'ActionSendMessage' .");
         ActionResult<Wo> result = new ActionResult<>();
         Wo wraps = new Wo();
         if (jsonElement == null) {
             throw new ExceptionSendMessageEmpty();
         }
-
         Wi wi = convertToWrapIn(jsonElement, Wi.class);
         if (StringUtils.isEmpty(wi.getPerson()) || StringUtils.isEmpty(wi.getMessage())) {
             throw new ExceptionSendMessageEmpty();
         }
-
         try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
             Business business = new Business(emc);
-            logger.info("person:" + wi.getPerson());
-
-//			HuaweiPushConfig config = Config.pushConfig().getHuaweiPushConfig();
-//			if (config != null && Config.pushConfig().getHuaweiPushEnable()) { // 华为推送通道
-//				logger.info("华为推送通道启用中，消息发送到华为");
-//				List<PushDevice> pushDeviceList = business.pushDeviceFactory().listHuaweiDevice(wi.getPerson());
-//				// 华为推送只支持Android端， ios 使用苹果自带的apns推送
-//				if (pushDeviceList != null && !pushDeviceList.isEmpty()) {
-//					List<String> androidList = pushDeviceList.stream()
-//							.filter((p) -> p.getDeviceType().equals(PushDevice.DEVICE_TYPE_ANDROID))
-//							.map(PushDevice::getDeviceId).collect(Collectors.toList());
-//					if (!androidList.isEmpty()) {
-//						send2HuaweiPushOnlyAndroid(androidList, wi.getMessage());
-//					}
-//
-//					List<String> iosList = pushDeviceList.stream()
-//							.filter((p) -> p.getDeviceType().equals(PushDevice.DEVICE_TYPE_IOS))
-//							.map(PushDevice::getDeviceId).collect(Collectors.toList());
-//					if (!iosList.isEmpty()) {
-//						send2APNS(iosList, wi.getMessage());
-//					}
-//
-//				} else {
-//					logger.info("华为推送设备为空，" + wi.getPerson());
-//				}
-//			} else {
-            logger.info("极光推送通道启用中，消息发送到极光推送");
+            logger.info("极光推送通道启用中，消息发送到极光推送，人员：{}", wi.getPerson());
             List<PushDevice> pushDeviceList = business.pushDeviceFactory().listJpushDevice(wi.getPerson());
             if (pushDeviceList != null && !pushDeviceList.isEmpty()) {
                 send2Jpush(pushDeviceList, wi.getMessage(), business.pushDeviceFactory().jpushClient());
             } else {
-                logger.info("极光推送设备为空，" + wi.getPerson());
+                logger.info("极光推送设备为空，{}", wi.getPerson());
             }
-//			}
-
             wraps.setValue(true);
             result.setData(wraps);
-
-//            List<String> deviceList = business.organization().personAttribute()
-//                    .listAttributeWithPersonWithName(wi.getPerson(), ActionListAll.DEVICE_PERSON_ATTR_KEY);
-//            if(ListTools.isNotEmpty( deviceList ) ){
-//                List<String> jiguangDeviceList = new ArrayList<>();
-//                for (int i = 0; i < deviceList.size(); i++) {
-//                    String deviceId = "";
-//                    try {
-//                        String[] split = deviceList.get(i).split("_");
-//                        deviceId = split[0];
-//                        logger.info("device Id:" + deviceId);
-//                    }catch (Exception e){
-//                        logger.error(e);
-//                    }
-//                    if (deviceId != null && !deviceId.isEmpty()) {
-//                        jiguangDeviceList.add(deviceId);
-//                    }
-//                }
-//                if(ListTools.isNotEmpty( jiguangDeviceList ) ) {
-//                    //send
-//                    wraps.setValue(true);
-//                    result.setData(wraps);
-//                }else {
-//                    ExceptionSendMessageDeviceEmpty empty = new ExceptionSendMessageDeviceEmpty();
-//                    result.error( empty );
-//                }
-//
-//            }else {
-//                ExceptionSendMessageDeviceEmpty empty = new ExceptionSendMessageDeviceEmpty();
-//                result.error( empty );
-//            }
         } catch (Exception e) {
             logger.error(e);
             throw new ExceptionSendMessage(e, "系统发送推送消息时异常!");
-//            result.error( exception );
         }
         logger.info("action 'ActionSendMessage' execute completed!");
         return result;
-    }
-
-    /**
-     * 华为推送消息
-     *
-     * @param androidList
-     * @param message
-     * @throws Exception
-     */
-    private void send2HuaweiPushOnlyAndroid(List<String> androidList, String message) throws Exception {
-        logger.info("开始发送华为推送消息， " + message);
-        if (!androidList.isEmpty()) {
-            com.x.jpush.assemble.control.huawei.model.Message m = androidMessage(androidList, message);
-            sendHuaweiMessage(m);
-        } else {
-            logger.info("没有Android 设备需要发送消息");
-        }
-
-    }
-
-    private void sendHuaweiMessage(com.x.jpush.assemble.control.huawei.model.Message msg) throws Exception {
-        HashMap<String, Object> sendBody = new HashMap<>();
-        sendBody.put("validate_only", false);
-        sendBody.put("message", msg);
-        List<NameValuePair> heads = new ArrayList<>();
-        String url = Config.pushConfig().getHuaweiPushConfig().getPushUrl();
-        logger.info("华为推送地址：" + url);
-        String accessToken = Config.pushConfig().getHuaweiPushConfig().accessToken();
-        logger.info("华为推送accessToken ：" + accessToken);
-        heads.add(new NameValuePair("Authorization", "Bearer " + accessToken));
-        String body = XGsonBuilder.instance().toJson(sendBody);
-        logger.info("发送消息：" + body);
-        HuaweiSendResponse result = HttpConnection.postAsObject(url, heads, body, HuaweiSendResponse.class);
-        logger.info("华为消息发送完成，code：" + result.getCode() + ", msg:" + result.getMsg() + ",  requestId: "
-                + result.getRequestId());
-    }
-
-    /**
-     * 华为Android消息
-     *
-     * @param deviceList
-     * @param message
-     * @return
-     */
-    private com.x.jpush.assemble.control.huawei.model.Message androidMessage(List<String> deviceList, String message) {
-        com.x.jpush.assemble.control.huawei.model.Notification notification = com.x.jpush.assemble.control.huawei.model.Notification
-                .builder().setTitle(message).setBody(message).build();
-        // 添加一条角标
-        com.x.jpush.assemble.control.huawei.android.BadgeNotification badgeNotification = com.x.jpush.assemble.control.huawei.android.BadgeNotification
-                .builder().setAddNum(1)
-                .setBadgeClass(JpushConst.launchActivity).build();
-        com.x.jpush.assemble.control.huawei.android.ClickAction clickAction = com.x.jpush.assemble.control.huawei.android.ClickAction
-                .builder().setType(3)// 启动应用
-                .build();
-        com.x.jpush.assemble.control.huawei.android.AndroidNotification androidNotification = com.x.jpush.assemble.control.huawei.android.AndroidNotification
-                .builder().setTitle(message).setBody(message).setDefaultSound(true).setAutoCancel(false)
-                .setBadge(badgeNotification).setClickAction(clickAction).setForegroundShow(true)
-                .setVisibility(Visibility.PUBLIC.getValue()).setImportance(Importance.NORMAL.getValue()).build();
-
-        com.x.jpush.assemble.control.huawei.android.AndroidConfig config = com.x.jpush.assemble.control.huawei.android.AndroidConfig
-                .builder().setCollapseKey(-1).setUrgency(Urgency.HIGH.getValue()).setNotification(androidNotification)
-                .build();
-
-        return com.x.jpush.assemble.control.huawei.model.Message.builder().addAllToken(deviceList)
-                .setNotification(notification).setAndroidConfig(config).build();
-
     }
 
     /**
@@ -243,17 +99,48 @@ public class ActionSendMessage extends StandardJaxrsAction {
 								.build())
                 .build();
 
-
+        // 第三方通道的特殊参数
         Map<String, JsonObject> thirdPartyChannel = new HashMap<>();
-        // huawei 参数
-        JsonObject huawei = new JsonObject();
-        huawei.addProperty("importance", "NORMAL");
-        huawei.addProperty("category", "WORK");
-        thirdPartyChannel.put("huawei", huawei);
-        // xiaomi参数
-        JsonObject xiaomi = new JsonObject();
-        xiaomi.addProperty("channel_id", "113850");
-        thirdPartyChannel.put("xiaomi", xiaomi);
+        // huawei 参数 importance：NORMAL ， category：WORK
+        Map<String, String> huaweiProperty = Config.pushConfig().getHuawei();
+        if (huaweiProperty != null && !huaweiProperty.isEmpty()) {
+            JsonObject huawei = new JsonObject();
+            for (Map.Entry<String, String> entry : huaweiProperty.entrySet()) {
+                huawei.addProperty(entry.getKey(), entry.getValue());
+            }
+//            huawei.addProperty("importance", "NORMAL");
+//            huawei.addProperty("category", "WORK");
+            thirdPartyChannel.put("huawei", huawei);
+        }
+        // xiaomi参数 channel_id:xxxxx
+        Map<String, String> xiaomiProperty = Config.pushConfig().getXiaomi();
+        if (xiaomiProperty != null && !xiaomiProperty.isEmpty()) {
+            JsonObject xiaomi = new JsonObject();
+            for (Map.Entry<String, String> entry : xiaomiProperty.entrySet()) {
+                xiaomi.addProperty(entry.getKey(), entry.getValue());
+            }
+//            xiaomi.addProperty("channel_id", "113850");
+            thirdPartyChannel.put("xiaomi", xiaomi);
+        }
+
+        // oppo参数
+        Map<String, String>  oppoProperty = Config.pushConfig().getOppo();
+        if (oppoProperty != null && !oppoProperty.isEmpty()) {
+            JsonObject oppo = new JsonObject();
+            for (Map.Entry<String, String> entry : oppoProperty.entrySet()) {
+                oppo.addProperty(entry.getKey(), entry.getValue());
+            }
+            thirdPartyChannel.put("oppo", oppo);
+        }
+        // vivo参数
+        Map<String, String>  vivoProperty = Config.pushConfig().getVivo();
+        if (vivoProperty != null && !vivoProperty.isEmpty()) {
+            JsonObject vivo = new JsonObject();
+            for (Map.Entry<String, String> entry : vivoProperty.entrySet()) {
+                vivo.addProperty(entry.getKey(), entry.getValue());
+            }
+            thirdPartyChannel.put("vivo", vivo);
+        }
 
         PushPayload pushPayload = PushPayload.newBuilder().setPlatform(Platform.all())
                 .setAudience(Audience.registrationId(jiguangDeviceList))
@@ -268,77 +155,6 @@ public class ActionSendMessage extends StandardJaxrsAction {
         logger.info("极光推送 body: {}", pushPayload.toString());
         PushResult pushResult = client.sendPush(pushPayload);
         logger.info("极光推送 发送结果:{}.", pushResult);
-    }
-
-    /**
-     * 消息推送到 苹果消息推送中心
-     *
-     * @param deviceList
-     * @param message
-     */
-    private void send2APNS(List<String> deviceList, String message) throws Exception {
-        logger.info("开始发送APNS推送消息。。。。。。。。。。。device: " + ListTools.toStringJoin(deviceList));
-        File file = Config.pushConfig().getAPNSKeystoreFilePath();
-        if (file == null || !file.exists()) {
-            throw new ExceptionSendMessageAPNSFileNotExist();
-        }
-        String password = Config.pushConfig().getApnsKeystorePassword();
-        if (StringUtils.isEmpty(password)) {
-            throw new ExceptionSendMessageAPNSPasswordEmpty();
-        }
-        PushNotificationBigPayload payload = PushNotificationBigPayload.complex();
-        payload.addAlert(message);
-        payload.addBadge(1);
-        List<PushedNotification> notifications = Push.payload(payload, file, password, true, deviceList);
-        if (notifications != null) {
-            try {
-                for (int i = 0; i < notifications.size(); i++) {
-                    PushedNotification n = notifications.get(i);
-                    if (n.isSuccessful()) {
-                        logger.info("发送成功" + n.getDevice().getToken());
-                    } else {
-                        logger.info("发送失败" + n.getDevice().getToken());
-                        Exception e = n.getException();
-                        e.printStackTrace();
-                    }
-                }
-            } catch (Exception e) {
-                logger.error(e);
-            }
-        } else {
-            logger.info("苹果推送发送失败， 没有返回值");
-        }
-        logger.info("苹果推送 发送完成！");
-    }
-
-    public static class HuaweiSendResponse {
-        private String code;
-        private String msg;
-        private String requestId;
-
-        public String getCode() {
-            return code;
-        }
-
-        public void setCode(String code) {
-            this.code = code;
-        }
-
-        public String getMsg() {
-            return msg;
-        }
-
-        public void setMsg(String msg) {
-            this.msg = msg;
-        }
-
-        public String getRequestId() {
-            return requestId;
-        }
-
-        public void setRequestId(String requestId) {
-            this.requestId = requestId;
-        }
     }
 
     public static class Wi extends GsonPropertyObject {
