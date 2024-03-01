b4a54665c09f7c40e6cb6172a1c93741242fe988
wuhengshui <wuhengshui@o2oa.net>
2024-03-01 14:10:38 +0800

    增加htmlToWeb的接口，用于html文本转换成html文件存储于平台前端目录中

--- o2server/x_general_assemble_control/src/main/java/com/x/general/assemble/control/jaxrs/generalfile/GeneralFileAction.java
+++ o2server/x_general_assemble_control/src/main/java/com/x/general/assemble/control/jaxrs/generalfile/GeneralFileAction.java
@@ -1,20 +1,19 @@
 package com.x.general.assemble.control.jaxrs.generalfile;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
+import com.google.gson.JsonElement;
 import com.x.base.core.project.annotation.JaxrsDescribe;
 import com.x.base.core.project.annotation.JaxrsMethodDescribe;
 import com.x.base.core.project.annotation.JaxrsParameterDescribe;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
+import com.x.base.core.project.http.HttpMediaType;
 import com.x.base.core.project.jaxrs.ResponseFactory;
 import com.x.base.core.project.jaxrs.StandardJaxrsAction;
 import com.x.base.core.project.logger.Logger;
@@ -46,6 +45,25 @@ public class GeneralFileAction extends StandardJaxrsAction {
 		EffectivePerson effectivePerson = this.effectivePerson(request);
 		try {
 			result = new ActionGet().execute(effectivePerson, flag);
+		} catch (Exception e) {
+			LOGGER.error(e, effectivePerson, request, null);
+			result.error(e);
+		}
+		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
+	}
+
+
+	@JaxrsMethodDescribe(value = "发送html文本到webServer下。", action = ActionHtmlToWeb.class)
+	@POST
+	@Path("{id}/html/to/web")
+	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void htmlToWeb(@Suspended final AsyncResponse asyncResponse, @Context HttpServletRequest request,
+						  @JaxrsParameterDescribe("证件ID") @PathParam("id") String id,JsonElement jsonElement) {
+		ActionResult<ActionHtmlToWeb.Wo> result = new ActionResult<>();
+		EffectivePerson effectivePerson = this.effectivePerson(request);
+		try {
+			result = new ActionHtmlToWeb().execute(effectivePerson,id,jsonElement);
 		} catch (Exception e) {
 			LOGGER.error(e, effectivePerson, request, null);
 			result.error(e);
