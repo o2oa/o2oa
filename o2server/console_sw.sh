531e467da0eb90e53ba179cff960955b5ef8d58a
o2null <zhourui@o2oa.net>
2024-03-07 14:19:49 +0800

    修正打包脚本

---
+++ o2server/console_sw.sh
@@ -0,0 +1,1 @@
+$(cd "$(dirname "$0")"; pwd)/jvm/sw_java11/bin/java -javaagent:$(cd "$(dirname "$0")"; pwd)/console.jar=shadow -cp $(cd "$(dirname "$0")"; pwd)/console.jar com.x.server.console.Shadow
