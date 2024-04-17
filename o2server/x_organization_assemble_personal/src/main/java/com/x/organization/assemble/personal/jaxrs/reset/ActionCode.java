08c2344f4dcb9e728dbffee9744814e87db3d9e8
o2sword <171715986@qq.com>
2024-04-17 17:02:41 +0800

    个人忘记密码安全性修改2

--- o2server/x_organization_assemble_personal/src/main/java/com/x/organization/assemble/personal/jaxrs/reset/ActionCode.java
+++ o2server/x_organization_assemble_personal/src/main/java/com/x/organization/assemble/personal/jaxrs/reset/ActionCode.java
@@ -12,7 +12,6 @@ import com.x.base.core.project.tools.DefaultCharset;
 import com.x.organization.assemble.personal.Business;
 import com.x.organization.core.entity.Person;
 import org.apache.commons.lang3.BooleanUtils;
-import org.codehaus.plexus.util.StringUtils;
 
 import java.net.URLDecoder;
 
@@ -20,8 +19,6 @@ class ActionCode extends BaseAction {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ActionCode.class);
 	ActionResult<WrapOutBoolean> execute(String credential) throws Exception {
 		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-			ActionResult<WrapOutBoolean> result = new ActionResult<>();
-			WrapOutBoolean wrap = new WrapOutBoolean();
 			Business business = new Business(emc);
 			if (BooleanUtils.isNotTrue(Config.collect().getEnable())) {
 				throw new ExceptionDisableCollect();
@@ -31,17 +28,15 @@ class ActionCode extends BaseAction {
 			LOGGER.info("{} 用户进行忘记密码修改操作", credential);
 			Person person = business.person().getWithCredential(credential);
 			if (null == person) {
-				throw new ExceptionSendCodeError();
+				throw new ExceptionSendCodeResult();
 			}
 
 			person = emc.find(person.getId(), Person.class);
 			if (!Config.person().isMobile(person.getMobile())) {
-				throw new ExceptionSendCodeError();
+				throw new ExceptionSendCodeResult();
 			}
 			business.instrument().code().create(person.getMobile());
-			wrap.setValue(true);
-			result.setData(wrap);
-			return result;
+			throw new ExceptionSendCodeResult();
 		}
 	}
 
