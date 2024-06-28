71c6709029155318525b3585f3f5150676aed4da
fancy <fancylou@outlook.com>
2024-06-28 15:17:54 +0800

    修复移动端 cms 创建文档没有带 data 的问题

--- o2web/source/o2_core/o2/xScript/PageEnvironment.js
+++ o2web/source/o2_core/o2/xScript/PageEnvironment.js
@@ -2219,6 +2219,22 @@ if (!MWF.xScript || !MWF.xScript.PageEnvironment) {
                     if (category) {
                         options["category"] = category
                     }
+                    if (data) {
+                        options["data"] = data
+                    }
+                    if (identity) {
+                        options["identity"] = identity
+                    }
+                    if (typeof(latest) == 'undefined' || latest === null) {
+                        options["latest"] = true
+                    } else {
+                        options["latest"] = latest
+                    }
+                    if (typeof(ignoreTitle) == 'undefined' || ignoreTitle === null) {
+                        options["ignoreTitle"] = false
+                    } else {
+                        options["ignoreTitle"] = ignoreTitle
+                    }
                     if (window.o2android && window.o2android.postMessage) {
                         var body = {
                             type: "createO2CmsDocument",
