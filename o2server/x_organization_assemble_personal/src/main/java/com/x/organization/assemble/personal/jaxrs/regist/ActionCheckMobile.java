551cf049734e7718ae2c812a13405f9166c1bbda
o2sword <171715986@qq.com>
2024-07-24 10:48:16 +0800

    修复注册校验手机号码接口未限制的问题

--- o2server/x_organization_assemble_personal/src/main/java/com/x/organization/assemble/personal/jaxrs/regist/ActionCheckMobile.java
+++ o2server/x_organization_assemble_personal/src/main/java/com/x/organization/assemble/personal/jaxrs/regist/ActionCheckMobile.java
@@ -10,32 +10,38 @@ import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.jaxrs.WrapBoolean;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
+import org.apache.commons.lang3.StringUtils;
 
 class ActionCheckMobile extends BaseAction {
 
-	private static Logger logger = LoggerFactory.getLogger(ActionCheckMobile.class);
+    private static Logger logger = LoggerFactory.getLogger(ActionCheckMobile.class);
 
-	ActionResult<Wo> execute(EffectivePerson effectivePerson,String mobile) throws Exception {
+    ActionResult<Wo> execute(EffectivePerson effectivePerson, String mobile) throws Exception {
 
-		ActionResult<Wo> result = new ActionResult<>();
-		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-			if (!Config.person().isMobile(mobile)) {
-				throw new ExceptionInvalidMobile(mobile);
-			}
-			if (this.mobileExisted(emc, mobile)) {
-				throw new ExceptionMobileExist(mobile);
-			}
-			if (BooleanUtils.isNotTrue(Config.collect().getEnable())) {
-				throw new ExceptionDisableCollect();
-			}
-			Wo wo = new Wo();
-			wo.setValue(true);
-			result.setData(wo);
-			return result;
-		}
-	}
+        ActionResult<Wo> result = new ActionResult<>();
+        try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+            if (com.x.base.core.project.config.Person.REGISTER_TYPE_DISABLE.equals(
+                    Config.person().getRegister())) {
+                throw new ExceptionDisableRegist();
+            }
+            if (!Config.person().isMobile(mobile)) {
+                throw new ExceptionInvalidMobile(mobile);
+            }
+            if (this.mobileExisted(emc, mobile)) {
+                throw new ExceptionMobileExist(mobile);
+            }
+            if (BooleanUtils.isNotTrue(Config.collect().getEnable())) {
+                throw new ExceptionDisableCollect();
+            }
+            Wo wo = new Wo();
+            wo.setValue(true);
+            result.setData(wo);
+            return result;
+        }
+    }
 
-	public static class Wo extends WrapBoolean {
-	}
+    public static class Wo extends WrapBoolean {
+
+    }
 
 }
