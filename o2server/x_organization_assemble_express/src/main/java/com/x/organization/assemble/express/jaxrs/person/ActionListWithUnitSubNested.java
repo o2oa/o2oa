f225e4cf43e12871100ed97c3b33aa73cf48ca26
o2sword <171715986@qq.com>
2024-08-05 10:29:32 +0800

    修复根据组织查询用户in条件值过多的问题

--- o2server/x_organization_assemble_express/src/main/java/com/x/organization/assemble/express/jaxrs/person/ActionListWithUnitSubNested.java
+++ o2server/x_organization_assemble_express/src/main/java/com/x/organization/assemble/express/jaxrs/person/ActionListWithUnitSubNested.java
@@ -1,5 +1,6 @@
 package com.x.organization.assemble.express.jaxrs.person;
 
+import com.x.organization.core.entity.Person;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -28,63 +29,70 @@ import com.x.organization.core.entity.Unit;
 
 class ActionListWithUnitSubNested extends BaseAction {
 
-	ActionResult<Wo> execute(EffectivePerson effectivePerson, JsonElement jsonElement) throws Exception {
-		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
-			Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
-			ActionResult<Wo> result = new ActionResult<>();
-			Business business = new Business(emc);
-			CacheKey cacheKey = new CacheKey(this.getClass(), wi.getUnitList());
-			Optional<?> optional = CacheManager.get(cacheCategory, cacheKey);
-			if (optional.isPresent()) {
-				result.setData((Wo) optional.get());
-			} else {
-				Wo wo = this.list(business, wi);
-				CacheManager.put(cacheCategory, cacheKey, wo);
-				result.setData(wo);
-			}
-			return result;
-		}
-	}
+    ActionResult<Wo> execute(EffectivePerson effectivePerson, JsonElement jsonElement)
+            throws Exception {
+        try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
+            Wi wi = this.convertToWrapIn(jsonElement, Wi.class);
+            ActionResult<Wo> result = new ActionResult<>();
+            Business business = new Business(emc);
+            CacheKey cacheKey = new CacheKey(this.getClass(), wi.getUnitList());
+            Optional<?> optional = CacheManager.get(cacheCategory, cacheKey);
+            if (optional.isPresent()) {
+                result.setData((Wo) optional.get());
+            } else {
+                Wo wo = this.list(business, wi);
+                CacheManager.put(cacheCategory, cacheKey, wo);
+                result.setData(wo);
+            }
+            return result;
+        }
+    }
 
-	public static class Wi extends GsonPropertyObject {
+    public static class Wi extends GsonPropertyObject {
 
-		@FieldDescribe("组织")
-		private List<String> unitList = new ArrayList<>();
+        @FieldDescribe("组织")
+        private List<String> unitList = new ArrayList<>();
 
-		public List<String> getUnitList() {
-			return unitList;
-		}
+        public List<String> getUnitList() {
+            return unitList;
+        }
 
-		public void setUnitList(List<String> unitList) {
-			this.unitList = unitList;
-		}
+        public void setUnitList(List<String> unitList) {
+            this.unitList = unitList;
+        }
 
-	}
+    }
 
-	public static class Wo extends WoPersonListAbstract {
+    public static class Wo extends WoPersonListAbstract {
 
-	}
+    }
 
-	private Wo list(Business business, Wi wi) throws Exception {
-		List<Unit> os = business.unit().pick(wi.getUnitList());
-		List<String> unitIds = new ArrayList<>();
-		for (Unit o : os) {
-			unitIds.add(o.getId());
-			unitIds.addAll(business.unit().listSubNested(o.getId()));
-		}
-		unitIds = ListTools.trim(unitIds, true, true);
-		EntityManager em = business.entityManagerContainer().get(Identity.class);
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery<String> cq = cb.createQuery(String.class);
-		Root<Identity> root = cq.from(Identity.class);
-		Predicate p = root.get(Identity_.unit).in(unitIds);
-		List<String> personIds = em.createQuery(cq.select(root.get(Identity_.person)).where(p))
-				.getResultList().stream().distinct().collect(Collectors.toList());
-		personIds = ListTools.trim(personIds, true, true);
-		List<String> values = business.person().listPersonDistinguishedNameSorted(personIds);
-		Wo wo = new Wo();
-		wo.getPersonList().addAll(values);
-		return wo;
-	}
+    private Wo list(Business business, Wi wi) throws Exception {
+        List<Unit> os = business.unit().pick(wi.getUnitList());
+        List<String> unitIds = new ArrayList<>();
+        for (Unit o : os) {
+            unitIds.add(o.getId());
+            unitIds.addAll(business.unit().listSubNested(o.getId()));
+        }
+        unitIds = ListTools.trim(unitIds, true, true);
+        EntityManager em = business.entityManagerContainer().get(Identity.class);
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<String> cq = cb.createQuery(String.class);
+        Root<Identity> root = cq.from(Identity.class);
+        Predicate p = root.get(Identity_.unit).in(unitIds);
+        List<String> personIds = em.createQuery(cq.select(root.get(Identity_.person)).where(p))
+                .getResultList().stream().distinct().collect(Collectors.toList());
+        personIds = ListTools.trim(personIds, true, true);
+        List<Person> personList = new ArrayList<>();
+        for (List<String> subPersonIds : ListTools.batch(personIds, 1000)) {
+            personList.addAll(business.entityManagerContainer().fetch(subPersonIds, Person.class,
+                    ListTools.toList(Person.distinguishedName_FIELDNAME)));
+        }
+        List<String> values = personList.stream().map(Person::getDistinguishedName).sorted()
+                .collect(Collectors.toList());
+        Wo wo = new Wo();
+        wo.getPersonList().addAll(values);
+        return wo;
+    }
 
 }
