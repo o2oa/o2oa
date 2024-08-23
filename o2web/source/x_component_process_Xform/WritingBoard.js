a9f99f3c434e848b7f99363220964576f3f66703
unknown <caixiangyi@zoneland.net>
2024-08-23 17:34:54 +0800

    手写板

--- o2web/source/x_component_process_Xform/WritingBoard.js
+++ o2web/source/x_component_process_Xform/WritingBoard.js
@@ -421,17 +421,19 @@ MWF.xApplication.process.Xform.WritingBoard = MWF.APPWritingBoard = new Class(
             return true;
         },
         validation: function (routeName, opinion) {
-            if (!this.validationConfig(routeName, opinion)) return false;
+            if( !this.isReadonly() ){
+                if (!this.validationConfig(routeName, opinion)) return false;
 
-            if (!this.json.validation) return true;
-            if (!this.json.validation.code) return true;
-            this.currentRouteName = routeName;
-            var flag = this.form.Macro.exec(this.json.validation.code, this);
-            this.currentRouteName = "";
-            if (!flag) flag = MWF.xApplication.process.Xform.LP.notValidation;
-            if (flag.toString() != "true") {
-                this.notValidationMode(flag);
-                return false;
+                if (!this.json.validation) return true;
+                if (!this.json.validation.code) return true;
+                this.currentRouteName = routeName;
+                var flag = this.form.Macro.exec(this.json.validation.code, this);
+                this.currentRouteName = "";
+                if (!flag) flag = MWF.xApplication.process.Xform.LP.notValidation;
+                if (flag.toString() != "true") {
+                    this.notValidationMode(flag);
+                    return false;
+                }
             }
             return true;
         },
