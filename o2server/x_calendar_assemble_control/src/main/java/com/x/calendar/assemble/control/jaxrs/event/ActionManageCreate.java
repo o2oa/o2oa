61937a4593d97e2c432147b8d961ce9fc644750f
wuhengshui <wuhengshui@o2oa.net>
2024-09-11 14:27:50 +0800

    优化管理员发起日程安排的接口，增加了权限控制

--- o2server/x_calendar_assemble_control/src/main/java/com/x/calendar/assemble/control/jaxrs/event/ActionManageCreate.java
+++ o2server/x_calendar_assemble_control/src/main/java/com/x/calendar/assemble/control/jaxrs/event/ActionManageCreate.java
@@ -5,6 +5,7 @@ import com.x.base.core.entity.JpaObject;
 import com.x.base.core.project.annotation.AuditLog;
 import com.x.base.core.project.bean.WrapCopier;
 import com.x.base.core.project.bean.WrapCopierFactory;
+import com.x.base.core.project.exception.ExceptionAccessDenied;
 import com.x.base.core.project.exception.PromptException;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
@@ -35,6 +36,10 @@ public class ActionManageCreate extends BaseAction {
 		List<String> groupNames = null;
 		List<String> ids = null;
 		String personName = effectivePerson.getDistinguishedName();
+
+		if(!effectivePerson.isManager()){
+			throw new ExceptionAccessDenied(effectivePerson.getDistinguishedName());
+		}
 
 		Boolean check = true;
 
