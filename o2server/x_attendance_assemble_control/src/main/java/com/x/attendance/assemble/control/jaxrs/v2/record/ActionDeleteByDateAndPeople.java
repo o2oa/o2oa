596a0e815e4fd165c914b5d9c1b43cb94d0cef8c
fancy <fancylou@outlook.com>
2024-09-29 10:58:31 +0800

    考勤删除打卡记录需要重新统计

--- o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/jaxrs/v2/record/ActionDeleteByDateAndPeople.java
+++ o2server/x_attendance_assemble_control/src/main/java/com/x/attendance/assemble/control/jaxrs/v2/record/ActionDeleteByDateAndPeople.java
@@ -1,14 +1,18 @@
 package com.x.attendance.assemble.control.jaxrs.v2.record;
 
 import com.x.attendance.assemble.control.Business;
+import com.x.attendance.assemble.control.ThisApplication;
 import com.x.attendance.assemble.control.jaxrs.v2.ExceptionEmptyParameter;
 import com.x.attendance.assemble.control.jaxrs.v2.ExceptionWithMessage;
+import com.x.attendance.assemble.control.schedule.v2.QueueAttendanceV2DetailModel;
 import com.x.base.core.container.EntityManagerContainer;
 import com.x.base.core.container.factory.EntityManagerContainerFactory;
 import com.x.base.core.project.exception.ExceptionAccessDenied;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.http.WrapOutBoolean;
+import com.x.base.core.project.logger.Logger;
+import com.x.base.core.project.logger.LoggerFactory;
 import com.x.base.core.project.tools.DateTools;
 import org.apache.commons.lang3.StringUtils;
 
@@ -19,6 +23,8 @@ import java.util.Date;
  * Copyright © 2023 O2. All rights reserved.
  */
 public class ActionDeleteByDateAndPeople extends BaseAction {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ActionDeleteByDateAndPeople.class);
 
 
     ActionResult<Wo> execute(EffectivePerson effectivePerson, String peopleDn, String date) throws  Exception {
@@ -42,6 +48,9 @@ public class ActionDeleteByDateAndPeople extends BaseAction {
                 throw new ExceptionWithMessage("日期格式错误，需要格式：yyyy-MM-dd");
             }
             deleteOldRecords(peopleDn, date, emc, business);
+            // 删除了旧的记录，重新生成
+            LOGGER.info("发起考勤数据生成，Date：{} person: {}", date, peopleDn);
+            ThisApplication.queueV2Detail.send(new QueueAttendanceV2DetailModel(peopleDn, date));
             ActionResult<Wo> result = new ActionResult<>();
             Wo wo = new Wo();
             wo.setValue(true);
