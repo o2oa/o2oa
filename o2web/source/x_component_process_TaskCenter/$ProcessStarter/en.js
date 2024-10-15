255536f1eec8f88ded64146364db78f46d5ecaa7
unknown <caixiangyi@zoneland.net>
2024-10-15 16:24:52 +0800

    修复循环发起流程可能的错误

--- o2web/source/x_component_process_TaskCenter/$ProcessStarter/en.js
+++ o2web/source/x_component_process_TaskCenter/$ProcessStarter/en.js
@@ -1,3 +1,6 @@
+MWF.xApplication.process = MWF.xApplication.process || {};
+MWF.xApplication.process.TaskCenter = MWF.xApplication.process.TaskCenter || {};
+MWF.xApplication.process.TaskCenter.ProcessStarter = MWF.xApplication.process.TaskCenter.ProcessStarter || {};
 MWF.xApplication.process.TaskCenter.ProcessStarter.lp = {
     "start": "Start Process",
 	"department": "Department",
