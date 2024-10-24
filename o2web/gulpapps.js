a5133df65f485211726121486f0d8dac46704052
unknown <caixiangyi@zoneland.net>
2024-10-24 17:52:26 +0800

    指定应用选择流程

--- o2web/gulpapps.js
+++ o2web/gulpapps.js
@@ -97,8 +97,7 @@ var apps = [
     { "folder": "x_component_appstore_application", "tasks": [] },
     { "folder": "x_component_systemconfig", "tasks": [] },
     { "folder": "x_component_attendancev2", "tasks": [] },
-    { "folder": "x_init", "tasks": [] },
-    { "folder": "x_component_custom_vuetest", "tasks": [] }
+    { "folder": "x_init", "tasks": [] }
 ];
 
 module.exports = apps;
