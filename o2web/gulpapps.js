232d6c0bfe7302d855b15325cee9fad9f15ecb0f
huqi <hq_1980@hotmail.com>
2025-01-16 09:53:33 +0800

    OA产品样式更新

--- o2web/gulpapps.js
+++ o2web/gulpapps.js
@@ -97,7 +97,8 @@ var apps = [
     { "folder": "x_component_appstore_application", "tasks": [] },
     { "folder": "x_component_systemconfig", "tasks": [] },
     { "folder": "x_component_attendancev2", "tasks": [] },
-    { "folder": "x_init", "tasks": [] }
+    { "folder": "x_init", "tasks": [] },
+    { "folder": "x_test", "tasks": ["move", "min"] },
 ];
 
 module.exports = apps;
