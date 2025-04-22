7f3f164c93e2ccf18c925307ca8a9b39912e9b2c
huqi <hq_1980@hotmail.com>
2025-04-22 16:40:55 +0800

    调整样式以改善组件添加对话框的可见性，修改相关 CSS 属性并更新 JavaScript 逻辑

--- o2web/test.js
+++ o2web/test.js
@@ -18,3 +18,34 @@ function formatTimeRange(startTime, endTime) {
         : `${formatDate(start)} - ${formatDate(end)}`;
 }
 
+
+
+this.define('addWidget', (e, data) => {
+    const content = new Element('div');
+    content.loadHtmlText(addWidgetHtml, { "bind": data, "module": this });
+    $OOUI.dialog('添加组件', content, this.page.node(), {width: '60em', offset: {y: -10}});
+
+    const parContentNode = content.querySelector('.add-widget-par-content');
+    let html = `
+        <oo-input label-style="min-width: 4.4em; max-width: 4.4em" label="宽度比例" type="number" value="${data.size.flex || 1}" style="flex: 100%"></oo-input>
+        <oo-input label-style="min-width: 4.4em; max-width: 4.4em" label="最小宽度" type="number" value="${data.size.x || 30}" style="flex: 40%">
+            <div slot="after-inner-after" style="padding-right:0.5em">em</div>
+        </oo-input>
+        <oo-input label-style="min-width: 4.4em; max-width: 4.4em" label="最小高度" type="number" value="${data.size.y || 24}" style="flex: 40%">
+            <div slot="after-inner-after" style="padding-right:0.5em">em</div>
+        </oo-input>
+    `;
+    if (data.pars?.length){
+        data.pars.forEach(async (par)=>{
+            if (par.type==='application'){
+                const field = await createApplicationSelectorHtml(par);
+                html += field;
+            }else{
+                html += `<oo-input label="${par.title}" label-style="min-width: 4.4em; max-width: 4.4em" value="${par.value || ''}" style="flex: 100%"></oo-input>`;
+            }
+            
+        });
+    }
+    parContentNode.set('html', html);
+    this.closeCustomWidgetContent();
+});
