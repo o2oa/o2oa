69103b1c3730a62ce53b0108f5978b841bbb409a
o2sword <171715986@qq.com>
2025-06-04 10:37:37 +0800

    修复数据中心数据表匿名查询权限的问题

--- o2server/x_query_assemble_surface/src/main/java/com/x/query/assemble/surface/jaxrs/TableJaxrsFilter.java
+++ o2server/x_query_assemble_surface/src/main/java/com/x/query/assemble/surface/jaxrs/TableJaxrsFilter.java
@@ -1,10 +1,9 @@
 package com.x.query.assemble.surface.jaxrs;
 
+import com.x.base.core.project.jaxrs.CipherManagerUserJaxrsFilter;
 import javax.servlet.annotation.WebFilter;
 
-import com.x.base.core.project.jaxrs.AnonymousCipherManagerUserJaxrsFilter;
-
 @WebFilter(urlPatterns = "/jaxrs/table/*", asyncSupported = true)
-public class TableJaxrsFilter extends AnonymousCipherManagerUserJaxrsFilter {
+public class TableJaxrsFilter extends CipherManagerUserJaxrsFilter {
 
 }
