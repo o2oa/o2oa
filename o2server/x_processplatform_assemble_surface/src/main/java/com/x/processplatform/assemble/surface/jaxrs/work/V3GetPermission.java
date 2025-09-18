3cf90f087fe61886e2bdcb841c9eff0240dd0488
o2null <zhourui@o2oa.net>
2025-09-18 16:38:52 +0800

    增加enable输出

--- o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/work/V3GetPermission.java
+++ o2server/x_processplatform_assemble_surface/src/main/java/com/x/processplatform/assemble/surface/jaxrs/work/V3GetPermission.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.BooleanUtils;
+
 import com.x.base.core.container.EntityManagerContainer;
 import com.x.base.core.container.factory.EntityManagerContainerFactory;
+import com.x.base.core.project.annotation.FieldDescribe;
 import com.x.base.core.project.gson.GsonPropertyObject;
 import com.x.base.core.project.http.ActionResult;
 import com.x.base.core.project.http.EffectivePerson;
@@ -14,6 +17,8 @@ import com.x.base.core.project.logger.LoggerFactory;
 import com.x.base.core.project.organization.OrganizationDefinition;
 import com.x.base.core.project.organization.Role;
 import com.x.processplatform.assemble.surface.Business;
+import com.x.processplatform.assemble.surface.Control;
+import com.x.processplatform.assemble.surface.JobControlBuilder;
 import com.x.processplatform.core.entity.content.Review;
 import com.x.processplatform.core.entity.content.Work;
 import com.x.processplatform.core.entity.content.WorkCompleted;
@@ -38,33 +43,40 @@ class V3GetPermission extends BaseAction {
 		List<String> names = new ArrayList<>();
 		try (EntityManagerContainer emc = EntityManagerContainerFactory.instance().create()) {
 			Business business = new Business(emc);
-			work = emc.find(workOrWorkCompleted, Work.class);
-			if (null == work) {
-				workCompleted = emc.flag(workOrWorkCompleted, WorkCompleted.class);
+			Control control = new JobControlBuilder(effectivePerson, business, workOrWorkCompleted).enableAllowVisit()
+					.build();
+			if (BooleanUtils.isTrue(control.getAllowVisit())) {
+				wo.setEnable(true);
+				work = emc.find(workOrWorkCompleted, Work.class);
+				if (null == work) {
+					workCompleted = emc.flag(workOrWorkCompleted, WorkCompleted.class);
+				}
+				List<Review> reviews = new ArrayList<>();
+				if (null != work) {
+					applicationId = work.getApplication();
+					processId = work.getProcess();
+					job = work.getJob();
+				} else if (null != workCompleted) {
+					applicationId = workCompleted.getApplication();
+					processId = workCompleted.getProcess();
+					job = workCompleted.getJob();
+				}
+				Application application = business.application().pick(applicationId);
+				names.addAll(application.getControllerList());
+				Process process = business.process().pick(processId);
+				names.addAll(process.getControllerList());
+				reviews = emc.listEqual(Review.class, Review.job_FIELDNAME, job);
+				names.addAll(reviews.stream().map(Review::getPerson).collect(Collectors.toList()));
+				List<Role> roles = business.organization().role().listObject(
+						List.of(OrganizationDefinition.Manager, OrganizationDefinition.ProcessPlatformManager));
+				roles.stream().forEach(o -> {
+					names.addAll(o.getGroupList());
+					names.addAll(o.getPersonList());
+				});
+				wo.setAccessibleList(names.stream().distinct().collect(Collectors.toList()));
+			} else {
+				wo.setEnable(false);
 			}
-			List<Review> reviews = new ArrayList<>();
-			if (null != work) {
-				applicationId = work.getApplication();
-				processId = work.getProcess();
-				job = work.getJob();
-			} else if (null != workCompleted) {
-				applicationId = workCompleted.getApplication();
-				processId = workCompleted.getProcess();
-				job = workCompleted.getJob();
-			}
-			Application application = business.application().pick(applicationId);
-			names.addAll(application.getMaintainerList());
-			Process process = business.process().pick(processId);
-			names.addAll(process.getControllerList());
-			reviews = emc.listEqual(Review.class, Review.job_FIELDNAME, job);
-			names.addAll(reviews.stream().map(Review::getPerson).collect(Collectors.toList()));
-			List<Role> roles = business.organization().role()
-					.listObject(List.of(OrganizationDefinition.Manager, OrganizationDefinition.ProcessPlatformManager));
-			roles.stream().forEach(o -> {
-				names.addAll(o.getGroupList());
-				names.addAll(o.getPersonList());
-			});
-			wo.setAccessibleList(names.stream().distinct().collect(Collectors.toList()));
 		}
 		result.setData(wo);
 		return result;
@@ -74,6 +86,10 @@ class V3GetPermission extends BaseAction {
 
 		private static final long serialVersionUID = -8740908895201213169L;
 
+		@FieldDescribe("是否有权限访问")
+		private Boolean enable = false;
+
+		@FieldDescribe("可访问标识")
 		private List<String> accessibleList = new ArrayList<>();
 
 		private List<String> managableList = new ArrayList<>();
@@ -92,6 +108,14 @@ class V3GetPermission extends BaseAction {
 
 		public void setManagableList(List<String> managableList) {
 			this.managableList = managableList;
+		}
+
+		public Boolean getEnable() {
+			return enable;
+		}
+
+		public void setEnable(Boolean enable) {
+			this.enable = enable;
 		}
 
 	}
