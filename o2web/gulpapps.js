e2b6fec8eab7cdcca02108fff9ea6b0abb0efcac
unknown <22236703@qq.com>
2025-09-18 09:49:29 +0800

    修复最后管理人员在慢速模式下多次保存可能会保存多份的问题

--- o2web/gulpapps.js
+++ o2web/gulpapps.js
@@ -99,10 +99,7 @@ var apps = [
     { "folder": "x_component_appstore_application", "tasks": [] },
     { "folder": "x_component_systemconfig", "tasks": [] },
     { "folder": "x_component_attendancev2", "tasks": [] },
-    { "folder": "x_init", "tasks": [] },
-    { "folder": "x_test", "tasks": ["move", "min"] },
-    { "folder": "x_component_custom_demo2", "tasks": ["move", "min"] },
-    { "folder": "x_component_custom_vuedemo", "tasks": [] }
+    { "folder": "x_init", "tasks": [] }
 ];
 
 module.exports = apps;
