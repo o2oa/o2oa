ca5e1e35360166c91bb65b39adf214eef29b5162
o2null <zhourui@o2oa.net>
2025-10-30 14:42:10 +0800

    update

--- o2server/x_query_assemble_surface/src/main/java/com/x/query/assemble/surface/jaxrs/view/ExecuteV2Search.java
+++ o2server/x_query_assemble_surface/src/main/java/com/x/query/assemble/surface/jaxrs/view/ExecuteV2Search.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.queryparser.classic.QueryParserBase;
 import org.apache.lucene.search.BooleanClause;
@@ -31,6 +32,7 @@ import org.apache.lucene.util.BytesRef;
 import com.google.gson.Gson;
 import com.hankcs.lucene.HanLPAnalyzer;
 import com.x.base.core.project.bean.tuple.Pair;
+import com.x.base.core.project.exception.ExceptionAccessDenied;
 import com.x.base.core.project.gson.XGsonBuilder;
 import com.x.base.core.project.logger.Logger;
 import com.x.base.core.project.logger.LoggerFactory;
@@ -42,41 +44,12 @@ import com.x.query.core.express.plan.Runtime;
 
 public class ExecuteV2Search extends BaseAction {
 
-	private static Logger LOGGER = LoggerFactory.getLogger(ExecuteV2Search.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(ExecuteV2Search.class);
 
 	private static Gson gson = XGsonBuilder.instance();
 
 	public Pair<List<String>, Long> search(View view, Runtime runtime, String search, Integer page, Integer size)
 			throws Exception {
-		if (View.TYPE_CMS.equals(view.getType())) {
-			CmsPlan cmsPlan = gson.fromJson(view.getData(), CmsPlan.class);
-			final List<String> appInfoList = new ArrayList<>();
-			final List<String> catgoryInfoList = new ArrayList<>();
-			cmsPlan.where.appInfoList.stream().forEach(o -> appInfoList.add(o.id));
-			cmsPlan.where.categoryInfoList.stream().forEach(o -> catgoryInfoList.add(o.id));
-			return cms();
-		} else {
-			ProcessPlatformPlan processPlatformPlan = gson.fromJson(view.getData(), ProcessPlatformPlan.class);
-			this.setProcessEdition(processPlatformPlan);
-			final List<String> processIdList = new ArrayList<>();
-			processPlatformPlan.where.processList.stream().forEach(o -> processIdList.add(o.id));
-			return processPlatform(processIdList, runtime.authList, search.toLowerCase(), page, size);
-		}
-
-	}
-
-	private Pair<List<String>, Long> cms() {
-		List<String> list = new ArrayList<>();
-		try {
-
-		} catch (Exception e) {
-			LOGGER.error(e);
-		}
-		return Pair.of(list, 0L);
-	}
-
-	private Pair<List<String>, Long> processPlatform(List<String> processList, List<String> authList, String search,
-			Integer page, Integer size) {
 		List<String> list = new ArrayList<>();
 		Optional<Directory> opt = Indexs.directory(Indexs.CATEGORY_SEARCH, Indexs.KEY_ENTIRE, true);
 		if (opt.isEmpty()) {
@@ -88,30 +61,20 @@ public class ExecuteV2Search extends BaseAction {
 				DirectoryReader reader = DirectoryReader.open(directory)) {
 			IndexSearcher searcher = new IndexSearcher(reader);
 
-			Query permissionFilter = new TermInSetQuery(Indexs.FIELD_READERS, authList.stream().filter(Objects::nonNull)
-					.<BytesRef>map(BytesRef::new).collect(Collectors.toList()));
-
-			Query processFilter = new TermInSetQuery(Indexs.FIELD_PROCESS, processList.stream().filter(Objects::nonNull)
-					.<BytesRef>map(BytesRef::new).collect(Collectors.toList()));
-
-			QueryParser bodyParser = new QueryParser(Indexs.FIELD_BODY, analyzer);
-			Query boostedBodyQuery = new BoostQuery(bodyParser.parse(QueryParserBase.escape(search)), 10f);
-
-			QueryParser attachmentParser = new QueryParser(Indexs.FIELD_ATTACHMENT, analyzer);
-			Query boostedAttachmentQuery = new BoostQuery(attachmentParser.parse(QueryParserBase.escape(search)), 5f);
-
-			Query exactItemQuery = new BoostQuery(new TermQuery(new Term(Indexs.FIELD_ITEMLIST, search)), 20f);
-			Query prefixItemQuery = new BoostQuery(new PrefixQuery(new Term(Indexs.FIELD_ITEMLIST, search)), 15f);
-			DisjunctionMaxQuery itemListQuery = new DisjunctionMaxQuery(Arrays.asList(exactItemQuery, prefixItemQuery),
-					/* tieBreaker */ 0.0f);
+			Query permissionFilter = termInSet(Indexs.FIELD_READERS, runtime.authList);
+			Query scopeFilter = scopeFilter(view, runtime);
+			Query bodyQuery = bodyQuery(search, analyzer);
+			Query attachmentQuery = attachmentQuery(search, analyzer);
+			Query itemQuery = this.itemQuery(search);
+			Query recencyQuery = this.recencyQuery();
 
 			BooleanQuery.Builder qb = new BooleanQuery.Builder();
 			qb.add(permissionFilter, BooleanClause.Occur.FILTER); // 不参与打分
-			qb.add(processFilter, BooleanClause.Occur.FILTER); // 不参与打分
+			qb.add(scopeFilter, BooleanClause.Occur.FILTER); // 不参与打分
 
-			BooleanQuery query = qb.add(boostedBodyQuery, BooleanClause.Occur.SHOULD)
-					.add(boostedAttachmentQuery, BooleanClause.Occur.SHOULD)
-					.add(itemListQuery, BooleanClause.Occur.SHOULD).setMinimumNumberShouldMatch(1).build();
+			BooleanQuery query = qb.add(bodyQuery, BooleanClause.Occur.SHOULD)
+					.add(attachmentQuery, BooleanClause.Occur.SHOULD).add(itemQuery, BooleanClause.Occur.SHOULD)
+					.add(recencyQuery, BooleanClause.Occur.SHOULD).setMinimumNumberShouldMatch(1).build();
 
 			// Page with searchAfter
 			TopDocs pageDocs = page(searcher, query, page, size);
@@ -125,6 +88,47 @@ public class ExecuteV2Search extends BaseAction {
 			LOGGER.error(e);
 		}
 		return Pair.of(list, 0L);
+	}
+
+	private Query scopeFilter(View view, Runtime runtime) throws Exception {
+		if (View.TYPE_CMS.equals(view.getType())) {
+			return this.cmsScopeFilter(view).orElseThrow(() -> new ExceptionAccessDenied(runtime.person));
+		} else {
+			return this.processPlatformScopeFilter(view).orElseThrow(() -> new ExceptionAccessDenied(runtime.person));
+		}
+	}
+
+	private Optional<Query> cmsScopeFilter(View view) {
+		CmsPlan cmsPlan = gson.fromJson(view.getData(), CmsPlan.class);
+		final List<String> appInfoList = new ArrayList<>();
+		final List<String> categoryInfoList = new ArrayList<>();
+		cmsPlan.where.appInfoList.stream().forEach(o -> appInfoList.add(o.id));
+		cmsPlan.where.categoryInfoList.stream().forEach(o -> categoryInfoList.add(o.id));
+		if (appInfoList.isEmpty() && categoryInfoList.isEmpty()) {
+			return Optional.empty();
+		}
+		if (!appInfoList.isEmpty() && !categoryInfoList.isEmpty()) {
+			BooleanQuery.Builder b = new BooleanQuery.Builder();
+			b.add(termInSet(Indexs.FIELD_APPID, appInfoList), BooleanClause.Occur.SHOULD);
+			b.add(termInSet(Indexs.FIELD_CATEGORYID, categoryInfoList), BooleanClause.Occur.SHOULD);
+			b.setMinimumNumberShouldMatch(1);
+			return Optional.of(b.build());
+		} else if (!appInfoList.isEmpty()) {
+			return Optional.of(termInSet(Indexs.FIELD_APPID, appInfoList));
+		} else {
+			return Optional.of(termInSet(Indexs.FIELD_CATEGORYID, categoryInfoList));
+		}
+	}
+
+	private Optional<Query> processPlatformScopeFilter(View view) throws Exception {
+		ProcessPlatformPlan processPlatformPlan = gson.fromJson(view.getData(), ProcessPlatformPlan.class);
+		this.setProcessEdition(processPlatformPlan);
+		final List<String> processIdList = new ArrayList<>();
+		processPlatformPlan.where.processList.stream().forEach(o -> processIdList.add(o.id));
+		if (processIdList.isEmpty()) {
+			return Optional.empty();
+		}
+		return Optional.of(termInSet(Indexs.FIELD_PROCESS, processIdList));
 	}
 
 	/**
@@ -144,8 +148,38 @@ public class ExecuteV2Search extends BaseAction {
 		return searcher.searchAfter(last, q, size);
 	}
 
+	private static Query termInSet(String field, List<String> values) {
+		List<BytesRef> brs = new ArrayList<>(values.size());
+		for (String v : values) {
+			brs.add(new BytesRef(v));
+		}
+		return new TermInSetQuery(field, brs);
+	}
+
 	private static TopDocs emptyTopDocs() {
 		return new TopDocs(new TotalHits(0, TotalHits.Relation.EQUAL_TO), new ScoreDoc[0]);
+	}
+
+	private Query recencyQuery() {
+		long now = System.currentTimeMillis();
+		long pivot = TimeUnit.DAYS.toMillis(7); // “半衰期”：与 now 相差 7 天时拿到一半加分
+		return LongPoint.newDistanceFeatureQuery(Indexs.FIELD_UPDATETIME, 10f, now, pivot);
+	}
+
+	private Query bodyQuery(String search, HanLPAnalyzer analyzer) throws ParseException {
+		QueryParser bodyParser = new QueryParser(Indexs.FIELD_BODY, analyzer);
+		return new BoostQuery(bodyParser.parse(QueryParserBase.escape(search)), 10f);
+	}
+
+	private Query attachmentQuery(String search, HanLPAnalyzer analyzer) throws ParseException {
+		QueryParser attachmentParser = new QueryParser(Indexs.FIELD_ATTACHMENT, analyzer);
+		return new BoostQuery(attachmentParser.parse(QueryParserBase.escape(search)), 5f);
+	}
+
+	private Query itemQuery(String search) {
+		Query exactItemQuery = new BoostQuery(new TermQuery(new Term(Indexs.FIELD_ITEMLIST, search)), 20f);
+		Query prefixItemQuery = new BoostQuery(new PrefixQuery(new Term(Indexs.FIELD_ITEMLIST, search)), 15f);
+		return new DisjunctionMaxQuery(Arrays.asList(exactItemQuery, prefixItemQuery), /* tieBreaker */ 0.0f);
 	}
 
 }
