d6b15441ff64a926527eb3ffb17d6d9f96014cee
unknown <22236703@qq.com>
2025-11-11 17:31:52 +0800

    修复oo组件只读的时候校验的问题

--- o2web/source/x_component_process_Xform/OOAddress.js
+++ o2web/source/x_component_process_Xform/OOAddress.js
@@ -247,11 +247,17 @@ MWF.xApplication.process.Xform.OOAddress = MWF.APPOOAddress = new Class({
     },
 
     notValidationMode: function (text) {
-        this.validationText = text;
-        this.node.checkValidity();
+        if(!this.isNotValidationMode){
+            this.isNotValidationMode = true;
+            this.validationText = text;
+            this.node.checkValidity();
+        }
     },
     validationMode: function () {
-        this.validationText = '';
-        this.node.unInvalidStyle();
+        if(this.isNotValidationMode){
+            this.isNotValidationMode = false;
+            this.validationText = '';
+            this.node.unInvalidStyle();
+        }
     },
 });
--- o2web/source/x_component_process_Xform/OOTextarea.js
+++ o2web/source/x_component_process_Xform/OOTextarea.js
@@ -192,11 +192,17 @@ MWF.xApplication.process.Xform.OOTextarea = MWF.APPOOTextarea = new Class({
     },
 
     notValidationMode: function (text) {
-        this.validationText = text;
-        this.node.checkValidity();
+        if(!this.isNotValidationMode){
+            this.isNotValidationMode = true;
+            this.validationText = text;
+            this.node.checkValidity();
+        }
     },
     validationMode: function () {
-        this.validationText = '';
-        this.node.unInvalidStyle();
+        if(this.isNotValidationMode){
+            this.isNotValidationMode = false;
+            this.validationText = '';
+            this.node.unInvalidStyle();
+        }
     }
 });
