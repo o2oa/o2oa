c551a365b8782282904f945a2e6b6f53dc168d58
o2sword <171715986@qq.com>
2025-12-18 09:47:52 +0800

    网盘模块改为系统模块之一

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/x_pan_assemble_control.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/x_pan_assemble_control.java
@@ -1,7 +1,6 @@
-package com.x.pan.assemble.control;
+package com.x.base.core.project;
 
 import com.x.base.core.entity.StorageType;
-import com.x.base.core.project.Deployable;
 import com.x.base.core.project.annotation.Module;
 import com.x.base.core.project.annotation.ModuleCategory;
 import com.x.base.core.project.annotation.ModuleType;
@@ -15,7 +14,7 @@ import com.x.base.core.project.annotation.ModuleType;
  * customJars 需要访问的自定义工程
  * @author sword
  */
-@Module(type = ModuleType.ASSEMBLE, category = ModuleCategory.CUSTOM, name = "新版企业网盘", packageName = "com.x.pan.assemble.control",
+@Module(type = ModuleType.ASSEMBLE, category = ModuleCategory.OFFICIAL, name = "新版企业网盘", packageName = "com.x.pan.assemble.control",
 		containerEntities = {"com.x.file.core.entity.personal.Folder2","com.x.file.core.entity.personal.Attachment2",
 				"com.x.file.core.entity.personal.Share","com.x.file.core.entity.open.OriginFile",
 				"com.x.file.core.entity.personal.Recycle","com.x.file.core.entity.open.FileConfig",
@@ -25,7 +24,7 @@ import com.x.base.core.project.annotation.ModuleType;
 				"com.x.pan.core.entity.AttachmentVersion","com.x.pan.core.entity.LockInfo",
 				"com.x.organization.core.entity.Identity",
 				"com.x.organization.core.entity.Group", "com.x.organization.core.entity.Unit"},
-		storeJars = { "x_organization_core_entity", "x_organization_core_express", "x_file_core_entity" },
-		storageTypes = { StorageType.file }, customJars = { "x_pan_core_entity" })
+		storeJars = { "x_organization_core_entity", "x_organization_core_express", "x_file_core_entity", "x_pan_core_entity" },
+		storageTypes = { StorageType.file })
 public class x_pan_assemble_control extends Deployable {
 }
