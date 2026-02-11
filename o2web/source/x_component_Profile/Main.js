abf2f52143fb994666971b7875f94b1add0d8e5b
unknown <22236703@qq.com>
2026-02-11 16:32:00 +0800

    一键下载

--- o2web/source/x_component_Profile/Main.js
+++ o2web/source/x_component_Profile/Main.js
@@ -382,7 +382,7 @@ MWF.xApplication.Profile.Main = new Class({
         this.ideasSaveAction.addEvent("click", function(){
             MWF.require("MWF.widget.UUID", function(){
                 var data = {};
-                data.ideas = this.ideasArea.get("value").split("\n");
+                data.ideas = this.ideasArea.get("value").split("\n").filter(v=>!!v);
                 MWF.UD.putData("idea", data, function(){
                     this.notice(this.lp.ideaSaveOk, "success");
                 }.bind(this));
