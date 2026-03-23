02398e856f8f93eee5dbac959707e67bb1000f89
Tommy <hq_1980@hotmail.com>
2026-03-23 09:59:18 +0800

    优化组建

--- o2web/source/x_component_TeamWork_Task/lp/zh-cn.js
+++
@@ -1,5 +0,0 @@
-MWF.xApplication.TeamWork = MWF.xApplication.TeamWork || {};
-MWF.xApplication.TeamWork.Task = MWF.xApplication.TeamWork.Task || {};
-MWF.xApplication.TeamWork.Task.LP = {
-	"title":"任务"
-};
