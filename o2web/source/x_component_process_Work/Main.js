8b545a9b11e198d4ef8550f2c894ee45a8f90528
caixiangyi <caixiangyi@zoneland.net>
2026-04-14 16:41:44 +0800

    修复新建草稿是表单未更新的问题

--- o2web/source/x_component_process_Work/Main.js
+++ o2web/source/x_component_process_Work/Main.js
@@ -386,7 +386,7 @@ MWF.xApplication.process.Work.Main = new Class({
                 // this.unLoading();
 
             }
-        }.bind(this), "failure": function(){}}, [work.form, work.application]);
+        }.bind(this), "failure": function(){}}, [work.form, new Date().getTime()]);
     },
 
     createWorkNode: function(work, node, completed){
