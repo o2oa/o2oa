2c4886d0bffc530f1eb5b841f30cb5d26ecd8fd4
caixiangyi <caixiangyi@zoneland.net>
2026-05-06 13:11:54 +0800

    修复匿名打开门户报错的问题

--- o2web/source/o2_core/o2.js
+++ o2web/source/o2_core/o2.js
@@ -2180,7 +2180,7 @@ if (!window.o2) {
                             delete _resGetQueue[_addr];
 
                             if (xhr && xhr.status === 401){
-                                if (!layout.loginDlg) {
+                                if (!layout.loginDlg && layout.session?.user?.name) {
                                     var json = JSON.decode(xhr.responseText);
 
                                     const node = new Element("div", {styles: {height: "100%"}});
--- o2web/source/x_desktop/js/base_loader.js
+++ o2web/source/x_desktop/js/base_loader.js
@@ -139,22 +139,40 @@ if (!layout.isReady) {
                 layout.user = data;
                 layout.session = layout.session || {};
 
-                o2.Actions.load("x_organization_assemble_express").PersonAction.detail(data.distinguishedName, null, function (json) {
+                if(data.distinguishedName){
+                    o2.Actions.load("x_organization_assemble_express").PersonAction.detail(data.distinguishedName, null, function (json) {
+                        layout.session.user = data;
+                        layout.session.token = data.token;
+                        layout.desktop.session = layout.session;
+
+                        layout.session.userDetail = json.data;
+                        layout.session.userDetail.list = [].concat(
+                            layout.session.userDetail.groupList || [],
+                            layout.session.userDetail.identityList || [],
+                            layout.session.userDetail.personAttributeList || [],
+                            layout.session.userDetail.roleList || [],
+                            layout.session.userDetail.unitDutyList || [],
+                            layout.session.userDetail.unitList || []
+                        )
+                        if (cb) cb(json);
+                    });
+                }else{
                     layout.session.user = data;
                     layout.session.token = data.token;
                     layout.desktop.session = layout.session;
 
-                    layout.session.userDetail = json.data;
-                    layout.session.userDetail.list = [].concat(
-                        layout.session.userDetail.groupList || [], 
-                        layout.session.userDetail.identityList || [],
-                        layout.session.userDetail.personAttributeList || [],
-                        layout.session.userDetail.roleList || [],
-                        layout.session.userDetail.unitDutyList || [],
-                        layout.session.userDetail.unitList || []
-                    )
-                    if (cb) cb(json);
-                });
+                    layout.session.userDetail = {
+                        list: [],
+                        groupList: [],
+                        identityList: [],
+                        personAttributeList: [],
+                        roleList: [],
+                        unitDutyList: [],
+                        unitList: []
+                    };
+
+                    if (cb) cb({data: layout.session.userDetail});
+                }
             }
 
             layout.sessionPromise = new Promise(function (resolve, reject) {
