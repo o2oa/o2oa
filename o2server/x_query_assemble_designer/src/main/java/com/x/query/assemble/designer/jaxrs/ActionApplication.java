62b1759dcb7f275818b458fbb0702a00d48bc52c
o2sword <171715986@qq.com>
2026-05-11 16:19:10 +0800

    删除神经网络后端api接口

--- o2server/x_query_assemble_designer/src/main/java/com/x/query/assemble/designer/jaxrs/ActionApplication.java
+++ o2server/x_query_assemble_designer/src/main/java/com/x/query/assemble/designer/jaxrs/ActionApplication.java
@@ -1,21 +1,18 @@
 package com.x.query.assemble.designer.jaxrs;
 
-import java.util.Set;
-
-import javax.ws.rs.ApplicationPath;
-
 import com.x.base.core.project.jaxrs.AbstractActionApplication;
 import com.x.query.assemble.designer.jaxrs.designer.DesignerAction;
 import com.x.query.assemble.designer.jaxrs.id.IdAction;
 import com.x.query.assemble.designer.jaxrs.importmodel.ImportModelAction;
 import com.x.query.assemble.designer.jaxrs.input.InputAction;
-import com.x.query.assemble.designer.jaxrs.neural.NeuralAction;
 import com.x.query.assemble.designer.jaxrs.output.OutputAction;
 import com.x.query.assemble.designer.jaxrs.query.QueryAction;
 import com.x.query.assemble.designer.jaxrs.stat.StatAction;
 import com.x.query.assemble.designer.jaxrs.statement.StatementAction;
 import com.x.query.assemble.designer.jaxrs.table.TableAction;
 import com.x.query.assemble.designer.jaxrs.view.ViewAction;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("jaxrs")
 public class ActionApplication extends AbstractActionApplication {
@@ -25,7 +22,6 @@ public class ActionApplication extends AbstractActionApplication {
 		classes.add(ViewAction.class);
 		classes.add(StatAction.class);
 		classes.add(IdAction.class);
-		classes.add(NeuralAction.class);
 		classes.add(OutputAction.class);
 		classes.add(InputAction.class);
 		classes.add(TableAction.class);
--- o2server/x_query_assemble_surface/src/main/java/com/x/query/assemble/surface/jaxrs/ActionApplication.java
+++ o2server/x_query_assemble_surface/src/main/java/com/x/query/assemble/surface/jaxrs/ActionApplication.java
@@ -1,19 +1,16 @@
 package com.x.query.assemble.surface.jaxrs;
 
-import java.util.Set;
-
-import javax.ws.rs.ApplicationPath;
-
 import com.x.base.core.project.jaxrs.AbstractActionApplication;
 import com.x.query.assemble.surface.jaxrs.importmodel.ImportModelAction;
 import com.x.query.assemble.surface.jaxrs.morelikethis.MoreLikeThisAction;
-import com.x.query.assemble.surface.jaxrs.neural.NeuralAction;
 import com.x.query.assemble.surface.jaxrs.query.QueryAction;
 import com.x.query.assemble.surface.jaxrs.search.SearchAction;
 import com.x.query.assemble.surface.jaxrs.stat.StatAction;
 import com.x.query.assemble.surface.jaxrs.statement.StatementAction;
 import com.x.query.assemble.surface.jaxrs.table.TableAction;
 import com.x.query.assemble.surface.jaxrs.view.ViewAction;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("jaxrs")
 public class ActionApplication extends AbstractActionApplication {
@@ -22,7 +19,6 @@ public class ActionApplication extends AbstractActionApplication {
         classes.add(QueryAction.class);
         classes.add(ViewAction.class);
         classes.add(StatAction.class);
-        classes.add(NeuralAction.class);
         classes.add(TableAction.class);
         classes.add(StatementAction.class);
         classes.add(ImportModelAction.class);
