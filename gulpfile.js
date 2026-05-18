8dad551eebb410fcf2e43fc6f9d1e155f3b94260
胡起 <hq_1980@hotmail.com>
2026-05-18 03:05:48 +0000

    编辑gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ function check_language_pack(token){
 
 var downloadHost = "g.o2oa.net";
 var protocol = "https";
-var commonUrl = "/o2oa/evn-o2server-commons/-/archive/9.2/evn-o2server-commons-9.2.tar.gz?path=commons";
+var commonUrl = "/o2oa/evn-o2server-commons/-/archive/9.2.1/evn-o2server-commons-9.2.1.tar.gz?path=commons";
 var jvmUrls = {
     "all": "/o2oa/evn-o2server-jvm/-/archive/master/evn-o2server-jvm-master.tar.gz?path=jvm",
     "linux_java11": "/o2oa/evn-o2server-jvm/-/archive/master/evn-o2server-jvm-master.tar.gz?path=jvm/linux_java11",
@@ -291,7 +291,7 @@ function move_commons(){
     console.log(`---------------------------------------------------------------------
   . move commons files to o2server/commons ...
 ---------------------------------------------------------------------`);
-    return gulp.src("o2server/tmp/evn-o2server-commons-9.2-commons/commons/**/*")
+    return gulp.src("o2server/tmp/evn-o2server-commons-9.2.1-commons/commons/**/*")
         .pipe(gulp.dest("o2server/commons/"));
 }
 function move_jvm(){
@@ -308,7 +308,7 @@ function move_jvm(){
         .pipe(gulp.dest("o2server/jvm/"));
 }
 async function clear_commons_git(cb) {
-    var dest = ['o2server/tmp/evn-o2server-commons-9.2-commons/', 'o2server/commons_git.tar.gz'];
+    var dest = ['o2server/tmp/evn-o2server-commons-9.2.1-commons/', 'o2server/commons_git.tar.gz'];
     await del(dest, {force: true});
     cb();
 }
