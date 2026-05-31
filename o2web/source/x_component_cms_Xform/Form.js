4b876c369ac89fc3f565fdcc9514b4e9b4429a57
unknown <caixiangyi@zoneland.net>
2026-05-31 23:25:33 +0800

    内容管理保存文档改为局部保存

--- o2web/source/x_component_cms_Xform/Form.js
+++ o2web/source/x_component_cms_Xform/Form.js
@@ -259,7 +259,7 @@ MWF.xApplication.cms.Xform.Form = MWF.CMSForm = new Class(
                             this.container.loadCssText(json.data.text);
                         });
                     });
-                } 
+                }
                 if (this.json.cssLink) this.container.loadCss(this.json.cssLink);
 
                 this.container.set("html", this.html);
@@ -1139,20 +1139,30 @@ MWF.xApplication.cms.Xform.Form = MWF.CMSForm = new Class(
             documentData.pictureList = specialData.pictures;
             documentData.summary = specialData.summary;
             documentData.cloudPictures = specialData.cloudPictures;
-            documentData.docData = data;
+            //documentData.docData = data;
             delete documentData.attachmentList;
             if (this.officeList) {
                 this.officeList.each(function (module) {
                     module.save();
                 });
             }
+
+            this.modifedData = {};
+            this.setModifedData(data);
+
             var copyData = Object.clone(data);
             this.documentAction.saveDocument(documentData, function () {
-                this.businessData.data.isNew = false;
-                this.businessData.originalData = null;
-                this.businessData.originalData = copyData;
-                this.saving = false;
-                if (callback && typeof callback === "function") callback();
+                o2.Actions.load('x_cms_assemble_control').DataAction.updateWithDocument(
+                    this.businessData.document.id,
+                    this.modifedData,
+                    ()=>{
+                        this.businessData.data.isNew = false;
+                        this.businessData.originalData = null;
+                        this.businessData.originalData = copyData;
+                        this.saving = false;
+                        if (callback && typeof callback === "function") callback();
+                    }, null, !sync
+                )
             }.bind(this), null, !sync);
         },
         saveDocument: function (callback, sync, silent) {
@@ -1185,7 +1195,7 @@ MWF.xApplication.cms.Xform.Form = MWF.CMSForm = new Class(
             documentData.pictureList = specialData.pictures;
             documentData.summary = specialData.summary;
             documentData.cloudPictures = specialData.cloudPictures;
-            documentData.docData = data;
+            //documentData.docData = data;
             delete documentData.attachmentList;
             this.fireEvent("postSave", [documentData]);
             if (this.officeList) {
@@ -1193,20 +1203,30 @@ MWF.xApplication.cms.Xform.Form = MWF.CMSForm = new Class(
                     module.save();
                 });
             }
+
+            this.modifedData = {};
+            this.setModifedData(data);
+
             var copyData = Object.clone(data);
             this.documentAction.saveDocument(documentData, function () {
                 //this.documentAction.saveData(function(json){
-                if(!silent)this.app.notice(MWF.xApplication.cms.Xform.LP.dataSaved, "success");
-                this.businessData.data.isNew = false;
-                this.businessData.originalData = null;
-                this.businessData.originalData = copyData;
-                this.fireEvent("afterSave", [this, documentData]);
-                if (this.app) if (this.app.fireEvent) this.app.fireEvent("afterSave",[this, documentData]);
-                if (callback && typeof callback === "function") callback();
-                this.saving = false;
-                if( !this.json.notReloadWhenSave ){
-                    this._reloadReadForm();
-                }
+                    o2.Actions.load('x_cms_assemble_control').DataAction.updateWithDocument(
+                        this.businessData.document.id,
+                        this.modifedData,
+                        ()=>{
+                            if(!silent)this.app.notice(MWF.xApplication.cms.Xform.LP.dataSaved, "success");
+                            this.businessData.data.isNew = false;
+                            this.businessData.originalData = null;
+                            this.businessData.originalData = copyData;
+                            this.fireEvent("afterSave", [this, documentData]);
+                            if (this.app) if (this.app.fireEvent) this.app.fireEvent("afterSave",[this, documentData]);
+                            if (callback && typeof callback === "function") callback();
+                            this.saving = false;
+                            if( !this.json.notReloadWhenSave ){
+                                this._reloadReadForm();
+                            }
+                        }, null, !sync
+                    )
                 //}.bind(this), null, this.businessData.document.id, data, !sync );
             }.bind(this), null, !sync);
         },
