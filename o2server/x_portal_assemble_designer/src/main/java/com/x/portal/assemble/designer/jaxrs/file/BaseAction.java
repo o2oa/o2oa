17f144edc0b6ca5b53ef12c3ad0006ab8e44b093
o2sword <171715986@qq.com>
2026-07-02 16:05:11 +0800

    修复门户批量上传资源存在失败的情况

--- o2server/x_portal_assemble_designer/src/main/java/com/x/portal/assemble/designer/jaxrs/file/BaseAction.java
+++ o2server/x_portal_assemble_designer/src/main/java/com/x/portal/assemble/designer/jaxrs/file/BaseAction.java
@@ -1,27 +1,32 @@
 package com.x.portal.assemble.designer.jaxrs.file;
 
-import java.util.Date;
-
 import com.x.base.core.project.http.EffectivePerson;
 import com.x.base.core.project.jaxrs.StandardJaxrsAction;
 import com.x.base.core.project.tools.URLTools;
 import com.x.portal.assemble.designer.Business;
 import com.x.portal.core.entity.File;
+import java.util.Date;
+import java.util.concurrent.locks.ReentrantLock;
 
 abstract class BaseAction extends StandardJaxrsAction {
-
+	private static final ReentrantLock lock = new ReentrantLock();
 	protected void updateCreator(File file,EffectivePerson effectivePerson){
 		file.setLastUpdatePerson(effectivePerson.getDistinguishedName());
 		file.setLastUpdateTime(new Date());
 	}
 
 	String getShortUrlCode(Business business, File file, int length) throws Exception{
-		String code = URLTools.shortUrl(file.getId(), length);
-		if (business.entityManagerContainer().duplicateWithFlags(file.getId(), File.class, code)) {
-			length++;
-			return getShortUrlCode(business, file, length);
-		}else{
-			return code;
+		lock.lock();
+		try {
+			String code = URLTools.shortUrl(file.getId(), length);
+			if (business.entityManagerContainer().duplicateWithFlags(file.getId(), File.class, code)) {
+				length++;
+				return getShortUrlCode(business, file, length);
+			}else{
+				return code;
+			}
+		} finally {
+			lock.unlock();
 		}
 	}
 
