6e3d5607412481b4329b4854a0ce6329419175e3
o2sword <171715986@qq.com>
2026-08-07 17:34:51 +0800

    控制api.json访问,增加加密访问接口

---
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/jaxrs/describe/DescribeAction.java
@@ -0,0 +1,48 @@
+package com.x.base.core.project.jaxrs.describe;
+
+import com.x.base.core.project.annotation.DescribeScope;
+import com.x.base.core.project.annotation.JaxrsDescribe;
+import com.x.base.core.project.annotation.JaxrsMethodDescribe;
+import com.x.base.core.project.http.ActionResult;
+import com.x.base.core.project.http.EffectivePerson;
+import com.x.base.core.project.http.HttpMediaType;
+import com.x.base.core.project.jaxrs.ResponseFactory;
+import com.x.base.core.project.jaxrs.StandardJaxrsAction;
+import com.x.base.core.project.logger.Logger;
+import com.x.base.core.project.logger.LoggerFactory;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+@Path("describe")
+@JaxrsDescribe(value = "工程业务信息.", scope = DescribeScope.system)
+public class DescribeAction extends StandardJaxrsAction {
+
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DescribeAction.class);
+
+	@JaxrsMethodDescribe(value = "工程业务信息.", action = ActionGet.class)
+	@GET
+	@Produces(HttpMediaType.APPLICATION_JSON_UTF_8)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void get(@Suspended final AsyncResponse asyncResponse, @Context ServletContext servletContext,
+			@Context HttpServletRequest request) {
+		ActionResult<ActionGet.Wo> result = new ActionResult<>();
+		EffectivePerson effectivePerson = this.effectivePerson(request);
+		try {
+			result = new ActionGet().execute(effectivePerson, servletContext);
+		} catch (Exception e) {
+			LOGGER.error(e, effectivePerson, request, null);
+			result.error(e);
+		}
+		asyncResponse.resume(ResponseFactory.getEntityTagActionResultResponse(request, result));
+	}
+
+}
