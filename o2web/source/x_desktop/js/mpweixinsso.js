4678837ad0a8ea5ea3ea9e1cd96ea28a573af98f
fancy <fancylou@outlook.com>
2026-08-10 11:33:08 +0800

    微信公众号，绑定用户后弹出错误的 bug 修复

--- o2web/source/x_desktop/js/mpweixinsso.js
+++ o2web/source/x_desktop/js/mpweixinsso.js
@@ -7,56 +7,60 @@ layout.addReady(function(){
     (function(layout){
         layout.mobile = true;
         var href = locate.href;
-        if (href.indexOf("debugger") !== -1) layout.debugger = true;
-
-        var _loginSuccess =  function (toUri) {
-            if (toUri) {
-                history.replaceState(null, "page", toUri);
-                toUri.toURI().go();
+        if (href.indexOf("debugger") != -1) layout.debugger = true;
+ 
+        var _bindWeixin2User = function (redirect) {
+            var uri = href.toURI();
+            var code = uri.getData("code"); //微信code
+            if (code) {
+                o2.Actions.load("x_organization_assemble_authentication").MPweixinAction.bindWithCode(code, function (json) {
+                    //绑定成功
+                    // var box = new Element("div", { "style": "text-align: center;" }).inject($("appContent"));
+                    // new Element("h2", { "text": "绑定成功！" }).inject(box);
+                    // o2.api.page.notice("绑定成功！", "success");
+                    _redirectTo(redirect);
+                }, function (err) {
+                    console.error(err);
+                })
+            } else {
+                console.error('没有传入微信code无法绑定');
+            }
+        };
+        var _redirectTo = function (redirect) {
+            if (redirect) {
+                history.replaceState(null, "page", redirect);
+                redirect.toURI().go();
             } else {
                 history.replaceState(null, "page", "../x_desktop/appMobile.html?app=process.TaskCenter");
                 "appMobile.html?app=process.TaskCenter".toURI().go();
             }
         };
-        var _bindWeixin2UserAndGo = function (openid, toUri) {
-            o2.Actions.load("x_organization_assemble_authentication").MPweixinAction.bindOpenId(openid, function (json) {
-                console.log("绑定成功", json);
-                _loginSuccess(toUri);
-            }, function (err) {
-                console.error(err);
-                _loginSuccess(toUri);
-            })
-        };
         
+        ///////////////////////////
         var uri = href.toURI();
         var redirect = uri.getData("redirect"); //登录成功后跳转地址
         var code = uri.getData("code"); //微信code
         console.log("code：" + code)
         var type = uri.getData("type"); // bind 是绑定
         console.log("type：" + type)
-
-        o2.Actions.load("x_organization_assemble_authentication").MPweixinAction.loginWithCode(code, function (json) {
-            if (json.data && json.data.unbind === true) { // 未绑定
-                // 登录并绑定
-                if (layout.session && layout.session.user){
-                    _bindWeixin2UserAndGo(json.data.mpwxopenId, redirect);
-                }else{
-                    if (layout.sessionPromise){
-                        layout.sessionPromise.then(function(){
-                            _bindWeixin2UserAndGo(json.data.mpwxopenId, redirect);
-                        },function(){});
-                    }
+        if (type && type === "bind") { // 绑定要登录
+            if (layout.session && layout.session.user){
+                _bindWeixin2User(redirect);
+            }else{
+                if (layout.sessionPromise){
+                    layout.sessionPromise.then(function(){
+                        _bindWeixin2User(redirect);
+                    },function(){});
                 }
-            } else {
-                layout.session.user = json.data;
-                _loginSuccess(redirect)
             }
-        }, function (err) {
-            console.error(err);
-            //绑定成功
-            const box = new Element("div", { "style": "text-align: center;" }).inject($("appContent"));
-            new Element("h2", { "text": "单点失败！" }).inject(box);
-        });
+        } else { //code 单点登录
+            o2.Actions.load("x_organization_assemble_authentication").MPweixinAction.loginWithCode(code, function (json) {
+                layout.session.user = json.data;
+                _redirectTo(redirect);
+            }, function (err) {
+                console.error(err)
+            });
+        }
 
     })(layout);
 });
