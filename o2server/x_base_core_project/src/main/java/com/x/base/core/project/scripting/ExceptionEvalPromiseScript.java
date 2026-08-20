3520f2bddd3202a415fcb759b17a8fddd4b62deb
o2sword <171715986@qq.com>
2026-08-20 14:03:56 +0800

    修复脚本加载漏洞

--- o2server/x_base_core_project/src/main/java/com/x/base/core/project/scripting/ExceptionEvalPromiseScript.java
+++ o2server/x_base_core_project/src/main/java/com/x/base/core/project/scripting/ExceptionEvalPromiseScript.java
@@ -2,7 +2,7 @@ package com.x.base.core.project.scripting;
 
 import com.x.base.core.project.exception.PromptException;
 
-class ExceptionEvalPromiseScript extends PromptException {
+public class ExceptionEvalPromiseScript extends PromptException {
 
 	private static final long serialVersionUID = 4132300948670472899L;
 
